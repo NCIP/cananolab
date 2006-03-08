f2d55e8b6246b503c26ff03612fb32c4f17739f2
Jennifer Zeng <zengje>
2006-03-08 13:16:43 +0000

    Initial Version.
    SVN-Revision: 6822

---
+++ src/gov/nih/nci/calab/service/workflow/UseAliquotService.java
@@ -0,0 +1,51 @@
+package gov.nih.nci.calab.service.workflow;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UseAliquotService {
+	
+	/**
+	 * Retriving all unmasked aliquot in the system, for use aliquot
+	 * 
+	 * @return a list of aliquot id
+	 */
+	public List<String> getAliquot()
+	{
+		// Need detail....
+		
+		
+		List aliquotIds = new ArrayList();
+		aliquotIds.add("NCL-0-1234");
+		aliquotIds.add("NCL-0-1234-0");
+		aliquotIds.add("NCL-1-1234-1");
+		aliquotIds.add("NCL-1-1234");
+		aliquotIds.add("NCL-2-1235");
+		aliquotIds.add("NCL-5-1234");
+		aliquotIds.add("NCL-5-1234-0");
+		aliquotIds.add("NCL-5-1234-1");
+		aliquotIds.add("NCL-6-1234");
+		aliquotIds.add("NCL-6-1235");
+		
+		return aliquotIds;
+	}
+
+	/**
+	 * Retrive aliquots associate with Assay
+	 * 
+	 * @return a list of aliquot id 
+	 */
+	public List<String> getAliquotByAssay(String assayTypeName, String assayName)
+	{
+		// Detail impl needed
+		
+		List aliquotIds = new ArrayList();
+		aliquotIds.add("NCL-0-1234");
+		aliquotIds.add("NCL-0-1234-0");
+		aliquotIds.add("NCL-1-1234-1");
+		aliquotIds.add("NCL-1-1234");
+		aliquotIds.add("NCL-2-1235");
+		
+		return aliquotIds;
+	}
+}
