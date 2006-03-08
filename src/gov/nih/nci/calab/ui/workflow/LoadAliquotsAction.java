376d940c52d65f86454955c9bb20ac12a320b78e
Sue Pan <sue.pan@nih.gov>
2006-03-08 19:31:11 +0000

    added basic error handling
    SVN-Revision: 90

--- src/gov/nih/nci/calab/ui/workflow/LoadAliquotsAction.java
+++ src/gov/nih/nci/calab/ui/workflow/LoadAliquotsAction.java
@@ -1,4 +1,13 @@
 package gov.nih.nci.calab.ui.workflow;
+/**
+ * This class prepares aliquot IDs to be used in the Use Aliquot input form.
+ * 
+ * @author pansu
+ */
+
+/* CVS $Id: LoadAliquotsAction.java,v 1.2 2006-03-08 19:31:11 pansu Exp $ */
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.log4j.*;
 
@@ -19,6 +28,14 @@ public class LoadAliquotsAction extends AbstractBaseAction  {
 		ActionForward forward=null;
 		try {
 			/**@todo fill in details for getting aliquot IDs */
+			// tmp codes to be replaced.
+			List allAliquotIds=new ArrayList();
+			allAliquotIds.add("NCL-3-1");
+			allAliquotIds.add("NCL-3-2");
+			allAliquotIds.add("NCL-3-3");
+			request.getSession().setAttribute("allAliquotIds", allAliquotIds);
+			// end of tmp codes
+			
 			forward=mapping.findForward("success");	
 		}
 		catch(Exception e) {
@@ -27,6 +44,12 @@ public class LoadAliquotsAction extends AbstractBaseAction  {
 			forward=mapping.findForward("failure");
 		}
 		return forward;
+	}
+	
+	public boolean loginRequired() {
+		//temporarily set to false until login module is working
+		return false; 
+		//return true;
 	}
 }
 
