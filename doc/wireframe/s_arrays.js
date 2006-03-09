072e631157e2829627dfe43564acb137ec8dbb62
thangars <thangars>
2006-03-09 22:34:50 +0000

    added tool tips
    SVN-Revision: 119

--- doc/wireframe/s_arrays.js
+++ doc/wireframe/s_arrays.js
@@ -122,14 +122,16 @@ S:s_CSSTop	// STYLE Array to use for this menu
 ]
 );
 
+///////////////////////////////////////////////////////////////////////
 //TOOL TIP
+///////////////////////////////////////////////////////////////////////
 
 //workflow Search section assayName
 s_add(
 {N:'workflowSearch_assayName',LV:1,W:186,T:'mouseY-10',L:'mouseX+5',P:false,S:s_CSSTop},
 [
 {U:'',T:'Assay Name',SELECTED:true},
-{U:'',T:'The Assay name could be wild carded by \'*\'.',ROLL:true}
+{U:'',T:'The Assay name could be wild carded by \'*\'.',NOROLL:true}
 ]
 );
 //Assay Type
@@ -137,14 +139,116 @@ s_add(
 {N:'workflowSearch_assayType',LV:1,W:186,T:'mouseY+10',L:'mouseX+10',P:false,S:s_CSSTop},
 [
 {U:'',T:'Assay Type',SELECTED:true},
-{U:'',T:'The Assay type could be selected from the list.',ROLL:true}
+{U:'',T:'The Assay type could be selected from the list.',NOROLL:true}
 ]
 );
 //workflow Search section assay Run Date
 s_add(
-{N:'workflowSearch_assayName',LV:1,W:186,T:'mouseY-10',L:'mouseX+5',P:false,S:s_CSSTop},
+{N:'workflowSearch_assayRunDate',LV:1,W:186,T:'mouseY-10',L:'mouseX+5',P:false,S:s_CSSTop},
 [
-{U:'',T:'Assay Name',SELECTED:true},
-{U:'',T:'The Assay name could be wild carded by \'*\'.',ROLL:true}
+{U:'',T:'Assay Run Date',SELECTED:true},
+{U:'',T:'The Assay date could be selected using the icon or entered in mm/dd/yyyy format.',ROLL:true}
+]
+);
+//workflow Search section aliquot Id 
+s_add(
+{N:'workflowSearch_aliquotId',LV:1,W:186,T:'mouseY-10',L:'mouseX+5',P:false,S:s_CSSTop},
+[
+{U:'',T:'Aliquot Id',SELECTED:true},
+{U:'',T:'The Assay name could be wild carded by \'*\'.',NOROLL:true}
+]
+);
+//workflow Search on Masked aliquots 
+s_add(
+{N:'workflowSearch_maskedAliquots',LV:1,W:186,T:'mouseY-10',L:'mouseX+5',P:false,S:s_CSSTop},
+[
+{U:'',T:'Include Masked Aliquots',SELECTED:true},
+{U:'',T:'Check to search on Masked Aliquots.',NOROLL:true}
+]
+);
+//workflow Search on File names
+s_add(
+{N:'workflowSearch_fileName',LV:1,W:186,T:'mouseY-50',L:'mouseX+5',P:false,S:s_CSSTop},
+[
+{U:'',T:'File Name',SELECTED:true},
+{U:'',T:'The File Name could be wild carded by \'*\'. Select whether the file is In or Out.',NOROLL:true}
+]
+);
+//workflow Search File Submission Date
+s_add(
+{N:'workflowSearch_fileSubmissionDate',LV:1,W:186,T:'mouseY-100',L:'mouseX+5',P:false,S:s_CSSTop},
+[
+{U:'',T:'File Submission Date',SELECTED:true},
+{U:'',T:'The File Submission date could be selected using the icon or entered in mm/dd/yyyy format.',NOROLL:true}
+]
+);
+//workflow Search section File Type
+s_add(
+{N:'workflowSearch_fileType',LV:1,W:186,T:'mouseY-100',L:'mouseX+5',P:false,S:s_CSSTop},
+[
+{U:'',T:'File Type',SELECTED:true},
+{U:'',T:'Available File extension type could be selected from the list .',NOROLL:true}
+]
+);
+//workflow Search section File Submitter
+s_add(
+{N:'workflowSearch_fileSubmitter',LV:1,W:186,T:'mouseY+20',L:'mouseX+5',P:false,S:s_CSSTop},
+[
+{U:'',T:'File Submitter',SELECTED:true},
+{U:'',T:'Available File submitter could be selected from the list .',NOROLL:true}
+]
+);
+//workflow Search on Masked Files
+s_add(
+{N:'workflowSearch_maskedFiles',LV:1,W:186,T:'mouseY-10',L:'mouseX+5',P:false,S:s_CSSTop},
+[
+{U:'',T:'Include Masked Files',SELECTED:true},
+{U:'',T:'Check to search on Masked Files.',NOROLL:true}
+]
+);
+
+//////////////////////////////////////
+//Sample Search on Masked Files
+/////////////////////////////////////
+s_add(
+{N:'sampleSearch_sampleId',LV:1,W:186,T:'mouseY-70',L:'mouseX+5',P:false,S:s_CSSTop},
+[
+{U:'',T:'Sample ID',SELECTED:true},
+{U:'',T:'The Sample ID could be could be selected from the list.',NOROLL:true}
+]
+);
+s_add(
+{N:'sampleSearch_lotId',LV:1,W:186,T:'mouseY-70',L:'mouseX+5',P:false,S:s_CSSTop},
+[
+{U:'',T:'Lot ID',SELECTED:true},
+{U:'',T:'The Lot ID could be could be selected from the list.',NOROLL:true}
+]
+);
+s_add(
+{N:'sampleSearch_aliquotId',LV:1,W:186,T:'mouseY-70',L:'mouseX+5',P:false,S:s_CSSTop},
+[
+{U:'',T:'Aliquot ID',SELECTED:true},
+{U:'',T:'The Aliquot ID could be could be selected from the list.',NOROLL:true}
+]
+);
+s_add(
+{N:'sampleSearch_sampleType',LV:1,W:186,T:'mouseY-10',L:'mouseX+120',P:false,S:s_CSSTop},
+[
+{U:'',T:'Sample Type',SELECTED:true},
+{U:'',T:'The Sample Type could be could be selected from the list.',NOROLL:true}
+]
+);
+s_add(
+{N:'sampleSearch_dateAccessioned',LV:1,W:186,T:'mouseY-10',L:'mouseX+15',P:false,S:s_CSSTop},
+[
+{U:'',T:'Date Accessioned',SELECTED:true},
+{U:'',T:'The Date at which the sample was entered into the system. The Date could be selected using the icon or entered in mm/dd/yyyy format.',NOROLL:true}
+]
+);
+s_add(
+{N:'sampleSearch_sampleSubmitter',LV:1,W:186,T:'mouseY-10',L:'mouseX+20',P:false,S:s_CSSTop},
+[
+{U:'',T:'Sample Submitter',SELECTED:true},
+{U:'',T:'The Name of the user who submitted the Sample.',NOROLL:true}
 ]
 );
