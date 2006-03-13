91c861d0c5085dcee7c6e4f378e4950593dc6c92
Sue Pan <sue.pan@nih.gov>
2006-03-13 21:36:50 +0000

    converted to use ManageSampleService
    SVN-Revision: 167

--- src/gov/nih/nci/calab/ui/administration/LoadSampleInfoAction.java
+++ src/gov/nih/nci/calab/ui/administration/LoadSampleInfoAction.java
@@ -19,6 +19,7 @@ import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 import org.apache.struts.validator.DynaValidatorActionForm;
 
+import gov.nih.nci.calab.service.administration.ManageSampleService;
 import gov.nih.nci.calab.ui.core.*;
 
 public class LoadSampleInfoAction extends AbstractBaseAction {
@@ -34,29 +35,21 @@ public class LoadSampleInfoAction extends AbstractBaseAction {
 			DynaValidatorActionForm theForm = (DynaValidatorActionForm) form;
 			String sampleId = (String) theForm.get("sampleId");
 			String lotId = (String) theForm.get("lotId");
-			Integer numberOfContainers=(Integer)theForm.get("numberOfContainers");
-			//TODO fill in details for sample information */
-			
+
+			ManageSampleService service=new ManageSampleService();
 			//set default form values
-			if (sampleId==null) {
-			  theForm.set("sampleId", "NCL-6-1");// tmp code to be replaced
+			if (sampleId.length()==0) {
+			  theForm.set("sampleId", service.getDefaultSampleId());
 			}
-			if (lotId==null) {
-			  theForm.set("lotId", "1"); //tmp code to be replaced
+			if (lotId.length()==0) {
+			  theForm.set("lotId", service.getDefaultLotId()); 
 			}
-			if (numberOfContainers==null) {
-			  theForm.set("numberOfContainers", 1);
-			}		
 			
 			List sampleTypes = null;
 			if (session.getAttribute("allSampleTypes") != null) {
 				sampleTypes = (List) session.getAttribute("allSampleTypes");
 			} else {
-				// tmp code to be replaced
-				sampleTypes = new ArrayList();
-				sampleTypes.add("Dentrimer");
-				sampleTypes.add("Polymer");
-				// end of tmp code
+				sampleTypes=service.getAllSampleTypes();
 				session.setAttribute("allSampleTypes", sampleTypes);
 			}
 			ContainerInfoBean containerInfo = null;
@@ -64,36 +57,7 @@ public class LoadSampleInfoAction extends AbstractBaseAction {
 				containerInfo = (ContainerInfoBean) session
 						.getAttribute("containerInfo");
 			} else {
-				// tmp code to be replaced
-				List containerTypes = new ArrayList();
-				containerTypes.add("Tube");
-				containerTypes.add("Vial");
-				containerTypes.add("Other");
-
-				List quantityUnits = new ArrayList();
-				quantityUnits.add("g");
-				quantityUnits.add("mg");
-
-				List concentrationUnits = new ArrayList();
-				concentrationUnits.add("g/ml");
-				concentrationUnits.add("mg/ml");
-
-				List volumeUnits = new ArrayList();
-				volumeUnits.add("ml");
-				volumeUnits.add("ul");
-
-				List rooms = new ArrayList();
-				rooms.add("250");
-				rooms.add("117");
-
-				List freezers = new ArrayList();
-				freezers.add("F1");
-				freezers.add("F2");
-
-				containerInfo = new ContainerInfoBean(containerTypes,
-						quantityUnits, concentrationUnits, volumeUnits, rooms,
-						freezers);
-				// end of tmp code
+				containerInfo=service.getContainerInfo();
 				session.setAttribute("sampleContainerInfo", containerInfo);
 			}
 			request.setAttribute("sampleId", sampleId);
