917d57bf623ec7a32df81daff0b3afa601f3c17b
Sue Pan <sue.pan@nih.gov>
2006-03-15 18:44:40 +0000

    first deposit for release 0.5
    SVN-Revision: 6920

---
+++ src/gov/nih/nci/calab/dto/administration/SampleBean.java
@@ -0,0 +1,138 @@
+package gov.nih.nci.calab.dto.administration;
+
+/**
+ * This class represents all properties of a sample that need to be viewed and saved.
+ * @author pansu
+ *
+ */
+
+/* CVS $Id: SampleBean.java,v 1.1 2006-03-15 18:44:40 pansu Exp $ */
+public class SampleBean {
+	private String sampleId;
+	private String sampleType;
+	private String sampleSOP;
+	private String sampleDescription;
+	private String vendor;
+	private String vendorSampleId;
+	private String dateReceived;
+	private String lotId;
+	private String lotDescription;
+	private String solubility;
+	private String numberOfContainers;
+	private String generalComments;
+	
+	public SampleBean(String sampleId, String sampleType, String sampleSOP, String sampleDescription, String vendor, String vendorSampleId, String dateReceived, String solubility, String lotId, String lotDescription, String numberOfContainers, String generalComments) {
+		super();
+		// TODO Auto-generated constructor stub
+		this.sampleId = sampleId;
+		this.sampleType = sampleType;
+		this.sampleSOP = sampleSOP;
+		this.sampleDescription = sampleDescription;
+		this.vendor = vendor;
+		this.vendorSampleId = vendorSampleId;
+		this.dateReceived = dateReceived;
+		this.lotId = lotId;
+		this.lotDescription = lotDescription;
+		this.solubility = solubility;
+		this.numberOfContainers = numberOfContainers;
+		this.generalComments = generalComments;
+	}
+
+	public String getDateReceived() {
+		return dateReceived;
+	}
+
+	public void setDateReceived(String dateReceived) {
+		this.dateReceived = dateReceived;
+	}
+
+	public String getGeneralComments() {
+		return generalComments;
+	}
+
+	public void setGeneralComments(String generalComments) {
+		this.generalComments = generalComments;
+	}
+
+	public String getLotDescription() {
+		return lotDescription;
+	}
+
+	public void setLotDescription(String lotDescription) {
+		this.lotDescription = lotDescription;
+	}
+
+	public String getLotId() {
+		return lotId;
+	}
+
+	public void setLotId(String lotId) {
+		this.lotId = lotId;
+	}
+
+	public String getNumberOfContainers() {
+		return numberOfContainers;
+	}
+
+	public void setNumberOfContainers(String numberOfContainers) {
+		this.numberOfContainers = numberOfContainers;
+	}
+
+	public String getSampleDescription() {
+		return sampleDescription;
+	}
+
+	public void setSampleDescription(String sampleDescription) {
+		this.sampleDescription = sampleDescription;
+	}
+
+	public String getSampleId() {
+		return sampleId;
+	}
+
+	public void setSampleId(String sampleId) {
+		this.sampleId = sampleId;
+	}
+
+	public String getSampleSOP() {
+		return sampleSOP;
+	}
+
+	public void setSampleSOP(String sampleSOP) {
+		this.sampleSOP = sampleSOP;
+	}
+
+	public String getSampleType() {
+		return sampleType;
+	}
+
+	public void setSampleType(String sampleType) {
+		this.sampleType = sampleType;
+	}
+
+	public String getSolubility() {
+		return solubility;
+	}
+
+	public void setSolubility(String solubility) {
+		this.solubility = solubility;
+	}
+
+	public String getVendor() {
+		return vendor;
+	}
+
+	public void setVendor(String vendor) {
+		this.vendor = vendor;
+	}
+
+	public String getVendorSampleId() {
+		return vendorSampleId;
+	}
+
+	public void setVendorSampleId(String vendorSampleId) {
+		this.vendorSampleId = vendorSampleId;
+	}
+	
+
+}
