c31235fe1a5691b0e7f2a305219c622381fb60ac
Sue Pan <sue.pan@nih.gov>
2006-03-16 15:06:10 +0000

    moved getAliquots to LookupService
    SVN-Revision: 6948

--- src/gov/nih/nci/calab/service/workflow/ExecuteWorkflowService.java
+++ src/gov/nih/nci/calab/service/workflow/ExecuteWorkflowService.java
@@ -42,31 +42,6 @@ public class ExecuteWorkflowService {
 	}
 
 	/**
-	 * Retriving all unmasked aliquot in the system, for use aliquot
-	 * 
-	 * @return a list of aliquot id
-	 */
-	public List<String> getAliquots()
-	{
-		// Need detail....
-		
-		
-		List aliquotIds = new ArrayList();
-		aliquotIds.add("NCL-0-1234");
-		aliquotIds.add("NCL-0-1234-0");
-		aliquotIds.add("NCL-1-1234-1");
-		aliquotIds.add("NCL-1-1234");
-		aliquotIds.add("NCL-2-1235");
-		aliquotIds.add("NCL-5-1234");
-		aliquotIds.add("NCL-5-1234-0");
-		aliquotIds.add("NCL-5-1234-1");
-		aliquotIds.add("NCL-6-1234");
-		aliquotIds.add("NCL-6-1235");
-		
-		return aliquotIds;
-	}
-
-	/**
 	 * Retrive aliquots associate with Assay
 	 * 
 	 * @return a list of aliquot id 
