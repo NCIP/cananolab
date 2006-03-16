7fb8fcb23e1161d7346ac7827135b1e9fad9b430
Sue Pan <sue.pan@nih.gov>
2006-03-16 21:54:49 +0000

    added getAliquotCreateMethods
    SVN-Revision: 6973

--- src/gov/nih/nci/calab/service/administration/ManageAliquotService.java
+++ src/gov/nih/nci/calab/service/administration/ManageAliquotService.java
@@ -1,5 +1,30 @@
 package gov.nih.nci.calab.service.administration;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 
+ * @author pansu
+ * 
+ */
+
+/* CVS $Id: ManageAliquotService.java,v 1.2 2006-03-16 21:54:49 pansu Exp $ */
+
 public class ManageAliquotService {
 
+	/**
+	 * 
+	 * @return all methods for creating aliquots
+	 */
+	public List<String> getAliquotCreateMethods() {
+		List createMethods = new ArrayList();
+		createMethods.add("Solubilized");
+		createMethods.add("Liatholized");
+		return createMethods;
+	}
+	
+	public int getDefaultAliquotMatrixColumnNumber() {
+		return 10;
+	}
 }
