a1bbf91983cdbadc17ea9028c918e076c48cafb9
Sue Pan <sue.pan@nih.gov>
2006-03-16 15:06:31 +0000

    moved some common lookups to LookupService
    SVN-Revision: 6949

--- src/gov/nih/nci/calab/service/administration/ManageSampleService.java
+++ src/gov/nih/nci/calab/service/administration/ManageSampleService.java
@@ -5,22 +5,9 @@ import gov.nih.nci.calab.dto.administration.ContainerInfoBean;
 import java.util.ArrayList;
 import java.util.List;
 
-/* CVS $Id: ManageSampleService.java,v 1.4 2006-03-15 21:58:01 pansu Exp $ */
+/* CVS $Id: ManageSampleService.java,v 1.5 2006-03-16 15:06:31 pansu Exp $ */
 public class ManageSampleService {
-  /**
-   * 
-   * @return all sample types
-   */
-  public List<String> getAllSampleTypes() {
-		// tmp code to be replaced
-		List<String> sampleTypes = new ArrayList();
-		sampleTypes.add("Dentrimer");
-		sampleTypes.add("Polymer");
-		// end of tmp code
-		
-		return sampleTypes;
-  }
-  
+ 
   public List<String> getAllSampleSOPs() {
 	  List<String> sampleSOPs=new ArrayList();
 	  sampleSOPs.add("http://www.cnn.com");
@@ -47,41 +34,5 @@ public class ManageSampleService {
 	  return lotId;
   }
   
-  /**
-   * 
-   * @return the default container information in a form of ContainerInfoBean
-   */
-  public ContainerInfoBean getContainerInfo() {
-		// tmp code to be replaced
-		List containerTypes = new ArrayList();
-		containerTypes.add("Tube");
-		containerTypes.add("Vial");
-		containerTypes.add("Other");
-
-		List quantityUnits = new ArrayList();
-		quantityUnits.add("g");
-		quantityUnits.add("mg");
-
-		List concentrationUnits = new ArrayList();
-		concentrationUnits.add("g/ml");
-		concentrationUnits.add("mg/ml");
-
-		List volumeUnits = new ArrayList();
-		volumeUnits.add("ml");
-		volumeUnits.add("ul");
-
-		List rooms = new ArrayList();
-		rooms.add("250");
-		rooms.add("117");
-
-		List freezers = new ArrayList();
-		freezers.add("F1");
-		freezers.add("F2");
-
-		ContainerInfoBean containerInfo = new ContainerInfoBean(containerTypes,
-				quantityUnits, concentrationUnits, volumeUnits, rooms,
-				freezers);
-		// end of tmp code
-		return containerInfo;
-  }
+ 
 }
