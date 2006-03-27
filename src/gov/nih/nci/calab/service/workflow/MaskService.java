fde685a456df2682e200c72436c67f2f8d3a52e1
Jayfus Doswell <doswellj>
2006-03-27 20:37:52 +0000

    Added MaskService to generalize masking caLab objects
    SVN-Revision: 360

--- src/gov/nih/nci/calab/service/workflow/MaskService.java
+++ src/gov/nih/nci/calab/service/workflow/MaskService.java
@@ -1,14 +1,22 @@
 package gov.nih.nci.calab.service.workflow;
 
+/**
+ * Generalizes Mask functionality for masking Aliquot, File, etc.  
+ * @author doswellj
+ * @param strType Type of Mask (e.g., aliquot, file, run, etc.)
+ * @param strId	  The id associated to the type
+ * @param strDescription The mask description associated to the mask type and Id. 
+ *
+ */
 public class MaskService 
 {
-    public void setMask(String strType, String strID, String strDescription)
+    public void setMask(String strType, String strId, String strDescription)
     {
-        if (strType.equals("Aliquot"))
+        if (strType.equals("aliquot"))
         {
             //TODO Find Aliquot record based on the its id and set the status to "Masked" and its Description
         }
-        if (strType.equals("File"))
+        if (strType.equals("file"))
         {
         	//TODO Find File record based on the its id and set the status to "Masked" and its Description
         }
