78bc1c85a19946c66ab2fbb61a6b145b93aadb36
thangars <thangars>
2006-04-03 12:48:22 +0000

    added dtree node, added script for moving multiple list
    SVN-Revision: 7168

--- web/javascript/script.js
+++ web/javascript/script.js
@@ -41,6 +41,7 @@ function get_tree()
 		d.add(14,1,'PCC-1','javascript:gotoPage(\'assayActionMenu.jsp\')', '', '', '');
 		d.add(15,0,'In Vitro Assays','javascript:void(0)', '', '', '');
 		d.add(16,0,'In Vivo Assays','javascript:void(0)', '', '', '');
+		d.add(17,0,'Physical Characterization Assays','javascript:void(0)', '', '', '');
 		
 		document.write(d);
 }
@@ -57,4 +58,49 @@ function openWindow(pageURL, name, width, height) {
     window.open (pageURL, name, "alwaysRaised,dependent,status,scrollbars,resizable,width="+width+",height="+height); 
     
 }
+/**
+moveItems is a function used in moving items in one multiple select to the other.
+*/
+
+			function moveItems(fbox, tbox) 
+			{
+			var arrFbox = new Array();
+			var arrTbox = new Array();
+			var arrLookup = new Array();
+			var i;
+			for (i = 0; i < tbox.options.length; i++) {
+			arrLookup[tbox.options[i].text] = tbox.options[i].value;
+			arrTbox[i] = tbox.options[i].text;
+			}
+			var fLength = 0;
+			var tLength = arrTbox.length;
+			for(i = 0; i < fbox.options.length; i++) {
+			arrLookup[fbox.options[i].text] = fbox.options[i].value;
+			if (fbox.options[i].selected && fbox.options[i].value != "") {
+			arrTbox[tLength] = fbox.options[i].text;
+			tLength++;
+			}
+			else {
+			arrFbox[fLength] = fbox.options[i].text;
+			fLength++;
+			   }
+			}
+			arrFbox.sort();
+			arrTbox.sort();
+			fbox.length = 0;
+			tbox.length = 0;
+			var c;
+			for(c = 0; c < arrFbox.length; c++) {
+			var no = new Option();
+			no.value = arrLookup[arrFbox[c]];
+			no.text = arrFbox[c];
+			fbox[c] = no;
+			}
+			for(c = 0; c < arrTbox.length; c++) {
+			var no = new Option();
+			no.value = arrLookup[arrTbox[c]];
+			no.text = arrTbox[c];
+			tbox[c] = no;
+			   }
+			}
 
