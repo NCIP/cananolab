afbba59d15c964050308045f87f0c3f7ed8b94c8
Jennifer Zeng <zengje>
2006-04-06 17:58:53 +0000

    New objects.
    SVN-Revision: 7221

---
+++ src/gov/nih/nci/calab/domain/SampleType.java
@@ -0,0 +1,65 @@
+package gov.nih.nci.calab.domain;
+
+import gov.nih.nci.system.applicationservice.*;
+import java.util.*;
+
+/**
+ * <!-- LICENSE_TEXT_START -->
+ * <!-- LICENSE_TEXT_END -->
+ */
+ 
+  /**
+
+   */
+
+public  class SampleType 
+	implements java.io.Serializable 
+{
+	private static final long serialVersionUID = 1234567890L;
+
+	
+	   
+	   private java.lang.Long id;
+	   public  java.lang.Long getId(){
+	      return id;
+	   }
+	   public void setId( java.lang.Long id){
+	      this.id = id;
+	   }
+	
+	   
+	   private java.lang.String name;
+	   public  java.lang.String getName(){
+	      return name;
+	   }
+	   public void setName( java.lang.String name){
+	      this.name = name;
+	   }
+	
+	   
+		public boolean equals(Object obj){
+			boolean eq = false;
+			if(obj instanceof AssayType) {
+				AssayType c =(AssayType)obj; 			 
+				Long thisId = getId();		
+				
+					if(thisId != null && thisId.equals(c.getId())) {
+					   eq = true;
+				    }		
+				
+			}
+			return eq;
+		}
+		
+		public int hashCode(){
+			int h = 0;
+			
+			if(getId() != null) {
+				h += getId().hashCode();
+			}
+			
+			return h;
+	}
+	
+	
+}
