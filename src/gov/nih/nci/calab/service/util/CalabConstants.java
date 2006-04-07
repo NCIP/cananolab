8a9cf0cc2b7681aac878741b88880a0e8474adbd
Jennifer Zeng <zengje>
2006-04-07 13:26:01 +0000

    Constants are used in the caLAB.  dateformat, and others.
    SVN-Revision: 501

---
+++ src/gov/nih/nci/calab/service/util/CalabConstants.java
@@ -0,0 +1,11 @@
+package gov.nih.nci.calab.service.util;
+
+public class CalabConstants {
+	
+	public static final String DATE_FORMAT = "mm/dd/yyyy";
+	public static final String STORAGE_BOX = "Box";
+	public static final String STORAGE_SHELF = "Shelf";
+	public static final String STORAGE_RACK = "Rack";
+	public static final String STORAGE_FREEZER = "Freezer";
+	public static final String STORAGE_ROOM = "Room";
+}
