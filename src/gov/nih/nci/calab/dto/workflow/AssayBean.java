c6ddd78a962ee8feb0f6171d4a947b4300457a55
Jennifer Zeng <zengje>
2006-04-12 13:44:26 +0000

    Assay DTO object.
    SVN-Revision: 7342

---
+++ src/gov/nih/nci/calab/dto/workflow/AssayBean.java
@@ -0,0 +1,37 @@
+package gov.nih.nci.calab.dto.workflow;
+
+public class AssayBean {
+	
+	private String assayId;
+	private String assayName;
+	private String assayType;
+	
+	public AssayBean(String assayId, String assayName, String assayType) {
+		super();
+		// TODO Auto-generated constructor stub
+		this.assayId = assayId;
+		this.assayName = assayName;
+		this.assayType = assayType;
+	}
+	public String getAssayId() {
+		return assayId;
+	}
+	public void setAssayId(String assayId) {
+		this.assayId = assayId;
+	}
+	public String getAssayName() {
+		return assayName;
+	}
+	public void setAssayName(String assayName) {
+		this.assayName = assayName;
+	}
+	public String getAssayType() {
+		return assayType;
+	}
+	public void setAssayType(String assayType) {
+		this.assayType = assayType;
+	}
+	
+	
+
+}
