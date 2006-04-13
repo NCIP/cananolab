a4604be440fa7689693e61ffefcdf3e02ad2d77e
Jennifer Zeng <zengje>
2006-04-13 19:24:53 +0000

    Beans for constructing workflow
    SVN-Revision: 634

---
+++ src/gov/nih/nci/calab/dto/workflow/RunBean.java
@@ -0,0 +1,76 @@
+/**
+ * 
+ */
+package gov.nih.nci.calab.dto.workflow;
+
+import java.util.List;
+
+/**
+ * @author zengje
+ *
+ */
+public class RunBean {
+	
+	private String id;
+	private String name;
+	private List aliquotBeans;
+	private List inputFileBeans;
+	private List outputFileBeans;
+	
+	/**
+	 * 
+	 */
+	public RunBean() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	public RunBean(String id, String name) {
+		super();
+		// TODO Auto-generated constructor stub
+		this.id = id;
+		this.name = name;
+	}
+
+	public List getAliquotBeans() {
+		return aliquotBeans;
+	}
+
+	public void setAliquotBeans(List aliquotBeans) {
+		this.aliquotBeans = aliquotBeans;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public List getInputFileBeans() {
+		return inputFileBeans;
+	}
+
+	public void setInputFileBeans(List inputFileBeans) {
+		this.inputFileBeans = inputFileBeans;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public List getOutputFileBeans() {
+		return outputFileBeans;
+	}
+
+	public void setOutputFileBeans(List outputFileBeans) {
+		this.outputFileBeans = outputFileBeans;
+	}
+
+	
+}
