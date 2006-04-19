4ba4bb7dba2516b867c35be03ef7060180b68755
Sue Pan <sue.pan@nih.gov>
2006-04-19 19:53:37 +0000

    organized imports
    SVN-Revision: 812

--- src/gov/nih/nci/calab/service/login/LoginService.java
+++ src/gov/nih/nci/calab/service/login/LoginService.java
@@ -1,29 +1,9 @@
 package gov.nih.nci.calab.service.login;
 
-import java.util.HashSet;
-import java.util.Hashtable;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Set;
-
-import org.apache.log4j.Logger;
-
-import gov.nih.nci.security.AuthenticationManager;
-import gov.nih.nci.security.authorization.domainobjects.ProtectionElement;
-import gov.nih.nci.security.authorization.domainobjects.ProtectionElementPrivilegeContext;
-import gov.nih.nci.security.authorization.domainobjects.User;
-import gov.nih.nci.security.dao.ProtectionElementSearchCriteria;
-import gov.nih.nci.security.dao.UserSearchCriteria;
-import gov.nih.nci.security.exceptions.CSException;
-import gov.nih.nci.security.exceptions.CSObjectNotFoundException;
-import gov.nih.nci.security.exceptions.CSTransactionException;
-import gov.nih.nci.security.SecurityServiceProvider;
-import gov.nih.nci.security.authorization.domainobjects.Role;
-import gov.nih.nci.security.authorization.domainobjects.ProtectionGroup;
-import gov.nih.nci.security.dao.RoleSearchCriteria;
-import gov.nih.nci.security.dao.ProtectionGroupSearchCriteria;
-
 import gov.nih.nci.calab.dto.security.SecurityBean;
+import gov.nih.nci.security.AuthenticationManager;
+import gov.nih.nci.security.SecurityServiceProvider;
+import gov.nih.nci.security.exceptions.CSException;
 
 /** 
  * The LoginService authenticates users into the calab system.
