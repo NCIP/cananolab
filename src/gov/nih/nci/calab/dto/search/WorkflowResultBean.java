eec7353969ac521222b2d01c7b0b934575602e48
Sue Pan <sue.pan@nih.gov>
2006-04-19 19:52:49 +0000

    updated to use other workflow related dto beans
    SVN-Revision: 809

--- src/gov/nih/nci/calab/dto/search/WorkflowResultBean.java
+++ src/gov/nih/nci/calab/dto/search/WorkflowResultBean.java
@@ -1,6 +1,9 @@
 package gov.nih.nci.calab.dto.search;
 
-import gov.nih.nci.calab.service.util.StringUtils;
+import gov.nih.nci.calab.dto.administration.AliquotBean;
+import gov.nih.nci.calab.dto.workflow.AssayBean;
+import gov.nih.nci.calab.dto.workflow.FileBean;
+import gov.nih.nci.calab.dto.workflow.RunBean;
 
 /**
  * This class includes all properties of a workflow to be displayed in the
@@ -10,139 +13,51 @@ import gov.nih.nci.calab.service.util.StringUtils;
  * 
  */
 public class WorkflowResultBean {
-	private String fileName="";
-
-	private String assayType="";
-
-	private String assayName="";
-
-	private String assayRunName="";
+	private FileBean file;
+	private AssayBean assay;
+	private AliquotBean aliquot;
+	private RunBean run;
 	
-	private String assayRunDate="";
-
-	private String aliquotName="";
-
-	private String fileSubmissionDate="";
-
-	private String fileSubmitter="";
-
-	private String fileMaskStatus="";
-
-	public WorkflowResultBean(String fileName, String assayType, String assayName,
+	public WorkflowResultBean(String filePath, String assayType, String assayName,
 			String assayRunName, String assayRunDate, String aliquotName, String fileSubmissionDate,
 			String fileSubmitter, String fileMaskStatus) {
 		super();
 		// TODO Auto-generated constructor stub
-		this.fileName = fileName;
-		this.assayType = assayType;
-		this.assayName = assayName;
-		this.assayRunName=assayRunName;
-		this.assayRunDate = assayRunDate;
-		this.aliquotName = aliquotName;
-		this.fileSubmissionDate = fileSubmissionDate;
-		this.fileSubmitter = fileSubmitter;
-		this.fileMaskStatus = fileMaskStatus;
+		this.file=new FileBean(filePath, fileSubmissionDate, fileSubmitter, fileMaskStatus);
+		this.assay=new AssayBean(assayName, assayType);		
+		this.run=new RunBean("", assayRunName, assayRunDate);
+		this.aliquot=new AliquotBean(aliquotName);	
 	}
 
-	public String getAliquotName() {
-		return aliquotName;
+	public AliquotBean getAliquot() {
+		return aliquot;
 	}
 
-	public void setAliquotName(String aliquotName) {
-		this.aliquotName = aliquotName;
+	public void setAliquot(AliquotBean aliquot) {
+		this.aliquot = aliquot;
 	}
 
-	public String getAssayName() {
-		return assayName;
+	public AssayBean getAssay() {
+		return assay;
 	}
 
-	public void setAssayName(String assayName) {
-		this.assayName = assayName;
+	public void setAssay(AssayBean assay) {
+		this.assay = assay;
 	}
 
-	public String getAssayRunDate() {
-		return assayRunDate;
+	public FileBean getFile() {
+		return file;
 	}
 
-	public void setAssayRunDate(String assayRunDate) {
-		this.assayRunDate = assayRunDate;
+	public void setFile(FileBean file) {
+		this.file = file;
 	}
 
-	public String getAssayType() {
-		return assayType;
+	public RunBean getRun() {
+		return run;
 	}
 
-	public void setAssayType(String assayType) {
-		this.assayType = assayType;
-	}
-
-	public String getFileMaskStatus() {
-		return fileMaskStatus;
-	}
-
-	public void setFileMaskStatus(String fileMaskStatus) {
-		this.fileMaskStatus = fileMaskStatus;
-	}
-
-	public String getFileName() {
-		return fileName;
-	}
-
-	public void setFileName(String fileName) {
-		this.fileName = fileName;
-	}
-
-	public String getFileSubmissionDate() {
-		return fileSubmissionDate;
-	}
-
-	public void setFileSubmissionDate(String fileSubmissionDate) {
-		this.fileSubmissionDate = fileSubmissionDate;
-	}
-
-	public String getFileSubmitter() {
-		return fileSubmitter;
-	}
-
-	public void setFileSubmitter(String fileSubmitter) {
-		this.fileSubmitter = fileSubmitter;
-	}
-
-	public int hashCode() {
-		return toString().hashCode();
-	}
-	
-	public String toString() {
-		String[] thisAttrs=new String[] {fileName, assayType, assayName,
-				assayRunDate, aliquotName, fileSubmissionDate,
-				fileSubmitter, fileMaskStatus};
-		return StringUtils.join(thisAttrs,":");		
-	}
-	
-	public int compareTo(Object b) {
-		if (b instanceof WorkflowResultBean) {
-			WorkflowResultBean bWorkflow=(WorkflowResultBean)b;			
-			return toString().compareTo(bWorkflow.toString());
-		}
-		else {
-			return -1;
-		}
-	}
-	
-	public boolean equals(Object b) {
-		if (this.compareTo(b)==0) {
-			return true;
-		}
-		else {
-			return false;
-		}
-	}
-
-	public String getAssayRunName() {
-		return assayRunName;
-	}
-
-	public void setAssayRunName(String assayRunName) {
-		this.assayRunName = assayRunName;
+	public void setRun(RunBean run) {
+		this.run = run;
 	}
 }
