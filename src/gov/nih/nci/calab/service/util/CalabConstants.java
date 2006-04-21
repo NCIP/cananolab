eeb625be496d3e72319645feb8262af3608e75c6
Jennifer Zeng <zengje>
2006-04-21 13:14:26 +0000

    Added Other-status
    SVN-Revision: 869

--- src/gov/nih/nci/calab/service/util/CalabConstants.java
+++ src/gov/nih/nci/calab/service/util/CalabConstants.java
@@ -4,6 +4,7 @@ public class CalabConstants {
 	
 	public static final String DATE_FORMAT = "MM/dd/yyyy";
 	
+	// Storage element
 	public static final String STORAGE_BOX = "Box";
 	public static final String STORAGE_SHELF = "Shelf";
 	public static final String STORAGE_RACK = "Rack";
@@ -11,21 +12,28 @@ public class CalabConstants {
 	public static final String STORAGE_ROOM = "Room";
 	public static final String STORAGE_LAB = "Lab";
 	
+	// DataStatus
 	public static final String MASK_STATUS = "Masked";
+	public static final String OTHER_STATUS = "Active";
+	
+	// for Container type
 	public static final String OTHER = "Other";
 	
+	// Sample Container type
 	public static final String ALIQUOT = "Aliquot";
 	public static final String SAMPLE_CONTAINER = "Sample_container";
-	
+	// Run Name
 	public static final String RUN = "Run";
+	// File upload
     public static final String FILEUPLOAD_PROPERTY = "fileupload.properties";
     public static final String UNCOMPRESSED_FILE_DIRECTORY="decompressedFiles";
     
     public static final String EMPTY = "N/A";
-    
+    // File input/output type
     public static final String INPUT = "input";
     public static final String OUTPUT = "output";
-
+    
+    // zip file name
     public static final String ALL_FILES = "ALL_FILES";
     
     public static final String URI_SEPERATOR = "/";
