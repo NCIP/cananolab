dec1d4133ea516faa719359a5f06956ddf2e554c
Jennifer Zeng <zengje>
2006-05-01 20:06:49 +0000

    calab exception
    SVN-Revision: 7813

---
+++ gov/nih/nci/calab/exception/CalabException.java
@@ -0,0 +1,35 @@
+package gov.nih.nci.calab.exception;
+
+/**
+ * @author zengje
+ *
+ */
+
+public class CalabException extends Exception {
+
+	private static final long serialVersionUID = 1234567890L;
+ 
+	/**
+	 * 
+	 */
+	public CalabException() {
+		super();
+
+	}
+
+	public CalabException(String message) {
+		super(message);
+
+	}
+
+	public CalabException(String message, Throwable cause) {
+		super(message, cause);
+
+	}
+
+	public CalabException(Throwable cause) {
+		super(cause);
+
+	}
+
+}
