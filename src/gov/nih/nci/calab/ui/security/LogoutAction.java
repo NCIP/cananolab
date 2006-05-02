adcd8c03df748df7f4ae21cebe67c99cb0abb25a
Sue Pan <sue.pan@nih.gov>
2006-05-02 20:11:47 +0000

    updated to use declarative exception handling
    SVN-Revision: 7838

--- src/gov/nih/nci/calab/ui/security/LogoutAction.java
+++ src/gov/nih/nci/calab/ui/security/LogoutAction.java
@@ -6,7 +6,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import org.apache.log4j.Logger;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
@@ -22,30 +21,21 @@ import org.apache.struts.action.ActionMessages;
 /* CVS $Id: */
 
 public class LogoutAction extends AbstractBaseAction {
-	private static Logger logger = Logger.getLogger(LogoutAction.class);
-
 	public ActionForward executeTask(ActionMapping mapping, ActionForm form,
 			HttpServletRequest request, HttpServletResponse response)
 			throws Exception {
 
 		ActionForward forward = null;
-		ActionMessages msgs = new ActionMessages();
+
 		HttpSession session = request.getSession(false);
-		try {
-			if (session != null) {			
-				session.invalidate();
-				ActionMessage error = new ActionMessage("msg.logout");
-				msgs.add("msg", error);
-				saveMessages(request, msgs);
-			}
-			forward = mapping.findForward("success");
-		} catch (Exception e) {
-			ActionMessage error = new ActionMessage("error.logout");
-			msgs.add("error", error);
+		if (session != null) {
+			session.invalidate();
+			ActionMessages msgs = new ActionMessages();
+			ActionMessage error = new ActionMessage("msg.logout");
+			msgs.add("msg", error);
 			saveMessages(request, msgs);
-			logger.error("Error in logging out the user", e);
-			forward = mapping.findForward("failure");
 		}
+		forward = mapping.findForward("success");
 		return forward;
 	}
 
