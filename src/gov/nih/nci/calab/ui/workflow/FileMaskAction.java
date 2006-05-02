3dbe54736a2a89dc618a616f56157e0deff3fc90
Sue Pan <sue.pan@nih.gov>
2006-05-02 19:06:40 +0000

    set filesToMask as request attribute only when not empty
    SVN-Revision: 1091

--- src/gov/nih/nci/calab/ui/workflow/FileMaskAction.java
+++ src/gov/nih/nci/calab/ui/workflow/FileMaskAction.java
@@ -23,65 +23,65 @@ import org.apache.struts.action.DynaActionForm;
  * This class handle workflow file mask process.
  * 
  * @author zhoujim, pansu
- *
+ * 
  */
 
-public class FileMaskAction extends AbstractBaseAction
-{
-    /**
-     * This method is setting up the parameters for the workflow mask files.
-     * 
-     * @param mapping
-     * @param form
-     * @param request
-     * @param response
-     * @return mapping forward
-     */
-    public ActionForward executeTask(ActionMapping mapping, 
-                               ActionForm form,
-                               HttpServletRequest request,
-                               HttpServletResponse response)
-    {
-        HttpSession session = request.getSession();
-        DynaActionForm fileForm = (DynaActionForm)form;
-        ExecuteWorkflowService workflowService = new ExecuteWorkflowService();
-        String runId=(String)fileForm.get("runId");
-        RunBean runBean = workflowService.getAssayInfoByRun((ExecuteWorkflowBean)session.getAttribute("workflow"), runId);
-    	
-        fileForm.set("assayType", runBean.getAssayBean().getAssayType());
-        fileForm.set("assayName", runBean.getAssayBean().getAssayName());
-        fileForm.set("runName", runBean.getName());    
-        
-        String inout=(String)fileForm.get("inout");
-        //getting inout from request attribute when parameter is not available
-        if (request.getAttribute("inout")!=null) {
-        	inout=(String)request.getAttribute("inout");
-        	fileForm.set("inout",(String)request.getAttribute("inout"));
-        }
-         
-        // Retrieve filename(not uri) from database
-        List<FileBean> fileBeanList = new ArrayList<FileBean>();
-        if (inout.equalsIgnoreCase(CalabConstants.INPUT)) {
-            List<FileBean> allfiles = runBean.getInputFileBeans();
-            for(FileBean fileBean: allfiles){
-            	if (!fileBean.getFileMaskStatus().equals(CalabConstants.MASK_STATUS)){
-            		fileBeanList.add(fileBean);
-            	}
-            }
-        } 
-        else if (inout.equalsIgnoreCase(CalabConstants.OUTPUT)) {
-            List<FileBean> allfiles = runBean.getOutputFileBeans();
-            for(FileBean fileBean: allfiles){
-            	if (!fileBean.getFileMaskStatus().equals(CalabConstants.MASK_STATUS)){
-            		fileBeanList.add(fileBean);
-            	}
-            }
-        }
-        request.setAttribute("filesToMask", fileBeanList);
-        return mapping.findForward("success");
-    }
-    
-    public boolean loginRequired() {        
-         return true;
-    }
+public class FileMaskAction extends AbstractBaseAction {
+	/**
+	 * This method is setting up the parameters for the workflow mask files.
+	 * 
+	 * @param mapping
+	 * @param form
+	 * @param request
+	 * @param response
+	 * @return mapping forward
+	 */
+	public ActionForward executeTask(ActionMapping mapping, ActionForm form,
+			HttpServletRequest request, HttpServletResponse response) {
+		HttpSession session = request.getSession();
+		DynaActionForm fileForm = (DynaActionForm) form;
+		ExecuteWorkflowService workflowService = new ExecuteWorkflowService();
+		String runId = (String) fileForm.get("runId");
+		RunBean runBean = workflowService.getAssayInfoByRun(
+				(ExecuteWorkflowBean) session.getAttribute("workflow"), runId);
+
+		fileForm.set("assayType", runBean.getAssayBean().getAssayType());
+		fileForm.set("assayName", runBean.getAssayBean().getAssayName());
+		fileForm.set("runName", runBean.getName());
+
+		String inout = (String) fileForm.get("inout");
+		// getting inout from request attribute when parameter is not available
+		if (request.getAttribute("inout") != null) {
+			inout = (String) request.getAttribute("inout");
+			fileForm.set("inout", (String) request.getAttribute("inout"));
+		}
+
+		// Retrieve filename(not uri) from database
+		List<FileBean> fileBeanList = new ArrayList<FileBean>();
+		if (inout.equalsIgnoreCase(CalabConstants.INPUT)) {
+			List<FileBean> allfiles = runBean.getInputFileBeans();
+			for (FileBean fileBean : allfiles) {
+				if (!fileBean.getFileMaskStatus().equals(
+						CalabConstants.MASK_STATUS)) {
+					fileBeanList.add(fileBean);
+				}
+			}
+		} else if (inout.equalsIgnoreCase(CalabConstants.OUTPUT)) {
+			List<FileBean> allfiles = runBean.getOutputFileBeans();
+			for (FileBean fileBean : allfiles) {
+				if (!fileBean.getFileMaskStatus().equals(
+						CalabConstants.MASK_STATUS)) {
+					fileBeanList.add(fileBean);
+				}
+			}
+		}
+		if (!fileBeanList.isEmpty()) {
+			request.setAttribute("filesToMask", fileBeanList);
+		}
+		return mapping.findForward("success");
+	}
+
+	public boolean loginRequired() {
+		return true;
+	}
 }
