0a49c668951a8e73cf69f810885afec0dcd037f0
Sue Pan <sue.pan@nih.gov>
2006-05-03 18:55:47 +0000

    first deposit for release 0.5
    SVN-Revision: 7857

---
+++ gov/nih/nci/calab/exception/InvalidSessionException.java
@@ -0,0 +1,34 @@
+package gov.nih.nci.calab.exception;
+
+/**
+ * This class represents the exception to be thrown when the web session expires
+ * or when the user is not logged in.
+ * 
+ * @author pansu
+ * 
+ */
+public class InvalidSessionException extends CalabException {
+
+	private static final long serialVersionUID = 1234567890L;
+
+	public InvalidSessionException() {
+		super("User is not logged in or session expired");
+	}
+
+	public InvalidSessionException(String message) {
+		super(message);
+	}
+
+	public InvalidSessionException(String message, Throwable cause) {
+		super(message, cause);	
+	}
+	
+	public InvalidSessionException(Throwable cause) {
+		super(cause);	
+	}
+	
+	public static void main(String[] args) {
+		InvalidSessionException e = new InvalidSessionException();
+		System.out.println(e.getMessage());
+	}
+}
