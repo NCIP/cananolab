85cff4619c69d5e9ce1df30cfb1491b34d4b8b23
Jennifer Zeng <zengje>
2006-05-04 21:43:53 +0000

    Online help first draft.
    SVN-Revision: 1144

---
+++ web/webHelp/caLAB_0.5/wwhdata/js/toc.js
@@ -0,0 +1,35 @@
+function  WWHBookData_AddTOCEntries(P)
+{
+var A=P.fN("Welcome to caLAB","0");
+A=P.fN("caLAB Online Help","1");
+var B=A.fN("About caLAB","1#1064573");
+var C=B.fN("Introduction to caLAB","1#1085961");
+C=B.fN("caLAB Welcome Login Page","2");
+C=B.fN("caLAB Welcome Workflow Page","3");
+C=B.fN("caLAB Online Help","4");
+C=B.fN("Navigation in caLAB","5");
+B=A.fN("Working in caLAB","6");
+C=B.fN("caLAB Workflow   Overview","6#1085986");
+C=B.fN("Executing a caLAB Workflow  ","7");
+var D=C.fN("Creating a Sample","8");
+D=C.fN("Creating Aliquots","9");
+D=C.fN("Editing an Aliquot ","10");
+D=C.fN("Aliquot Naming Conventions","11");
+D=C.fN("Viewing an Aliquot","12");
+D=C.fN("Using an Aliquot","13");
+D=C.fN("Masking an Aliquot","14");
+D=C.fN("Creating an Assay Run","15");
+D=C.fN("Creating a Run","16");
+D=C.fN("Assay Run In and Out Folders","17");
+C=B.fN("Working with Files","18");
+D=C.fN("Uploading Input/Output Files","19");
+D=C.fN("caLAB File Naming Conventions","20");
+D=C.fN("Masking a File","21");
+D=C.fN("Downloading Files","22");
+C=B.fN("Launching a caLAB Search","23");
+D=C.fN("    Searching a Workflow","24");
+D=C.fN("Workflow Search Results","25");
+D=C.fN("  Searching Sample Containers","26");
+D=C.fN("Sample Container Search Results","27");
+B=A.fN("Application Support","28");
+}
