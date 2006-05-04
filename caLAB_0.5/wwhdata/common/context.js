51386640f8d598aa64dc05962d55df68f1b25cc2
Jennifer Zeng <zengje>
2006-05-04 21:43:53 +0000

    Online help first draft.
    SVN-Revision: 7884

---
+++ caLAB_0.5/wwhdata/common/context.js
@@ -0,0 +1,4 @@
+function  WWHBookData_Context()
+{
+  return "caLAB_0.5";
+}
---
+++ caLAB_0.5/wwhdata/common/topics.js
@@ -0,0 +1,21 @@
+function  WWHBookData_MatchTopic(P)
+{
+var C=null;
+if(P=="welcome_login")C="About caLAB.2.2.html#1069239";
+if(P=="welcome_workflow")C="About caLAB.2.3.html#1079711";
+if(P=="create_sample")C="About caLAB.2.8.html#1069635";
+if(P=="create_aliquot")C="About caLAB.2.9.html#1071201";
+if(P=="edit_aliquot")C="About caLAB.2.10.html#1078187";
+if(P=="view_aliquot")C="About caLAB.2.12.html";
+if(P=="mask_aliquot")C="About caLAB.2.14.html#1073979";
+if(P=="create_assay_run")C="About caLAB.2.16.html#1087237";
+if(P=="in_folder")C="About caLAB.2.17.html";
+if(P=="file_upload")C="About caLAB.2.19.html#1086942";
+if(P=="mask_files")C="About caLAB.2.21.html#1078900";
+if(P=="download_files")C="About caLAB.2.22.html#1085617";
+if(P=="search_workflow")C="About caLAB.2.24.html#1073772";
+if(P=="workflow_search_results")C="About caLAB.2.25.html#1073690";
+if(P=="search_sample_container")C="About caLAB.2.26.html#1069067";
+if(P=="sample_search_results")C="About caLAB.2.27.html#1085437";
+return C;
+}
