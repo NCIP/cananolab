b30a1c0b536621ea4edc94a176adc87f2ad1873b
Sue Pan <sue.pan@nih.gov>
2006-05-09 18:37:56 +0000

    updated getting parameters
    SVN-Revision: 7955

--- gov/nih/nci/calab/ui/workflow/FileUploadAction.java
+++ gov/nih/nci/calab/ui/workflow/FileUploadAction.java
@@ -1,8 +1,6 @@
 package gov.nih.nci.calab.ui.workflow;
 
-import gov.nih.nci.calab.dto.workflow.ExecuteWorkflowBean;
 import gov.nih.nci.calab.dto.workflow.FileBean;
-import gov.nih.nci.calab.dto.workflow.RunBean;
 import gov.nih.nci.calab.service.util.CalabConstants;
 import gov.nih.nci.calab.service.util.PropertyReader;
 import gov.nih.nci.calab.service.util.SpecialCharReplacer;
@@ -25,209 +23,197 @@ import javax.servlet.http.HttpSession;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
-import org.apache.struts.validator.DynaValidatorActionForm;
+import org.apache.struts.action.DynaActionForm;
 
 /**
  * This class handle workflow upload process.
  * 
  * @author zhoujim
- *
+ * 
  */
 
-public class FileUploadAction extends AbstractDispatchAction
-{
-    private static org.apache.log4j.Logger logger_ =
-        org.apache.log4j.Logger.getLogger(FileUploadAction.class);
-    
-    private ExecuteWorkflowService workflowService = new ExecuteWorkflowService();
+public class FileUploadAction extends AbstractDispatchAction {
+	private static org.apache.log4j.Logger logger_ = org.apache.log4j.Logger
+			.getLogger(FileUploadAction.class);
 
-    /**
-     * This method is setting up the parameters for the workflow input upload files
-     * or output upload files.
-     * 
-     * @param mapping
-     * @param form
-     * @param request
-     * @param response
-     * @return mapping forward
-     */
-    public ActionForward setup(ActionMapping mapping, 
-                               ActionForm form,
-                               HttpServletRequest request,
-                               HttpServletResponse response)
-    {
-        DynaValidatorActionForm fileForm = (DynaValidatorActionForm)form;
- 
-        HttpSession session = request.getSession();
- 
-        String runId = (request.getParameter("runId") == null)? (String)session.getAttribute("runId"): request.getParameter("runId");
-        RunBean runBean = workflowService.getAssayInfoByRun((ExecuteWorkflowBean)session.getAttribute("workflow"), runId);
-       
-        SpecialCharReplacer specialCharReplacer = new SpecialCharReplacer();
-        String assayType=specialCharReplacer.getReplacedString(runBean.getAssayBean().getAssayType());
-        String assayName=specialCharReplacer.getReplacedString(runBean.getAssayBean().getAssayName());
-        String runName=specialCharReplacer.getReplacedString(runBean.getName());
+	private ExecuteWorkflowService workflowService = new ExecuteWorkflowService();
 
-        fileForm.set("assayType", specialCharReplacer.getReplacedString(runBean.getAssayBean().getAssayType()));
-        fileForm.set("assay", assayName);
-        fileForm.set("run", runName);
-//        fileForm.set("inout", request.getParameter("inout"));
-        String inout=(String)fileForm.get("inout");
-        fileForm.set("archiveValue", PropertyReader.getProperty(CalabConstants.FILEUPLOAD_PROPERTY,"archiveValue"));
-        fileForm.set("servletURL", PropertyReader.getProperty(CalabConstants.FILEUPLOAD_PROPERTY,"servletURL"));
-        fileForm.set("notifyURL", PropertyReader.getProperty(CalabConstants.FILEUPLOAD_PROPERTY,"notifyURL"));
-        fileForm.set("defaultURL", PropertyReader.getProperty(CalabConstants.FILEUPLOAD_PROPERTY, "defaultURL"));
-        fileForm.set("sid", session.getId());
-        fileForm.set("module", "calab");
-        fileForm.set("permissibleFileExtension", "*");
-        
-        HttpFileUploadSessionData hFileUploadData = (HttpFileUploadSessionData)session.getAttribute("httpFileUploadSessionData");
-        if (hFileUploadData != null)
-        {
-            session.removeAttribute("httpFileUploadSessionData");
-        }
-        hFileUploadData = new HttpFileUploadSessionData();
-        hFileUploadData.setInout(inout);
-        hFileUploadData.setRunId(runId);
-        hFileUploadData.setAssay(assayName);
-        hFileUploadData.setAssayType(assayType);
-        hFileUploadData.setRun(runName);
-        
-        // set where this upload from
-        hFileUploadData.setFromType(request.getParameter("type"));
-        
-        
-        
-        session.setAttribute("httpFileUploadSessionData", hFileUploadData);
-        
-        return mapping.findForward("success");
-    }
-    
-    public ActionForward persistFileUploadData(ActionMapping mapping, 
-            ActionForm form,
-            HttpServletRequest request,
-            HttpServletResponse response) throws Exception
-    {
-        ActionForward forward = null;
-        HttpFileUploadSessionData mySessionData = (HttpFileUploadSessionData)request.getSession().getAttribute("httpFileUploadSessionData");
-        String path = PropertyReader.getProperty(CalabConstants.FILEUPLOAD_PROPERTY, "fileRepositoryDir");
-        String relativePathName = mySessionData.getAssayType() + File.separator 
-                                    + mySessionData.getAssay() + File.separator
-                                    + mySessionData.getRun()   + File.separator
-                                    + mySessionData.getInout();
-        String fullPathName = path + relativePathName;
+	/**
+	 * This method is setting up the parameters for the workflow input upload
+	 * files or output upload files.
+	 * 
+	 * @param mapping
+	 * @param form
+	 * @param request
+	 * @param response
+	 * @return mapping forward
+	 */
+	public ActionForward setup(ActionMapping mapping, ActionForm form,
+			HttpServletRequest request, HttpServletResponse response) {
+		HttpSession session = request.getSession();
+		DynaActionForm fileForm = (DynaActionForm) form;
 
-        String mode = (String)request.getParameter("mode");
-        HttpSession session = request.getSession();
-      
-        if ("stop".equals(mode))
-        {
-            if (mySessionData != null)
-            {
-                mySessionData.setIsStopped(true);
-                List list = mySessionData.getFileList();
-                
-                
-                for (Iterator it = list.iterator(); it.hasNext();)
-                {
-                    HttpUploadedFileData data = (HttpUploadedFileData)it.next();
-                    
-                    try
-                    {
-                       File file = new File(fullPathName, data.getFileName());
-                    
-                        if (file.exists())
-                        {
-                            file.delete();
-                        }
-                        
-                        //delete uncompressed files
-                        String fileName = null;
-                        FileNameConvertor fnConvertor = new FileNameConvertor();
-                        fileName = fnConvertor.getConvertedFileName(data.getFileName());
-                        
-                        File uncompressedFile = new File(fullPathName, fileName);
-                        if (uncompressedFile.exists())
-                        {
-                            uncompressedFile.delete();
-                        }
-                    }
-                    catch (Exception e)
-                    {
-                        logger_.info("Mode " + mode  + ": File " + data.getFileName() + " can't be deleted due to io error");
-                    }
-                }
-                //clear the cache
-                mySessionData.clearList();
-            }
-            forward = null;
-        } // end of if "stop".equals(mode)
-        
-        //Right after uploaded all the files, parsing service will be called
-        //to start process these files.  This allows multiple uploads from
-        //the same applet.  After each upload, the file parsing process is
-        //put into a queue for one by one processing.
-        else if ("success".equals(mode))
-        {
-            //Should not occur, but in case.
-            if (mySessionData == null)
-            {
-                logger_.debug("Session data not found");
-                return mapping.findForward("expiredSession");
-            }
-            
-            List fileList = mySessionData.getFileList();
-            String inout = mySessionData.getInout();
-            String runId = mySessionData.getRunId();
-            
-            logger_.info("Persist file upload data to database ");
-            // use CalabConstants.URI_SEPERATOR to save in the database, which might be different from physical file structure/seperator
-            String uriPathName = mySessionData.getAssayType() + CalabConstants.URI_SEPERATOR 
-                                        + mySessionData.getAssay() + CalabConstants.URI_SEPERATOR
-                                        + mySessionData.getRun()   + CalabConstants.URI_SEPERATOR
-                                        + mySessionData.getInout();
-            String unzipFilePath= CalabConstants.URI_SEPERATOR + uriPathName + CalabConstants.URI_SEPERATOR + CalabConstants.UNCOMPRESSED_FILE_DIRECTORY;
-            workflowService.saveFile(fileList,unzipFilePath,runId,inout,(String)session.getAttribute("creator"));
-            
-            session.setAttribute("newWorkflowCreated", "true");
-            //After data persistence, we need to create all.zip for all upload files,
-            //which includes previous uploaded file and uploaded files currently.
-                      
-            List<FileBean> fileBeans = workflowService.getLastestFileListByRun(runId, inout);
-                      
-            FilePacker fPacker = new FilePacker();
-            String uncompressedFileDirecory = fullPathName + File.separator 
-                                + CalabConstants.UNCOMPRESSED_FILE_DIRECTORY;
-            fPacker.removeOldZipFile(uncompressedFileDirecory);
-            
-            List<String>  fileNameHolder = new ArrayList<String>();
-            for(FileBean fileBean: fileBeans)
-            {
-                 fileNameHolder.add(fileBean.getFilename());
-            }
-            String[] needToPackFiles = new String[fileNameHolder.size()];
-            for(int i = 0; i < needToPackFiles.length; i++)
-            {
-                needToPackFiles[i] = (String)fileNameHolder.get(i);
-            }
-            boolean isCreated = fPacker.packFiles(needToPackFiles,uncompressedFileDirecory );
-            logger_.info("Creating ALL_FILES.zip is " + isCreated);
-            mySessionData.clearList();
-            
-            forward = null;
-        }
-        //If user says it's done, then move to our defaut page for this action.
-        else if ("done".equals(mode))
-        {
-        	// The page will be forwarded to the DefaultURl defined in fileupload.properties
-            forward = null;
-        }
-                
-        return forward;    
+		String runId = (String) fileForm.get("runId");
+		String assayType = (String) fileForm.get("assayType");
+		String assayName = (String) fileForm.get("assayName");
+		String runName = (String) fileForm.get("runName");
+		String inout = (String) fileForm.get("inout");
 
-    }
-    
-    public boolean loginRequired() {        
-         return true;
-    }
+		SpecialCharReplacer specialCharReplacer = new SpecialCharReplacer();
+		assayType = specialCharReplacer.getReplacedString(assayType);
+		assayName = specialCharReplacer.getReplacedString(assayName);
+		runName = specialCharReplacer.getReplacedString(runName);
+
+		fileForm.set("archiveValue", PropertyReader.getProperty(
+				CalabConstants.FILEUPLOAD_PROPERTY, "archiveValue"));
+		fileForm.set("servletURL", PropertyReader.getProperty(
+				CalabConstants.FILEUPLOAD_PROPERTY, "servletURL"));
+		fileForm.set("notifyURL", PropertyReader.getProperty(
+				CalabConstants.FILEUPLOAD_PROPERTY, "notifyURL"));
+		fileForm.set("defaultURL", PropertyReader.getProperty(
+				CalabConstants.FILEUPLOAD_PROPERTY, "defaultURL"));
+		fileForm.set("sid", session.getId());
+		fileForm.set("module", "calab");
+		fileForm.set("permissibleFileExtension", "*");
+
+		HttpFileUploadSessionData hFileUploadData = new HttpFileUploadSessionData();
+		hFileUploadData.setInout(inout);
+		hFileUploadData.setRunId(runId);
+		hFileUploadData.setAssay(assayName);
+		hFileUploadData.setAssayType(assayType);
+		hFileUploadData.setRun(runName);
+		// set where this upload from
+		hFileUploadData.setFromType(request.getParameter("menuType"));
+
+		session.setAttribute("httpFileUploadSessionData", hFileUploadData);
+
+		return mapping.findForward("success");
+	}
+
+	public ActionForward persistFileUploadData(ActionMapping mapping,
+			ActionForm form, HttpServletRequest request,
+			HttpServletResponse response) throws Exception {
+		ActionForward forward = null;
+		HttpFileUploadSessionData mySessionData = (HttpFileUploadSessionData) request
+				.getSession().getAttribute("httpFileUploadSessionData");
+		String path = PropertyReader.getProperty(
+				CalabConstants.FILEUPLOAD_PROPERTY, "fileRepositoryDir");
+		String relativePathName = mySessionData.getAssayType() + File.separator
+				+ mySessionData.getAssay() + File.separator
+				+ mySessionData.getRun() + File.separator
+				+ mySessionData.getInout();
+		String fullPathName = path + relativePathName;
+
+		String mode = (String) request.getParameter("mode");
+		HttpSession session = request.getSession();
+
+		if ("stop".equals(mode)) {
+			if (mySessionData != null) {
+				mySessionData.setIsStopped(true);
+				List list = mySessionData.getFileList();
+
+				for (Iterator it = list.iterator(); it.hasNext();) {
+					HttpUploadedFileData data = (HttpUploadedFileData) it
+							.next();
+
+					try {
+						File file = new File(fullPathName, data.getFileName());
+
+						if (file.exists()) {
+							file.delete();
+						}
+
+						// delete uncompressed files
+						String fileName = null;
+						FileNameConvertor fnConvertor = new FileNameConvertor();
+						fileName = fnConvertor.getConvertedFileName(data
+								.getFileName());
+
+						File uncompressedFile = new File(fullPathName, fileName);
+						if (uncompressedFile.exists()) {
+							uncompressedFile.delete();
+						}
+					} catch (Exception e) {
+						logger_.info("Mode " + mode + ": File "
+								+ data.getFileName()
+								+ " can't be deleted due to io error");
+					}
+				}
+				// clear the cache
+				mySessionData.clearList();
+			}
+			forward = null;
+		} // end of if "stop".equals(mode)
+
+		// Right after uploaded all the files, parsing service will be called
+		// to start process these files. This allows multiple uploads from
+		// the same applet. After each upload, the file parsing process is
+		// put into a queue for one by one processing.
+		else if ("success".equals(mode)) {
+			// Should not occur, but in case.
+			if (mySessionData == null) {
+				logger_.debug("Session data not found");
+				return mapping.findForward("expiredSession");
+			}
+
+			List fileList = mySessionData.getFileList();
+			String inout = mySessionData.getInout();
+			String runId = mySessionData.getRunId();
+
+			logger_.info("Persist file upload data to database ");
+			// use CalabConstants.URI_SEPERATOR to save in the database, which
+			// might be different from physical file structure/seperator
+			String uriPathName = mySessionData.getAssayType()
+					+ CalabConstants.URI_SEPERATOR + mySessionData.getAssay()
+					+ CalabConstants.URI_SEPERATOR + mySessionData.getRun()
+					+ CalabConstants.URI_SEPERATOR + mySessionData.getInout();
+			String unzipFilePath = CalabConstants.URI_SEPERATOR + uriPathName
+					+ CalabConstants.URI_SEPERATOR
+					+ CalabConstants.UNCOMPRESSED_FILE_DIRECTORY;
+			workflowService.saveFile(fileList, unzipFilePath, runId, inout,
+					(String) session.getAttribute("creator"));
+
+			session.setAttribute("newWorkflowCreated", "true");
+			// After data persistence, we need to create all.zip for all upload
+			// files,
+			// which includes previous uploaded file and uploaded files
+			// currently.
+
+			List<FileBean> fileBeans = workflowService.getLastestFileListByRun(
+					runId, inout);
+
+			FilePacker fPacker = new FilePacker();
+			String uncompressedFileDirecory = fullPathName + File.separator
+					+ CalabConstants.UNCOMPRESSED_FILE_DIRECTORY;
+			fPacker.removeOldZipFile(uncompressedFileDirecory);
+
+			List<String> fileNameHolder = new ArrayList<String>();
+			for (FileBean fileBean : fileBeans) {
+				fileNameHolder.add(fileBean.getFilename());
+			}
+			String[] needToPackFiles = new String[fileNameHolder.size()];
+			for (int i = 0; i < needToPackFiles.length; i++) {
+				needToPackFiles[i] = (String) fileNameHolder.get(i);
+			}
+			boolean isCreated = fPacker.packFiles(needToPackFiles,
+					uncompressedFileDirecory);
+			logger_.info("Creating ALL_FILES.zip is " + isCreated);
+			mySessionData.clearList();
+
+			forward = null;
+		}
+		// If user says it's done, then move to our defaut page for this action.
+		else if ("done".equals(mode)) {
+			// The page will be forwarded to the DefaultURl defined in
+			// fileupload.properties
+			forward = null;
+		}
+
+		return forward;
+
+	}
+
+	public boolean loginRequired() {
+		return true;
+	}
 }
