28259e1a2dd48bb37c0ab559a27e9eb4cd79cec9
Sue Pan <sue.pan@nih.gov>
2006-05-10 18:33:06 +0000

    updated forward path to include parameters for upload link
    SVN-Revision: 1240

--- src/gov/nih/nci/calab/ui/workflow/CreateRunAction.java
+++ src/gov/nih/nci/calab/ui/workflow/CreateRunAction.java
@@ -9,8 +9,6 @@ package gov.nih.nci.calab.ui.workflow;
 
 import gov.nih.nci.calab.dto.workflow.RunBean;
 import gov.nih.nci.calab.exception.CalabException;
-import gov.nih.nci.calab.service.util.SpecialCharReplacer;
-import gov.nih.nci.calab.service.util.file.HttpFileUploadSessionData;
 import gov.nih.nci.calab.service.workflow.ExecuteWorkflowService;
 import gov.nih.nci.calab.ui.core.AbstractBaseAction;
 
@@ -21,8 +19,6 @@ import javax.servlet.http.HttpSession;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
-import org.apache.struts.action.ActionMessage;
-import org.apache.struts.action.ActionMessages;
 import org.apache.struts.validator.DynaValidatorForm;
 
 public class CreateRunAction extends AbstractBaseAction {
@@ -30,7 +26,7 @@ public class CreateRunAction extends AbstractBaseAction {
 	public ActionForward executeTask(ActionMapping mapping, ActionForm form,
 			HttpServletRequest request, HttpServletResponse response)
 			throws Exception {
-		
+
 		HttpSession session = request.getSession();
 		// TODO fill in details for aliquot information */
 		DynaValidatorForm theForm = (DynaValidatorForm) form;
@@ -42,15 +38,16 @@ public class CreateRunAction extends AbstractBaseAction {
 		String runBy = (String) theForm.get("runBy");
 		String runDate = (String) theForm.get("runDate");
 		if (!isValidDate(runDate)) {
-//			ActionMessages msgs = new ActionMessages();
-//			ActionMessage msg = new ActionMessage("errors.date", "Run Date");
-//			msgs.add("error", msg);
-//			saveMessages(request, msgs);
-//			
-//			ActionForward forward = mapping.findForward("input");
-//
-//			return forward;
-			throw new CalabException("Year of run date must be in a four digit format.");
+			// ActionMessages msgs = new ActionMessages();
+			// ActionMessage msg = new ActionMessage("errors.date", "Run Date");
+			// msgs.add("error", msg);
+			// saveMessages(request, msgs);
+			//			
+			// ActionForward forward = mapping.findForward("input");
+			//
+			// return forward;
+			throw new CalabException(
+					"Year of run date must be in a four digit format.");
 		}
 
 		// get user and date information from session
@@ -64,40 +61,46 @@ public class CreateRunAction extends AbstractBaseAction {
 		String[] aliquotIds = (String[]) theForm.get("assignedAliquot");
 		String comments = (String) theForm.get("aliquotComment");
 
-		workflowService.saveRunAliquots(runBean.getId(), aliquotIds, comments, creator,
-				creationDate);
+		workflowService.saveRunAliquots(runBean.getId(), aliquotIds, comments,
+				creator, creationDate);
 
 		session.setAttribute("newWorkflowCreated", "true");
 
-		HttpFileUploadSessionData hFileUploadData=new HttpFileUploadSessionData();
-		SpecialCharReplacer specialCharReplacer = new SpecialCharReplacer();
-		String assayType = specialCharReplacer.getReplacedString(runBean.getAssayBean().getAssayType());
-		String assayName = specialCharReplacer.getReplacedString(runBean.getAssayBean().getAssayName());
-		String runName = specialCharReplacer.getReplacedString(runBean.getName());
-
-		hFileUploadData.setRunId(runBean.getId());
-		hFileUploadData.setAssay(assayName);
-		hFileUploadData.setAssayType(assayType);
-		hFileUploadData.setRun(runName);
-		hFileUploadData.setFromType("upload");
-		session.setAttribute("httpFileUploadSessionData", hFileUploadData);
-
-		// set Forward
-		ActionForward forward= mapping.findForward("success");
-				
+		// add parameters to forward
+		String extra = constructGetParams(runBean);
+		String newPath = mapping.findForward("success").getPath() + extra;
+		ActionForward forward = new ActionForward();
+		forward.setPath(newPath);
 		return forward;
 	}
 
 	public boolean loginRequired() {
 		return true;
 	}
-	
+
+	private String constructGetParams(RunBean runBean) {
+		String menuType = "&menuType=upload";
+
+		String runId = "&runId=" + runBean.getId();
+
+		String runName = "&runName=" + runBean.getName();
+
+		String assayName = "&assayName="
+				+ runBean.getAssayBean().getAssayName();
+
+		String assayType = "&assayType="
+				+ runBean.getAssayBean().getAssayType();
+
+		String extra = menuType + runId + runName + assayName + assayType;
+		return extra;
+	}
+
 	private boolean isValidDate(String date) {
-		String year = date.substring(date.lastIndexOf("/")+1);
-		if (year.length()<4){
+		String year = date.substring(date.lastIndexOf("/") + 1);
+		if (year.length() < 4) {
 			return false;
 		}
 		return true;
 	}
-	
+
 }
