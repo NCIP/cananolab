f689a7035ea241fd4a91ddfbd190884b6b837075
Sue Pan <sue.pan@nih.gov>
2006-05-11 21:47:13 +0000

    optimized imports
    SVN-Revision: 1249

--- src/gov/nih/nci/calab/ui/security/LoginAction.java
+++ src/gov/nih/nci/calab/ui/security/LoginAction.java
@@ -12,7 +12,6 @@ import javax.servlet.http.HttpSession;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
-import org.apache.struts.action.ActionMessages;
 import org.apache.struts.validator.DynaValidatorForm;
 
 /**
