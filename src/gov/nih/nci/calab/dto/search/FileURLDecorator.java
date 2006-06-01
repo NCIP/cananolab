5b33b3dc4cf6925dc3d04033ad9dbc0315652a80
Sue Pan <sue.pan@nih.gov>
2006-06-01 22:28:29 +0000

    first deposit for release 0.5
    SVN-Revision: 1353

---
+++ src/gov/nih/nci/calab/dto/search/FileURLDecorator.java
@@ -0,0 +1,48 @@
+package gov.nih.nci.calab.dto.search;
+
+import gov.nih.nci.calab.service.util.SpecialCharReplacer;
+
+import javax.servlet.jsp.PageContext;
+
+import org.displaytag.decorator.DisplaytagColumnDecorator;
+import org.displaytag.exception.DecoratorException;
+import org.displaytag.properties.MediaTypeEnum;
+
+/**
+ * This decorator is used to for display tag to correctly display dynamic links on files
+ * @author pansu
+ *
+ */
+public class FileURLDecorator implements DisplaytagColumnDecorator {
+
+	public Object decorate(Object arg0, PageContext arg1, MediaTypeEnum arg2)
+			throws DecoratorException {
+		WorkflowResultBean workflow = (WorkflowResultBean) arg0;
+		String assayType = workflow.getAssay().getAssayType();
+		String assayName = workflow.getAssay().getAssayName();
+		String runName = workflow.getRun().getName();
+
+		SpecialCharReplacer specialCharReplacer = new SpecialCharReplacer();
+		assayType = specialCharReplacer.getReplacedString(assayType);
+		assayName = specialCharReplacer.getReplacedString(assayName);
+		runName = specialCharReplacer.getReplacedString(runName);
+
+		String fileURL = "downloadSearchedFile.do?method=downloadFile&fileName="
+				+ workflow.getFile().getFilename()
+				+ "&runId="
+				+ workflow.getRun().getId()
+				+ "&runName="
+				+ runName
+				+ "&inout="
+				+ workflow.getFile().getInoutType()
+				+ "&assayType="
+				+ assayType
+				+ "&assayName=" + assayName;
+
+		String link = "<a href=" + fileURL + ">"
+				+ workflow.getFile().getShortFilename() + "<br>"
+				+ workflow.getFile().getTimePrefix() + "</a>";
+
+		return link;
+	}
+}
