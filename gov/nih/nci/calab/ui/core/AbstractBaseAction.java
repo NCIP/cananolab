086ffb13ebd2d166a01cd7e422328d390217d060
Sue Pan <sue.pan@nih.gov>
2006-08-02 16:38:10 +0000

    added method canUserExecute
    SVN-Revision: 8252

--- gov/nih/nci/calab/ui/core/AbstractBaseAction.java
+++ gov/nih/nci/calab/ui/core/AbstractBaseAction.java
@@ -17,19 +17,21 @@ package gov.nih.nci.calab.ui.core;
  * @author pansu
  */
 
-/* CVS $Id: AbstractBaseAction.java,v 1.10 2006-05-03 19:07:44 pansu Exp $ */
+/* CVS $Id: AbstractBaseAction.java,v 1.11 2006-08-02 16:38:10 pansu Exp $ */
 
 import gov.nih.nci.calab.exception.InvalidSessionException;
+import gov.nih.nci.calab.exception.NoAccessException;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.apache.struts.action.Action;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 
-public abstract class AbstractBaseAction extends Action {	
+public abstract class AbstractBaseAction extends Action {
 
 	public ActionForward execute(ActionMapping mapping, ActionForm form,
 			HttpServletRequest request, HttpServletResponse response)
@@ -37,9 +39,17 @@ public abstract class AbstractBaseAction extends Action {
 
 		ActionForward forward = null;
 		if (!loginRequired() || loginRequired() && isUserLoggedIn(request)) {
+			if (loginRequired()) {
+				boolean accessStatus = canUserExecute(request.getSession());
+				if (!accessStatus) {
+					throw new NoAccessException(
+							"You don't have access to class: "
+									+ this.getClass().getName());
+				}
+			}
 			forward = executeTask(mapping, form, request, response);
 		} else {
-			throw new InvalidSessionException();			
+			throw new InvalidSessionException();
 		}
 
 		return forward;
@@ -69,4 +79,16 @@ public abstract class AbstractBaseAction extends Action {
 	 * Provide implementation of the abstract method in each subclass.
 	 */
 	public abstract boolean loginRequired();
+
+	/**
+	 * Check whether the current user in the session can perform the action
+	 * 
+	 * @param session
+	 * @return
+	 * @throws Exception
+	 */
+	public boolean canUserExecute(HttpSession session) throws Exception {
+		return InitSessionSetup.getInstance().canUserExecuteClass(session,
+				this.getClass());
+	}
 }
--- gov/nih/nci/calab/ui/core/AbstractDispatchAction.java
+++ gov/nih/nci/calab/ui/core/AbstractDispatchAction.java
@@ -1,59 +1,11 @@
-/*
- The caArray Software License, Version 1.0
-
- Copyright 2004 SAIC. This software was developed in conjunction with the National 
- Cancer Institute, and so to the extent government employees are co-authors, any 
- rights in such works shall be subject to Title 17 of the United States Code, 
- section 105.
-
- Redistribution and use in source and binary forms, with or without modification, 
- are permitted provided that the following conditions are met:
-
- 1. Redistributions of source code must retain the above copyright notice, this 
- list of conditions and the disclaimer of Article 3, below. Redistributions in 
- binary form must reproduce the above copyright notice, this list of conditions 
- and the following disclaimer in the documentation and/or other materials 
- provided with the distribution.
-
- 2. Affymetrix Pure Java run time library needs to be downloaded from  
- (http://www.affymetrix.com/support/developer/runtime_libraries/index.affx) 
- after agreeing to the licensing terms from the Affymetrix. 
-
- 3. The end-user documentation included with the redistribution, if any, must 
- include the following acknowledgment:
-
- "This product includes software developed by the Science Applications International 
- Corporation (SAIC) and the National Cancer Institute (NCI).”
-
- If no such end-user documentation is to be included, this acknowledgment shall 
- appear in the software itself, wherever such third-party acknowledgments 
- normally appear.
-
- 4. The names "The National Cancer Institute", "NCI", 
- “Science Applications International Corporation”, and "SAIC" must not be used to 
- endorse or promote products derived from this software.
-
- 5. This license does not authorize the incorporation of this software into any 
- proprietary programs. This license does not authorize the recipient to use any 
- trademarks owned by either NCI or SAIC.
-
- 6. THIS SOFTWARE IS PROVIDED "AS IS," AND ANY EXPRESSED OR IMPLIED WARRANTIES, 
- (INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND 
- FITNESS FOR A PARTICULAR PURPOSE) ARE DISCLAIMED. IN NO EVENT SHALL THE NATIONAL 
- CANCER INSTITUTE, SAIC, OR THEIR AFFILIATES BE LIABLE FOR ANY DIRECT, INDIRECT, 
- INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
- PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; 
- OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
- WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
- ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY
- OF SUCH DAMAGE.
- */
 package gov.nih.nci.calab.ui.core;
 
 import gov.nih.nci.calab.exception.InvalidSessionException;
+import gov.nih.nci.calab.exception.NoAccessException;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
@@ -64,18 +16,26 @@ public abstract class AbstractDispatchAction extends DispatchAction {
 	public ActionForward execute(ActionMapping mapping, ActionForm form,
 			HttpServletRequest request, HttpServletResponse response)
 			throws Exception {
-		ActionForward forward = null;		
+		ActionForward forward = null;
 
-		//response.setHeader("Cache-Control", "no-cache");
+		// response.setHeader("Cache-Control", "no-cache");
 
 		if (isCancelled(request))
 			return mapping.findForward("cancel");
 
 		// TODO fill in the common operations */
 		if (!loginRequired() || loginRequired() && isUserLoggedIn(request)) {
+			if (loginRequired()) {
+				boolean accessStatus = canUserExecute(request.getSession());
+				if (!accessStatus) {
+					throw new NoAccessException(
+							"You don't have access to class: "
+									+ this.getClass().getName());
+				}
+			}
 			forward = super.execute(mapping, form, request, response);
 		} else {
-			throw new InvalidSessionException();	
+			throw new InvalidSessionException();
 		}
 		return forward;
 	}
@@ -95,4 +55,14 @@ public abstract class AbstractDispatchAction extends DispatchAction {
 
 	public abstract boolean loginRequired();
 
+	/**
+	 * Check whether the current user in the session can perform the action
+	 * @param session
+	 * @return
+	 * @throws Exception
+	 */
+	public boolean canUserExecute(HttpSession session) throws Exception {
+		return InitSessionSetup.getInstance().canUserExecuteClass(session,
+				this.getClass());
+	}
 }
