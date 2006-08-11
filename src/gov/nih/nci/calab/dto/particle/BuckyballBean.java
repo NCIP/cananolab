6f66edcaa99f2a4dc1da14f92c3d1fd8ac0c043d
Jennifer Zeng <zengje>
2006-08-11 12:54:54 +0000

    DTO object for different type of particle
    SVN-Revision: 1558

---
+++ src/gov/nih/nci/calab/dto/particle/BuckyballBean.java
@@ -0,0 +1,18 @@
+package gov.nih.nci.calab.dto.particle;
+
+public class BuckyballBean extends ParticleBean {
+	private String structure;
+	
+	public BuckyballBean(){
+	}
+
+	public String getStructure() {
+		return structure;
+	}
+
+	public void setStructure(String structure) {
+		this.structure = structure;
+	}
+	
+	
+}
