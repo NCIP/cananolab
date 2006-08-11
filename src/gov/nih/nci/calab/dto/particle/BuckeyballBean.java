fba9a14bf0aa7d3a1236dbee8921e1ecb96a3a02
Sue Pan <sue.pan@nih.gov>
2006-08-11 21:32:30 +0000

    corrected spelling
    SVN-Revision: 1568

---
+++ src/gov/nih/nci/calab/dto/particle/BuckeyballBean.java
@@ -0,0 +1,17 @@
+package gov.nih.nci.calab.dto.particle;
+
+public class BuckeyballBean extends ParticleBean {
+	private String structure;
+	
+	public BuckeyballBean(){
+	}
+
+	public String getStructure() {
+		return structure;
+	}
+
+	public void setStructure(String structure) {
+		this.structure = structure;
+	}
+	
+}
