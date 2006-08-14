813ce00c795956eb8fd440d84afc93f1df0f1e82
Sue Pan <sue.pan@nih.gov>
2006-08-14 21:07:00 +0000

    added skeleton codes
    SVN-Revision: 1596

---
+++ src/gov/nih/nci/calab/service/search/SearchNanoparticleService.java
@@ -0,0 +1,19 @@
+package gov.nih.nci.calab.service.search;
+
+import gov.nih.nci.calab.dto.particle.ParticleBean;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SearchNanoparticleService {
+
+	public List<ParticleBean> basicSearch(String particleSource,
+			String particleType, String functionType,
+			String characterizationType, String[] keywords) {
+		// TODO fill in dataabse code
+		List<ParticleBean> particles = new ArrayList<ParticleBean>();
+
+		return particles;
+	}
+
+}
