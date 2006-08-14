65d5d1f447318be607cb6287aeac78728c0e8d65
Sue Pan <sue.pan@nih.gov>
2006-08-14 21:06:40 +0000

    added other attributes
    SVN-Revision: 8335

--- gov/nih/nci/calab/dto/particle/ParticleBean.java
+++ gov/nih/nci/calab/dto/particle/ParticleBean.java
@@ -1,22 +1,24 @@
 package gov.nih.nci.calab.dto.particle;
 
-public class ParticleBean {
-	private String id;
+import gov.nih.nci.calab.dto.inventory.SampleBean;
+
+public class ParticleBean extends SampleBean {
 
 	private String name;
 
+	private String particleType;
+	
+	private String functionTypes;
+	
+	private String characterizationTypes;
+
+	private String keywords;
+
 	public ParticleBean(String id, String name) {
-		super();
-		// TODO Auto-generated constructor stub
-		this.id = id;
-		this.name = name;
+		super(id, name);
 	}
 
 	public ParticleBean() {
-	}
-
-	public String getId() {
-		return id;
 	}
 
 	public String getName() {
@@ -25,6 +27,38 @@ public class ParticleBean {
 
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	public String getParticleType() {
+		return particleType;
+	}
+
+	public void setParticleType(String particleType) {
+		this.particleType = particleType;
+	}
+
+	public String getCharacterizationTypes() {
+		return characterizationTypes;
+	}
+
+	public void setCharacterizationTypes(String characterizationTypes) {
+		this.characterizationTypes = characterizationTypes;
+	}
+
+	public String getFunctionTypes() {
+		return functionTypes;
+	}
+
+	public void setFunctionTypes(String functionTypes) {
+		this.functionTypes = functionTypes;
+	}
+
+	public String getKeywords() {
+		return keywords;
+	}
+
+	public void setKeywords(String keywords) {
+		this.keywords = keywords;
 	}
 
 }
