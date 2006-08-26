0fd496042d8573f7323782f53396018074b5b0fc
Sue Pan <sue.pan@nih.gov>
2006-08-26 01:53:30 +0000

    updated to extend CompositionBean
    SVN-Revision: 8429

--- gov/nih/nci/calab/dto/characterization/composition/LiposomeBean.java
+++ gov/nih/nci/calab/dto/characterization/composition/LiposomeBean.java
@@ -1,24 +1,15 @@
 package gov.nih.nci.calab.dto.characterization.composition;
 
-import gov.nih.nci.calab.dto.characterization.CharacterizationBean;
 
-public class LiposomeBean extends CharacterizationBean {
+public class LiposomeBean extends CompositionBean {
 	private String polymerized;
-	private String numberOfComponents;
 
 	public LiposomeBean() {
 	}
 
-	public String getNumberOfComponents() {
-		return numberOfComponents;
-	}
 
 	public String getPolymerized() {
 		return polymerized;
-	}
-
-	public void setNumberOfComponents(String numberOfComponents) {
-		this.numberOfComponents = numberOfComponents;
 	}
 
 	public void setPolymerized(String polymerized) {
