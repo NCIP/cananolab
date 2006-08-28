8d621a4e37fe938884349b0dd711fe801a99f486
Sue Pan <sue.pan@nih.gov>
2006-08-28 18:21:15 +0000

    added core, shells and coatings
    SVN-Revision: 1696

--- src/gov/nih/nci/calab/dto/characterization/composition/QuantumDotBean.java
+++ src/gov/nih/nci/calab/dto/characterization/composition/QuantumDotBean.java
@@ -8,37 +8,10 @@ package gov.nih.nci.calab.dto.characterization.composition;
  * @author Zeng
  *
  */
-public class QuantumDotBean extends CompositionBean {
-	private String core;
+public class QuantumDotBean extends MetalParticleBean {
 
-	private String shell;
-
-	private String treatment;
-
-	public QuantumDotBean() {		
+	public QuantumDotBean() {
+		super();		
 	}
-
-	public String getCore() {
-		return core;
-	}
-
-	public void setCore(String core) {
-		this.core = core;
-	}
-
-	public String getShell() {
-		return shell;
-	}
-
-	public void setShell(String shell) {
-		this.shell = shell;
-	}
-
-	public String getTreatment() {
-		return treatment;
-	}
-
-	public void setTreatment(String treatment) {
-		this.treatment = treatment;
-	}
+	
 }
