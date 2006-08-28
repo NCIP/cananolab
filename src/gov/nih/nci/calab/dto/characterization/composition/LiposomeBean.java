f528f25a644a40c17673e8f6df79fedcaf347a79
Sue Pan <sue.pan@nih.gov>
2006-08-28 18:20:58 +0000

    added polymerName
    SVN-Revision: 8435

--- src/gov/nih/nci/calab/dto/characterization/composition/LiposomeBean.java
+++ src/gov/nih/nci/calab/dto/characterization/composition/LiposomeBean.java
@@ -3,10 +3,11 @@ package gov.nih.nci.calab.dto.characterization.composition;
 
 public class LiposomeBean extends CompositionBean {
 	private String polymerized;
+	private String polymerName;
 
 	public LiposomeBean() {
+		super();
 	}
-
 
 	public String getPolymerized() {
 		return polymerized;
@@ -14,6 +15,16 @@ public class LiposomeBean extends CompositionBean {
 
 	public void setPolymerized(String polymerized) {
 		this.polymerized = polymerized;
+	}
+
+
+	public String getPolymerName() {
+		return polymerName;
+	}
+
+
+	public void setPolymerName(String polymerName) {
+		this.polymerName = polymerName;
 	}
 	
 }
