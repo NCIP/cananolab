c083678b6f4d4a4a10dc477880273400b271723a
Sue Pan <sue.pan@nih.gov>
2006-08-31 14:00:56 +0000

    added viewTitle
    SVN-Revision: 1706

--- src/gov/nih/nci/calab/dto/characterization/CharacterizationBean.java
+++ src/gov/nih/nci/calab/dto/characterization/CharacterizationBean.java
@@ -1,8 +1,20 @@
 package gov.nih.nci.calab.dto.characterization;
 
+/**
+ * This class represents shared characterization properties to be shown in
+ * characterization view pages.
+ * 
+ * @author pansu
+ * 
+ */
 public class CharacterizationBean {
 	private String characterizationClassification;
+
 	private String characterizationSource;
+
+	// used to distinguish different instances of characterizations, which are
+	// shown as different links on the view pages.
+	private String viewTitle;
 
 	public String getCharacterizationSource() {
 		return characterizationSource;
@@ -20,4 +32,14 @@ public class CharacterizationBean {
 			String characterizationClassification) {
 		this.characterizationClassification = characterizationClassification;
 	}
+
+	public String getViewTitle() {
+		return viewTitle;
+	}
+
+	public void setViewTitle(String viewTitle) {
+		this.viewTitle = viewTitle;
+	}
+
+
 }
