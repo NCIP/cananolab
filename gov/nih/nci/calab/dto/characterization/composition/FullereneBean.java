b99c1779dd1fe927f2f43d634b53c00d53ebbc4d
Sue Pan <sue.pan@nih.gov>
2006-08-31 14:02:23 +0000

    refactored from BuckeyballBean
    SVN-Revision: 8451

--- gov/nih/nci/calab/dto/characterization/composition/FullereneBean.java
+++ gov/nih/nci/calab/dto/characterization/composition/FullereneBean.java
@@ -1,57 +1,24 @@
-/**
- * 
- */
 package gov.nih.nci.calab.dto.characterization.composition;
 
-
 /**
- * @author Zeng
+ * This class represents properties of a Fullerene composition to be shown in
+ * the view page.
+ * @author pansu
  *
  */
 public class FullereneBean extends CompositionBean {
-	private String chirality;
-	
-	private String growthDiameter;
-	
-	private String averageLength;
-	
-	private String wallType;
+	private String numberOfCarbons;
 	
 	public FullereneBean(){
 		super();
 	}
 
-	public String getAverageLength() {
-		return averageLength;
+	public String getNumberOfCarbons() {
+		return numberOfCarbons;
 	}
 
-	public void setAverageLength(String averageLength) {
-		this.averageLength = averageLength;
+	public void setNumberOfCarbons(String numberOfCarbons) {
+		this.numberOfCarbons = numberOfCarbons;
 	}
-
-	public String getGrowthDiameter() {
-		return growthDiameter;
-	}
-
-	public void setGrowthDiameter(String growthDiameter) {
-		this.growthDiameter = growthDiameter;
-	}
-
-	public String getChirality() {
-		return chirality;
-	}
-
-	public void setChirality(String chirality) {
-		this.chirality = chirality;
-	}
-
-	public String getWallType() {
-		return wallType;
-	}
-
-	public void setWallType(String wallType) {
-		this.wallType = wallType;
-	}
-	
 	
 }
