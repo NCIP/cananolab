245126fead559ccf69fd9441afed5fa43291e0af
Jennifer Zeng <zengje>
2006-08-31 20:15:13 +0000

    Changed from Fulleren to CarbonNanotube
    SVN-Revision: 1733

---
+++ src/gov/nih/nci/calab/domain/nano/characterization/physical/composition/CarbonNanotubeComposition.java
@@ -0,0 +1,96 @@
+package gov.nih.nci.calab.domain.nano.characterization.physical.composition;
+
+import gov.nih.nci.calab.domain.nano.particle.Nanoparticle;
+
+import java.util.Collection;
+import java.util.HashSet;
+
+public class CarbonNanotubeComposition implements Composition {
+	private Float chirality;
+	private Float growthDiameter;
+	private Float averageLength;
+	private String wallType;
+	
+	private Long id;
+	private String source;
+	private String classification;
+	private Collection<Nanoparticle> nanoparticleCollection;
+	private Collection<ComposingElement> composingElementCollection = new HashSet<ComposingElement>();
+		
+	public CarbonNanotubeComposition() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Long getId() {
+		return this.id;
+	}
+
+	public void setSource(String source) {
+		this.source = source;
+	}
+
+	public String getSource() {
+		return this.source;
+	}
+
+	public void setClassification(String classification) {
+		this.classification = classification;
+	}
+
+	public String getClassification() {
+		return this.classification;
+	}
+
+	public Float getAverageLength() {
+		return averageLength;
+	}
+
+	public void setAverageLength(Float averageLength) {
+		this.averageLength = averageLength;
+	}
+
+	public Float getChirality() {
+		return chirality;
+	}
+
+	public void setChirality(Float chirality) {
+		this.chirality = chirality;
+	}
+
+	public Float getGrowthDiameter() {
+		return growthDiameter;
+	}
+
+	public String getWallType() {
+		return wallType;
+	}
+
+	public void setWallType(String wallType) {
+		this.wallType = wallType;
+	}
+
+	public void setGrowthDiameter(Float growthDiameter) {
+		this.growthDiameter = growthDiameter;
+	}
+
+	public void setNanoparticleCollection(Collection<Nanoparticle> particleCollcection) {
+		this.nanoparticleCollection = particleCollcection;
+	}
+
+	public Collection<Nanoparticle> getNanoparticleCollection() {
+		return this.nanoparticleCollection;
+	}
+	
+	public void setComposingElementCollection(Collection<ComposingElement> element){
+		this.composingElementCollection = element;
+	}
+	
+	public Collection<ComposingElement> getComposingElementCollection(){
+		return this.composingElementCollection;
+	}
+}
