5117fa9e60de406b21a1fb2d0d8f4329fc18000b
Sue Pan <sue.pan@nih.gov>
2006-09-05 21:27:57 +0000

    added new constructor
    SVN-Revision: 8502

--- gov/nih/nci/calab/dto/particle/ParticleBean.java
+++ gov/nih/nci/calab/dto/particle/ParticleBean.java
@@ -1,24 +1,33 @@
 package gov.nih.nci.calab.dto.particle;
 
+import gov.nih.nci.calab.domain.Keyword;
+import gov.nih.nci.calab.domain.nano.characterization.Characterization;
+import gov.nih.nci.calab.domain.nano.characterization.physical.composition.Composition;
+import gov.nih.nci.calab.domain.nano.particle.Nanoparticle;
 import gov.nih.nci.calab.dto.inventory.SampleBean;
+import gov.nih.nci.calab.service.util.CalabConstants;
+
+import java.util.Collection;
 
 /**
- * This class represents shared properties of nanoparticles to be shown in the view page.
+ * This class represents shared properties of nanoparticles to be shown in the
+ * view page.
+ * 
  * @author pansu
- *
+ * 
  */
 public class ParticleBean extends SampleBean {
 
-	private String particleCategory;
+	private String particleClassification;
 
 	private String[] functionTypes;
 
 	private String[] characterizationTypes;
 
-	private String[] keywords;	
+	private String[] keywords;
 
 	private String[] visibilityGroups;
-	
+
 	public ParticleBean(String id, String name) {
 		super(id, name);
 	}
@@ -27,15 +36,43 @@ public class ParticleBean extends SampleBean {
 	}
 
 	public ParticleBean(String id, String name, String particleSource,
-			String particleType, String particleCategory, String[] functionTypes,
-			String[] characterizationTypes, String[] keywords) {
+			String particleType, String particleClassification,
+			String[] functionTypes, String[] characterizationTypes,
+			String[] keywords) {
 		this(id, name);
 		setSampleType(particleType);
 		setSampleSource(particleSource);
-		this.particleCategory = particleCategory;
+		this.particleClassification = particleClassification;
 		this.functionTypes = functionTypes;
 		this.characterizationTypes = characterizationTypes;
-		this.keywords = keywords;		
+		this.keywords = keywords;
+	}
+
+	public ParticleBean(Nanoparticle particle) {
+		this(particle.getId().toString(), particle.getName());
+		this.setSampleType(particle.getType());
+		this.setSampleSource(particle.getSource().getOrganizationName());
+		this.particleClassification = particle.getClassification();
+		Collection<Keyword> keywordCol = particle.getKeywordCollection();
+		int i = 0;
+		keywords = new String[keywordCol.size()];
+		for (Keyword keywordObj : keywordCol) {
+			keywords[i] = keywordObj.getName();
+			i++;
+		}
+		Collection<Characterization> characterizationCol = particle
+				.getCharacterizationCollection();
+		int j = 0;
+		characterizationTypes = new String[characterizationCol.size()];
+		for (Characterization charObj : characterizationCol) {
+			String charType="";
+			if (charObj instanceof Composition) {
+				charType=CalabConstants.COMPOSITION_CHARACTERIZATION;
+			}
+			//TODO add other types here
+			characterizationTypes[j] = charObj.getClassification()+":"+charType;
+			j++;
+		}
 	}
 
 	public String[] getCharacterizationTypes() {
@@ -62,14 +99,14 @@ public class ParticleBean extends SampleBean {
 		this.keywords = keywords;
 	}
 
-	public String getParticleCategory() {
-		return particleCategory;
+	public String getParticleClassification() {
+		return particleClassification;
 	}
 
-	public void setParticleCategory(String particleCategory) {
-		this.particleCategory = particleCategory;
+	public void setParticleClassification(String particleClassification) {
+		this.particleClassification = particleClassification;
 	}
-	
+
 	public String[] getVisibilityGroups() {
 		return visibilityGroups;
 	}
