ee8b98bbc821fb636513edc612e7029d23b80be9
Sue Pan <sue.pan@nih.gov>
2006-09-05 21:30:40 +0000

    added different types of characterizations
    SVN-Revision: 1768

--- src/gov/nih/nci/calab/service/util/CalabConstants.java
+++ src/gov/nih/nci/calab/service/util/CalabConstants.java
@@ -1,10 +1,10 @@
 package gov.nih.nci.calab.service.util;
 
 public class CalabConstants {
-	public static final String CSM_APP_NAME="calab";
-	
+	public static final String CSM_APP_NAME = "calab";
+
 	public static final String DATE_FORMAT = "MM/dd/yyyy";
-	
+
 	public static final String ACCEPT_DATE_FORMAT = "MM/dd/yy";
 
 	// Storage element
@@ -55,9 +55,27 @@ public class CalabConstants {
 	public static final String ALL_FILES = "ALL_FILES";
 
 	public static final String URI_SEPERATOR = "/";
-	
+
 	// caLAB property file
 	public static final String CALAB_PROPERTY = "calab.properties";
-	
+
 	public static final String PARTICLE_PROPERTY = "particle.properties";
+
+	public static final String PHYSICAL_CHARACTERIZATION = "Physical";
+	
+	public static final String COMPOSITION_CHARACTERIZATION = "Composition";
+
+	public static final String INVITRO_CHARACTERIZATION = "In Vitro";
+
+	public static final String INVIVO_CHARACTERIZATION = "In Vivo";
+
+	public static final String TOXICITY_CHARACTERIZATION = "Toxicity";
+
+	public static final String CYTOXICITY_CHARACTERIZATION = "Cytoxicity";
+
+	public static final String BLOOD_CONTACT_IMMUNOTOXICITY_CHARACTERIZATION = "Blood Contact";
+
+	public static final String IMMUNE_CELL_FUNCTION_IMMUNOTOXICITY_CHARACTERIZATION = "Immune Cell Function";
+
+	public static final String METABOLIC_STABILITY_TOXICITY_CHARACTERIZATION = "Metabolic Stability";
 }
