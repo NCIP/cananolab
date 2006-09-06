0c9c03a3f46a3e0c646d397c3d2e1f6d256e0f1f
Sue Pan <sue.pan@nih.gov>
2006-09-06 20:51:21 +0000

    added implementations of getDomainObj
    SVN-Revision: 8527

--- src/gov/nih/nci/calab/dto/characterization/composition/ComplexParticleBean.java
+++ src/gov/nih/nci/calab/dto/characterization/composition/ComplexParticleBean.java
@@ -1,28 +1,20 @@
 package gov.nih.nci.calab.dto.characterization.composition;
 
 import gov.nih.nci.calab.domain.nano.characterization.Characterization;
+import gov.nih.nci.calab.domain.nano.characterization.physical.composition.ComplexComposition;
 
 /**
- * This class represents properties of a Complext Nanoparticle composition to be shown in
- * the view page.
+ * This class represents properties of a Complext Nanoparticle composition to be
+ * shown in the view page.
+ * 
  * @author pansu
- *
+ * 
  */
 public class ComplexParticleBean extends CompositionBean {
 	private String name;
 
-	private String description;
-
 	public ComplexParticleBean() {
 		super();
-	}
-
-	public String getDescription() {
-		return description;
-	}
-
-	public void setDescription(String description) {
-		this.description = description;
 	}
 
 	public String getName() {
@@ -32,8 +24,17 @@ public class ComplexParticleBean extends CompositionBean {
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public Characterization getDomainObj() {
-		//TODO fill in details;
-		return null;
+		ComplexComposition doComp = new ComplexComposition();
+		doComp.setName(name);
+		doComp.setSource(getCharacterizationSource());
+		doComp.setClassification(getCharacterizationClassification());
+		doComp.setIdentificationName(getViewTitle());
+		doComp.setDescription(getDescription());
+		for (ComposingElementBean element : getComposingElements()) {
+			doComp.getComposingElementCollection().add(element.getDomainObj());
+		}
+		return doComp;
 	}
 }
