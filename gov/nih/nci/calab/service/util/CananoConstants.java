39b6d0698a26e761c524e0e162e75a96b5458b27
Jennifer Zeng <zengje>
2006-09-07 16:39:04 +0000

    Constants for nano
    SVN-Revision: 8546

---
+++ gov/nih/nci/calab/service/util/CananoConstants.java
@@ -0,0 +1,46 @@
+package gov.nih.nci.calab.service.util;
+
+public class CananoConstants {
+	
+	public static final String PARTICLE_PROPERTY = "particle.properties";
+
+	public static final String PHYSICAL_CHARACTERIZATION = "Physical";
+	
+	public static final String COMPOSITION_CHARACTERIZATION = "Composition";
+
+	public static final String INVITRO_CHARACTERIZATION = "In Vitro";
+
+	public static final String INVIVO_CHARACTERIZATION = "In Vivo";
+
+	public static final String TOXICITY_CHARACTERIZATION = "Toxicity";
+
+	public static final String CYTOXICITY_CHARACTERIZATION = "Cytoxicity";
+
+	public static final String BLOOD_CONTACT_IMMUNOTOXICITY_CHARACTERIZATION = "Blood Contact";
+
+	public static final String IMMUNE_CELL_FUNCTION_IMMUNOTOXICITY_CHARACTERIZATION = "Immune Cell Function";
+
+	public static final String METABOLIC_STABILITY_TOXICITY_CHARACTERIZATION = "Metabolic Stability";
+	
+	public static final String PHYSICAL_SIZE = "Size";
+	
+	public static final String PHYSICAL_SHAPE = "Shape";
+	
+	public static final String PHYSICAL_MOLECULAR_WEIGHT = "MolecularWeight";
+	
+	public static final String PHYSICAL_SOLUBILITY = "Solubility";
+	
+	public static final String PHYSICAL_SURFACE = "Surface";
+	
+	public static final String PHYSICAL_STABILITY = "Stability";
+	
+	public static final String PHYSICAL_PURITY = "Purity";
+	
+	public static final String PHYSICAL_FUNCTIONAL = "Functional";
+	
+	public static final String PHYSICAL_MORPHOLOGY = "Morphology";
+	
+	public static final String PHYSICAL_COMPOSITION = "Composition";
+	
+
+}
