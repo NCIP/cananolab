72aed560da4f2ca6c1b7c6d8156835e67fcf5e1c
Sue Pan <sue.pan@nih.gov>
2006-09-07 19:45:47 +0000

    added particle types
    SVN-Revision: 8554

--- src/gov/nih/nci/calab/service/util/CananoConstants.java
+++ src/gov/nih/nci/calab/service/util/CananoConstants.java
@@ -1,11 +1,21 @@
 package gov.nih.nci.calab.service.util;
 
+import gov.nih.nci.calab.dto.characterization.composition.CarbonNanotubeBean;
+import gov.nih.nci.calab.dto.characterization.composition.ComplexParticleBean;
+import gov.nih.nci.calab.dto.characterization.composition.DendrimerBean;
+import gov.nih.nci.calab.dto.characterization.composition.EmulsionBean;
+import gov.nih.nci.calab.dto.characterization.composition.FullereneBean;
+import gov.nih.nci.calab.dto.characterization.composition.LiposomeBean;
+import gov.nih.nci.calab.dto.characterization.composition.MetalParticleBean;
+import gov.nih.nci.calab.dto.characterization.composition.PolymerBean;
+import gov.nih.nci.calab.dto.characterization.composition.QuantumDotBean;
+
 public class CananoConstants {
-	
+
 	public static final String PARTICLE_PROPERTY = "particle.properties";
 
 	public static final String PHYSICAL_CHARACTERIZATION = "Physical";
-	
+
 	public static final String COMPOSITION_CHARACTERIZATION = "Composition";
 
 	public static final String INVITRO_CHARACTERIZATION = "In Vitro";
@@ -21,26 +31,43 @@ public class CananoConstants {
 	public static final String IMMUNE_CELL_FUNCTION_IMMUNOTOXICITY_CHARACTERIZATION = "Immune Cell Function";
 
 	public static final String METABOLIC_STABILITY_TOXICITY_CHARACTERIZATION = "Metabolic Stability";
-	
+
 	public static final String PHYSICAL_SIZE = "Size";
-	
+
 	public static final String PHYSICAL_SHAPE = "Shape";
-	
+
 	public static final String PHYSICAL_MOLECULAR_WEIGHT = "MolecularWeight";
-	
+
 	public static final String PHYSICAL_SOLUBILITY = "Solubility";
-	
+
 	public static final String PHYSICAL_SURFACE = "Surface";
-	
+
 	public static final String PHYSICAL_STABILITY = "Stability";
-	
+
 	public static final String PHYSICAL_PURITY = "Purity";
-	
+
 	public static final String PHYSICAL_FUNCTIONAL = "Functional";
-	
+
 	public static final String PHYSICAL_MORPHOLOGY = "Morphology";
-	
+
 	public static final String PHYSICAL_COMPOSITION = "Composition";
-	
+
+	public static final String DENDRIMER_TYPE = "Dendrimer";
+
+	public static final String POLYMER_TYPE = "Polymer";
+
+	public static final String LIPOSOME_TYPE = "Liposome";
+
+	public static final String CARBON_NANOTUBE_TYPE = "Carbon Nanotube";
+
+	public static final String FULLERENE_TYPE = "Fullerene";
+
+	public static final String QUANTUM_DOT_TYPE = "Quantum Dot";
+
+	public static final String METAL_PARTICLE_TYPE = "Metal Particle";
+
+	public static final String EMULSION_TYPE = "Emulsion";
+
+	public static final String COMPLEX_PARTICLE_TYPE = "Complex Particle";
 
 }
