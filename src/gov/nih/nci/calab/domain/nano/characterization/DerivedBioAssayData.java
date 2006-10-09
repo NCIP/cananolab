3dd82176c5a8ee73dbb9ce5d3efe2612100ccf92
Jennifer Zeng <zengje>
2006-10-09 14:24:09 +0000

    New Objects for Characterization.
    SVN-Revision: 8709

---
+++ src/gov/nih/nci/calab/domain/nano/characterization/DerivedBioAssayData.java
@@ -0,0 +1,59 @@
+/**
+ * 
+ */
+package gov.nih.nci.calab.domain.nano.characterization;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+/**
+ * @author zengje
+ *
+ */
+public class DerivedBioAssayData {
+
+	private Long id;
+	private String type;
+	private String file;
+	
+	private Collection<Datum> datumCollection = new ArrayList<Datum>();
+	/**
+	 * 
+	 */
+	public DerivedBioAssayData() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public Collection<Datum> getDatumCollection() {
+		return datumCollection;
+	}
+
+	public void setDatumCollection(Collection<Datum> datumCollection) {
+		this.datumCollection = datumCollection;
+	}
+
+	public String getFile() {
+		return file;
+	}
+
+	public void setFile(String file) {
+		this.file = file;
+	}
+
+}
