8a5a527b70f4f19a114d0b5d20fb866e5f1c79ec
Denny Chan <chand>
2006-10-10 22:19:13 +0000

    Initial version.
    SVN-Revision: 8723

---
+++ gov/nih/nci/calab/dto/characterization/DatumBean.java
@@ -0,0 +1,77 @@
+package gov.nih.nci.calab.dto.characterization;
+
+import gov.nih.nci.calab.domain.Measurement;
+import gov.nih.nci.calab.domain.nano.characterization.Datum;
+
+/**
+ * This class represents the data within a characterization file to be shown in
+ * the view page.
+ * 
+ * @author chand
+ * 
+ */
+public class DatumBean {
+	private String id;
+
+	private String type;
+
+	private String value;
+
+	private String valueUnit;
+
+	public DatumBean() {
+
+	}
+	
+	public DatumBean(Datum datum) {
+		this.id = datum.getId().toString();
+		this.type = datum.getType();
+		this.value = datum.getValue().getValue();
+		this.valueUnit = datum.getValue().getUnitOfMeasurement();
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+	public String getValueUnit() {
+		return valueUnit;
+	}
+
+	public void setValueUnit(String valueUnit) {
+		this.valueUnit = valueUnit;
+	}
+
+	public Datum getDomainObj() {
+		Datum tableData = new Datum();
+		if (getId() != null && getId().length() > 0) {
+			tableData.setId(new Long(getId()));
+		}
+		tableData.setType(type);
+		Measurement measurement=new Measurement();
+		measurement.setValue(value);
+		measurement.setUnitOfMeasurement(valueUnit);
+		tableData.setValue(measurement);
+		return tableData;
+	}
+}
---
+++ gov/nih/nci/calab/ui/submit/LoadDerivedBioAssayDataAction.java
@@ -0,0 +1,102 @@
+package gov.nih.nci.calab.ui.submit;
+
+/**
+ * This class associates a assay result file with a characterization.  
+ *  
+ * @author pansu
+ */
+
+/* CVS $Id: LoadDerivedBioAssayDataAction.java,v 1.1 2006-10-10 22:19:13 chand Exp $ */
+
+import java.io.File;
+
+import gov.nih.nci.calab.dto.characterization.CharacterizationFileBean;
+import gov.nih.nci.calab.dto.characterization.DerivedBioAssayDataBean;
+import gov.nih.nci.calab.dto.characterization.SizeBean;
+import gov.nih.nci.calab.service.submit.SubmitNanoparticleService;
+import gov.nih.nci.calab.service.util.CalabConstants;
+import gov.nih.nci.calab.service.util.PropertyReader;
+import gov.nih.nci.calab.ui.core.AbstractDispatchAction;
+import gov.nih.nci.calab.ui.core.InitSessionSetup;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.apache.log4j.Logger;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+import org.apache.struts.upload.FormFile;
+import org.apache.struts.validator.DynaValidatorForm;
+
+public class LoadDerivedBioAssayDataAction extends AbstractDispatchAction {
+	private static Logger logger = Logger.getLogger(LoadDerivedBioAssayDataAction.class);
+	
+	public ActionForward submit(ActionMapping mapping, ActionForm form,
+			HttpServletRequest request, HttpServletResponse response)
+			throws Exception {
+		ActionForward forward = null;
+
+		DynaValidatorForm theForm = (DynaValidatorForm) form;
+		String particleName = (String) theForm.get("particleName");
+		String title = (String) theForm.get("title");
+		String description = (String) theForm.get("description");
+		String comments = (String) theForm.get("comments");
+		String keywords = (String) theForm.get("keywords");
+		String[] visibilities = (String[]) theForm.get("visibilities");
+		String[] keywordList = keywords.split("\r\n");
+
+		String fileSource = (String) theForm.get("fileSource");
+		CharacterizationFileBean fileBean = null;
+		SubmitNanoparticleService service = new SubmitNanoparticleService();
+
+		String fileNumber = (String) theForm.get("fileNumber");
+		
+		if (fileSource.equals("new")) {
+			FormFile file = (FormFile) theForm.get("file");
+			String path = PropertyReader.getProperty(CalabConstants.FILEUPLOAD_PROPERTY, "fileRepositoryDir");
+			path = path + particleName + "\\";
+        	File pathDir = new File (path);
+            if ( !pathDir.exists() ) pathDir.mkdirs();
+
+			fileBean = service.saveCharacterizationFile(particleName, file,
+					title, description, comments, keywordList, visibilities, path, fileNumber);
+		} else {
+			String fileId = (String) theForm.get("fileId");
+			fileBean = service.getFile(fileId);
+		}
+
+		request.getSession().setAttribute("characterizationFile" + fileNumber,
+				fileBean);
+		
+		String forwardPage = (String) theForm.get("forwardPage");
+		forward = mapping.findForward(forwardPage);
+
+		return forward;
+	}
+
+	public ActionForward setup(ActionMapping mapping, ActionForm form,
+			HttpServletRequest request, HttpServletResponse response)
+			throws Exception {
+		HttpSession session = request.getSession();
+		InitSessionSetup.getInstance().clearWorkflowSession(session);
+		InitSessionSetup.getInstance().clearSearchSession(session);
+		InitSessionSetup.getInstance().clearInventorySession(session);
+		InitSessionSetup.getInstance().setAllAssayTypeAssays(session);
+		String particleName = (String) request.getAttribute("particleName");
+		InitSessionSetup.getInstance().setAllRunFiles(session, particleName);		
+		String fileNumber = (String) request.getAttribute("fileNumber");
+		String loadFileForward = (String) request
+				.getAttribute("loadFileForward");
+		DynaValidatorForm theForm = (DynaValidatorForm) form;
+		theForm.set("particleName", particleName);
+		theForm.set("fileNumber", fileNumber);
+		theForm.set("forwardPage", loadFileForward);
+		return mapping.getInputForward();
+	}
+
+	public boolean loginRequired() {
+		return true;
+	}
+}
