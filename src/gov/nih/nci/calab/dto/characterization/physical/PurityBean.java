5488205cc2f961634f812d6c57dcba883812f1cb
Denny Chan <chand>
2006-11-02 18:29:05 +0000

    Initial version.
    SVN-Revision: 8795

---
+++ src/gov/nih/nci/calab/dto/characterization/physical/PurityBean.java
@@ -0,0 +1,125 @@
+package gov.nih.nci.calab.dto.characterization.physical;
+
+import gov.nih.nci.calab.domain.Measurement;
+import gov.nih.nci.calab.domain.nano.characterization.physical.Purity;
+import gov.nih.nci.calab.domain.nano.characterization.physical.Stressor;
+import gov.nih.nci.calab.domain.nano.characterization.Characterization;
+import gov.nih.nci.calab.domain.nano.characterization.DerivedBioAssayData;
+
+import gov.nih.nci.calab.dto.characterization.CharacterizationBean;
+import gov.nih.nci.calab.dto.characterization.DerivedBioAssayDataBean;
+import gov.nih.nci.calab.dto.characterization.DatumBean;
+
+import java.util.List;
+
+
+/**
+ * This class represents the Purity characterization information to be shown in
+ * the view page.
+ * 
+ * @author chande
+ * 
+ */
+public class PurityBean extends CharacterizationBean {
+	private String id;
+	private String homogeneityInLigand;
+	private String residualSolvents;
+	private String freeComponents;
+	private String freeComponentsUnit;
+
+	
+	public PurityBean() {
+		super();
+		initSetup();
+	}
+	
+	public PurityBean(Purity aChar) {
+		super(aChar);
+
+		this.id = aChar.getId().toString();
+		this.homogeneityInLigand = aChar.getHomogeneityInLigand().toString();
+		this.residualSolvents = aChar.getResidualSolvents();
+		this.freeComponents = aChar.getFreeComponents().getValue();
+		this.freeComponentsUnit = aChar.getFreeComponents().getUnitOfMeasurement();
+	}
+	
+	public void setDerivedBioAssayData(
+			List<DerivedBioAssayDataBean> derivedBioAssayData) {
+		super.setDerivedBioAssayData(derivedBioAssayData);
+		initSetup();
+	}
+	
+	public void initSetup() {
+		/*
+		for (DerivedBioAssayDataBean table:getDerivedBioAssayData()) {
+			DatumBean average=new DatumBean();
+			average.setType("Average");
+			average.setValueUnit("nm");
+			DatumBean zaverage=new DatumBean();
+			zaverage.setType("Z-Average");
+			zaverage.setValueUnit("nm");
+			DatumBean pdi=new DatumBean();
+			pdi.setType("PDI");
+			table.getDatumList().add(average);
+			table.getDatumList().add(zaverage);
+			table.getDatumList().add(pdi);
+		}
+		*/
+	}
+	
+	public Purity getDomainObj() {
+		Purity purity = new Purity();
+		super.updateDomainObj(purity);
+		
+		if (this.id != null && !this.id.equals(""))
+			purity.setId(new Long(this.id));
+		
+		purity.setHomogeneityInLigand(new Float(this.homogeneityInLigand));
+		purity.setResidualSolvents(this.residualSolvents);
+		purity.setFreeComponents(new Measurement(this.freeComponents, this.freeComponentsUnit));
+		
+		return purity;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getFreeComponents() {
+		return freeComponents;
+	}
+
+	public void setFreeComponents(String freeComponents) {
+		this.freeComponents = freeComponents;
+	}
+
+	public String getFreeComponentsUnit() {
+		return freeComponentsUnit;
+	}
+
+	public void setFreeComponentsUnit(String freeComponentsUnit) {
+		this.freeComponentsUnit = freeComponentsUnit;
+	}
+
+	public String getHomogeneityInLigand() {
+		return homogeneityInLigand;
+	}
+
+	public void setHomogeneityInLigand(String homogeneityInLigand) {
+		this.homogeneityInLigand = homogeneityInLigand;
+	}
+
+	public String getResidualSolvents() {
+		return residualSolvents;
+	}
+
+	public void setResidualSolvents(String residualSolvents) {
+		this.residualSolvents = residualSolvents;
+	}
+
+
+}
---
+++ src/gov/nih/nci/calab/dto/characterization/physical/StabilityBean.java
@@ -0,0 +1,169 @@
+package gov.nih.nci.calab.dto.characterization.physical;
+
+import gov.nih.nci.calab.domain.Measurement;
+import gov.nih.nci.calab.domain.nano.characterization.physical.Stability;
+import gov.nih.nci.calab.domain.nano.characterization.physical.Stressor;
+import gov.nih.nci.calab.domain.nano.characterization.Characterization;
+import gov.nih.nci.calab.domain.nano.characterization.DerivedBioAssayData;
+
+import gov.nih.nci.calab.dto.characterization.CharacterizationBean;
+import gov.nih.nci.calab.dto.characterization.DerivedBioAssayDataBean;
+import gov.nih.nci.calab.dto.characterization.DatumBean;
+
+import java.util.List;
+
+
+/**
+ * This class represents the Stability characterization information to be shown in
+ * the view page.
+ * 
+ * @author chande
+ * 
+ */
+public class StabilityBean extends CharacterizationBean {
+	private String id;
+	private String longTermStorage;
+	private String longTermStorageUnit;
+	private String shortTermStorage;
+	private String shortTermStorageUnit;
+	private String stressResult;
+	private String releaseKineticsDescription;
+	private String measurementType;
+	private StressorBean stressor = new StressorBean();
+	
+	public StabilityBean() {
+		super();
+		initSetup();
+	}
+	
+	public StabilityBean(Stability aChar) {
+		super(aChar);
+
+		this.id = aChar.getId().toString();
+		this.longTermStorage = aChar.getLongTermStorage().getValue();
+		this.longTermStorageUnit = aChar.getLongTermStorage().getUnitOfMeasurement();
+		this.shortTermStorage = aChar.getShortTermStorage().getValue();
+		this.shortTermStorageUnit = aChar.getShortTermStorage().getUnitOfMeasurement();
+		this.stressResult = aChar.getStressResult();
+		this.releaseKineticsDescription = aChar.getReleaseKineticsDescription();
+		this.measurementType = aChar.getMeasurementType();
+		if (aChar.getStressor() != null) {
+			this.stressor = new StressorBean(aChar.getStressor());
+		}
+		
+	}
+	
+	public void setDerivedBioAssayData(
+			List<DerivedBioAssayDataBean> derivedBioAssayData) {
+		super.setDerivedBioAssayData(derivedBioAssayData);
+		initSetup();
+	}
+	
+	public void initSetup() {
+		/*
+		for (DerivedBioAssayDataBean table:getDerivedBioAssayData()) {
+			DatumBean average=new DatumBean();
+			average.setType("Average");
+			average.setValueUnit("nm");
+			DatumBean zaverage=new DatumBean();
+			zaverage.setType("Z-Average");
+			zaverage.setValueUnit("nm");
+			DatumBean pdi=new DatumBean();
+			pdi.setType("PDI");
+			table.getDatumList().add(average);
+			table.getDatumList().add(zaverage);
+			table.getDatumList().add(pdi);
+		}
+		*/
+	}
+	
+	public Stability getDomainObj() {
+		Stability stability = new Stability();
+		super.updateDomainObj(stability);
+		
+	    if (this.id != null && !this.id.equals(""))
+	    	stability.setId(new Long(id));
+		stability.setLongTermStorage(new Measurement(this.longTermStorage, this.longTermStorageUnit));
+		stability.setShortTermStorage(new Measurement(this.shortTermStorage, this.shortTermStorageUnit));
+		stability.setStressResult(this.stressResult);
+		stability.setReleaseKineticsDescription(this.releaseKineticsDescription);
+		stability.setMeasurementType(this.measurementType);
+		if (this.stressor != null)
+			stability.setStressor(this.stressor.getDomainObj());
+		
+		return stability;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getLongTermStorage() {
+		return longTermStorage;
+	}
+
+	public void setLongTermStorage(String longTermStorage) {
+		this.longTermStorage = longTermStorage;
+	}
+
+	public String getLongTermStorageUnit() {
+		return longTermStorageUnit;
+	}
+
+	public void setLongTermStorageUnit(String longTermStorageUnit) {
+		this.longTermStorageUnit = longTermStorageUnit;
+	}
+
+	public String getMeasurementType() {
+		return measurementType;
+	}
+
+	public void setMeasurementType(String measurementType) {
+		this.measurementType = measurementType;
+	}
+
+	public String getReleaseKineticsDescription() {
+		return releaseKineticsDescription;
+	}
+
+	public void setReleaseKineticsDescription(String releaseKineticsDescription) {
+		this.releaseKineticsDescription = releaseKineticsDescription;
+	}
+
+	public String getShortTermStorage() {
+		return shortTermStorage;
+	}
+
+	public void setShortTermStorage(String shortTermStorage) {
+		this.shortTermStorage = shortTermStorage;
+	}
+
+	public String getShortTermStorageUnit() {
+		return shortTermStorageUnit;
+	}
+
+	public void setShortTermStorageUnit(String shortTermStorageUnit) {
+		this.shortTermStorageUnit = shortTermStorageUnit;
+	}
+
+	public StressorBean getStressor() {
+		return stressor;
+	}
+
+	public void setStressor(StressorBean stressor) {
+		this.stressor = stressor;
+	}
+
+	public String getStressResult() {
+		return stressResult;
+	}
+
+	public void setStressResult(String stressResult) {
+		this.stressResult = stressResult;
+	}
+
+}
