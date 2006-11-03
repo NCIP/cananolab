dfadff8437b6f14dc366199a87bfee86fa2194a4
Jennifer Zeng <zengje>
2006-11-03 21:18:26 +0000

    for Function wireframe
    SVN-Revision: 8816

---
+++ gov/nih/nci/calab/dto/function/LinkageBean.java
@@ -0,0 +1,67 @@
+/**
+ * 
+ */
+package gov.nih.nci.calab.dto.function;
+
+/**
+ * @author zengje
+ *
+ */
+public class LinkageBean {
+	
+	private String id;
+	private String description;
+	private String type;
+	private String value;
+	
+	private AgentBean agent = new AgentBean();
+	
+	/**
+	 * 
+	 */
+	public LinkageBean() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String linkageProperty) {
+		this.value = linkageProperty;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public AgentBean getAgent() {
+		return agent;
+	}
+
+	public void setAgent(AgentBean agent) {
+		this.agent = agent;
+	}
+
+}
