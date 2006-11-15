3b19d072d4af8dd94f3f6b11ab4a65630de13831
Jennifer Zeng <zengje>
2006-11-15 14:47:30 +0000

    Added report, associatedFile and derivedDataFile
    SVN-Revision: 2116

---
+++ src/gov/nih/nci/calab/domain/AssociatedFile.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package gov.nih.nci.calab.domain;
+
+
+public class AssociatedFile extends LabFile {
+
+	/**
+	 * 
+	 */
+	public AssociatedFile() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+}
