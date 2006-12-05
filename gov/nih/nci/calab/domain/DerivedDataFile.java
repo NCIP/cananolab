86bbe36c9683f55e86fba17653109cee4c4796b7
Jennifer Zeng <zengje>
2006-12-05 14:36:02 +0000

    Added keywordCollection
    SVN-Revision: 9084

--- gov/nih/nci/calab/domain/DerivedDataFile.java
+++ gov/nih/nci/calab/domain/DerivedDataFile.java
@@ -3,18 +3,28 @@
  */
 package gov.nih.nci.calab.domain;
 
+import java.util.Collection;
+import java.util.HashSet;
+
 /**
  * @author zengje
  *
  */
 public class DerivedDataFile extends LabFile {
-
+	
+	private Collection<Keyword> keywordCollection = new HashSet<Keyword>();
 	/**
 	 * 
 	 */
 	public DerivedDataFile() {
 		super();
 		// TODO Auto-generated constructor stub
+	}
+	public Collection<Keyword> getKeywordCollection() {
+		return keywordCollection;
+	}
+	public void setKeywordCollection(Collection<Keyword> keywordCollection) {
+		this.keywordCollection = keywordCollection;
 	}
 
 }
