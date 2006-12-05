f001ae8908035e7bf23904b53a022b4ff521b23e
Sue Pan <sue.pan@nih.gov>
2006-12-05 22:54:54 +0000

    added getDomainObj
    SVN-Revision: 9107

--- gov/nih/nci/calab/dto/function/AntibodyBean.java
+++ gov/nih/nci/calab/dto/function/AntibodyBean.java
@@ -1,5 +1,7 @@
 package gov.nih.nci.calab.dto.function;
 
+import gov.nih.nci.calab.domain.nano.function.Antibody;
+
 public class AntibodyBean extends AgentBean {
 	private String name;
 	private String species;
@@ -15,5 +17,11 @@ public class AntibodyBean extends AgentBean {
 	public void setSpecies(String species) {
 		this.species = species;
 	}
-	
+	public Antibody getDomainObj() {
+		Antibody antibody=new Antibody();
+		super.updateDomainObj(antibody);
+		antibody.setName(name);
+		antibody.setSpecies(species);
+		return antibody;
+	}
 }
