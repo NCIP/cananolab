75f3460fafadf344de274abd5feb3856741ff705
Jennifer Zeng <zengje>
2006-12-12 23:18:34 +0000

    Clean up the constructor and use Constants value.
    SVN-Revision: 2531

--- src/gov/nih/nci/calab/dto/characterization/physical/MolecularWeightBean.java
+++ src/gov/nih/nci/calab/dto/characterization/physical/MolecularWeightBean.java
@@ -5,6 +5,7 @@ import gov.nih.nci.calab.domain.nano.characterization.physical.MolecularWeight;
 import gov.nih.nci.calab.dto.characterization.CharacterizationBean;
 import gov.nih.nci.calab.dto.characterization.DatumBean;
 import gov.nih.nci.calab.dto.characterization.DerivedBioAssayDataBean;
+import gov.nih.nci.calab.service.util.CananoConstants;
 
 import java.util.List;
 
@@ -23,24 +24,6 @@ public class MolecularWeightBean extends CharacterizationBean {
 
 	public MolecularWeightBean(Characterization aChar) {
 		super(aChar);
-		/*
-		 * this.setCharacterizationSource(aChar.getSource());
-		 * this.setViewTitle(aChar.getIdentificationName());
-		 * this.setDescription(aChar.getDescription()); if
-		 * (aChar.getInstrument() != null) {
-		 * this.getInstrument().setType(aChar.getInstrument().getInstrumentType().getName());
-		 * this.getInstrument().setDescription(aChar.getInstrument().getDescription());
-		 * this.getInstrument().setManufacturer(aChar.getInstrument().getManufacturer().getName()); }
-		 * this.setNumberOfDerivedBioAssayData(Integer.valueOf(aChar.getDerivedBioAssayDataCollection().size()).toString());
-		 * for (DerivedBioAssayData table :
-		 * aChar.getDerivedBioAssayDataCollection()) { DerivedBioAssayDataBean
-		 * ctBean = new DerivedBioAssayDataBean(table);
-		 * this.getDerivedBioAssayData().add(ctBean); } if
-		 * (aChar.getCharacterizationProtocol() != null) {
-		 * this.getCharacterizationProtocol().setId(aChar.getCharacterizationProtocol().getId());
-		 * this.getCharacterizationProtocol().setName(aChar.getCharacterizationProtocol().getName());
-		 * this.getCharacterizationProtocol().setVersion(aChar.getCharacterizationProtocol().getVersion()); }
-		 */
 	}
 
 	public void setDerivedBioAssayDataList(
@@ -53,7 +36,7 @@ public class MolecularWeightBean extends CharacterizationBean {
 		for (DerivedBioAssayDataBean table : getDerivedBioAssayDataList()) {
 			if (table.getDatumList().isEmpty()) {
 				DatumBean mw = new DatumBean();
-				mw.setType("Molecular Weight");
+				mw.setType(CananoConstants.PHYSICAL_MOLECULAR_WEIGHT);
 				mw.setValueUnit("kDa");
 
 				table.getDatumList().add(mw);
