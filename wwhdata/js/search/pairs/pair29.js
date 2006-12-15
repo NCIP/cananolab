44279d993e8976d38be7a3f1c75a48404700a22b
Jennifer Zeng <zengje>
2006-12-15 15:13:52 +0000

    caLAB1.0 online help draft
    SVN-Revision: 9335

---
+++ wwhdata/js/search/pairs/pair29.js
@@ -0,0 +1,9 @@
+function FileData_Pairs(x)
+{
+x.t("working","files");
+x.t("getting","started");
+x.t("started","calab");
+x.t("1.0","working");
+x.t("calab","1.0");
+x.t("calab.2.29.html","getting");
+}
