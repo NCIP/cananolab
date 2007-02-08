d32ae65bbebf83e61dcf4a1225b231a70d6fbbd2
Jennifer Zeng <zengje>
2007-02-08 20:28:13 +0000

    Move the report/associatedFile association to Nanoparticle
    SVN-Revision: 9564

--- src/gov/nih/nci/calab/domain/Report.java
+++ src/gov/nih/nci/calab/domain/Report.java
@@ -3,12 +3,18 @@
  */
 package gov.nih.nci.calab.domain;
 
+import gov.nih.nci.calab.domain.nano.particle.Nanoparticle;
+
+import java.util.Collection;
+import java.util.HashSet;
+
 /**
  * @author zengje
  * 
  */
 public class Report extends LabFile {
 	private static final long serialVersionUID = 1234567890L;
+	private Collection<Nanoparticle> particleCollection = new HashSet<Nanoparticle>();
 
 	/**
 	 * 
@@ -16,6 +22,14 @@ public class Report extends LabFile {
 	public Report() {
 		super();
 		// TODO Auto-generated constructor stub
+	}
+
+	public Collection<Nanoparticle> getParticleCollection() {
+		return particleCollection;
+	}
+
+	public void setParticleCollection(Collection<Nanoparticle> particleCollection) {
+		this.particleCollection = particleCollection;
 	}
 
 }
