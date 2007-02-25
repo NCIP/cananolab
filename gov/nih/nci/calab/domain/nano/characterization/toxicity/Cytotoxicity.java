6018e5cb77e1b4964b365fe9d4bf4d384c078a2c
Jennifer Zeng <zengje>
2007-02-25 19:44:22 +0000

    Change interface to class
    SVN-Revision: 9574

--- gov/nih/nci/calab/domain/nano/characterization/toxicity/Cytotoxicity.java
+++ gov/nih/nci/calab/domain/nano/characterization/toxicity/Cytotoxicity.java
@@ -2,9 +2,26 @@ package gov.nih.nci.calab.domain.nano.characterization.toxicity;
 
 import gov.nih.nci.calab.domain.nano.characterization.Characterization;
 
-public interface Cytotoxicity extends Characterization {
-	public String getCellLine();
-	public void setCellLine(String cellLine);
-	public String getCellDeathMethod();
-	public void setCellDeathMethod(String cellDeathMethod);
+public class Cytotoxicity extends Toxicity {
+private static final long serialVersionUID = 1234567890L;
+	
+	private String cellLine;
+	private String cellDeathMethod;
+	
+	public String getCellDeathMethod() {
+		return this.cellDeathMethod;
+	}
+
+	public void setCellDeathMethod(String cellDeathMethod) {
+		this.cellDeathMethod = cellDeathMethod;
+	}
+
+	public String getCellLine() {
+		return cellLine;
+	}
+
+	public void setCellLine(String cellLine) {
+		this.cellLine = cellLine;
+	}
+
 }
--- gov/nih/nci/calab/domain/nano/characterization/toxicity/ImmunoToxicity.java
+++ gov/nih/nci/calab/domain/nano/characterization/toxicity/ImmunoToxicity.java
@@ -2,7 +2,26 @@ package gov.nih.nci.calab.domain.nano.characterization.toxicity;
 
 import gov.nih.nci.calab.domain.nano.characterization.Characterization;
 
-public interface ImmunoToxicity extends Characterization {
-	public String getImmunotoxiticyType();
-	public void setImmunotoxiticyType(String type);
+public class ImmunoToxicity extends Toxicity {
+
+	private static final long serialVersionUID = 1234567890L;
+	
+	private String immunotoxicityType;
+	
+	/* (non-Javadoc)
+	 * @see gov.nih.nci.calab.domain.nano.characterization.toxicity.ImmunoToxicity#getImmunotoxiticyType()
+	 */
+	public String getImmunotoxiticyType() {
+		return this.immunotoxicityType;
+	}
+
+	/* (non-Javadoc)
+	 * @see gov.nih.nci.calab.domain.nano.characterization.toxicity.ImmunoToxicity#setImmunotoxiticyType(java.lang.String)
+	 */
+	public void setImmunotoxiticyType(String type) {
+		// TODO Auto-generated method stub
+		this.immunotoxicityType = type;
+
+	}
+
 }
