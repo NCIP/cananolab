3ee37edcf2dfe5325c59e5ece2a925d0aa3d1c3f
Sue Pan <sue.pan@nih.gov>
2007-03-13 14:54:48 +0000

    updated to pass IDs only and use generic getPublicDataIds
    SVN-Revision: 9626

--- gov/nih/nci/calab/service/remote/RemoteQueryFacade.java
+++ gov/nih/nci/calab/service/remote/RemoteQueryFacade.java
@@ -1,10 +1,5 @@
 package gov.nih.nci.calab.service.remote;
 
-import gov.nih.nci.calab.domain.AssociatedFile;
-import gov.nih.nci.calab.domain.OutputFile;
-import gov.nih.nci.calab.domain.Report;
-import gov.nih.nci.calab.domain.nano.particle.Nanoparticle;
-
 import java.util.List;
 
 /**
@@ -15,20 +10,9 @@ import java.util.List;
  */
 public interface RemoteQueryFacade {
 
-	public boolean isPublicData(String data) throws Exception;
+	public boolean isPublicId(String dataId) throws Exception;
 
-	public Nanoparticle[] getPublicNanoparticles(List<Nanoparticle> particles)
-			throws Exception;
-
-	public Report[] getPublicReports(List<Report> reports) throws Exception;
-
-	public AssociatedFile[] getPublicAssociatedFiles(
-			List<AssociatedFile> associatedFiles) throws Exception;
-
-	public OutputFile[] getPublicOutputFiles(List<OutputFile> files)
-			throws Exception;
-
-	public List getPublicData(List dataObjects) throws Exception;
+	public List<String> getPublicDataIds(String[] dataIds) throws Exception;
 
 	public byte[] retrievePublicFileContent(Long fileId) throws Exception;
 }
