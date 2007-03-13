ace0d2d738447a2d760ce454b1a443b75d34f763
Sue Pan <sue.pan@nih.gov>
2007-03-13 14:54:48 +0000

    updated to pass IDs only and use generic getPublicDataIds
    SVN-Revision: 2886

--- src/gov/nih/nci/calab/service/remote/RemoteQueryFacadeImpl.java
+++ src/gov/nih/nci/calab/service/remote/RemoteQueryFacadeImpl.java
@@ -1,11 +1,6 @@
 package gov.nih.nci.calab.service.remote;
 
 import gov.nih.nci.calab.db.HibernateDataAccess;
-import gov.nih.nci.calab.domain.AssociatedFile;
-import gov.nih.nci.calab.domain.LabFile;
-import gov.nih.nci.calab.domain.OutputFile;
-import gov.nih.nci.calab.domain.Report;
-import gov.nih.nci.calab.domain.nano.particle.Nanoparticle;
 import gov.nih.nci.calab.service.common.FileService;
 import gov.nih.nci.calab.service.util.CaNanoLabConstants;
 
@@ -31,17 +26,18 @@ public class RemoteQueryFacadeImpl implements RemoteQueryFacade {
 	public RemoteQueryFacadeImpl() {
 	}
 
-	public boolean isPublicData(String data) throws Exception {
+	public boolean isPublicId(String dataId) throws Exception {
 		Collection<String> publicDataCollection = getPublicProtectionGroupCollection();
-		if (publicDataCollection.contains(data)) {
+		if (publicDataCollection.contains(dataId)) {
 			return true;
 		} else {
 			return false;
 		}
 	}
 
-	private Collection<String> getPublicProtectionGroupCollection() throws Exception {
-		Collection<String> publicDataCollection=new ArrayList<String>();
+	private Collection<String> getPublicProtectionGroupCollection()
+			throws Exception {
+		Collection<String> publicDataCollection = new ArrayList<String>();
 		HibernateDataAccess hda = HibernateDataAccess.getInstance();
 
 		String query = "select distinct a.PROTECTION_GROUP_NAME from csm_protection_group a, csm_role b, csm_user_group_role_pg c, csm_group d	"
@@ -70,60 +66,6 @@ public class RemoteQueryFacadeImpl implements RemoteQueryFacade {
 		return publicDataCollection;
 	}
 
-	public Nanoparticle[] getPublicNanoparticles(List<Nanoparticle> particles)
-			throws Exception {
-		List<Nanoparticle> publicParticles = new ArrayList<Nanoparticle>();
-		Collection<String> publicDataCollection=getPublicProtectionGroupCollection();
-		for (Nanoparticle particle : particles) {
-			if (publicDataCollection.contains(particle.getName().toString())) {
-				publicParticles.add(particle);
-			}
-		}
-		Nanoparticle[] publicParticleArray = new Nanoparticle[publicParticles
-				.size()];
-		return publicParticles.toArray(publicParticleArray);
-	}
-
-	public Report[] getPublicReports(List<Report> reports) throws Exception {
-		List<Report> publicReports = new ArrayList<Report>();
-		Collection<String> publicDataCollection=getPublicProtectionGroupCollection();
-		for (Report report : reports) {
-			if (publicDataCollection.contains(report.getId().toString())) {
-				publicReports.add(report);
-			}
-		}
-		Report[] publicReportArray = new Report[publicReports.size()];
-		return publicReports.toArray(publicReportArray);
-	}
-
-	public AssociatedFile[] getPublicAssociatedFiles(
-			List<AssociatedFile> associatedFiles) throws Exception {
-		List<AssociatedFile> publicAssociatedFiles = new ArrayList<AssociatedFile>();
-		Collection<String> publicDataCollection=getPublicProtectionGroupCollection();
-		for (AssociatedFile associatedFile : associatedFiles) {
-			if (publicDataCollection
-					.contains(associatedFile.getId().toString())) {
-				publicAssociatedFiles.add(associatedFile);
-			}
-		}
-		AssociatedFile[] publicAssociatedFileArray = new AssociatedFile[publicAssociatedFiles
-				.size()];
-		return publicAssociatedFiles.toArray(publicAssociatedFileArray);
-	}
-
-	public OutputFile[] getPublicOutputFiles(List<OutputFile> files)
-			throws Exception {
-		List<OutputFile> publicFiles = new ArrayList<OutputFile>();
-		Collection<String> publicDataCollection=getPublicProtectionGroupCollection();
-		for (OutputFile file : files) {
-			if (publicDataCollection.contains(file.getId().toString())) {
-				publicFiles.add(file);
-			}
-		}
-		OutputFile[] publicFileArray = new OutputFile[publicFiles.size()];
-		return publicFiles.toArray(publicFileArray);
-	}
-
 	/**
 	 * Filter out non-public data. Currently only Nanoparticle, Report,
 	 * AssociatedFile and OutputFile have been set visibility in caNanoLab thus
@@ -132,33 +74,20 @@ public class RemoteQueryFacadeImpl implements RemoteQueryFacade {
 	 * AssociatedFile or OutputFile.
 	 * 
 	 */
-	public List getPublicData(List dataObjects) throws Exception {
-		List processed = new ArrayList();
-		Object firstObj = dataObjects.get(0);
-		Collection<String> publicDataCollection=getPublicProtectionGroupCollection();
-		if (firstObj instanceof Nanoparticle) {
-			for (Object obj : dataObjects) {
-				if (publicDataCollection.contains(((Nanoparticle) obj)
-						.getName())) {
-					processed.add(obj);
-				}
-			}
-		} else if (firstObj instanceof Report
-				|| firstObj instanceof AssociatedFile
-				|| firstObj instanceof OutputFile) {
-			for (Object obj : dataObjects) {
-				if (publicDataCollection.contains(((LabFile) obj).getId()
-						.toString())) {
-					processed.add(obj);
-				}
+	public List<String> getPublicDataIds(String[] dataIds) throws Exception {
+		List<String> publicIds = new ArrayList<String>();
+		Collection<String> publicDataCollection = getPublicProtectionGroupCollection();
+		for (String id : dataIds) {
+			if (publicDataCollection.contains(id)) {
+				publicIds.add(id);
 			}
 		}
-		return processed;
+		return publicIds;
 	}
 
 	public byte[] retrievePublicFileContent(Long fileId) throws Exception {
 		byte[] fileData = null;
-		Collection<String> publicDataCollection=getPublicProtectionGroupCollection();
+		Collection<String> publicDataCollection = getPublicProtectionGroupCollection();
 		if (publicDataCollection.contains(fileId.toString())) {
 			FileService fileService = new FileService();
 			fileData = fileService.getFileContent(fileId);
