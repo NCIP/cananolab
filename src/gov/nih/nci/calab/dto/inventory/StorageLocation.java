831b32befd91d490e795b5ec202ee2916ae8b514
Sue Pan <sue.pan@nih.gov>
2007-05-07 18:52:12 +0000

    removed other
    SVN-Revision: 9740

--- src/gov/nih/nci/calab/dto/inventory/StorageLocation.java
+++ src/gov/nih/nci/calab/dto/inventory/StorageLocation.java
@@ -8,62 +8,22 @@ import gov.nih.nci.calab.service.util.StringUtils;
  * @author pansu
  * 
  */
-/* CVS $Id: StorageLocation.java,v 1.3 2007-01-09 22:53:58 pansu Exp $ */
+/* CVS $Id: StorageLocation.java,v 1.4 2007-05-07 18:52:12 pansu Exp $ */
 
 public class StorageLocation {
 	private String lab = "";
 
 	private String room = "";
 
-	private String otherRoom = "";
-
 	private String freezer = "";
 
-	private String otherFreezer = "";
-
 	private String shelf = "";
-
-	private String otherShelf = "";
 
 	private String rack = "";
 
 	private String box = "";
 
-	private String otherBox = "";
-
 	public StorageLocation() {
-	}
-
-	public String getOtherBox() {
-		return otherBox;
-	}
-
-	public void setOtherBox(String otherBox) {
-		this.otherBox = otherBox;
-	}
-
-	public String getOtherFreezer() {
-		return otherFreezer;
-	}
-
-	public void setOtherFreezer(String otherFreezer) {
-		this.otherFreezer = otherFreezer;
-	}
-
-	public String getOtherRoom() {
-		return otherRoom;
-	}
-
-	public void setOtherRoom(String otherRoom) {
-		this.otherRoom = otherRoom;
-	}
-
-	public String getOtherShelf() {
-		return otherShelf;
-	}
-
-	public void setOtherShelf(String otherShelf) {
-		this.otherShelf = otherShelf;
 	}
 
 	public StorageLocation(StorageLocation loc) {
@@ -73,10 +33,6 @@ public class StorageLocation {
 		shelf = loc.getShelf();
 		rack = loc.getRack();
 		box = loc.getBox();
-		otherRoom=loc.getOtherRoom();
-		otherFreezer=loc.getOtherFreezer();
-		otherShelf=loc.getOtherShelf();
-		otherBox=loc.getOtherBox();
 	}
 
 	public StorageLocation(String lab, String room, String freezer,
@@ -91,19 +47,15 @@ public class StorageLocation {
 		this.box = box;
 	}
 
-	public StorageLocation(String room, String otherRoom, String freezer,
-			String otherFreezer, String shelf, String otherShelf, String box,
-			String otherBox) {
+	public StorageLocation(String room, String freezer,
+			String shelf, String box
+			) {
 		super();
 		// TODO Auto-generated constructor stub
 		this.room = room;
-		this.otherRoom = otherRoom;
 		this.freezer = freezer;
-		this.otherFreezer = otherFreezer;
 		this.shelf = shelf;
-		this.otherShelf = otherShelf;
 		this.box = box;
-		this.otherBox = otherBox;
 	}
 
 	public String getBox() {
@@ -158,7 +110,8 @@ public class StorageLocation {
 		String locationStr = "";
 		String lab = (getLab() == null) ? "" : "Lab " + getLab();
 		String room = (getRoom() == null) ? "" : "Room " + getRoom();
-		String freezer = (getFreezer() == null) ? "" : "Freezer " + getFreezer();
+		String freezer = (getFreezer() == null) ? "" : "Freezer "
+				+ getFreezer();
 		String shelf = (getShelf() == null) ? "" : "Shelf " + getShelf();
 		String rack = (getRack() == null) ? "" : "Rack " + getRack();
 		String box = (getBox() == null) ? "" : "Box " + getBox();
