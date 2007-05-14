6e56f8631f65225618cb2749bb923664bfdcabca
Sue Pan <sue.pan@nih.gov>
2007-05-14 15:02:43 +0000

    added a return type for writeUploadedFile
    SVN-Revision: 9790

--- gov/nih/nci/calab/service/common/FileService.java
+++ gov/nih/nci/calab/service/common/FileService.java
@@ -127,7 +127,7 @@ public class FileService {
 		}
 	}
 
-	public void writeUploadedFile(FormFile uploadedFile, String filePath,
+	public String writeUploadedFile(FormFile uploadedFile, String filePath,
 			boolean addTimeStampPrefix) throws IOException {
 		File pathDir = new File(filePath);
 		if (!pathDir.exists())
@@ -139,7 +139,8 @@ public class FileService {
 		}
 		String fullFileName = filePath + File.separator + fileName;
 		FileOutputStream oStream = new FileOutputStream(new File(fullFileName));		
-		writeFile(uploadedFile.getInputStream(), oStream);		
+		writeFile(uploadedFile.getInputStream(), oStream);
+		return fileName;
 	}
 
 	public void writeFile(InputStream is, FileOutputStream os)
