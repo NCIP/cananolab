17af6077f1fcc84e7098be33b29f09ba61ae8752
Sue Pan <sue.pan@nih.gov>
2007-05-15 18:18:30 +0000

    Refactored out search report into a separate class
    SVN-Revision: 3081

--- src/gov/nih/nci/calab/service/search/SearchNanoparticleService.java
+++ src/gov/nih/nci/calab/service/search/SearchNanoparticleService.java
@@ -2,9 +2,7 @@ package gov.nih.nci.calab.service.search;
 
 import gov.nih.nci.calab.db.DataAccessProxy;
 import gov.nih.nci.calab.db.IDataAccess;
-import gov.nih.nci.calab.domain.AssociatedFile;
 import gov.nih.nci.calab.domain.LabFile;
-import gov.nih.nci.calab.domain.Report;
 import gov.nih.nci.calab.domain.nano.characterization.Characterization;
 import gov.nih.nci.calab.domain.nano.function.Function;
 import gov.nih.nci.calab.domain.nano.particle.Nanoparticle;
@@ -16,11 +14,10 @@ import gov.nih.nci.calab.dto.function.FunctionBean;
 import gov.nih.nci.calab.dto.particle.ParticleBean;
 import gov.nih.nci.calab.exception.CalabException;
 import gov.nih.nci.calab.service.security.UserService;
-import gov.nih.nci.calab.service.util.CaNanoLabConstants;
 import gov.nih.nci.calab.service.util.CaNanoLabComparators;
+import gov.nih.nci.calab.service.util.CaNanoLabConstants;
 import gov.nih.nci.calab.service.util.StringUtils;
 
-import java.io.File;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
@@ -31,7 +28,7 @@ import java.util.Map;
 import org.apache.log4j.Logger;
 
 /**
- * This class includes methods invovled in searching nanoparticles and reports.
+ * This class includes methods invovled in searching nanoparticles.
  * 
  * @author pansu
  * 
@@ -412,142 +409,6 @@ public class SearchNanoparticleService {
 		return filteredReports;
 	}
 
-	public List<LabFileBean> getReportByParticle(String particleName,
-			String particleType, UserBean user) throws Exception {
-		List<LabFileBean> fileBeans = new ArrayList<LabFileBean>();
-		IDataAccess ida = (new DataAccessProxy())
-				.getInstance(IDataAccess.HIBERNATE);
-
-		try {
-
-			ida.open();
-			List results = ida
-					.search("select report.id, report.filename, report.path from Nanoparticle particle join particle.reportCollection "
-							+ " report where particle.name='"
-							+ particleName
-							+ "' and particle.type='" + particleType + "'");
-
-			for (Object obj : results) {
-				String reportId = ((Object[]) obj)[0].toString();
-				String fileName = (String) (((Object[]) obj)[1]);
-				String path = (String) (((Object[]) obj)[2]);
-				String toolTip = "";
-				int idx = path.lastIndexOf(File.separator);
-				if (idx > 0)
-					toolTip = path.substring(idx + 1);
-
-				LabFileBean fileBean = new LabFileBean();
-				fileBean.setId(reportId);
-				fileBean.setPath(path);
-				fileBean.setName(fileName);
-				fileBean.setType(CaNanoLabConstants.REPORT);
-				fileBeans.add(fileBean);
-			}
-
-			UserService userService = new UserService(
-					CaNanoLabConstants.CSM_APP_NAME);
-
-			fileBeans = userService.getFilteredFiles(user, fileBeans);
-		} catch (Exception e) {
-			logger.error("Problem finding report info for particle: "
-					+ particleName);
-			throw e;
-		} finally {
-			ida.close();
-		}
-		return fileBeans;
-	}
-
-	public List<LabFileBean> searchReports(String reportTitle,
-			String reportType, String particleType, String[] functionTypes,
-			UserBean user) throws Exception {
-		List<LabFileBean> reports = new ArrayList<LabFileBean>();
-		IDataAccess ida = (new DataAccessProxy())
-				.getInstance(IDataAccess.HIBERNATE);
-		try {
-
-			ida.open();
-			List<Object> paramList = new ArrayList<Object>();
-			List<String> whereList = new ArrayList<String>();
-			String where = "";
-			String functionTypeFrom = "";
-
-			if (reportTitle.length() > 0) {
-				where = "where ";
-				if (reportTitle.indexOf("*") != -1) {
-					reportTitle = reportTitle.replace('*', '%');
-					whereList.add("report.title like ?");
-				} else {
-					whereList.add("report.title=? ");
-				}
-				paramList.add(reportTitle.toUpperCase());
-			}
-			if (particleType.length() > 0) {
-				paramList.add(particleType);
-				where = "where ";
-				whereList.add("particle.type=? ");
-			}
-			if (functionTypes != null && functionTypes.length > 0) {
-				List<String> inList = new ArrayList<String>();
-				where = "where ";
-				for (String functionType : functionTypes) {
-					paramList.add(functionType);
-					inList.add("?");
-				}
-				functionTypeFrom = "join particle.functionCollection function ";
-				whereList.add("function.type in ("
-						+ StringUtils.join(inList, ", ") + ") ");
-			}
-			String whereStr = StringUtils.join(whereList, " and ");
-
-			String hqlString = "select distinct report from Nanoparticle particle ";
-			List results = null;
-			if (reportType.length() == 0) {
-				String hqlString1 = hqlString
-						+ "join particle.reportCollection report "
-						+ functionTypeFrom + where + whereStr;
-				results = ida.searchByParam(hqlString1, paramList);
-				String hqlString2 = hqlString
-						+ "join particle.associatedFileCollection report "
-						+ functionTypeFrom + where + whereStr;
-				List results2 = ida.searchByParam(hqlString2, paramList);
-				if (results2 != null) {
-					results.addAll(results2);
-				}
-			} else {
-				if (reportType.equals(CaNanoLabConstants.REPORT)) {
-					hqlString += "join particle.reportCollection report ";
-				} else if (reportType.equals(CaNanoLabConstants.ASSOCIATED_FILE)) {
-					hqlString += "join particle.associatedFileCollection report ";
-				}
-				hqlString += functionTypeFrom + where + whereStr;
-				results = ida.searchByParam(hqlString, paramList);
-			}
-
-			for (Object obj : results) {
-				LabFileBean fileBean =null;
-				if (obj instanceof Report) {
-					fileBean=new LabFileBean((Report)obj, CaNanoLabConstants.REPORT);
-				}
-				else {
-					fileBean=new LabFileBean((AssociatedFile)obj, CaNanoLabConstants.ASSOCIATED_FILE);
-				}
-				reports.add(fileBean);
-			}
-		} catch (Exception e) {
-			logger.error("Problem finding report info.");
-			throw e;
-		} finally {
-			ida.close();
-		}
-
-		UserService userService = new UserService(CaNanoLabConstants.CSM_APP_NAME);
-
-		List<LabFileBean> filteredReports = userService.getFilteredFiles(
-				user, reports);
-
-		return filteredReports;
-	}
 
 	/**
 	 * Avanced nanoparticle search based on more detailed meta data.
