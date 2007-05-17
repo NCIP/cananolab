85e03f1f6897925636d23c8a5149a907f8122e5d
Sue Pan <sue.pan@nih.gov>
2007-05-17 20:24:23 +0000

    added constructors for taking in domain objects.
    SVN-Revision: 9841

--- src/gov/nih/nci/calab/dto/common/ProtocolFileBean.java
+++ src/gov/nih/nci/calab/dto/common/ProtocolFileBean.java
@@ -3,23 +3,32 @@
  */
 package gov.nih.nci.calab.dto.common;
 
-import java.util.List;
-import gov.nih.nci.calab.dto.characterization.CharacterizationBean;
+import gov.nih.nci.calab.domain.ProtocolFile;
+import gov.nih.nci.calab.service.util.CaNanoLabConstants;
+
 /**
  * @author zengje
- *
+ * 
  */
-public class ProtocolFileBean extends LabFileBean{
+public class ProtocolFileBean extends LabFileBean {
 	private ProtocolBean protocolBean = new ProtocolBean();
+
 	/**
 	 * 
 	 */
 	public ProtocolFileBean() {
 		super();
 	}
+
+	public ProtocolFileBean(ProtocolFile protocolFile) {
+		super(protocolFile, CaNanoLabConstants.PROTOCOL_FILE);
+		protocolBean=new ProtocolBean(protocolFile.getProtocol());
+	}
+
 	public ProtocolBean getProtocolBean() {
 		return protocolBean;
 	}
+
 	public void setProtocolBean(ProtocolBean protocolBean) {
 		this.protocolBean = protocolBean;
 	}
