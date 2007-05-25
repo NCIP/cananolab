453ff6ebc2b9be40e6d7945f37091191be264c54
Hangjiong Chen <chenhang>
2007-05-25 19:38:35 +0000

    Changed file tilte to protocol name for link
    SVN-Revision: 9877

--- src/gov/nih/nci/calab/dto/search/ProtocolDecorator.java
+++ src/gov/nih/nci/calab/dto/search/ProtocolDecorator.java
@@ -15,22 +15,15 @@ import gov.nih.nci.calab.dto.common.ProtocolFileBean;
  * 
  */
 public class ProtocolDecorator extends TableDecorator {
-	private static String TABLE_START = "<table width=\"100%\" align=\"center\"><tr><td>";
-	private static String TR_TD_START = "<tr><td>";
-	private static String TR_TD_END = "</td></tr>";
-	private static String TD_START = "<td>";
-	private static String TD_END = "</td>";
-	private static String TD_START_END = "</td><td>";
-	private static String TABLE_END = "</table>";
 	public SortableName getEditProtocolURL() {
 		LabFileBean file = (LabFileBean) getCurrentRowObject();
 		// replace space with special char
 		String fileId = file.getId();
 		String editProtocolURL = "updateProtocol.do?dispatch=setupUpdate&fileId="
 				+ fileId;
-		String link = "<a href=" + editProtocolURL + ">" + file.getTitle()
+		String link = "<a href=" + editProtocolURL + ">" + ((ProtocolFileBean)file).getProtocolBean().getName()
 				+ "</a>";
-		SortableName sortableLink = new SortableName(file.getTitle(), link);
+		SortableName sortableLink = new SortableName(((ProtocolFileBean)file).getProtocolBean().getName(), link);
 		return sortableLink;
 	}
 
@@ -40,35 +33,12 @@ public class ProtocolDecorator extends TableDecorator {
 		String fileId = file.getId();
 		String editProtocolURL = "updateProtocol.do?dispatch=setupView&fileId="
 				+ fileId;
-		String link = "<a href=" + editProtocolURL + ">" + file.getTitle()
+		String link = "<a href=" + editProtocolURL + ">" + ((ProtocolFileBean)file).getProtocolBean().getName()
 				+ "</a>";
-		SortableName sortableLink = new SortableName(file.getTitle(), link);
+		SortableName sortableLink = new SortableName(((ProtocolFileBean)file).getProtocolBean().getName(), link);
 		return sortableLink;
 	}
-	/*
-	public String getViewProtocolURL() {
-		ProtocolBean pb = (ProtocolBean)getCurrentRowObject();
-		List<ProtocolFileBean> files = pb.getFileBeanList();
-		// replace space with special char
-		StringBuffer sb = new StringBuffer(TABLE_START);
-		boolean isStart = true;
-		for (ProtocolFileBean file : files ){
-			if (!isStart){
-				sb.append(TR_TD_START);
-			}
-			sb.append(file.getVersion());
-			sb.append(TD_START_END);
-			sb.append(file.getTitle());
-			sb.append(TD_START_END);
-			sb.append(file.getDescription());
-			sb.append(TR_TD_END);
-			isStart = false;
-		}
-		sb.append(TABLE_END);
 
-		return sb.toString();
-	}
-	*/
 public SortableName getRemoteDownloadURL() {
 		LabFileBean file = (LabFileBean) getCurrentRowObject();
 		// TODO add URL
