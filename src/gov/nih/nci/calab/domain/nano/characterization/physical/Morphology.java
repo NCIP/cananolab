b1026c7884457dcca86030deded8f76708f391ee
Sue Pan <sue.pan@nih.gov>
2007-06-01 15:59:39 +0000

    removed unneeded child methods
    SVN-Revision: 3154

--- src/gov/nih/nci/calab/domain/nano/characterization/physical/Morphology.java
+++ src/gov/nih/nci/calab/domain/nano/characterization/physical/Morphology.java
@@ -1,138 +1,24 @@
 package gov.nih.nci.calab.domain.nano.characterization.physical;
 
-import gov.nih.nci.calab.domain.Instrument;
 import gov.nih.nci.calab.domain.nano.characterization.Characterization;
-import gov.nih.nci.calab.domain.nano.characterization.CharacterizationProtocol;
-import gov.nih.nci.calab.domain.nano.characterization.DerivedBioAssayData;
-import gov.nih.nci.calab.domain.nano.particle.Nanoparticle;
-
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Date;
 
 public class Morphology extends Characterization {
 
 	private static final long serialVersionUID = 1234567890L;
 
-	private Long id;
-
-	private String source;
-
-	private String description;
-
-	private String identificationName;
-
-	private String createdBy;
-
-	private Date createdDate;
-
-	private Collection<Nanoparticle> nanoparticleCollection;
-
-	private Collection<DerivedBioAssayData> derivedBioAssayDataCollection = new ArrayList<DerivedBioAssayData>();
-
-	private Instrument instrument;
-
-	private CharacterizationProtocol characterizationProtocol;
-
 	private String type;
-
-	private String classification;
-
-	private String name;
 
 	public Morphology() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
 
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public Long getId() {
-		return this.id;
-	}
-
-	public void setSource(String source) {
-		this.source = source;
-	}
-
-	public String getSource() {
-		return this.source;
-	}
-
 	public String getClassification() {
 		return PHYSICAL_CHARACTERIZATION;
 	}
 
-	public String getDescription() {
-		return description;
-	}
-
-	public void setDescription(String description) {
-		this.description = description;
-	}
-
-	public String getIdentificationName() {
-		return identificationName;
-	}
-
-	public void setIdentificationName(String identificationName) {
-		this.identificationName = identificationName;
-	}
-
 	public String getName() {
 		return PHYSICAL_MORPHOLOGY;
-	}
-
-	public void setNanoparticleCollection(Collection<Nanoparticle> particles) {
-		this.nanoparticleCollection = particles;
-	}
-
-	public Collection<Nanoparticle> getNanoparticleCollection() {
-		return this.nanoparticleCollection;
-	}
-
-	public String getCreatedBy() {
-		return createdBy;
-	}
-
-	public void setCreatedBy(String createdBy) {
-		this.createdBy = createdBy;
-	}
-
-	public Date getCreatedDate() {
-		return createdDate;
-	}
-
-	public void setCreatedDate(Date createdDate) {
-		this.createdDate = createdDate;
-	}
-
-	public Collection<DerivedBioAssayData> getDerivedBioAssayDataCollection() {
-		return derivedBioAssayDataCollection;
-	}
-
-	public void setDerivedBioAssayDataCollection(
-			Collection<DerivedBioAssayData> derivedBioAssayDataCollection) {
-		this.derivedBioAssayDataCollection = derivedBioAssayDataCollection;
-	}
-
-	public Instrument getInstrument() {
-		return instrument;
-	}
-
-	public void setInstrument(Instrument instrument) {
-		this.instrument = instrument;
-	}
-
-	public CharacterizationProtocol getCharacterizationProtocol() {
-		return characterizationProtocol;
-	}
-
-	public void setCharacterizationProtocol(
-			CharacterizationProtocol characterizationProtocol) {
-		this.characterizationProtocol = characterizationProtocol;
 	}
 
 	public String getType() {
@@ -142,13 +28,4 @@ public class Morphology extends Characterization {
 	public void setType(String type) {
 		this.type = type;
 	}
-
-	public void setClassification(String classification) {
-		this.classification = classification;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
 }
--- src/gov/nih/nci/calab/domain/nano/characterization/toxicity/ImmunoToxicity.java
+++ src/gov/nih/nci/calab/domain/nano/characterization/toxicity/ImmunoToxicity.java
@@ -1,27 +1,29 @@
 package gov.nih.nci.calab.domain.nano.characterization.toxicity;
 
-import gov.nih.nci.calab.domain.nano.characterization.Characterization;
-
 public class ImmunoToxicity extends Toxicity {
 
 	private static final long serialVersionUID = 1234567890L;
-	
+
 	private String immunotoxicityType;
-	
-	/* (non-Javadoc)
-	 * @see gov.nih.nci.calab.domain.nano.characterization.toxicity.ImmunoToxicity#getImmunotoxiticyType()
-	 */
+
 	public String getImmunotoxiticyType() {
 		return this.immunotoxicityType;
 	}
 
-	/* (non-Javadoc)
-	 * @see gov.nih.nci.calab.domain.nano.characterization.toxicity.ImmunoToxicity#setImmunotoxiticyType(java.lang.String)
-	 */
 	public void setImmunotoxiticyType(String type) {
 		// TODO Auto-generated method stub
 		this.immunotoxicityType = type;
-
 	}
 
+	public String getImmunotoxicityType() {
+		return immunotoxicityType;
+	}
+
+	public void setImmunotoxicityType(String immunotoxicityType) {
+		this.immunotoxicityType = immunotoxicityType;
+	}
+
+	public String getName() {
+		return IMMUNOTOXICITY;
+	}
 }
