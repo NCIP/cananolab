fa4ecd4d8b048c298d0ccb13e2844c80de302f85
Sue Pan <sue.pan@nih.gov>
2007-06-01 21:22:21 +0000

    updated updateDomainObj method
    SVN-Revision: 3168

--- src/gov/nih/nci/calab/dto/characterization/physical/MorphologyBean.java
+++ src/gov/nih/nci/calab/dto/characterization/physical/MorphologyBean.java
@@ -2,10 +2,7 @@ package gov.nih.nci.calab.dto.characterization.physical;
 
 import gov.nih.nci.calab.domain.nano.characterization.physical.Morphology;
 import gov.nih.nci.calab.dto.characterization.CharacterizationBean;
-import gov.nih.nci.calab.dto.characterization.DerivedBioAssayDataBean;
 import gov.nih.nci.calab.service.util.CaNanoLabConstants;
-
-import java.util.List;
 
 /**
  * This class represents the Morphology characterization information to be shown
@@ -29,63 +26,31 @@ public class MorphologyBean extends CharacterizationBean {
 
 	public MorphologyBean() {
 		super();
-		initSetup();
+	}
+
+	public MorphologyBean(MorphologyBean propBean, CharacterizationBean charBean) {
+		super(charBean);
+		this.type = propBean.getType();
+		this.otherType = propBean.getOtherType();
 	}
 
 	public MorphologyBean(Morphology aChar) {
 		super(aChar);
-		/*
-		 * this.setCharacterizationSource(aChar.getSource());
-		 * this.setViewTitle(aChar.getIdentificationName());
-		 * this.setDescription(aChar.getDescription()); if
-		 * (aChar.getInstrument() != null) {
-		 * this.getInstrument().setType(aChar.getInstrument().getInstrumentType().getName());
-		 * this.getInstrument().setDescription(aChar.getInstrument().getDescription());
-		 * this.getInstrument().setManufacturer(aChar.getInstrument().getManufacturer().getName()); }
-		 * this.setNumberOfDerivedBioAssayData(Integer.valueOf(aChar.getDerivedBioAssayDataCollection().size()).toString());
-		 * for (DerivedBioAssayData table :
-		 * aChar.getDerivedBioAssayDataCollection()) { DerivedBioAssayDataBean
-		 * ctBean = new DerivedBioAssayDataBean(table);
-		 * this.getDerivedBioAssayData().add(ctBean); } if
-		 * (aChar.getCharacterizationProtocol() != null) {
-		 * this.getCharacterizationProtocol().setId(aChar.getCharacterizationProtocol().getId());
-		 * this.getCharacterizationProtocol().setName(aChar.getCharacterizationProtocol().getName());
-		 * this.getCharacterizationProtocol().setVersion(aChar.getCharacterizationProtocol().getVersion()); }
-		 */
 		this.type = aChar.getType();
 	}
 
-	public void setDerivedBioAssayDataList(
-			List<DerivedBioAssayDataBean> derivedBioAssayData) {
-		super.setDerivedBioAssayDataList(derivedBioAssayData);
-		initSetup();
-	}
-
-	public void initSetup() {
-		/*
-		 * for (DerivedBioAssayDataBean table:getDerivedBioAssayData()) {
-		 * DatumBean average=new DatumBean(); average.setType("Average");
-		 * average.setValueUnit("nm"); DatumBean zaverage=new DatumBean();
-		 * zaverage.setType("Z-Average"); zaverage.setValueUnit("nm"); DatumBean
-		 * pdi=new DatumBean(); pdi.setType("PDI");
-		 * table.getDatumList().add(average);
-		 * table.getDatumList().add(zaverage); table.getDatumList().add(pdi); }
-		 */
-	}
-
-public Morphology getDomainObj() {
-		Morphology morphology = new Morphology();
+	public void updateDomainObj(Morphology morphology) {
 		super.updateDomainObj(morphology);
-		
-		if (this.type.equals(CaNanoLabConstants.OTHER) && !this.otherType.equalsIgnoreCase("")){
+
+		if (this.type.equals(CaNanoLabConstants.OTHER)
+				&& !this.otherType.equalsIgnoreCase("")) {
 			morphology.setType(this.otherType);
 		} else {
 			morphology.setType(this.type);
 		}
-			
-		
-		return morphology;
-	}	public String getType() {
+	}
+
+	public String getType() {
 		return type;
 	}
 
