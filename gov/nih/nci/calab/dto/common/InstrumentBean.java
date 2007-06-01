f7e883a8217bf8774a8345b3ecdef68077418fc2
Sue Pan <sue.pan@nih.gov>
2007-06-01 16:07:17 +0000

    first deposit for release 1.2
    SVN-Revision: 9899

---
+++ gov/nih/nci/calab/dto/common/InstrumentBean.java
@@ -0,0 +1,73 @@
+package gov.nih.nci.calab.dto.common;
+
+import gov.nih.nci.calab.domain.Instrument;
+import gov.nih.nci.calab.service.util.StringUtils;
+
+/**
+ * This class represents an instrument associated with instrument configuration
+ * 
+ * @author pansu
+ * 
+ */
+public class InstrumentBean {
+	private String id;
+
+	private String type;
+
+	private String manufacturer;
+
+	private String abbreviation;
+	
+	public InstrumentBean() {
+		
+	}
+
+	public InstrumentBean(String type, String description, String manufacturer,
+			String abbreviation) {
+		super();
+		this.type = type;
+		this.manufacturer = manufacturer;
+		this.abbreviation = abbreviation;
+	}
+
+	public InstrumentBean(Instrument instrument) {
+		this.type = StringUtils.convertToString(instrument.getType());
+		this.manufacturer = StringUtils.convertToString(instrument
+				.getManufacturer());
+		this.abbreviation = StringUtils.convertToString(instrument
+				.getAbbreviation());
+	}
+
+	public String getAbbreviation() {
+		return abbreviation;
+	}
+
+	public void setAbbreviation(String abbreviation) {
+		this.abbreviation = abbreviation;
+	}
+
+	public String getManufacturer() {
+		return manufacturer;
+	}
+
+	public void setManufacturer(String manufacturer) {
+		this.manufacturer = manufacturer;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+}
