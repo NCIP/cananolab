0905a6dbd4715fba7ad84c6a7c0c6728492a7165
Jennifer Zeng <zengje>
2007-06-12 16:04:21 +0000

    Added getName()
    SVN-Revision: 9992

--- gov/nih/nci/calab/domain/nano/characterization/invitro/LeukocyteProliferation.java
+++ gov/nih/nci/calab/domain/nano/characterization/invitro/LeukocyteProliferation.java
@@ -17,4 +17,8 @@ public class LeukocyteProliferation extends ImmunoToxicity {
 	public String getClassification() {
 		return INVITRO_CHARACTERIZATION;
 	}
+	
+	public String getName() {
+		return IMMUNOCELLFUNCTOX_LEUKOCYTE_PROLIFERATION;
+	}
 }
