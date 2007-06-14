e5930516f052c22e2720d736d6219259a1cd52e2
Sue Pan <sue.pan@nih.gov>
2007-06-14 17:59:53 +0000

    new for release 1.2
    SVN-Revision: 10022

---
+++ src/gov/nih/nci/calab/domain/nano/function/FunctionType.java
@@ -0,0 +1,27 @@
+package gov.nih.nci.calab.domain.nano.function;
+
+import java.io.Serializable;
+
+public class FunctionType implements Serializable {
+	private static final long serialVersionUID = 1234567890L;
+
+	private Long id;
+
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+}
