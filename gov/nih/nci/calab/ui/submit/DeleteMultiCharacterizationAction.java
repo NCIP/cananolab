5a9f4beee4a64de087edaf23f08fdb99f0bf6a1a
Jennifer Zeng <zengje>
2007-06-19 16:21:21 +0000

    Use session variable for characterization category
    SVN-Revision: 10074

--- gov/nih/nci/calab/ui/submit/DeleteMultiCharacterizationAction.java
+++ gov/nih/nci/calab/ui/submit/DeleteMultiCharacterizationAction.java
@@ -10,6 +10,7 @@ import gov.nih.nci.calab.ui.core.AbstractDispatchAction;
 import gov.nih.nci.calab.ui.core.InitSessionSetup;
 
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -33,45 +34,45 @@ public class DeleteMultiCharacterizationAction extends AbstractDispatchAction {
 		ActionForward forward = null;
 
 		DynaValidatorForm theForm = (DynaValidatorForm) form;
-		String particleName = (String) theForm.get("particleName");
-		String particleType = (String) theForm.get("particleType");
 
 		// setCharacterizationTypeCharacterizations
 		String deleteType = request.getParameter("charCategory");
-		SubmitNanoparticleService service = new SubmitNanoparticleService();
-		List<CharacterizationBean> charBeans = service
-				.getAllCharacterizationByType(particleName, particleType,
-						deleteType);
+		Map<String, List<CharacterizationBean>> charsMap = (Map<String, List<CharacterizationBean>>) (request
+				.getSession().getAttribute("allCharacterizations"));
+		List<CharacterizationBean> charBeans = charsMap.get(deleteType);
 
 		// convert charBeans to array due to the struts-config
-		theForm.set("charBeans", charBeans
-				.toArray(new CharacterizationBean[charBeans.size()]));
-		
+		if (charBeans != null) {
+			theForm.set("charBeans", charBeans
+					.toArray(new CharacterizationBean[charBeans.size()]));
+			request.setAttribute("charBeansValue", "true");
+		}
+
 		forward = mapping.findForward("success");
 
 		return forward;
 	}
 
-	public ActionForward deleteConfirmed(ActionMapping mapping, ActionForm form,
-			HttpServletRequest request, HttpServletResponse response)
-			throws Exception {
+	public ActionForward deleteConfirmed(ActionMapping mapping,
+			ActionForm form, HttpServletRequest request,
+			HttpServletResponse response) throws Exception {
 		ActionForward forward = null;
 
 		DynaValidatorForm theForm = (DynaValidatorForm) form;
-		String[] charIds = (String[])theForm.get("charIds");
-		String particleType = (String)theForm.get("particleType");
-		String particleName = (String)theForm.get("particleName");
-		
+		String[] charIds = (String[]) theForm.get("charIds");
+		String particleType = (String) theForm.get("particleType");
+		String particleName = (String) theForm.get("particleName");
+
 		// setCharacterizationTypeCharacterizations
 		SubmitNanoparticleService service = new SubmitNanoparticleService();
 		service.deleteCharacterizations(particleName, particleType, charIds);
 
 		// signal the session that characterization has been changed
 		request.getSession().setAttribute("newCharacterizationCreated", "true");
-		
+
 		InitSessionSetup.getInstance().setSideParticleMenu(request,
 				particleName, particleType);
-		
+
 		ActionMessages msgs = new ActionMessages();
 		ActionMessage msg = new ActionMessage("message.delete.characterization");
 		msgs.add("message", msg);
