47937c8d109e0f15c7ea05befa253fc638175aef
Sue Pan <sue.pan@nih.gov>
2007-07-18 20:48:37 +0000

    added MorphologyType, ShapeType and SolventType
    SVN-Revision: 10310

---
+++ src/gov/nih/nci/calab/domain/nano/characterization/physical/ShapeType.java
@@ -0,0 +1,7 @@
+package gov.nih.nci.calab.domain.nano.characterization.physical;
+
+import gov.nih.nci.calab.domain.LookupType;
+
+public class ShapeType extends LookupType {
+	private static final long serialVersionUID = 1234567890L;
+}
