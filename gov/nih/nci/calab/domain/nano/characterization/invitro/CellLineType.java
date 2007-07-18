68805c4d2b590e565ca3516de6329291e01b6063
Sue Pan <sue.pan@nih.gov>
2007-07-18 20:48:06 +0000

    added cellLineType
    SVN-Revision: 10309

---
+++ gov/nih/nci/calab/domain/nano/characterization/invitro/CellLineType.java
@@ -0,0 +1,7 @@
+package gov.nih.nci.calab.domain.nano.characterization.invitro;
+
+import gov.nih.nci.calab.domain.LookupType;
+
+public class CellLineType extends LookupType {
+	private static final long serialVersionUID = 1234567890L;
+}
