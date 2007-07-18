0d02ea38671f174597c654a681c2ebf2c0304db1
Sue Pan <sue.pan@nih.gov>
2007-07-18 20:47:52 +0000

    updated column to be name
    SVN-Revision: 10308

--- gov/nih/nci/calab/domain/MeasureType.java
+++ gov/nih/nci/calab/domain/MeasureType.java
@@ -9,26 +9,6 @@ package gov.nih.nci.calab.domain;
  * 
  */
 
-public class MeasureType implements java.io.Serializable {
+public class MeasureType extends LookupType {
 	private static final long serialVersionUID = 1234567890L;
-
-	private java.lang.Long id;
-
-	public java.lang.Long getId() {
-		return id;
-	}
-
-	public void setId(java.lang.Long id) {
-		this.id = id;
-	}
-
-	private java.lang.String name;
-
-	public java.lang.String getName() {
-		return name;
-	}
-
-	public void setName(java.lang.String name) {
-		this.name = name;
-	}
 }
--- gov/nih/nci/calab/domain/SampleType.java
+++ gov/nih/nci/calab/domain/SampleType.java
@@ -10,54 +10,7 @@ package gov.nih.nci.calab.domain;
 
    */
 
-public  class SampleType 
-	implements java.io.Serializable 
+public  class SampleType extends LookupType
 {
 	private static final long serialVersionUID = 1234567890L;
-
-	
-	   
-	   private java.lang.Long id;
-	   public  java.lang.Long getId(){
-	      return id;
-	   }
-	   public void setId( java.lang.Long id){
-	      this.id = id;
-	   }
-	
-	   
-	   private java.lang.String name;
-	   public  java.lang.String getName(){
-	      return name;
-	   }
-	   public void setName( java.lang.String name){
-	      this.name = name;
-	   }
-	
-	   
-		public boolean equals(Object obj){
-			boolean eq = false;
-			if(obj instanceof AssayType) {
-				AssayType c =(AssayType)obj; 			 
-				Long thisId = getId();		
-				
-					if(thisId != null && thisId.equals(c.getId())) {
-					   eq = true;
-				    }		
-				
-			}
-			return eq;
-		}
-		
-		public int hashCode(){
-			int h = 0;
-			
-			if(getId() != null) {
-				h += getId().hashCode();
-			}
-			
-			return h;
-	}
-	
-	
 }
