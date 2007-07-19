6b1d4808f061380f78f7d49d3fbe0353a0cb864e
Sue Pan <sue.pan@nih.gov>
2007-07-19 15:25:35 +0000

    updated to extend LookupType
    SVN-Revision: 10339

--- gov/nih/nci/calab/domain/nano/characterization/CharacterizationFileType.java
+++ gov/nih/nci/calab/domain/nano/characterization/CharacterizationFileType.java
@@ -1,27 +1,7 @@
 package gov.nih.nci.calab.domain.nano.characterization;
 
-import java.io.Serializable;
+import gov.nih.nci.calab.domain.LookupType;
 
-public class CharacterizationFileType implements Serializable {
+public class CharacterizationFileType extends LookupType {
 	private static final long serialVersionUID = 1234567890L;
-
-	private Long id;
-
-	private String name;
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
 }
