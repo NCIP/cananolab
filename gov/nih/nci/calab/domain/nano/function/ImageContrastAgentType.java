cb34acb48e50ffcb890f5a2d62f3a7510fc2531f
Sue Pan <sue.pan@nih.gov>
2007-07-31 21:25:19 +0000

    added additional lookup types
    SVN-Revision: 10441

---
+++ gov/nih/nci/calab/domain/nano/function/ImageContrastAgentType.java
@@ -0,0 +1,7 @@
+package gov.nih.nci.calab.domain.nano.function;
+
+import gov.nih.nci.calab.domain.LookupType;
+
+public class ImageContrastAgentType extends LookupType {
+	private static final long serialVersionUID = 1234567890L;
+}
