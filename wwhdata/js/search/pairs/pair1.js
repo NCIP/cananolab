0fca6a7c7e9a11b0d5d949d6092a8ace505cd070
Jennifer Zeng <zengje>
2007-08-10 14:29:57 +0000

    1.2 release
    SVN-Revision: 10507

--- wwhdata/js/search/pairs/pair1.js
+++ wwhdata/js/search/pairs/pair1.js
@@ -1,9 +1,7 @@
 function FileData_Pairs(x)
 {
 x.t("managing","cananolab");
-x.t("navigation.","section");
-x.t("topics:","cananolab");
-x.t("interface","navigation.");
+x.t("interface","navigation");
 x.t("working","files");
 x.t("working","cananolab");
 x.t("inventory","working");
@@ -11,19 +9,21 @@ x.t("getting","started");
 x.t("includes","following");
 x.t("cananolab","interface");
 x.t("cananolab","inventory");
-x.t("cananolab","1.1");
+x.t("cananolab","1.2");
 x.t("cananolab","workflow");
 x.t("cananolab","online");
 x.t("cananolab","using");
 x.t("introduces","cananolab");
 x.t("started","cananolab");
-x.t("1.1","getting");
-x.t("1.1","section");
+x.t("1.2","getting");
+x.t("1.2","section");
 x.t("workflow","managing");
 x.t("section","includes");
 x.t("section","introduces");
 x.t("online","help");
 x.t("help","working");
-x.t("following","topics:");
+x.t("topics","cananolab");
+x.t("navigation","section");
+x.t("following","topics");
 x.t("using","cananolab");
 }
