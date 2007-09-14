51f1dde069e5234a870d10dc0338ebc6328a1af5
Sue Pan <sue.pan@nih.gov>
2007-09-14 16:54:44 +0000

    updated to check containerId
    SVN-Revision: 10628

--- gov/nih/nci/calab/dto/inventory/ContainerDecorator.java
+++ gov/nih/nci/calab/dto/inventory/ContainerDecorator.java
@@ -3,8 +3,8 @@ package gov.nih.nci.calab.dto.inventory;
 import org.displaytag.decorator.TableDecorator;
 
 /**
- * This decorator is used to for decorate different properties of a sample container to be
- * shown properly in the view page using display tag lib.
+ * This decorator is used to for decorate different properties of a sample
+ * container to be shown properly in the view page using display tag lib.
  * 
  * @author pansu
  * 
@@ -13,8 +13,14 @@ public class ContainerDecorator extends TableDecorator {
 	public String getContainerId() throws Exception {
 		ContainerBean container = (ContainerBean) getCurrentRowObject();
 		String checked = "";
-		if (getListIndex() == 0) {
+		if (getPageContext().getAttribute("containerId") != null
+				&& ((String) getPageContext().getAttribute("containerId"))
+						.equals(container.getContainerId())) {
 			checked = "checked";
+		} else if (getListIndex() == 0) {
+			checked = "checked";
+		} else {
+			checked = "";
 		}
 		String containerIdstr = "<input type='radio' name='containerId' value='"
 				+ container.getContainerId() + "' " + checked + ">";
