830168da92ca01c5aee03377035ed9246c455164
Sue Pan <sue.pan@nih.gov>
2007-09-28 14:47:38 +0000

    not needed for migration
    SVN-Revision: 10674

--- db/1.2.1/data_migration/from_1.2_oracle/delete_obsolete_data.sql
+++
@@ -1,9 +0,0 @@
-delete from report
-where report_pk_id not in
-(select file_pk_id
-from lab_file);
-
-delete from associated_file
-where associated_file_pk_id not in
-(select file_pk_id
-from lab_file);
