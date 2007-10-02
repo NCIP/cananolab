3bd154f8d6cd33fe46f07b4f45e1c0828827f0c8
Shuang Cai <cais>
2007-10-02 15:09:22 +0000

    add decode and replace functions to handel data with NULL and carriage return. The carriage return is replaced by a space.
    SVN-Revision: 3944

--- db/1.2.1/data_migration/from_1.2_oracle/oracle_data_dump.sql
+++ db/1.2.1/data_migration/from_1.2_oracle/oracle_data_dump.sql
@@ -1,23 +1,22 @@
 set	wrap off
-set linesize 1000
+set linesize 10000
 set	feedback off
 set	pagesize 0
 set	verify off
 set termout off
 set trimspool on
-
 spool tmp.sql
 
 
 prompt  select
-select  lower(column_name)||'||chr(9)||'
+select  'replace(decode(' || lower(column_name) || ', NULL, ''\N'', ' || lower(column_name) || '), chr(13)||chr(10), '' '') ||chr(9)||'
 from    user_tab_columns
 where   table_name = upper('&1') and
     column_id != (select max(column_id) from user_tab_columns where
              table_name = upper('&1'))
 order by column_id
 /
-select  lower(column_name)
+select  'replace(decode(' || lower(column_name) || ', NULL, ''\N'', ' || lower(column_name) || '), chr(13)||chr(10), '' '')'
 from    user_tab_columns
 where   table_name = upper('&1') and
     column_id = (select max(column_id) from user_tab_columns where
@@ -28,7 +27,9 @@ prompt  from    &1
 prompt  /
 
 spool off
+set linesize 10000
 set termout on
+alter session set nls_date_format='yyyy-mm-dd';
 spool &1
 @tmp.sql
 spool off
