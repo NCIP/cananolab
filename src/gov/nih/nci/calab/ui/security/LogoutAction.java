4af7a588419e113b85ea69f132dea63c73f061ea
Sue Pan <sue.pan@nih.gov>
2007-11-02 19:19:04 +0000

    updated to take care of session timeout
    SVN-Revision: 10756

--- src/gov/nih/nci/calab/ui/security/LogoutAction.java
+++ src/gov/nih/nci/calab/ui/security/LogoutAction.java
@@ -1,11 +1,10 @@
 package gov.nih.nci.calab.ui.security;
 
-import gov.nih.nci.calab.ui.core.AbstractBaseAction;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.apache.struts.action.Action;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
@@ -20,24 +19,23 @@ import org.apache.struts.action.ActionMessages;
 
 /* CVS $Id: */
 
-public class LogoutAction extends AbstractBaseAction {
-	public ActionForward executeTask(ActionMapping mapping, ActionForm form,
+public class LogoutAction extends Action {
+	public ActionForward execute(ActionMapping mapping, ActionForm form,
 			HttpServletRequest request, HttpServletResponse response)
 			throws Exception {
 
 		ActionForward forward = null;
 
-		HttpSession session = request.getSession(false);
-		if (session != null) {
-			//invalidate the old one
+		HttpSession session = request.getSession();
+		if (!session.isNew()) {
+			// invalidate the old one
 			session.invalidate();
-			//create a new one
-			session=request.getSession();
-			ActionMessages msgs = new ActionMessages();
-			ActionMessage error = new ActionMessage("msg.logout");
-			msgs.add("msg", error);
-			saveMessages(request, msgs);
-		}		
+		}
+		ActionMessages msgs = new ActionMessages();
+		ActionMessage error = new ActionMessage("msg.logout");
+		msgs.add("msg", error);
+		saveMessages(request, msgs);
+
 		forward = mapping.findForward("success");
 		return forward;
 	}
