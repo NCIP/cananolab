e932831cbe1269797666f2c6f100fd710c1ff8b7
Sue Pan <sue.pan@nih.gov>
2007-11-16 22:47:22 +0000

    added insert
    SVN-Revision: 4143

--- db/1.3/data_migration/app_schema_changes.sql
+++ db/1.3/data_migration/app_schema_changes.sql
@@ -15,10 +15,25 @@ drop table run_sample_container;
 DROP TABLE IF EXISTS def_composing_element_type;
 CREATE TABLE def_composing_element_type (
    composing_element_type_pk_id decimal (20, 0) NOT NULL,
-   name varchar (200) NOT NULL
+   name varchar (200) NOT NULL,
    PRIMARY KEY (composing_element_type_pk_id)
 )
 ENGINE=INNODB;
+
+
+INSERT INTO def_composing_element_type(composing_element_type_pk_id, name)
+VALUES (1, 'Core'),
+  (2, 'Shell'),
+  (3, 'Coating'),
+  (4, 'Monomer'),
+  (5, 'Lipid'),
+  (6, 'Modification'),
+  (7, 'Oil'),
+  (8, 'PFC'),
+  (9, 'Drug'),
+  (10, 'Image Contrast Agent');
+
+commit;
 
 -- Re-enable foreign key checks
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
