21622c13bec63d17bb345a073193807ba0f574b4
Sue Pan <sue.pan@nih.gov>
2007-11-19 21:15:52 +0000

    updated userBean
    SVN-Revision: 10885

--- src/gov/nih/nci/calab/ui/particle/LoadDerivedBioAssayDataAction.java
+++ src/gov/nih/nci/calab/ui/particle/LoadDerivedBioAssayDataAction.java
@@ -6,7 +6,7 @@ package gov.nih.nci.calab.ui.particle;
  * @author pansu
  */
 
-/* CVS $Id: LoadDerivedBioAssayDataAction.java,v 1.3 2007-11-16 22:30:15 pansu Exp $ */
+/* CVS $Id: LoadDerivedBioAssayDataAction.java,v 1.4 2007-11-19 21:15:52 pansu Exp $ */
 
 import gov.nih.nci.calab.dto.characterization.CharacterizationBean;
 import gov.nih.nci.calab.dto.characterization.DerivedBioAssayDataBean;
@@ -71,8 +71,8 @@ public class LoadDerivedBioAssayDataAction extends AbstractDispatchAction {
 				.getVisibilityGroups());
 		derivedBioAssayDataBean.setKeywords(fileBean.getKeywords());
 		derivedBioAssayDataBean.setCreatedDate(new Date());
-		String user = (String) request.getSession().getAttribute("user");
-		derivedBioAssayDataBean.setCreatedBy(user);
+		UserBean user = (UserBean) request.getSession().getAttribute("user");
+		derivedBioAssayDataBean.setCreatedBy(user.getLoginName());
 		ActionForward forward = new ActionForward();
 		forward.setPath(forwardPage);
 		return forward;
