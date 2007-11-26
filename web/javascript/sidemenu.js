9c46666eacfa4a6356d3825d9b1cef7a989a7d94
Shuang Cai <cais>
2007-11-26 17:10:59 +0000

    modified the event handling to open submenu under the In Vitro.
    SVN-Revision: 4178

--- web/javascript/sidemenu.js
+++ web/javascript/sidemenu.js
@@ -16,7 +16,8 @@ function init() {
     for (var i = 0; i < lis.length; i++) {
       var node = lis[i];
       if(node.nodeName.toLowerCase() == 'li') {	
-         	if(node.className == 'toplist') {
+         	if(node.className == 'controlList' ||
+         		node.className == 'controlList2') {
         		addEvent(node, 'click', getClickFor(node), false);
         		// node.getElementsByTagName('a')[0].className += ' subheader';
         	} else {
@@ -40,16 +41,21 @@ function mclick(e, targetElement) {
     var cnode = el.childNodes[i];
     
     var parentUl = el.parentNode;
-    if(cnode.nodeName.toLowerCase() == 'ul' ) {
-        if( cnode.style.display != 'block' ) {
-  			closeAllSideMenu();
+    if(cnode.nodeName.toLowerCase() == 'ul') {
+       if(el.className != 'controlList2') {
+          if( cnode.style.display != 'block' ) {
+  			closeAllSideMenu(parentUl);
      		cnode.style.display = 'block';
      		var uls = cnode.getElementsByTagName('ul');
   			for (var u = 0; u < uls.length; u++) {
-     			uls[u].style.display = 'block';
+  				if(uls[u].className != 'sublist_5_control' &&
+  				   uls[u].className != 'sublist_3_control') {
+  				
+     				uls[u].style.display = 'block';
+     			}
      		}
 
-    	} else if(parentUl.nodeName.toLowerCase() == 'ul' 
+    	   } else if(parentUl.nodeName.toLowerCase() == 'ul' 
     		/* && parentUl.className == 'slidingmenu' */ ) {
     		
     		cnode.style.display = 'none';
@@ -57,6 +63,26 @@ function mclick(e, targetElement) {
      		for (var j = 0; j < uls.length; j++) {
      			uls[j].style.display = 'none';
      		}
+     	   }
+    	} else {
+    	   if( cnode.style.display != 'block' ) {
+    	       cancelBubling(e);
+  			closeAllSideMenu(parentUl);
+     		cnode.style.display = 'block';
+     		var uls = cnode.getElementsByTagName('ul');
+  			for (var u = 0; u < uls.length; u++) {
+     			uls[u].style.display = 'block';
+     		}
+
+    	   } else if(parentUl.nodeName.toLowerCase() == 'ul' 
+    		/* && parentUl.className == 'slidingmenu' */ ) {
+    		
+    		cnode.style.display = 'none';
+    		var uls = cnode.getElementsByTagName('ul');
+     		for (var j = 0; j < uls.length; j++) {
+     			uls[j].style.display = 'none';
+     		}
+     	   }
     	}
     }
     if (window.event) {
@@ -68,9 +94,9 @@ function mclick(e, targetElement) {
   }
 }
 
-function closeAllSideMenu ()
+function closeAllSideMenu (ulroot)
 {
-	var ulroot = document.getElementById('menuroot');
+	//var ulroot = document.getElementById('menuroot');
 	var menuuls = ulroot.getElementsByTagName('ul');
 	for(var k=0; k<menuuls.length; k++) {
 		menuuls[k].style.display = 'none';
