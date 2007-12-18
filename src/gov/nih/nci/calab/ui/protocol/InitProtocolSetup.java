32d3cb23c51f7cb21e5a12ea7c18cbafa7a157eb
Sue Pan <sue.pan@nih.gov>
2007-12-18 16:44:56 +0000

    updated to use setProtocolFilesByCharType
    SVN-Revision: 11143

--- src/gov/nih/nci/calab/ui/protocol/InitProtocolSetup.java
+++ src/gov/nih/nci/calab/ui/protocol/InitProtocolSetup.java
@@ -1,18 +1,16 @@
 package gov.nih.nci.calab.ui.protocol;
 
+import gov.nih.nci.calab.domain.nano.characterization.Characterization;
 import gov.nih.nci.calab.dto.common.ProtocolFileBean;
 import gov.nih.nci.calab.exception.CaNanoLabException;
 import gov.nih.nci.calab.exception.CaNanoLabSecurityException;
 import gov.nih.nci.calab.exception.ParticleCharacterizationException;
 import gov.nih.nci.calab.exception.ProtocolException;
 import gov.nih.nci.calab.service.common.LookupService;
-import gov.nih.nci.calab.service.particle.NanoparticleCharacterizationService;
 import gov.nih.nci.calab.service.protocol.SearchProtocolService;
 import gov.nih.nci.calab.service.util.CaNanoLabConstants;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.SortedSet;
 
 import javax.servlet.http.HttpSession;
@@ -38,47 +36,29 @@ public class InitProtocolSetup {
 
 	public void setAllProtocolTypes(HttpSession session)
 			throws CaNanoLabException {
-		SortedSet<String> protocolTypes = LookupService
-				.getAllLookupTypes("ProtocolType");
-		session.setAttribute("protocolTypes", protocolTypes);
+		if (session.getAttribute("protocolTypes") == null
+				|| session.getAttribute("newProtocolCreated") != null) {
+			SortedSet<String> protocolTypes = LookupService
+					.getAllLookupTypes("ProtocolType");
+			session.setAttribute("protocolTypes", protocolTypes);
+		}
+		session.removeAttribute("newProtocolCreated");
 	}
 
-	public void setProtocolFilesByCharName(HttpSession session, String charName)
+	public void setProtocolFilesByCharType(HttpSession session, String charType)
 			throws ParticleCharacterizationException,
 			CaNanoLabSecurityException, ProtocolException {
-		Map<String, String> charNameToCharCategory = null;
-		// retrieve from application context if there
-		if (session.getServletContext().getAttribute(
-				"characterizationCategoryMap") == null) {
-			NanoparticleCharacterizationService charService = new NanoparticleCharacterizationService();
-			charNameToCharCategory = charService
-					.getCharacterizationCategoryMap();
-			session.getServletContext().setAttribute(
-					"characterizationCategoryMap", charNameToCharCategory);
-		} else {
-			charNameToCharCategory = new HashMap<String, String>(
-					(Map<? extends String, String>) session.getServletContext()
-							.getAttribute("characterizationCategoryMap"));
-		}
 		String protocolType = null;
-		String category = charNameToCharCategory.get(charName);
-		if (category != null
-				&& category
-						.equals(CaNanoLabConstants.PHYSICAL_CHARACTERIZATION_CATEGORY)) {
+		if (charType != null
+				&& charType
+						.equalsIgnoreCase(Characterization.PHYSICAL_CHARACTERIZATION)) {
 			protocolType = CaNanoLabConstants.PHYSICAL_ASSAY_PROTOCOL;
+		} else if (charType != null
+				&& charType
+						.equalsIgnoreCase(Characterization.INVITRO_CHARACTERIZATION)) {
+			protocolType = CaNanoLabConstants.INVITRO_ASSAY_PROTOCOL;
 		} else {
-			// get the top level category
-			String nextCategory = null;
-			while (category != null) {
-				nextCategory = category;
-				category = charNameToCharCategory.get(nextCategory);
-			}
-			if (nextCategory
-					.equals(CaNanoLabConstants.IN_VITRO_CHARACTERIZATION_CATEGORY)) {
-				protocolType = CaNanoLabConstants.INVITRO_ASSAY_PROTOCOL;
-			} else {
-				protocolType = null; // update if in vivo is implemented
-			}
+			protocolType = null; // update if in vivo is implemented
 		}
 		List<ProtocolFileBean> protocolFiles = null;
 		protocolFiles = searchProtocolService
