e063b71b9f1a2bdb63986561793fe13b28255a8c
Sue Pan <sue.pan@nih.gov>
2008-01-04 17:23:39 +0000

    updated to represent characterization summary information
    SVN-Revision: 11240

--- src/gov/nih/nci/calab/dto/characterization/CharacterizationSummaryBean.java
+++ src/gov/nih/nci/calab/dto/characterization/CharacterizationSummaryBean.java
@@ -1,40 +1,136 @@
 package gov.nih.nci.calab.dto.characterization;
 
-import java.util.HashMap;
+import gov.nih.nci.calab.dto.particle.ParticleBean;
+import gov.nih.nci.calab.service.util.StringUtils;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 import java.util.Map;
+import java.util.SortedSet;
+import java.util.TreeSet;
 
+/**
+ * This class represents summary view characterization properties to be shown in
+ * characterization summary view pages.
+ * 
+ * @author pansu
+ * 
+ */
 public class CharacterizationSummaryBean {
-	private CharacterizationBean charBean;
+	private String actionName; // physical or inVitro
 
-	private Map<String, String> datumMap = new HashMap<String, String>();
+	private ParticleBean particle = new ParticleBean();
 
-	private DerivedBioAssayDataBean charFile;
+	private String characterizationType; // physical or in vitro
+
+	private List<CharacterizationSummaryRowBean> summaryRows;
+
+	private SortedSet<String> columnLabels;
+
+	private List<CharacterizationBean> charBeans;
+
+	private String characterizationName; // e.g. Molecular Weight
+
+	private String exportFileName;
+
+	private String fullExportFileName;
+
+	private String dispatchActionName; // e.g. molecularWeight
+
+	public String getActionName() {
+		if (getCharacterizationType() != null)
+			this.actionName = StringUtils.getOneWordLowerCaseFirstLetter(this
+					.getCharacterizationType())
+					+ "Characterization";
+		return this.actionName;
+	}
 
 	public CharacterizationSummaryBean() {
-
+		particle = new ParticleBean();
 	}
 
-	public Map<String, String> getDatumMap() {
-		return datumMap;
+	public String getCharacterizationType() {
+		return characterizationType;
 	}
 
-	public void setDatumMap(Map<String, String> datumMap) {
-		this.datumMap = datumMap;
+	public void setCharacterizationType(String characterizationType) {
+		this.characterizationType = characterizationType;
 	}
 
-	public CharacterizationBean getCharBean() {
-		return charBean;
+	public ParticleBean getParticle() {
+		return particle;
 	}
 
-	public void setCharBean(CharacterizationBean charBean) {
-		this.charBean = charBean;
+	public void setParticle(ParticleBean particle) {
+		this.particle = particle;
 	}
 
-	public DerivedBioAssayDataBean getCharFile() {
-		return charFile;
+	public void setActionName(String actionName) {
+		this.actionName = actionName;
 	}
 
-	public void setCharFile(DerivedBioAssayDataBean charFile) {
-		this.charFile = charFile;
+	public List<CharacterizationSummaryRowBean> getSummaryRows() {
+		return summaryRows;
+	}
+
+	public void setSummaryRows(List<CharacterizationSummaryRowBean> summaryRows) {
+		this.summaryRows = summaryRows;
+		this.charBeans = new ArrayList<CharacterizationBean>();
+		this.columnLabels = new TreeSet<String>();
+		for (CharacterizationSummaryRowBean summaryBean : summaryRows) {
+			Map<String, String> datumMap = summaryBean.getDatumMap();
+			if (datumMap != null && !datumMap.isEmpty()) {
+				columnLabels.addAll(datumMap.keySet());
+			}
+			summaryBean.getCharBean().setParticle(particle);
+			if (!charBeans.contains(summaryBean.getCharBean())) {
+				charBeans.add(summaryBean.getCharBean());
+			}
+		}
+	}
+
+	public SortedSet<String> getColumnLabels() {
+		return columnLabels;
+	}
+
+	public List<CharacterizationBean> getCharBeans() {
+		return charBeans;
+	}
+
+	public String getCharacterizationName() {
+		return characterizationName;
+	}
+
+	public void setCharacterizationName(String characterizationName) {
+		this.characterizationName = characterizationName;
+	}
+
+	public String getExportFileName() {
+		exportFileName = composeExportFileName("summaryView");
+		return exportFileName;
+	}
+
+	public String getFullExportFileName() {
+		fullExportFileName = composeExportFileName("fullSummaryView");
+		return fullExportFileName;
+	}
+
+	private String composeExportFileName(String function) {
+		List<String> nameParts = new ArrayList<String>();
+		nameParts.add(particle.getSampleName());
+		nameParts.add(StringUtils
+				.getOneWordUpperCaseFirstLetter(characterizationName));
+		nameParts.add(function);
+		nameParts.add(StringUtils.convertDateToString(new Date(),
+				"yyyyMMdd_HH-mm-ss-SSS"));
+		exportFileName = StringUtils.join(nameParts, "_");
+		return exportFileName;
+	}
+
+	public String getDispatchActionName() {
+		dispatchActionName = StringUtils
+				.getOneWordLowerCaseFirstLetter(characterizationName);
+		return dispatchActionName;
 	}
 }
