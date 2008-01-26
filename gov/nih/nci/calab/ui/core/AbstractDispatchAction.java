27fd9c47dd9b2a0461d53e1a360a691c50053b43
Sue Pan <sue.pan@nih.gov>
2008-01-26 01:31:25 +0000

    removed cache-control logic and moved into struts-config.xml
    SVN-Revision: 11343

--- gov/nih/nci/calab/ui/core/AbstractDispatchAction.java
+++ gov/nih/nci/calab/ui/core/AbstractDispatchAction.java
@@ -22,15 +22,6 @@ public abstract class AbstractDispatchAction extends DispatchAction {
 			HttpServletRequest request, HttpServletResponse response)
 			throws Exception {
 		HttpSession session = request.getSession();
-		// Forces caches to obtain a new copy of the page from the origin
-		// server
-		response.setHeader("Cache-Control", "no-cache");
-		// Directs caches not to store the page under any circumstance
-		response.setHeader("Cache-Control", "no-store");
-		// Causes the proxy cache to see the page as "stale"
-		response.setDateHeader("Expires", 0);
-		// HTTP 1.0 backward compatibility
-		response.setHeader("Pragma", "no-cache");
 		UserBean user = (UserBean) session.getAttribute("user");
 
 		if (!loginRequired()) {
