da938dae09771a0b4e62ed491ea19ff042d37868
Sue Pan <sue.pan@nih.gov>
2008-04-04 14:37:50 +0000

    first deposit for 1.4
    SVN-Revision: 11433

---
+++ gov/nih/nci/cananolab/system/dao/CustomizedORMDAO.java
@@ -0,0 +1,28 @@
+package gov.nih.nci.cananolab.system.dao;
+
+import gov.nih.nci.system.dao.DAO;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * Customized to contain generic CRUD operations.
+ * 
+ * @author pansu
+ * 
+ */
+public interface CustomizedORMDAO extends DAO {
+
+	public void saveOrUpdate(Object object);
+
+	public Object load(Class domainClass, Serializable id);
+
+	public void delete(Object object);
+
+	public void deleteById(Class domainClass, Serializable id);
+
+	public List getAll(Class domainClass);
+
+	public Object getObject(Class domainClass, String uniqueKeyName,
+			Object uniqueKeyValue);
+}
