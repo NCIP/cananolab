2001d256f9ec81499d4a5f55013539a4755167c9
Sue Pan <sue.pan@nih.gov>
2008-04-04 18:34:48 +0000

    updated to find default function types
    SVN-Revision: 11434

---
+++ src/gov/nih/nci/cananolab/ui/core/CustomPlugIn.java
@@ -0,0 +1,43 @@
+package gov.nih.nci.cananolab.ui.core;
+
+import gov.nih.nci.cananolab.ui.particle.InitNanoparticleSetup;
+import gov.nih.nci.cananolab.ui.security.InitSecuritySetup;
+
+import javax.servlet.ServletException;
+
+import org.apache.log4j.Logger;
+import org.apache.struts.action.ActionServlet;
+import org.apache.struts.action.PlugIn;
+import org.apache.struts.config.ModuleConfig;
+
+/**
+ * Creates default CSM groups and sample types and initialize Hibernate
+ * configurations as soon as server starts up.
+ * 
+ * @author pansu
+ * 
+ */
+public class CustomPlugIn implements PlugIn {
+	Logger logger = Logger.getLogger(CustomPlugIn.class);
+
+	// This method will be called at application startup time
+	public void init(ActionServlet actionServlet, ModuleConfig config)
+			throws ServletException {
+		System.out.println("Entering CustomPlugIn.init()");		
+		try {
+			//set servlet context variables
+			InitNanoparticleSetup.getInstance().setDefaultFunctionTypes(
+					actionServlet.getServletContext());
+			InitSecuritySetup.getInstance().createDefaultCSMGroups();
+		} catch (Exception e) {
+			this.logger.error("Servlet initialization error", e);
+		}
+		System.out.println("Exiting CustomPlugIn.init()");
+	}
+
+	// This method will be called at application shutdown time
+	public void destroy() {
+		System.out.println("Entering CustomPlugIn.destroy()");
+		System.out.println("Exiting CustomPlugIn.destroy()");
+	}
+}
