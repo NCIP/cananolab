cab9cc6bea7e57fb7020187ec940118c8576b91c
Sue Pan <sue.pan@nih.gov>
2008-04-09 19:24:15 +0000

    updated to use NanoparticleSampleService
    SVN-Revision: 4744

--- src/gov/nih/nci/cananolab/dto/particle/NanoparticleDecorator.java
+++ src/gov/nih/nci/cananolab/dto/particle/NanoparticleDecorator.java
@@ -1,20 +1,13 @@
 package gov.nih.nci.cananolab.dto.particle;
 
-import gov.nih.nci.cananolab.domain.particle.characterization.Characterization;
-import gov.nih.nci.cananolab.domain.particle.samplecomposition.Function;
-import gov.nih.nci.cananolab.domain.particle.samplecomposition.base.ComposingElement;
-import gov.nih.nci.cananolab.domain.particle.samplecomposition.base.NanoparticleEntity;
-import gov.nih.nci.cananolab.domain.particle.samplecomposition.functionalization.FunctionalizingEntity;
 import gov.nih.nci.cananolab.dto.common.SortableName;
 import gov.nih.nci.cananolab.exception.CaNanoLabException;
 import gov.nih.nci.cananolab.service.particle.NanoparticleSampleService;
 import gov.nih.nci.cananolab.ui.core.InitSetup;
-import gov.nih.nci.cananolab.util.ClassUtils;
 import gov.nih.nci.cananolab.util.StringUtils;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
-import java.util.Collection;
 import java.util.SortedSet;
 import java.util.TreeSet;
 
@@ -77,24 +70,20 @@ public class NanoparticleDecorator extends TableDecorator {
 	public String getCompositionStr() throws CaNanoLabException {
 		ParticleBean particle = (ParticleBean) getCurrentRowObject();
 		SortedSet<String> compEntityNames = new TreeSet<String>();
-		if (particle.getParticleSample().getSampleComposition() != null) {
-			for (NanoparticleEntity entity : particle.getParticleSample()
-					.getSampleComposition().getNanoparticleEntityCollection()) {
-				String displayName = InitSetup.getInstance().getDisplayName(
-						ClassUtils.getShortClassName(entity.getClass()
-								.getName()),
-						this.getPageContext().getServletContext());
-				compEntityNames.add(displayName);
-			}
-			for (FunctionalizingEntity entity : particle.getParticleSample()
-					.getSampleComposition()
-					.getFunctionalizingEntityCollection()) {
-				String displayName = InitSetup.getInstance().getDisplayName(
-						ClassUtils.getShortClassName(entity.getClass()
-								.getName()),
-						this.getPageContext().getServletContext());
-				compEntityNames.add(displayName);
-			}
+		NanoparticleSampleService service = new NanoparticleSampleService();
+		SortedSet<String> nanoparticleEntityClassNames = service
+				.getStoredNanoparticleEntityClassNames(particle);
+		SortedSet<String> functionalizingEntityClassNames = service
+				.getStoredFunctionalizingEntityClassNames(particle);
+		for (String name: functionalizingEntityClassNames) {
+			String displayName = InitSetup.getInstance().getDisplayName(name,
+					this.getPageContext().getServletContext());
+			compEntityNames.add(displayName);
+		}
+		for (String name: nanoparticleEntityClassNames) {
+			String displayName = InitSetup.getInstance().getDisplayName(name,
+					this.getPageContext().getServletContext());
+			compEntityNames.add(displayName);
 		}
 		return StringUtils.join(compEntityNames, "<br>");
 	}
@@ -115,13 +104,13 @@ public class NanoparticleDecorator extends TableDecorator {
 
 	public String getCharacterizationStr() throws CaNanoLabException {
 		ParticleBean particle = (ParticleBean) getCurrentRowObject();
-		Collection<Characterization> characterizations = particle
-				.getParticleSample().getCharacterizationCollection();
+		NanoparticleSampleService service = new NanoparticleSampleService();
+		SortedSet<String> charClassNames = service
+				.getStoredCharacterizationClassNames(particle);
 
 		SortedSet<String> charNames = new TreeSet<String>();
-		for (Characterization chara : characterizations) {
-			String displayName = InitSetup.getInstance().getDisplayName(
-					ClassUtils.getShortClassName(chara.getClass().getName()),
+		for (String name : charClassNames) {
+			String displayName = InitSetup.getInstance().getDisplayName(name,
 					this.getPageContext().getServletContext());
 			charNames.add(displayName);
 		}
