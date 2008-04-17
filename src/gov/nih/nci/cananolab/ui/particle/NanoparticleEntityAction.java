43a4f6466970d1d9131b4609fbbbe599fb14aefe
Sue Pan <sue.pan@nih.gov>
2008-04-17 19:47:52 +0000

    updated create
    SVN-Revision: 11614

--- src/gov/nih/nci/cananolab/ui/particle/NanoparticleEntityAction.java
+++ src/gov/nih/nci/cananolab/ui/particle/NanoparticleEntityAction.java
@@ -8,13 +8,11 @@ package gov.nih.nci.cananolab.ui.particle;
  * @author pansu
  */
 
-/* CVS $Id: NanoparticleEntityAction.java,v 1.4 2008-04-17 17:16:53 cais Exp $ */
+/* CVS $Id: NanoparticleEntityAction.java,v 1.5 2008-04-17 19:47:52 pansu Exp $ */
 
-import gov.nih.nci.cananolab.domain.particle.samplecomposition.base.Dendrimer;
 import gov.nih.nci.cananolab.dto.common.UserBean;
 import gov.nih.nci.cananolab.dto.particle.ParticleBean;
 import gov.nih.nci.cananolab.dto.particle.composition.ComposingElementBean;
-import gov.nih.nci.cananolab.dto.particle.composition.FunctionBean;
 import gov.nih.nci.cananolab.dto.particle.composition.NanoparticleEntityBean;
 import gov.nih.nci.cananolab.exception.CaNanoLabSecurityException;
 import gov.nih.nci.cananolab.service.particle.NanoparticleCompositionService;
@@ -24,9 +22,7 @@ import gov.nih.nci.cananolab.ui.core.InitSetup;
 import gov.nih.nci.cananolab.ui.security.InitSecuritySetup;
 import gov.nih.nci.cananolab.util.CaNanoLabConstants;
 
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -52,46 +48,43 @@ public class NanoparticleEntityAction extends AbstractDispatchAction {
 	public ActionForward create(ActionMapping mapping, ActionForm form,
 			HttpServletRequest request, HttpServletResponse response)
 			throws Exception {
-		ActionForward forward = null;
-
-		HttpSession session = request.getSession();
 		DynaValidatorForm theForm = (DynaValidatorForm) form;
-		UserBean user = new UserBean("Test");
+		UserBean user = (UserBean) request.getSession().getAttribute("user");
 		NanoparticleCompositionService compositionService = new NanoparticleCompositionService();
-		// REMOVE test code
+		ParticleBean particleBean = initSetup(theForm, request);
 		NanoparticleEntityBean entityBean = (NanoparticleEntityBean) theForm
 				.get("entity");
-		entityBean.setType("dendrimer");
-		entityBean.setClassName(InitSetup.getInstance().getObjectName(
-				"dendrimer", session.getServletContext()));
-		entityBean.setDescription("This is a test");
-		Dendrimer dendrimer = new Dendrimer();
-		dendrimer.setBranch("1-4");
-		dendrimer.setGeneration((float) 1.5);
-		dendrimer.setCreatedBy(user.getLoginName());
-		dendrimer.setCreatedDate(new Date());
-		entityBean.setDendrimer(dendrimer);
-		ComposingElementBean composingElementBean = new ComposingElementBean();
-		composingElementBean.getDomainComposingElement().setType("core");
-		composingElementBean.getDomainComposingElement().setCreatedBy(
-				user.getLoginName());
-		composingElementBean.getDomainComposingElement().setCreatedDate(
-				new Date());
-		FunctionBean functionBean = new FunctionBean();
-		functionBean.setType("imaging");
-		functionBean.setDescription("test");
-		List<FunctionBean> inherentFunctions = new ArrayList<FunctionBean>();
-		inherentFunctions.add(functionBean);
-		composingElementBean.setInherentFunctions(inherentFunctions);
-		List<ComposingElementBean> composingElements = new ArrayList<ComposingElementBean>();
-		composingElements.add(composingElementBean);
-		entityBean.setComposingElements(composingElements);
-		// END OF REMOVE
-
-		ParticleBean particleBean = (ParticleBean) session
-				.getAttribute("particleSampleBean");
+		
+		Date now = new Date();
+		entityBean.getDomainEntity().setCreatedBy(user.getLoginName());
+		entityBean.getDomainEntity().setCreatedDate(now);
+		for (ComposingElementBean compElementBean : entityBean
+				.getComposingElements()) {
+			compElementBean.getDomainComposingElement().setCreatedBy(
+					user.getLoginName());
+			compElementBean.getDomainComposingElement().setCreatedDate(now);
+		}
+		
 		compositionService.saveNanoparticleEntity(particleBean, entityBean);
+		ActionForward forward = mapping.findForward("success");
 		return forward;
+	}
+
+	public ParticleBean initSetup(DynaValidatorForm theForm,
+			HttpServletRequest request) throws Exception {
+		String particleId = request.getParameter("particleId");
+		if (particleId == null) {
+			particleId = theForm.getString("particleId");
+		}
+		HttpSession session = request.getSession();
+		UserBean user = (UserBean) session.getAttribute("user");
+
+		NanoparticleSampleService service = new NanoparticleSampleService();
+		ParticleBean particleBean = service.findNanoparticleSampleBy(
+				particleId, user);
+		request.setAttribute("theParticle", particleBean);
+		theForm.set("particleId", particleId);
+		return particleBean;
 	}
 
 	/**
@@ -107,17 +100,9 @@ public class NanoparticleEntityAction extends AbstractDispatchAction {
 	public ActionForward setup(ActionMapping mapping, ActionForm form,
 			HttpServletRequest request, HttpServletResponse response)
 			throws Exception {
-		String particleId = request.getParameter("particleId");
-		HttpSession session = request.getSession();
-		UserBean user = (UserBean) session.getAttribute("user");
-		NanoparticleSampleService service = new NanoparticleSampleService();
-		ParticleBean particleBean = service.findNanoparticleSampleBy(
-				particleId, user);
 		DynaValidatorForm theForm = (DynaValidatorForm) form;
-		theForm.set("particleSampleBean", particleBean);
-		request.setAttribute("newParticleCreated", "true");
-		session.setAttribute("particleDataTree", InitNanoparticleSetup
-				.getInstance().getDataTree(particleBean, request));
+		ParticleBean particleBean = initSetup(theForm, request);
+		InitNanoparticleSetup.getInstance().getDataTree(particleBean, request);
 		InitNanoparticleSetup.getInstance().setNanoparticleEntityTypes(request);
 		InitNanoparticleSetup.getInstance().getEmulsionComposingElementTypes(
 				request);
@@ -130,15 +115,9 @@ public class NanoparticleEntityAction extends AbstractDispatchAction {
 			HttpServletRequest request, HttpServletResponse response)
 			throws Exception {
 		DynaValidatorForm theForm = (DynaValidatorForm) form;
-		String particleId = request.getParameter("particleId");
+		ParticleBean particleBean = initSetup(theForm, request);
 		HttpSession session = request.getSession();
 		UserBean user = (UserBean) session.getAttribute("user");
-		NanoparticleSampleService sampleService = new NanoparticleSampleService();
-		ParticleBean particleBean = sampleService.findNanoparticleSampleBy(
-				particleId, user);
-
-		theForm.set("particleSampleBean", particleBean);
-		request.setAttribute("theParticle", particleBean);
 		String entityId = request.getParameter("entityId");
 		NanoparticleCompositionService compService = new NanoparticleCompositionService();
 		NanoparticleEntityBean entityBean = compService
@@ -147,9 +126,7 @@ public class NanoparticleEntityAction extends AbstractDispatchAction {
 				entityBean.getClassName(), session.getServletContext());
 		entityBean.setType(entityType);
 		theForm.set("entity", entityBean);
-		request.setAttribute("newParticleCreated", "true");
-		session.setAttribute("particleDataTree", InitNanoparticleSetup
-				.getInstance().getDataTree(particleBean, request));
+		InitNanoparticleSetup.getInstance().getDataTree(particleBean, request);
 		InitNanoparticleSetup.getInstance().setNanoparticleEntityTypes(request);
 		InitNanoparticleSetup.getInstance().getEmulsionComposingElementTypes(
 				request);
