624dc4a5a5d888c4a0862b51ddc379858084d68b
Shuang Cai <cais>
2008-04-17 19:44:30 +0000

    added method to update composing element type dropdown menu.
    SVN-Revision: 11613

--- web/javascript/CompositionEntityManager.js
+++ web/javascript/CompositionEntityManager.js
@@ -7,3 +7,15 @@ function populatePage(pageData) {
 	dwr.util.setValue("entityInclude", pageData, {escapeHtml:false});
 }
 
+function getComposingElementOptions(particleEntityTypeId,
+									compEleTypeId) {
+	var compFuncTypeValue = dwr.util.getValue(particleEntityTypeId);
+	
+	CompositionEntityManager.getComposingElementTypeOptions(compFuncTypeValue, function(data) {
+			
+			dwr.util.removeAllOptions(compEleTypeId);
+			dwr.util.addOptions(compEleTypeId, ['']);
+    		dwr.util.addOptions(compEleTypeId, data);
+    		dwr.util.addOptions(compEleTypeId, ['[Other]']);
+  	});
+}
