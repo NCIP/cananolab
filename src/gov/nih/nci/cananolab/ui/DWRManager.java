df12e686f6e0b0d1dda8db001b0ff5f4870eadf5
Shuang Cai <cais>
2008-04-17 17:18:38 +0000

    added method to update composing element type drop down menu.
    SVN-Revision: 11609

--- src/gov/nih/nci/cananolab/ui/DWRManager.java
+++ src/gov/nih/nci/cananolab/ui/DWRManager.java
@@ -2,14 +2,18 @@ package gov.nih.nci.cananolab.ui;
 
 import gov.nih.nci.cananolab.exception.CaNanoLabException;
 import gov.nih.nci.cananolab.ui.core.InitSetup;
+import gov.nih.nci.cananolab.ui.particle.InitNanoparticleSetup;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
 
 import org.directwebremoting.WebContext;
 import org.directwebremoting.WebContextFactory;
+import org.directwebremoting.impl.DefaultWebContextBuilder;
 
 public class DWRManager {
 	public DWRManager() {
@@ -29,4 +33,32 @@ public class DWRManager {
 			return "";
 		}
 	}
+	
+	public String [] getComposingElementTypeOptions(String nanoparticleEntityType) {
+		
+		DefaultWebContextBuilder dwcb = new DefaultWebContextBuilder();
+		org.directwebremoting.WebContext webContext = dwcb.get();
+		HttpServletRequest request = webContext.getHttpServletRequest();
+	     
+        if(nanoparticleEntityType.equals("emulsion")) {
+        	try {
+        		List<String> emulsionCompList = InitNanoparticleSetup.getInstance().getEmulsionComposingElementTypes(request);
+        		String [] eleArray = new String [ emulsionCompList.size()];
+        		return emulsionCompList.toArray(eleArray);
+        	} catch (Exception e) {
+        		System.out.println("getEmulsionComposingElementTypes exception.");
+        		e.printStackTrace();
+        	}
+        } else {
+        	try {
+        		List<String> compList = InitNanoparticleSetup.getInstance().getComposingElementTypes(request);
+        		String [] eleArray = new String [ compList.size()];
+        		return compList.toArray(eleArray);
+        	} catch (Exception e) {
+        		System.out.println("getComposingElementTypes exception.");
+        		e.printStackTrace();
+        	}
+        }
+        return null;
+    }
 }
