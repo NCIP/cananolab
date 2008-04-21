20007901afe69ebf8a844999d7fa3d81b272ff2a
Sue Pan <sue.pan@nih.gov>
2008-04-21 23:10:51 +0000

    added createdDate
    SVN-Revision: 11680

--- src/gov/nih/nci/cananolab/dto/particle/ParticleDataLinkBean.java
+++ src/gov/nih/nci/cananolab/dto/particle/ParticleDataLinkBean.java
@@ -1,24 +1,29 @@
 package gov.nih.nci.cananolab.dto.particle;
 
+import java.util.Date;
+
 /**
  * This class represents a data link under the particle tree
+ * 
  * @author pansu
- *
+ * 
  */
 public class ParticleDataLinkBean {
-	//the data type displayed in the tree, e.g. Carbon Nanotube
+	// the data type displayed in the tree, e.g. Carbon Nanotube
 	private String dataDisplayType;
 
-	//the short class name representing the data, e.g. CarbonNanotube
+	// the short class name representing the data, e.g. CarbonNanotube
 	private String dataClassName;
 
-	//the id for the persisted data
+	// the id for the persisted data
 	private String dataId;
 
 	private String dataLink;
 
-	//Composition, Physical Characterization, In Vitro Characterization
+	// Composition, Physical Characterization, In Vitro Characterization
 	private String dataCategory;
+
+	private Date createdDate;
 
 	public ParticleDataLinkBean(String dataDisplayType, String dataClassName,
 			String dataLink, String dataCategory) {
@@ -29,9 +34,15 @@ public class ParticleDataLinkBean {
 	}
 
 	public ParticleDataLinkBean(String dataId, String category, String dataLink) {
-		this.dataId=dataId;
+		this.dataId = dataId;
 		this.dataCategory = category;
 		this.dataLink = dataLink;
+	}
+
+	public ParticleDataLinkBean(String dataId, String category,
+			String dataLink, Date createdDate) {
+		this(dataId, category, dataLink);
+		this.createdDate = createdDate;
 	}
 
 	public String getDataLink() {
@@ -74,4 +85,34 @@ public class ParticleDataLinkBean {
 		this.dataCategory = dataCategory;
 	}
 
+	public Date getCreatedDate() {
+		return createdDate;
+	}
+
+	public void setCreatedDate(Date createdDate) {
+		this.createdDate = createdDate;
+	}
+
+	public boolean equals(Object obj) {
+		if (this.compareTo(obj) == 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public int hashCode() {
+		if (getDataId() != null)
+			return getDataId().hashCode();
+		return 0;
+	}
+
+	public int compareTo(Object obj) {
+		ParticleDataLinkBean link2 = (ParticleDataLinkBean) obj;
+		if (getDataDisplayType().equals(link2.getDataDisplayType())) {
+			return getCreatedDate().compareTo(link2.getCreatedDate());
+		} else {
+			return getDataDisplayType().compareTo(link2.getDataDisplayType());
+		}
+	}
 }
