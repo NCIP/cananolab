d2ab18018023bdb6a8f700f5080c23f77cc03566
Sue Pan <sue.pan@nih.gov>
2008-04-22 15:24:57 +0000

    added to contain shared operations
    SVN-Revision: 4960

---
+++ src/gov/nih/nci/cananolab/ui/particle/BaseAnnotationAction.java
@@ -0,0 +1,43 @@
+package gov.nih.nci.cananolab.ui.particle;
+
+import gov.nih.nci.cananolab.ui.core.AbstractDispatchAction;
+import gov.nih.nci.cananolab.ui.security.InitSecuritySetup;
+import gov.nih.nci.cananolab.util.CaNanoLabConstants;
+import gov.nih.nci.cananolab.dto.common.UserBean;
+import gov.nih.nci.cananolab.dto.particle.ParticleBean;
+import gov.nih.nci.cananolab.exception.CaNanoLabSecurityException;
+import gov.nih.nci.cananolab.service.particle.NanoparticleSampleService;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.apache.struts.validator.DynaValidatorForm;
+
+public class BaseAnnotationAction extends AbstractDispatchAction {
+	public ParticleBean initSetup(DynaValidatorForm theForm,
+			HttpServletRequest request) throws Exception {
+		String particleId = request.getParameter("particleId");
+		if (particleId == null) {
+			particleId = theForm.getString("particleId");
+		}
+		HttpSession session = request.getSession();
+		UserBean user = (UserBean) session.getAttribute("user");
+
+		NanoparticleSampleService service = new NanoparticleSampleService();
+		ParticleBean particleBean = service.findNanoparticleSampleById(
+				particleId, user);
+		request.setAttribute("theParticle", particleBean);
+		theForm.set("particleId", particleId);
+		return particleBean;
+	}
+
+	public boolean loginRequired() {
+		return false;
+	}
+
+	public boolean canUserExecute(UserBean user)
+			throws CaNanoLabSecurityException {
+		return InitSecuritySetup.getInstance().userHasCreatePrivilege(user,
+				CaNanoLabConstants.CSM_PG_PARTICLE);
+	}
+}
