7aa3559f0ef22787eeeb0919a8e2ff42c822efd5
Sue Pan <sue.pan@nih.gov>
2008-04-23 21:31:01 +0000

    added sample source to visible group
    SVN-Revision: 11746

--- gov/nih/nci/cananolab/ui/particle/SubmitNanoparticleAction.java
+++ gov/nih/nci/cananolab/ui/particle/SubmitNanoparticleAction.java
@@ -6,9 +6,8 @@ package gov.nih.nci.cananolab.ui.particle;
  * @author pansu
  */
 
-/* CVS $Id: SubmitNanoparticleAction.java,v 1.14 2008-04-23 19:47:06 pansu Exp $ */
+/* CVS $Id: SubmitNanoparticleAction.java,v 1.15 2008-04-23 21:31:01 pansu Exp $ */
 
-import gov.nih.nci.cananolab.domain.particle.NanoparticleSample;
 import gov.nih.nci.cananolab.dto.common.UserBean;
 import gov.nih.nci.cananolab.dto.particle.ParticleBean;
 import gov.nih.nci.cananolab.exception.CaNanoLabSecurityException;
@@ -18,7 +17,7 @@ import gov.nih.nci.cananolab.ui.core.AbstractDispatchAction;
 import gov.nih.nci.cananolab.ui.security.InitSecuritySetup;
 import gov.nih.nci.cananolab.util.CaNanoLabConstants;
 
-import java.util.Date;
+import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -45,40 +44,62 @@ public class SubmitNanoparticleAction extends AbstractDispatchAction {
 				.getDomainParticleSample());
 
 		// set CSM visibility
+		// add sample source as a new CSM_GROUP and assign the sample to
+		// the new group
 		AuthorizationService authService = new AuthorizationService(
 				CaNanoLabConstants.CSM_APP_NAME);
+		String[] visibleGroups = new String[particleSampleBean
+				.getVisibilityGroups().length + 1];
+		for (int i = 0; i < visibleGroups.length - 1; i++) {
+			visibleGroups[i] = particleSampleBean.getVisibilityGroups()[i];
+		}
+		visibleGroups[visibleGroups.length - 1] = particleSampleBean
+				.getDomainParticleSample().getSource().getOrganizationName();
+		particleSampleBean.setVisibilityGroups(visibleGroups);
+
 		authService.setVisibility(particleSampleBean.getDomainParticleSample()
-				.getName(), particleSampleBean.getVisibilityGroups());
+				.getName(), visibleGroups);
+
 		theForm.set("particleSampleBean", particleSampleBean);
 		forward = mapping.findForward("success");
 		request.setAttribute("theParticle", particleSampleBean);
 		request.setAttribute("updateDataTree", "true");
-		InitNanoparticleSetup.getInstance().setAllNanoparticleSampleSources(
-				request);
 		InitNanoparticleSetup.getInstance().getDataTree(particleSampleBean,
 				request);
+		InitNanoparticleSetup.getInstance().setAllNanoparticleSampleSources(
+				request);
+		InitSecuritySetup.getInstance().setAllVisibilityGroups(request);
 		return forward;
 	}
 
 	public ActionForward setupUpdate(ActionMapping mapping, ActionForm form,
 			HttpServletRequest request, HttpServletResponse response)
 			throws Exception {
-		HttpSession session = request.getSession();
-		InitNanoparticleSetup.getInstance().setAllNanoparticleSampleSources(
-				request);
-		InitSecuritySetup.getInstance().setAllVisibilityGroups(session);
 		DynaValidatorForm theForm = (DynaValidatorForm) form;
 		String particleId = request.getParameter("particleId");
+		HttpSession session = request.getSession();
 		UserBean user = (UserBean) session.getAttribute("user");
 		NanoparticleSampleService service = new NanoparticleSampleService();
 		ParticleBean particleSampleBean = service.findNanoparticleSampleById(
 				particleId, user);
+		
+		// get assigned visible groups
+		AuthorizationService auth = new AuthorizationService(
+				CaNanoLabConstants.CSM_APP_NAME);
+		List<String> accessibleGroups = auth.getAccessibleGroups(
+				particleSampleBean.getDomainParticleSample().getName(),
+				CaNanoLabConstants.CSM_READ_ROLE);
+		String[] visibilityGroups = accessibleGroups.toArray(new String[0]);
+		particleSampleBean.setVisibilityGroups(visibilityGroups);
+
 		theForm.set("particleSampleBean", particleSampleBean);
 		request.setAttribute("theParticle", particleSampleBean);
 		request.setAttribute("updateDataTree", "true");
 		InitNanoparticleSetup.getInstance().getDataTree(particleSampleBean,
 				request);
-
+		InitNanoparticleSetup.getInstance().setAllNanoparticleSampleSources(
+				request);
+		InitSecuritySetup.getInstance().setAllVisibilityGroups(request);
 		return mapping.findForward("update");
 	}
 
@@ -91,11 +112,9 @@ public class SubmitNanoparticleAction extends AbstractDispatchAction {
 	public ActionForward setup(ActionMapping mapping, ActionForm form,
 			HttpServletRequest request, HttpServletResponse response)
 			throws Exception {
-		HttpSession session = request.getSession();
 		InitNanoparticleSetup.getInstance().setAllNanoparticleSampleSources(
 				request);
-		InitSecuritySetup.getInstance().setAllVisibilityGroups(session);
-
+		InitSecuritySetup.getInstance().setAllVisibilityGroups(request);
 		return mapping.getInputForward();
 	}
 
