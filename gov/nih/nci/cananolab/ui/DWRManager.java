292dcf5f7b8067b1ae98ca99d2f2b3ddb0425f1c
Sue Pan <sue.pan@nih.gov>
2008-04-24 22:31:41 +0000

    updated getEntityIncludePage
    SVN-Revision: 11786

--- gov/nih/nci/cananolab/ui/DWRManager.java
+++ gov/nih/nci/cananolab/ui/DWRManager.java
@@ -27,43 +27,47 @@ public class DWRManager {
 			ServletContext appContext = wctx.getServletContext();
 			String entityClassName = InitSetup.getInstance().getObjectName(
 					entityType, appContext);
-			String page = "/particle/composition/body" + entityClassName
+			String page = "/particle/composition/bodyNew" + entityClassName
 					+ "Info.jsp";
 			return wctx.forwardToString(page);
 		} catch (Exception e) {
 			return "";
 		}
 	}
-	
-	public String [] getComposingElementTypeOptions(String nanoparticleEntityType) {
-		
+
+	public String[] getComposingElementTypeOptions(String nanoparticleEntityType) {
+
 		DefaultWebContextBuilder dwcb = new DefaultWebContextBuilder();
 		org.directwebremoting.WebContext webContext = dwcb.get();
 		HttpServletRequest request = webContext.getHttpServletRequest();
-	     
-        if(nanoparticleEntityType.equals("emulsion")) {
-        	try {
-        		List<String> emulsionCompList = InitCompositionSetup.getInstance().getEmulsionComposingElementTypes(request);
-        		String [] eleArray = new String [ emulsionCompList.size()];
-        		return emulsionCompList.toArray(eleArray);
-        	} catch (Exception e) {
-        		System.out.println("getEmulsionComposingElementTypes exception.");
-        		e.printStackTrace();
-        	}
-        } else {
-        	try {
-        		List<String> compList = InitCompositionSetup.getInstance().getComposingElementTypes(request);
-        		String [] eleArray = new String [ compList.size()];
-        		return compList.toArray(eleArray);
-        	} catch (Exception e) {
-        		System.out.println("getComposingElementTypes exception.");
-        		e.printStackTrace();
-        	}
-        }
-        return null;
-    }
-	
-	public String [] getBiopolymerTypeOptions(String nanoparticleEntityType) {
+
+		if (nanoparticleEntityType.equals("emulsion")) {
+			try {
+				List<String> emulsionCompList = InitCompositionSetup
+						.getInstance()
+						.getEmulsionComposingElementTypes(request);
+				String[] eleArray = new String[emulsionCompList.size()];
+				return emulsionCompList.toArray(eleArray);
+			} catch (Exception e) {
+				System.out
+						.println("getEmulsionComposingElementTypes exception.");
+				e.printStackTrace();
+			}
+		} else {
+			try {
+				List<String> compList = InitCompositionSetup.getInstance()
+						.getComposingElementTypes(request);
+				String[] eleArray = new String[compList.size()];
+				return compList.toArray(eleArray);
+			} catch (Exception e) {
+				System.out.println("getComposingElementTypes exception.");
+				e.printStackTrace();
+			}
+		}
+		return null;
+	}
+
+	public String[] getBiopolymerTypeOptions(String nanoparticleEntityType) {
 		if (nanoparticleEntityType.equals("biopolymer")) {
 			DefaultWebContextBuilder dwcb = new DefaultWebContextBuilder();
 			org.directwebremoting.WebContext webContext = dwcb.get();
@@ -80,7 +84,7 @@ public class DWRManager {
 				e.printStackTrace();
 			}
 		}
-		return new String[] {""};
+		return new String[] { "" };
 	}
 
 	public String [] getAntibodyTypeOptions(String nanoparticleEntityType) {
