4d6e54f2056dfdc2ed7f315f3e5e1d4f62509bdd
Sue Pan <sue.pan@nih.gov>
2008-04-25 19:35:22 +0000

    added detailView and moved setupUpdate to BaseCharacterizationAction
    SVN-Revision: 11828

--- gov/nih/nci/cananolab/ui/particle/BaseCharacterizationAction.java
+++ gov/nih/nci/cananolab/ui/particle/BaseCharacterizationAction.java
@@ -1,24 +1,83 @@
 package gov.nih.nci.cananolab.ui.particle;
 
+import gov.nih.nci.cananolab.domain.particle.characterization.Characterization;
 import gov.nih.nci.cananolab.dto.common.UserBean;
 import gov.nih.nci.cananolab.dto.particle.ParticleBean;
 import gov.nih.nci.cananolab.dto.particle.characterization.CharacterizationBean;
 import gov.nih.nci.cananolab.dto.particle.characterization.DerivedBioAssayDataBean;
-import gov.nih.nci.cananolab.exception.CaNanoLabSecurityException;
-import gov.nih.nci.cananolab.service.particle.NanoparticleSampleService;
-import gov.nih.nci.cananolab.ui.security.InitSecuritySetup;
-import gov.nih.nci.cananolab.util.CaNanoLabConstants;
+import gov.nih.nci.cananolab.service.particle.NanoparticleCharacterizationService;
+import gov.nih.nci.cananolab.ui.core.InitSetup;
+import gov.nih.nci.cananolab.util.ClassUtils;
 
+import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 import org.apache.struts.validator.DynaValidatorForm;
 
-public class BaseCharacterizationAction extends BaseAnnotationAction {
+public abstract class BaseCharacterizationAction extends BaseAnnotationAction {
+	/**
+	 * Set up the input form for adding new characterization
+	 * 
+	 * @param mapping
+	 * @param form
+	 * @param request
+	 * @param response
+	 * @return
+	 * @throws Exception
+	 */
+	public ActionForward setup(ActionMapping mapping, ActionForm form,
+			HttpServletRequest request, HttpServletResponse response)
+			throws Exception {
+		DynaValidatorForm theForm = (DynaValidatorForm) form;
+		request.getSession().removeAttribute("characterizationForm");
+		ParticleBean particleBean = initSetup(theForm, request);
+		ServletContext appContext = request.getSession().getServletContext();
+		String submitType = request.getParameter("submitType");
+		String charClass = InitSetup.getInstance().getObjectName(submitType,
+				appContext);
+		setLookups(request, charClass);
+		return mapping.getInputForward();
+	}
+
+	protected abstract void setLookups(HttpServletRequest request,
+			String charClass) throws Exception;
+
+	protected abstract void setCharacterizationBean(DynaValidatorForm theForm,
+			Characterization chara) throws Exception;
+
+	public ActionForward setupUpdate(ActionMapping mapping, ActionForm form,
+			HttpServletRequest request, HttpServletResponse response)
+			throws Exception {
+		DynaValidatorForm theForm = (DynaValidatorForm) form;
+		Characterization chara = prepareCharacterization(theForm, request);
+		setCharacterizationBean(theForm, chara);
+		String className = ClassUtils.getShortClassName(chara.getClass()
+				.getCanonicalName());
+		setLookups(request, className);
+		return mapping.getInputForward();
+	}
+
+	public ActionForward setupView(ActionMapping mapping, ActionForm form,
+			HttpServletRequest request, HttpServletResponse response)
+			throws Exception {
+		return setupUpdate(mapping, form, request, response);
+	}
+
+	protected Characterization prepareCharacterization(
+			DynaValidatorForm theForm, HttpServletRequest request)
+			throws Exception {
+		ParticleBean particleBean = initSetup(theForm, request);
+		UserBean user = (UserBean) request.getSession().getAttribute("user");
+		String charId = request.getParameter("dataId");
+		NanoparticleCharacterizationService charService = new NanoparticleCharacterizationService();
+		Characterization chara = charService.findCharacterizationBy(charId,
+				user);
+		return chara;
+	}
 
 	public ActionForward addDerivedBioAssayData(ActionMapping mapping,
 			ActionForm form, HttpServletRequest request,
@@ -52,9 +111,9 @@ public class BaseCharacterizationAction extends BaseAnnotationAction {
 		return mapping.getInputForward();
 	}
 
-	public ActionForward addDerivedDatum(ActionMapping mapping, ActionForm form,
-			HttpServletRequest request, HttpServletResponse response)
-			throws Exception {
+	public ActionForward addDerivedDatum(ActionMapping mapping,
+			ActionForm form, HttpServletRequest request,
+			HttpServletResponse response) throws Exception {
 		// if user pressed cancel in load characterization file
 		String cancel = request.getParameter("cancel");
 		if (cancel != null) {
@@ -65,7 +124,8 @@ public class BaseCharacterizationAction extends BaseAnnotationAction {
 				.get("achar");
 		String fileIndexStr = (String) request.getParameter("compInd");
 		int fileInd = Integer.parseInt(fileIndexStr);
-		DerivedBioAssayDataBean derivedBioAssayData=achar.getDerivedBioAssayDataList().get(fileInd);
+		DerivedBioAssayDataBean derivedBioAssayData = achar
+				.getDerivedBioAssayDataList().get(fileInd);
 		derivedBioAssayData.addDerivedDatum();
 		return mapping.getInputForward();
 	}
@@ -85,10 +145,29 @@ public class BaseCharacterizationAction extends BaseAnnotationAction {
 		int fileInd = Integer.parseInt(fileIndexStr);
 		String dataIndexStr = (String) request.getParameter("childCompInd");
 		int dataInd = Integer.parseInt(dataIndexStr);
-		DerivedBioAssayDataBean derivedBioAssayData=achar.getDerivedBioAssayDataList().get(fileInd);
+		DerivedBioAssayDataBean derivedBioAssayData = achar
+				.getDerivedBioAssayDataList().get(fileInd);
 		derivedBioAssayData.removeDerivedDatum(dataInd);
 		return mapping.getInputForward();
 		// return mapping.getInputForward(); this gives an
 		// IndexOutOfBoundException in the jsp page
+	}
+
+	public ActionForward detailView(ActionMapping mapping, ActionForm form,
+			HttpServletRequest request, HttpServletResponse response)
+			throws Exception {
+		DynaValidatorForm theForm = (DynaValidatorForm) form;
+		Characterization chara = prepareCharacterization(theForm, request);
+		setCharacterizationBean(theForm, chara);
+		String particleId = request.getParameter("particleId");
+		String characterizationId = request.getParameter("dataId");
+		String submitType = request.getParameter("submitType");
+		String requestUrl = request.getRequestURL().toString();
+		String printLinkURL = requestUrl
+				+ "?page=0&dispatch=printDetailView&particleId=" + particleId
+				+ "&dataId=" + characterizationId + "&submitType=" + submitType;
+		request.getSession().setAttribute("printDetailViewLinkURL",
+				printLinkURL);
+		return mapping.findForward("detailView");
 	}
 }
