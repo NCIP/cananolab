01f9904ca2859783a27de9d788e419524645f0f0
Sue Pan <sue.pan@nih.gov>
2008-04-29 21:17:33 +0000

    added deleteAll and setupDeleteAll
    SVN-Revision: 11946

--- src/gov/nih/nci/cananolab/ui/particle/BaseAnnotationAction.java
+++ src/gov/nih/nci/cananolab/ui/particle/BaseAnnotationAction.java
@@ -2,15 +2,27 @@ package gov.nih.nci.cananolab.ui.particle;
 
 import gov.nih.nci.cananolab.dto.common.UserBean;
 import gov.nih.nci.cananolab.dto.particle.ParticleBean;
+import gov.nih.nci.cananolab.dto.particle.ParticleDataLinkBean;
 import gov.nih.nci.cananolab.exception.CaNanoLabSecurityException;
 import gov.nih.nci.cananolab.service.particle.NanoparticleSampleService;
 import gov.nih.nci.cananolab.ui.core.AbstractDispatchAction;
+import gov.nih.nci.cananolab.ui.core.InitSetup;
 import gov.nih.nci.cananolab.ui.security.InitSecuritySetup;
 import gov.nih.nci.cananolab.util.CaNanoLabConstants;
+import gov.nih.nci.cananolab.util.ClassUtils;
+
+import java.util.Map;
+import java.util.SortedSet;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+import org.apache.struts.action.ActionMessage;
+import org.apache.struts.action.ActionMessages;
 import org.apache.struts.validator.DynaValidatorForm;
 
 public abstract class BaseAnnotationAction extends AbstractDispatchAction {
@@ -46,13 +58,55 @@ public abstract class BaseAnnotationAction extends AbstractDispatchAction {
 				CaNanoLabConstants.CSM_PG_PARTICLE);
 	}
 
-	public void setupDataTree(DynaValidatorForm theForm,
-			HttpServletRequest request) throws Exception {
+	public Map<String, SortedSet<ParticleDataLinkBean>> setupDataTree(
+			DynaValidatorForm theForm, HttpServletRequest request)
+			throws Exception {
 		request.setAttribute("updateDataTree", "true");
 		String particleId = request.getParameter("particleId");
 		if (particleId == null) {
 			particleId = theForm.getString("particleId");
 		}
-		InitNanoparticleSetup.getInstance().getDataTree(particleId, request);
+		return InitNanoparticleSetup.getInstance().getDataTree(particleId,
+				request);
+	}
+
+	public ActionForward setupDeleteAll(ActionMapping mapping, ActionForm form,
+			HttpServletRequest request, HttpServletResponse response)
+			throws Exception {
+		String submitType = request.getParameter("submitType");
+		DynaValidatorForm theForm = (DynaValidatorForm) form;
+		Map<String, SortedSet<ParticleDataLinkBean>> dataTree = setupDataTree(
+				theForm, request);
+		SortedSet<ParticleDataLinkBean> dataToDelete = dataTree.get(submitType);
+		request.getSession().setAttribute("actionName",
+				dataToDelete.first().getDataLink());
+		request.getSession().setAttribute("dataToDelete", dataToDelete);
+		return mapping.findForward("annotationDeleteView");
+	}
+
+	public ActionForward deleteAll(ActionMapping mapping, ActionForm form,
+			HttpServletRequest request, HttpServletResponse response)
+			throws Exception {
+		DynaValidatorForm theForm = (DynaValidatorForm) form;
+		String submitType = request.getParameter("submitType");
+		String className = InitSetup.getInstance().getObjectName(submitType,
+				request.getSession().getServletContext());
+		String fullClassName = ClassUtils.getFullClass(className)
+				.getCanonicalName();
+
+		String[] dataIds = (String[]) theForm.get("idsToDelete");
+		NanoparticleSampleService sampleService = new NanoparticleSampleService();
+		for (String id : dataIds) {
+			sampleService.deleteAnnotationById(fullClassName, new Long(id));
+		}
+
+		setupDataTree(theForm, request);
+
+		ActionMessages msgs = new ActionMessages();
+		ActionMessage msg = new ActionMessage("message.deleteAnnotations",
+				submitType);
+		msgs.add(ActionMessages.GLOBAL_MESSAGE, msg);
+		saveMessages(request, msgs);
+		return mapping.findForward("success");
 	}
 }
