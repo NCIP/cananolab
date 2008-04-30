315b0dea8ccbc425955016c123eb285635537ccb
Sue Pan <sue.pan@nih.gov>
2008-04-30 22:11:37 +0000

    refactored for 1.4
    SVN-Revision: 11976

---
+++ gov/nih/nci/cananolab/dto/common/ReportDecorator.java
@@ -0,0 +1,47 @@
+package gov.nih.nci.cananolab.dto.common;
+
+import gov.nih.nci.cananolab.util.StringUtils;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Arrays;
+
+import org.displaytag.decorator.TableDecorator;
+
+/**
+ * This decorator is used to for decorate different properties of a report to be
+ * shown properly in the view page using display tag lib.
+ * 
+ * @author pansu
+ * 
+ */
+public class ReportDecorator extends TableDecorator {
+	public SortableName getEditReportURL() throws UnsupportedEncodingException {
+		ReportBean file = (ReportBean) getCurrentRowObject();
+		String fileId = file.getDomainReport().getId().toString();
+		String editReportURL = "submitReport.do?submitType=none&page=0&dispatch=setupUpdate&fileId="
+				+ fileId;
+		String link = "<a href=" + editReportURL + ">"
+				+ file.getDomainReport().getTitle() + "</a>";
+		SortableName sortableLink = new SortableName(file.getDomainReport()
+				.getTitle(), link);
+		return sortableLink;
+	}
+
+	public SortableName getViewReportURL() throws UnsupportedEncodingException {
+		ReportBean file = (ReportBean) getCurrentRowObject();
+		String fileId = file.getDomainReport().getId().toString();
+		String editReportURL = "submitReport.do?submitType=none&page=0&dispatch=setupView&fileId="
+				+ fileId;
+		String link = "<a href=" + editReportURL + ">"
+				+ file.getDomainReport().getTitle() + "</a>";
+		SortableName sortableLink = new SortableName(file.getDomainReport()
+				.getTitle(), link);
+		return sortableLink;
+	}
+
+	public String getParticleNames() {
+		ReportBean file = (ReportBean) getCurrentRowObject();
+		return StringUtils.sortJoin(Arrays.asList(file.getParticleNames()),
+				"<br>");
+	}
+}
