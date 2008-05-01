f06eeb4943df519349deedddd1170314c82ea4a5
Shuang Cai <cais>
2008-05-01 01:23:32 +0000

    for chemical association.
    SVN-Revision: 11980

--- gov/nih/nci/cananolab/dto/particle/composition/ChemicalAssociationBean.java
+++ gov/nih/nci/cananolab/dto/particle/composition/ChemicalAssociationBean.java
@@ -130,4 +130,11 @@ public class ChemicalAssociationBean {
 		this.createdBy = createdBy;
 	}
 
+	public void addFile() {
+		files.add(new LabFileBean());
+	}
+
+	public void removeFile(int ind) {
+		files.remove(ind);
+	}
 }
