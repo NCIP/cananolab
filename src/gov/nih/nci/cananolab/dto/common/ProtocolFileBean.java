d76ebb7120803396f1aace9acce00d9e5723b9ac
Sue Pan <sue.pan@nih.gov>
2008-05-07 10:28:31 +0000

    updated for 1.4
    SVN-Revision: 12196

--- src/gov/nih/nci/cananolab/dto/common/ProtocolFileBean.java
+++ src/gov/nih/nci/cananolab/dto/common/ProtocolFileBean.java
@@ -6,6 +6,9 @@ package gov.nih.nci.cananolab.dto.common;
 import gov.nih.nci.cananolab.domain.common.Protocol;
 import gov.nih.nci.cananolab.domain.common.ProtocolFile;
 
+import java.util.Date;
+import java.util.HashSet;
+
 /**
  * Protocol File view bean
  * 
@@ -31,18 +34,43 @@ public class ProtocolFileBean extends LabFileBean {
 				+ domainFile.getVersion();
 	}
 
+	// for dwr ajax in bodySubmitProtocol.jsp
+	public String getDomainFileId() {
+		return domainFile.getId().toString();
+	}
+
+	// for dwr ajax in bodySubmitProtocol.jsp
+	public String getDomainFileVersion() {
+		return domainFile.getVersion();
+	}
+
 	public boolean equals(Object obj) {
 		boolean eq = false;
 		if (obj instanceof ProtocolFileBean) {
 			ProtocolFileBean c = (ProtocolFileBean) obj;
 			Long thisId = domainFile.getId();
 			// String name = this.getName();
-			if (thisId != null
-					&& thisId.equals(c.getDomainFile().getId())) { // &&
+			if (thisId != null && thisId.equals(c.getDomainFile().getId())) { // &&
 				// name != null && name.equals(c.getName())) {
 				eq = true;
 			}
 		}
 		return eq;
+	}
+
+	public void setupDomainFile(String createdBy) throws Exception {
+		super.setupDomainFile(createdBy);
+		Protocol protocol = ((ProtocolFile) domainFile).getProtocol();
+		if (protocol.getId() == null) {
+			protocol.setCreatedBy(createdBy);
+			protocol.setCreatedDate(new Date());
+		}
+		if (protocol.getProtocolFileCollection() == null) {
+			protocol.setProtocolFileCollection(new HashSet<ProtocolFile>());
+		}
+		protocol.getProtocolFileCollection().add((ProtocolFile) domainFile);
+		if (((ProtocolFile) domainFile).getProtocol() == null) {
+			((ProtocolFile) domainFile).setProtocol(protocol);
+		}
 	}
 }
