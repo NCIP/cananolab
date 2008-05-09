359a39b0e349ac57786f64d40f151ae528da3d70
no_author <no_author@no_author.cvs2svn>
2008-05-09 12:38:23 +0000

    This commit was manufactured by cvs2svn to create tag 'QA_05082008_caNanoLab_1_4_build_1'.
    SVN-Revision: 12289

--- src/gov/nih/nci/calab/domain/nano/characterization/physical/ShapeType.java
+++
@@ -1,7 +0,0 @@
-package gov.nih.nci.calab.domain.nano.characterization.physical;
-
-import gov.nih.nci.calab.domain.LookupType;
-
-public class ShapeType extends LookupType {
-	private static final long serialVersionUID = 1234567890L;
-}
