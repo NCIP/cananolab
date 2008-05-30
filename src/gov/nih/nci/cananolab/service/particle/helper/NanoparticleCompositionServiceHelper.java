ad964954a2625d30b8d801b558eaa75cfa77e9f9
Qina Tan <tanq>
2008-05-30 23:39:07 +0000

    add HQL in helper
    SVN-Revision: 5769

--- src/gov/nih/nci/cananolab/service/particle/helper/NanoparticleCompositionServiceHelper.java
+++ src/gov/nih/nci/cananolab/service/particle/helper/NanoparticleCompositionServiceHelper.java
@@ -1,6 +1,7 @@
 package gov.nih.nci.cananolab.service.particle.helper;
 
 import gov.nih.nci.cananolab.domain.particle.samplecomposition.Function;
+import gov.nih.nci.cananolab.domain.particle.samplecomposition.SampleComposition;
 import gov.nih.nci.cananolab.domain.particle.samplecomposition.Target;
 import gov.nih.nci.cananolab.domain.particle.samplecomposition.base.ComposingElement;
 import gov.nih.nci.cananolab.domain.particle.samplecomposition.base.NanoparticleEntity;
@@ -8,8 +9,10 @@ import gov.nih.nci.cananolab.domain.particle.samplecomposition.chemicalassociati
 import gov.nih.nci.cananolab.domain.particle.samplecomposition.chemicalassociation.ChemicalAssociation;
 import gov.nih.nci.cananolab.domain.particle.samplecomposition.functionalization.ActivationMethod;
 import gov.nih.nci.cananolab.domain.particle.samplecomposition.functionalization.FunctionalizingEntity;
+import gov.nih.nci.cananolab.exception.ParticleCompositionException;
 import gov.nih.nci.cananolab.system.applicationservice.CustomizedApplicationService;
 import gov.nih.nci.system.client.ApplicationServiceProvider;
+import gov.nih.nci.system.query.hibernate.HQLCriteria;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -140,50 +143,173 @@ public class NanoparticleCompositionServiceHelper {
 
 	public Collection<Function> findInherentFunctionsByComposingElementId(
 			java.lang.String composingElementId) throws Exception {
-		// TODO: fill in HQL
 		Collection<Function> functions = new ArrayList<Function>();
+		try {
+			CustomizedApplicationService appService = (CustomizedApplicationService) ApplicationServiceProvider
+					.getApplicationService();	
+			HQLCriteria crit = new HQLCriteria(
+					"select cElement.inherentFunctionCollection from "+
+					"gov.nih.nci.cananolab.domain.particle.samplecomposition.base.ComposingElement cElement where cElement.id = "+
+					composingElementId);
+			List results = appService.query(crit);
+			for (Object obj : results) {
+				Function function = (Function) obj;
+				functions.add(function);
+			}
+		} catch (Exception e) {
+			logger
+					.error("Problem to retrieve InherentFunctions By ComposingElementId.",
+							e);
+			throw new ParticleCompositionException(
+					"Problem to retrieve retrieve InherentFunctions By ComposingElementId ");
+		}
 		return functions;
 	}
 
 	public Collection<Function> findFunctionsByFunctionalizingEntityId(
 			java.lang.String functionalizingEntityId) throws Exception {
-		// TODO: fill in HQL
 		Collection<Function> functions = new ArrayList<Function>();
+		try {
+			CustomizedApplicationService appService = (CustomizedApplicationService) ApplicationServiceProvider
+					.getApplicationService();	
+			HQLCriteria crit = new HQLCriteria(
+					"select entity.functionCollection from "+
+					"gov.nih.nci.cananolab.domain.particle.samplecomposition.functionalization.FunctionalizingEntity entity where entity.id = "+
+					functionalizingEntityId);
+			List results = appService.query(crit);
+			for (Object obj : results) {
+				Function function = (Function) obj;
+				functions.add(function);
+			}
+		} catch (Exception e) {
+			logger
+					.error("Problem to retrieve Functions By Functionalizing Entity Id.",
+							e);
+			throw new ParticleCompositionException(
+					"Problem to retrieve retrieve Functions By Functionalizing Entity Id ");
+		}
 		return functions;
 	}
 
 	public Collection<Target> findTargetsByFunctionId(
 			java.lang.String functionId) throws Exception {
 		Collection<Target> targets = new ArrayList<Target>();
-		// TODO: fill in HQL
+		try {
+			CustomizedApplicationService appService = (CustomizedApplicationService) ApplicationServiceProvider
+					.getApplicationService();				
+			HQLCriteria crit = new HQLCriteria(
+					"select function.targetCollection from "+
+					"gov.nih.nci.cananolab.domain.particle.samplecomposition.Function function where Function.id = "+
+					functionId);
+			List results = appService.query(crit);
+			for (Object obj : results) {
+				Target target = (Target) obj;
+				targets.add(target);
+			}
+		} catch (Exception e) {
+			logger
+					.error("Problem to retrieve Targets By Function Id.",
+							e);
+			throw new ParticleCompositionException(
+					"Problem to retrieve retrieve Targets By Function Id");
+		}
 		return targets;
 	}
 
 	public ActivationMethod findActivationMethodByFunctionalizingEntityId(
 			java.lang.String functionalizingEntityId) throws Exception {
 		ActivationMethod activationMethod = null;
-		// TODO fill in HQL
+		try {
+			CustomizedApplicationService appService = (CustomizedApplicationService) ApplicationServiceProvider
+					.getApplicationService();	
+			HQLCriteria crit = new HQLCriteria(
+					"select functionEntity.activationMethod from "+
+					"gov.nih.nci.cananolab.domain.particle.samplecomposition.functionalization.FunctionalizingEntity functionEntity where functionEntity.id = "+
+					functionalizingEntityId);
+			List results = appService.query(crit);
+			for (Object obj : results) {
+				activationMethod = (ActivationMethod) obj;
+			}
+		} catch (Exception e) {
+			logger
+					.error("Problem to retrieve Activation Method By FunctionalizingEntity Id.",
+							e);
+			throw new ParticleCompositionException(
+					"Problem to retrieve retrieve Activation Method By FunctionalizingEntity Id");
+		}
 		return activationMethod;
 	}
 
 	public Collection<ChemicalAssociation> findChemicalAssociationsByCompositionId(
 			java.lang.String compositionId) throws Exception {
-		// TODO: fill in HQL
 		Collection<ChemicalAssociation> assocs = new ArrayList<ChemicalAssociation>();
+		try {
+			CustomizedApplicationService appService = (CustomizedApplicationService) ApplicationServiceProvider
+					.getApplicationService();	
+			HQLCriteria crit = new HQLCriteria(
+					"select sampleComposition.chemicalAssociationCollection from "+
+					"gov.nih.nci.cananolab.domain.particle.samplecomposition.SampleComposition sampleComposition where sampleComposition.id = "+
+					compositionId);
+			List results = appService.query(crit);
+			for (Object obj : results) {
+				ChemicalAssociation chemicalAssociation = (ChemicalAssociation) obj;
+				assocs.add(chemicalAssociation);
+			}
+		} catch (Exception e) {
+			logger
+					.error("Problem to retrieve ChemicalAssociations.",
+							e);
+			throw new ParticleCompositionException(
+					"Problem to retrieve retrieve ChemicalAssociations");
+		}
 		return assocs;
 	}
 
 	public AssociatedElement findAssociatedElementA(
-			java.lang.String chemicalAssociationId) throws Exception {
-		// TODO: fill in HQL
-		AssociatedElement element = null;
+			java.lang.String chemicalAssociationId) throws Exception {		
+		AssociatedElement element = null;		
+		try {
+			CustomizedApplicationService appService = (CustomizedApplicationService) ApplicationServiceProvider
+					.getApplicationService();				
+			HQLCriteria crit = new HQLCriteria(
+					"select chemicalAssociation.associatedElementA from "+
+					"gov.nih.nci.cananolab.domain.particle.samplecomposition.chemicalassociation.ChemicalAssociation chemicalassociation where chemicalassociation.id = "+
+					chemicalAssociationId);
+			List results = appService.query(crit);
+			for (Object obj : results) {
+				element = (AssociatedElement) obj;
+			}
+		} catch (Exception e) {
+			logger
+					.error("Problem to retrieve AssociatedElementA.",
+							e);
+			throw new ParticleCompositionException(
+					"Problem to retrieve retrieve AssociatedElementA");
+		}
 		return element;
 	}
 
 	public AssociatedElement findAssociatedElementB(
-			java.lang.String chemicalAssociationId) throws Exception {
-		// TODO: fill in HQL
+			java.lang.String chemicalAssociationId) throws Exception {		
 		AssociatedElement element = null;
+		try {
+			CustomizedApplicationService appService = (CustomizedApplicationService) ApplicationServiceProvider
+					.getApplicationService();				
+			HQLCriteria crit = new HQLCriteria(
+					"select chemicalAssociation.associatedElementB from "+
+					"gov.nih.nci.cananolab.domain.particle.samplecomposition.chemicalassociation.ChemicalAssociation chemicalassociation where chemicalassociation.id = "+
+					chemicalAssociationId);
+			List results = appService.query(crit);
+			for (Object obj : results) {
+				element = (AssociatedElement) obj;
+			}
+		} catch (Exception e) {
+			logger
+					.error("Problem to retrieve AssociatedElementB.",
+							e);
+			throw new ParticleCompositionException(
+					"Problem to retrieve retrieve AssociatedElementB");
+		}
 		return element;
 	}
 }
