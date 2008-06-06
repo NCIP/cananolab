89e71d1405a6a8b90cbbe40724f5ce7b6649c6ce
Sue Pan <sue.pan@nih.gov>
2008-06-06 15:38:21 +0000

    updated to include more tests
    SVN-Revision: 5845

--- src/test/TestGrid.java
+++ src/test/TestGrid.java
@@ -1,12 +1,15 @@
 package test;
 
 import gov.nih.nci.cagrid.cananolab.client.CaNanoLabServiceClient;
+import gov.nih.nci.cagrid.cqlquery.Association;
+import gov.nih.nci.cagrid.cqlquery.Attribute;
 import gov.nih.nci.cagrid.cqlquery.CQLQuery;
-import gov.nih.nci.cagrid.cqlquery.QueryModifier;
+import gov.nih.nci.cagrid.cqlquery.Predicate;
 import gov.nih.nci.cagrid.cqlresultset.CQLQueryResults;
 import gov.nih.nci.cagrid.data.utilities.CQLQueryResultsIterator;
 import gov.nih.nci.cagrid.discovery.client.DiscoveryClient;
-import gov.nih.nci.cananolab.domain.particle.NanoparticleSample;
+import gov.nih.nci.cananolab.domain.particle.samplecomposition.Function;
+import gov.nih.nci.cananolab.domain.particle.samplecomposition.base.NanoparticleEntity;
 
 import org.apache.axis.message.addressing.EndpointReferenceType;
 
@@ -84,57 +87,218 @@ public class TestGrid {
 		// System.out.println(report.getId());
 		// }
 		//		
-//		CQLQuery query = new CQLQuery();
-//		gov.nih.nci.cagrid.cqlquery.Object target = new gov.nih.nci.cagrid.cqlquery.Object();
-//		target
-//				.setName("gov.nih.nci.cananolab.domain.particle.NanoparticleSample");
-//		query.setTarget(target);
-//		QueryModifier modifier = new QueryModifier();
-//		modifier.setCountOnly(true);
-//		query.setQueryModifier(modifier);
-//		CQLQueryResults results = gridClient.query(query);
-//		results
-//				.setTargetClassname("gov.nih.nci.cananolab.domain.particle.NanoparticleSample");
-//		CQLQueryResultsIterator iter = new CQLQueryResultsIterator(results);
-//		int count = 0;
-//		while (iter.hasNext()) {
-//			java.lang.Object obj = iter.next();
-//			count = ((Long) obj).intValue();
-//		}
-//		
+		// CQLQuery query = new CQLQuery();
+		// gov.nih.nci.cagrid.cqlquery.Object target = new
+		// gov.nih.nci.cagrid.cqlquery.Object();
+		// target
+		// .setName("gov.nih.nci.cananolab.domain.particle.NanoparticleSample");
+		// query.setTarget(target);
+		// QueryModifier modifier = new QueryModifier();
+		// modifier.setCountOnly(true);
+		// query.setQueryModifier(modifier);
+		// CQLQueryResults results = gridClient.query(query);
+		// results
+		// .setTargetClassname("gov.nih.nci.cananolab.domain.particle.NanoparticleSample");
+		// CQLQueryResultsIterator iter = new CQLQueryResultsIterator(results);
+		// int count = 0;
+		// while (iter.hasNext()) {
+		// java.lang.Object obj = iter.next();
+		// count = ((Long) obj).intValue();
+		// }
+		//		
+		// CQLQuery query = new CQLQuery();
+		// gov.nih.nci.cagrid.cqlquery.Object target = new
+		// gov.nih.nci.cagrid.cqlquery.Object();
+		// target.setName("gov.nih.nci.cananolab.domain.common.ProtocolFile");
+		// query.setTarget(target);
+		// QueryModifier modifier = new QueryModifier();
+		// modifier.setCountOnly(true);
+		// query.setQueryModifier(modifier);
+		//
+		// CQLQueryResults results = gridClient.query(query);
+		// results
+		// .setTargetClassname("gov.nih.nci.cananolab.domain.common.ProtocolFile");
+		// CQLQueryResultsIterator iter = new CQLQueryResultsIterator(results);
+		// int count = 0;
+		// while (iter.hasNext()) {
+		// java.lang.Object obj = iter.next();
+		// count = ((Long) obj).intValue();
+		// }
+		// System.out.println(count);
+		//		
+		// CQLQuery query = new CQLQuery();
+		// gov.nih.nci.cagrid.cqlquery.Object target = new
+		// gov.nih.nci.cagrid.cqlquery.Object();
+		// target.setName("gov.nih.nci.cananolab.domain.common.Protocol");
+		// Association association = new Association();
+		// association.setName("gov.nih.nci.cananolab.domain.common.ProtocolFile");
+		// association.setRoleName("protocolFileCollection");
+		// Attribute attribute = new Attribute();
+		// attribute.setName("id");
+		// attribute.setPredicate(Predicate.EQUAL_TO);
+		// attribute.setValue("102");
+		// association.setAttribute(attribute);
+		// target.setAssociation(association);
+		// query.setTarget(target);
+		// CQLQueryResults results = gridClient.query(query);
+		// results
+		// .setTargetClassname("gov.nih.nci.cananolab.domain.common.Protocol");
+		// CQLQueryResultsIterator iter = new CQLQueryResultsIterator(results);
+		// while (iter.hasNext()) {
+		// java.lang.Object obj = iter.next();
+		// Protocol protocol = (Protocol) obj;
+		// System.out.println(protocol.getName());
+		// }
+
+		// CQLQuery query = new CQLQuery();
+		// gov.nih.nci.cagrid.cqlquery.Object target = new
+		// gov.nih.nci.cagrid.cqlquery.Object();
+		// target
+		// .setName("gov.nih.nci.cananolab.domain.particle.characterization.Characterization");
+		// Association association = new Association();
+		// association
+		// .setName("gov.nih.nci.cananolab.domain.particle.NanoparticleSample");
+		// association.setRoleName("nanoparticleSample");
+		//
+		// Attribute attribute = new Attribute();
+		// attribute.setName("id");
+		// attribute.setPredicate(Predicate.EQUAL_TO);
+		// attribute.setValue("917504");
+		//
+		// association.setAttribute(attribute);
+		//
+		// target.setAssociation(association);
+		// query.setTarget(target);
+		// CQLQueryResults results = gridClient.query(query);
+		// results
+		// .setTargetClassname("gov.nih.nci.cananolab.domain.particle.characterization.Characterization");
+		// CQLQueryResultsIterator iter = new CQLQueryResultsIterator(results);
+		// Characterization chara = null;
+		// List<Characterization> characterizationCollection = new
+		// ArrayList<Characterization>();
+		// while (iter.hasNext()) {
+		// java.lang.Object obj = iter.next();
+		// chara = (Characterization) obj;
+		// // loadCharacterizationAssociations(chara);
+		// characterizationCollection.add(chara);
+		// System.out.println(chara.getIdentificationName());
+		// }
+
+		// CQLQuery query = new CQLQuery();
+		// gov.nih.nci.cagrid.cqlquery.Object target = new
+		// gov.nih.nci.cagrid.cqlquery.Object();
+		// target
+		// .setName("gov.nih.nci.cananolab.domain.particle.characterization.Characterization");
+		// Attribute attribute = new Attribute();
+		// attribute.setName("id");
+		// attribute.setPredicate(Predicate.EQUAL_TO);
+		// attribute.setValue("1376257");
+		// target.setAttribute(attribute);
+		// query.setTarget(target);
+		// CQLQueryResults results = gridClient.query(query);
+		// results
+		// .setTargetClassname("gov.nih.nci.cananolab.domain.particle.characterization.Characterization");
+		// CQLQueryResultsIterator iter = new CQLQueryResultsIterator(results);
+		// Characterization achar = null;
+		// while (iter.hasNext()) {
+		// java.lang.Object obj = iter.next();
+		// achar = (Characterization) obj;
+		// System.out.println(achar.getId());
+		// }
+
+		// CQLQuery query = new CQLQuery();
+		// gov.nih.nci.cagrid.cqlquery.Object target = new
+		// gov.nih.nci.cagrid.cqlquery.Object();
+		// target
+		// .setName("gov.nih.nci.cananolab.domain.particle.samplecomposition.SampleComposition");
+		// Association association = new Association();
+		// association
+		// .setName("gov.nih.nci.cananolab.domain.particle.NanoparticleSample");
+		// association.setRoleName("nanoparticleSample");
+		//
+		// Attribute attribute = new Attribute();
+		// attribute.setName("id");
+		// attribute.setPredicate(Predicate.EQUAL_TO);
+		// attribute.setValue("917504");
+		//
+		// association.setAttribute(attribute);
+		//
+		// target.setAssociation(association);
+		// query.setTarget(target);
+		// CQLQueryResults results = gridClient.query(query);
+		// results
+		// .setTargetClassname("gov.nih.nci.cananolab.domain.particle.samplecomposition.SampleComposition");
+		// CQLQueryResultsIterator iter = new CQLQueryResultsIterator(results);
+		// SampleComposition sampleComposition = null;
+		// while (iter.hasNext()) {
+		// java.lang.Object obj = iter.next();
+		// sampleComposition = (SampleComposition) obj;
+		// System.out.println(sampleComposition.getId());
+		// }
+
 		CQLQuery query = new CQLQuery();
 		gov.nih.nci.cagrid.cqlquery.Object target = new gov.nih.nci.cagrid.cqlquery.Object();
-		target.setName("gov.nih.nci.cananolab.domain.common.ProtocolFile");
-		query.setTarget(target);
-		QueryModifier modifier=new QueryModifier();
-		modifier.setCountOnly(true);
-		query.setQueryModifier(modifier);
+		target
+				.setName("gov.nih.nci.cananolab.domain.particle.samplecomposition.base.NanoparticleEntity");
+		Association association = new Association();
+		association
+				.setName("gov.nih.nci.cananolab.domain.particle.samplecomposition.SampleComposition");
+		association.setRoleName("sampleComposition");
 
+		Attribute attribute = new Attribute();
+		attribute.setName("id");
+		attribute.setPredicate(Predicate.EQUAL_TO);
+		attribute.setValue("1376256");
+		association.setAttribute(attribute);
+
+		target.setAssociation(association);
+		query.setTarget(target);
 		CQLQueryResults results = gridClient.query(query);
 		results
-				.setTargetClassname("gov.nih.nci.cananolab.domain.common.ProtocolFile");
+				.setTargetClassname("gov.nih.nci.cananolab.domain.particle.samplecomposition.base.NanoparticleEntity");
 		CQLQueryResultsIterator iter = new CQLQueryResultsIterator(results);
-		int count=0;
+		NanoparticleEntity nanoEntity = null;
+
 		while (iter.hasNext()) {
 			java.lang.Object obj = iter.next();
-			count=((Long)obj).intValue();
-		}	
-		System.out.println(count);
+			nanoEntity = (NanoparticleEntity) obj;
+			System.out.println(nanoEntity.getId());
+		}
 	}
 
 	public void testOperation(String serviceUrl) throws Exception {
 		CaNanoLabServiceClient gridClient = new CaNanoLabServiceClient(
 				serviceUrl);
 		System.out.println("Running operation .............");
-		String particleSource = "DNT";
-		String[] nanoparticleEntityClassNames = new String[] { "Dendrimer", "CarbonNanotube" };
-		String[] functionClassNames=new String[] {"TargetingFunction", "TherapeuticFunction"};
-		String[] characterizationClassNames=new String[] {"EnzymeInduction"};
-		NanoparticleSample[] particleSamples = gridClient
-				.getNanoparticleSamplesBy(particleSource,
-						nanoparticleEntityClassNames, null, functionClassNames, characterizationClassNames, null);
-		for (NanoparticleSample particleSample : particleSamples) {
-			System.out.println(particleSample.getName());
+		// String particleSource = "DNT";
+		// String[] nanoparticleEntityClassNames = new String[] { "Dendrimer",
+		// "CarbonNanotube" };
+		// String[] functionClassNames = new String[] { "TargetingFunction",
+		// "TherapeuticFunction" };
+		// String[] characterizationClassNames = new String[] {
+		// "EnzymeInduction" };
+		// NanoparticleSample[] particleSamples = gridClient
+		// .getNanoparticleSamplesBy(particleSource,
+		// nanoparticleEntityClassNames, null, functionClassNames,
+		// characterizationClassNames, null);
+		// for (NanoparticleSample particleSample : particleSamples) {
+		// System.out.println(particleSample.getName());
+		// }
+		//
+		// String protocolType = null;
+		// String protocolName = null;
+		// String protocolFileTitle = null;
+		// ProtocolFile[] protocolFiles = gridClient.getProtocolFilesBy(
+		// protocolType, protocolName, protocolFileTitle);
+		// for (ProtocolFile pf : protocolFiles) {
+		// System.out.println("PF: " + pf.getName());
+		// }
+
+		String composingElementId = "1409024";
+		Function[] functions = gridClient
+				.getInherentFunctionsByComposingElementId(composingElementId);
+		for (Function function : functions) {
+			System.out.println(function.getId());
 		}
 	}
 
@@ -153,9 +317,9 @@ public class TestGrid {
 				System.out.println("No grid nodes found");
 			}
 			test
-					.testCQL("http://137.187.250.17:18080/wsrf/services/cagrid/CaNanoLabService");
-			test
-					.testOperation("http://137.187.250.17:18080/wsrf/services/cagrid/CaNanoLabService");
+					.testCQL("http://165.112.132.67:8080/wsrf/services/cagrid/CaNanoLabService");
+			 test
+			 .testOperation("http://165.112.132.67:8080/wsrf/services/cagrid/CaNanoLabService");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
