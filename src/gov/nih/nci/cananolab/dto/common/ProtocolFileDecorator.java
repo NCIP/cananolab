6ed7d8092974103fb50050d648fafe031425bfe1
Qina Tan <tanq>
2008-06-17 21:29:32 +0000

    change slightly improve performance
    SVN-Revision: 6018

--- src/gov/nih/nci/cananolab/dto/common/ProtocolFileDecorator.java
+++ src/gov/nih/nci/cananolab/dto/common/ProtocolFileDecorator.java
@@ -23,13 +23,20 @@ public class ProtocolFileDecorator extends TableDecorator {
 		}
 		// replace space with special char
 		String fileId = file.getDomainFile().getId().toString();
-		String editProtocolURL = "submitProtocol.do?dispatch=setupUpdate&fileId="
-				+ fileId+"&location=local";
-		String link = "<a href="
-				+ editProtocolURL
-				+ ">"
-				+ ((ProtocolFile) (file.getDomainFile())).getProtocol()
-						.getName() + "</a>";
+//		String editProtocolURL = "submitProtocol.do?dispatch=setupUpdate&fileId="
+//				+ fileId+"&location=local";
+//		String link = "<a href="
+//				+ editProtocolURL
+//				+ ">"
+//				+ ((ProtocolFile) (file.getDomainFile())).getProtocol()
+//						.getName() + "</a>";
+		StringBuilder sb = new StringBuilder("<a href=");
+		sb.append("submitProtocol.do?dispatch=setupUpdate&fileId=");
+		sb.append(fileId);
+		sb.append("&location=local>");
+		sb.append(((ProtocolFile) (file.getDomainFile())).getProtocol().getName());
+		sb.append("</a>");
+		String link = sb.toString();
 		SortableName sortableLink = new SortableName(((ProtocolFile) (file
 				.getDomainFile())).getProtocol().getName(), link);
 		return sortableLink;
@@ -47,11 +54,20 @@ public class ProtocolFileDecorator extends TableDecorator {
 		SortableName sortableLink = null;
 		ProtocolFileBean file = (ProtocolFileBean) getCurrentRowObject();
 		if (file.getDomainFile().getName() != null) {
-			String downloadURL = "searchProtocol.do?dispatch=download"
-					+ "&fileId=" + file.getDomainFile().getId() + "&location="
-					+ file.getLocation();
-			String link = "<a href=" + downloadURL + ">"
-					+ file.getDomainFile().getName() + "</a>";
+//			String downloadURL = "searchProtocol.do?dispatch=download"
+//					+ "&fileId=" + file.getDomainFile().getId() + "&location="
+//					+ file.getLocation();
+//			String link = "<a href=" + downloadURL + ">"
+//					+ file.getDomainFile().getName() + "</a>";
+			StringBuilder sb = new StringBuilder("<a href=");
+			sb.append("searchProtocol.do?dispatch=download&fileId=");
+			sb.append(file.getDomainFile().getId());
+			sb.append("&location=");
+			sb.append(file.getLocation());
+			sb.append(">");
+			sb.append(file.getDomainFile().getName());
+			sb.append("</a>");
+			String link = sb.toString();
 			sortableLink = new SortableName(file.getDomainFile().getName(),
 					link);
 		} else {
