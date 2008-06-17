886f784a5a8da3904385c22d84eb029f8e816919
Qina Tan <tanq>
2008-06-17 21:29:32 +0000

    change slightly improve performance
    SVN-Revision: 12758

--- src/gov/nih/nci/cananolab/dto/common/ReportDecorator.java
+++ src/gov/nih/nci/cananolab/dto/common/ReportDecorator.java
@@ -22,10 +22,17 @@ public class ReportDecorator extends TableDecorator {
 			return getViewName();
 		}
 		String fileId = file.getDomainFile().getId().toString();
-		String editReportURL = "submitReport.do?submitType=none&page=0&dispatch=setupUpdate&fileId="
-				+ fileId+"&location=local";
-		String link = "<a href=" + editReportURL + ">"
-				+ file.getDomainFile().getTitle() + "</a>";
+//		String editReportURL = "submitReport.do?submitType=none&page=0&dispatch=setupUpdate&fileId="
+//				+ fileId+"&location=local";
+//		String link = "<a href=" + editReportURL + ">"
+//				+ file.getDomainFile().getTitle() + "</a>";
+		StringBuilder sb = new StringBuilder("<a href=");
+		sb.append("submitReport.do?submitType=none&page=0&dispatch=setupUpdate&fileId=");
+		sb.append(fileId);
+		sb.append("&location=local>");
+		sb.append(file.getDomainFile().getTitle());
+		sb.append("</a>");
+		String link = sb.toString();
 		SortableName sortableLink = new SortableName(file.getDomainFile()
 				.getTitle(), link);
 		return sortableLink;
@@ -35,12 +42,23 @@ public class ReportDecorator extends TableDecorator {
 		SortableName sortableLink = null;
 		ReportBean file = (ReportBean) getCurrentRowObject();
 		if (file.getDomainFile().getName() != null) {
-			String downloadURL = "searchReport.do?dispatch=download"
-					+ "&fileId=" + file.getDomainFile().getId() + "&location="
-					+ file.getLocation();
-			String link = "<a href=" + downloadURL + " target='"
-					+ file.getUrlTarget() + "'>"
-					+ file.getDomainFile().getName() + "</a>";
+//			String downloadURL = "searchReport.do?dispatch=download"
+//					+ "&fileId=" + file.getDomainFile().getId() + "&location="
+//					+ file.getLocation();
+//			String link = "<a href=" + downloadURL + " target='"
+//					+ file.getUrlTarget() + "'>"
+//					+ file.getDomainFile().getName() + "</a>";
+			StringBuilder sb = new StringBuilder("<a href=");
+			sb.append("searchReport.do?dispatch=download&fileId=");
+			sb.append(file.getDomainFile().getId());
+			sb.append("&location=");
+			sb.append(file.getLocation());
+			sb.append(" target='");
+			sb.append(file.getUrlTarget());
+			sb.append("'>");
+			sb.append(file.getDomainFile().getName());
+			sb.append("</a>");
+			String link = sb.toString();
 			sortableLink = new SortableName(file.getDomainFile().getName(),
 					link);
 		} else {
