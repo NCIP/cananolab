5f6e3a27f037c95077e5f5bd0b17995bdd9f79cf
Sue Pan <sue.pan@nih.gov>
2008-06-20 11:52:06 +0000

    update imports
    SVN-Revision: 12775

--- gov/nih/nci/cananolab/util/ExportUtils.java
+++ gov/nih/nci/cananolab/util/ExportUtils.java
@@ -1,9 +1,7 @@
 package gov.nih.nci.cananolab.util;
 
-import java.io.BufferedOutputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
