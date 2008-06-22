8428ff3e1a423d12192451ebf2bce2421a4b36a5
Shuang Cai <cais>
2008-06-22 14:33:23 +0000

    added public datum data  to csm_protection_group
    SVN-Revision: 12779

--- db/1.4/data_migration/datum.sql
+++ db/1.4/data_migration/datum.sql
@@ -21,3 +21,85 @@ SELECT
 FROM cananolab.datum d
 ORDER BY d.datum_pk_id, d.list_index
 ;
+
+drop table if exists canano.protection_group_tmp;
+
+CREATE TABLE canano.protection_group_tmp (
+   protection_group_name VARCHAR(100) NOT NULL
+)
+;
+
+INSERT into csm_protection_group (
+	protection_group_name,
+	application_id,
+	large_element_count_flag,
+	update_date
+)
+SELECT
+	distinct dd.datum_pk_id,
+	'2',
+	'0',
+	sysdate()
+FROM csm_group g,
+	csm_protection_group pg,
+	csm_user_group_role_pg upg,
+	nanoparticle_sample ns,
+	characterization cha,
+	derived_bioassay_data dbd,
+	derived_datum dd,
+	cananolab.datum d13
+WHERE g.group_id = upg.group_id
+AND g.group_name = 'Public'
+AND upg.protection_group_id = pg.protection_group_id
+AND pg.protection_group_name = ns.particle_sample_name
+AND ns.particle_sample_pk_id = cha.particle_sample_pk_id
+AND cha.characterization_pk_id = dbd.characterization_pk_id
+AND dbd.derived_bioassay_data_pk_id = dd.derived_bioassay_data_pk_id
+AND dd.datum_pk_id = d13.datum_pk_id
+;
+
+INSERT into protection_group_tmp (
+	protection_group_name
+)
+SELECT
+	distinct dd.datum_pk_id
+FROM csm_group g,
+	csm_protection_group pg,
+	csm_user_group_role_pg upg,
+	nanoparticle_sample ns,
+	characterization cha,
+	derived_bioassay_data dbd,
+	derived_datum dd,
+	cananolab.datum d13
+WHERE g.group_id = upg.group_id
+AND g.group_name = 'Public'
+AND upg.protection_group_id = pg.protection_group_id
+AND pg.protection_group_name = ns.particle_sample_name
+AND ns.particle_sample_pk_id = cha.particle_sample_pk_id
+AND cha.characterization_pk_id = dbd.characterization_pk_id
+AND dbd.derived_bioassay_data_pk_id = dd.derived_bioassay_data_pk_id
+AND dd.datum_pk_id = d13.datum_pk_id
+;
+
+
+INSERT into csm_user_group_role_pg (
+	group_id,
+	role_id,
+	protection_group_id,
+	update_date
+)
+SELECT
+	g.group_id,
+	cr.role_id,
+	pg.protection_group_id,
+	sysdate()
+FROM csm_group g,
+	csm_protection_group pg,
+	csm_role cr,
+	protection_group_tmp tmp
+WHERE pg.protection_group_name = tmp.protection_group_name
+AND g.group_name = 'Public'
+AND cr.role_name = 'R'
+;
+
+drop table protection_group_tmp;
