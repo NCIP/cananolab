958788d26c044a180aa5e2feefe2d9d59bec46ae
Qina Tan <tanq>
2008-07-01 20:30:12 +0000

    in order to show derived datum Average and Percent Cell Viability in application, name and unit are added to common_lookup
    SVN-Revision: 12785

---
+++ db/1.4/data_migration/insert_commonLookup_ncl.sql
@@ -0,0 +1,11 @@
+use canano;
+
+INSERT INTO canano.common_lookup (
+	name,
+	attribute,
+	value
+)
+VALUES('Size','derivedDatumName','Average'),
+('Average','unit','nm'),
+('CellViability','derivedDatumName','Percent Cell Viability'),
+('Percent Cell Viability','unit','%');
