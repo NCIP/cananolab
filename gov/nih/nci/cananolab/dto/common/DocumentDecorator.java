7c4063a6bf5582b07150e3dd38f37dcd6a346cfe
Shuang Cai <cais>
2008-08-12 17:21:18 +0000

    display publication and report detail view without particle ID.
    SVN-Revision: 12934

--- gov/nih/nci/cananolab/dto/common/DocumentDecorator.java
+++ gov/nih/nci/cananolab/dto/common/DocumentDecorator.java
@@ -93,7 +93,7 @@ public class DocumentDecorator extends TableDecorator {
 		String fileId = file.getDomainFile().getId().toString();
 		//TODO, submitReport or submitPublication
 		StringBuilder sb = new StringBuilder("<a href=");
-		sb.append("submitPublication.do?submitType=none&page=0&dispatch=detailView&publciationId=");
+		sb.append("submitPublication.do?submitType=none&page=0&dispatch=detailView&publicationId=");
 		sb.append(fileId);
 		sb.append("&location=");
 		sb.append(file.getLocation());
