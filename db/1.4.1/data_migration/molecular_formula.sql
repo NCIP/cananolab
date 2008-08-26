9afeb686e7370799c991b2a445b8155630e49d67
Qina Tan <tanq>
2008-08-26 18:00:41 +0000

    change molecular_formula length to 2000
    SVN-Revision: 13020

--- db/1.4.1/data_migration/molecular_formula.sql
+++ db/1.4.1/data_migration/molecular_formula.sql
@@ -1,7 +1,7 @@
 -- increase the molecular_formula column from VARCHAR(200) to VARCHAR(500)
 
 ALTER TABLE canano.associated_element
- CHANGE molecular_formula molecular_formula VARCHAR(500);
+ CHANGE molecular_formula molecular_formula VARCHAR(2000);
  
 ALTER TABLE canano.surface_chemistry
- CHANGE molecular_formula molecular_formula VARCHAR(500);
+ CHANGE molecular_formula molecular_formula VARCHAR(2000);
