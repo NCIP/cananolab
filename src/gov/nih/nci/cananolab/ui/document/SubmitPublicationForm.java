e8b5c083543e84082e755e87ea2c828c7994eb50
Qina Tan <tanq>
2008-09-17 20:04:54 +0000

    fix session scope form when deselecting all choices
    SVN-Revision: 6359

---
+++ src/gov/nih/nci/cananolab/ui/document/SubmitPublicationForm.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package gov.nih.nci.cananolab.ui.document;
+
+import gov.nih.nci.cananolab.dto.common.PublicationBean;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.struts.action.ActionMapping;
+import org.apache.struts.validator.DynaValidatorForm;
+
+/**
+ * @author tanq
+ *
+ */
+public class SubmitPublicationForm extends DynaValidatorForm{
+	public void reset(ActionMapping mapping, HttpServletRequest request) {
+		SubmitPublicationForm form = 
+			(SubmitPublicationForm)request.getSession().getAttribute("submitPublicationForm");
+		String dispatch = (String)form.get("dispatch");
+		//if addAuthor and validator fail, do not reset
+		if (dispatch!=null && !dispatch.equalsIgnoreCase("addAuthor")
+				&& !dispatch.equalsIgnoreCase("create")) {
+			PublicationBean pubBean = (PublicationBean)form.get("file");
+			pubBean.setResearchAreas(new String[0]);
+			pubBean.setVisibilityGroups(new String[0]);
+			pubBean.setParticleNames(new String[0]);		
+			this.set("file", pubBean);
+		}
+	}
+}
