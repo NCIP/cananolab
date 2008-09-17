f1f92210e0d02fff096a3a26b8c8566d0c934db0
Qina Tan <tanq>
2008-09-17 20:06:23 +0000

    if nanoparticle_sample_id is null, no record will be inserted. so no error when particle_smaple_pk_id is null
    SVN-Revision: 6360

--- db/1.4.1/data_migration/fix/setFieldsNotRequired.sql
+++ db/1.4.1/data_migration/fix/setFieldsNotRequired.sql
@@ -1,6 +1,3 @@
 
 ALTER TABLE canano.publication
  CHANGE research_area research_area VARCHAR(200); 
- 
-ALTER TABLE canano.nanoparticle_sample_publication
- CHANGE particle_sample_pk_id particle_sample_pk_id BIGINT;
