8eae81a5f84ae1ea0b188d88492a88c31e63ccf5
Qina Tan <tanq>
2008-09-24 21:33:50 +0000

    authors that have larger ids have larger created date
    SVN-Revision: 13138

--- src/gov/nih/nci/cananolab/dto/common/PublicationBean.java
+++ src/gov/nih/nci/cananolab/dto/common/PublicationBean.java
@@ -50,7 +50,7 @@ public class PublicationBean extends LabFileBean {
 			Collections.sort(authorslist, 
 					new Comparator<Author>() {
 			    public int compare(Author o1, Author o2) {
-			        return (int)(o1.getCreatedDate().compareTo(o2.getCreatedDate()));
+			        return (int)(o2.getCreatedDate().compareTo(o1.getCreatedDate()));
 			    }});
 			authors = authorslist;
 		}
