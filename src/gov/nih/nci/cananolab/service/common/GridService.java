1636c7f5583b64823043bd93540a331419556e2c
Sue Pan <sue.pan@nih.gov>
2008-10-07 15:21:35 +0000

    updated exception handling
    SVN-Revision: 13204

--- src/gov/nih/nci/cananolab/service/common/GridService.java
+++ src/gov/nih/nci/cananolab/service/common/GridService.java
@@ -4,28 +4,19 @@ import gov.nih.nci.cagrid.discovery.client.DiscoveryClient;
 import gov.nih.nci.cagrid.metadata.MetadataUtils;
 import gov.nih.nci.cagrid.metadata.ServiceMetadata;
 import gov.nih.nci.cagrid.metadata.dataservice.DomainModel;
-import gov.nih.nci.cagrid.metadata.exceptions.QueryInvalidException;
-import gov.nih.nci.cagrid.metadata.exceptions.RemoteResourcePropertyRetrievalException;
-import gov.nih.nci.cagrid.metadata.exceptions.ResourcePropertyRetrievalException;
 import gov.nih.nci.cananolab.dto.common.GridNodeBean;
 import gov.nih.nci.cananolab.exception.GridAutoDiscoveryException;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
-import java.util.TreeMap;
 
 import org.apache.axis.message.addressing.EndpointReferenceType;
-import org.apache.axis.types.URI.MalformedURIException;
 import org.apache.log4j.Logger;
 
 /**
- * Grid service utils for grid node discovery and grid node URL lookup.
+ * Grid service utils for grid node discovery and grid node lookup.
  * 
  * @author pansu
  * 
@@ -34,169 +25,51 @@ public class GridService {
 	private static Logger logger = Logger.getLogger(GridService.class);
 
 	/**
-	 * Query the grid index service by domain model name and return a map of
-	 * GridNodeBeans with keys being the hostnames.
+	 * Discover all grid services by the given domain model name and version
 	 * 
 	 * @param indexServiceURL
 	 * @param domainModelName
+	 * @param domainModelVersion
 	 * @return
 	 * @throws GridAutoDiscoveryException
 	 */
-	public static Map<String, GridNodeBean> discoverServices(
-			String indexServiceURL, String domainModelName, String appOwner)
-			throws GridAutoDiscoveryException {
-
-		Map<String, GridNodeBean> gridNodeMap = new TreeMap<String, GridNodeBean>();
-		EndpointReferenceType[] services = null;
+	public static List<GridNodeBean> discoverAllServices(
+			String indexServiceURL, String domainModelName,
+			String domainModelVersion) {
+		EndpointReferenceType[] returnedServices = null;
+		List<EndpointReferenceType> services = new ArrayList<EndpointReferenceType>();
 		try {
 			DiscoveryClient discoveryClient = new DiscoveryClient(
 					indexServiceURL);
-			services = discoveryClient
+			returnedServices = discoveryClient
 					.discoverDataServicesByDomainModel(domainModelName);
-		} catch (Exception e) {
-			String err = "Error in discovering caNanoLab nodes from the index server";
-			logger.error(err, e);
-			throw new GridAutoDiscoveryException(err, e);
-		}
-		if (services != null) {
-			for (EndpointReferenceType service : services) {
-				String address = service.getAddress().toString();
-				String hostName = "";
-				// catch RemoteResourcePropertyRetrievalException in case
-				// service is not successfully deregistered from the index
-				// server when the service is shut down.
-				try {
-					ServiceMetadata serviceMetaData = MetadataUtils
-							.getServiceMetadata(service);
-					if (serviceMetaData != null) {
-						if (serviceMetaData.getHostingResearchCenter() != null) {
-							if (serviceMetaData.getHostingResearchCenter()
-									.getResearchCenter() != null) {
-								hostName = serviceMetaData
-										.getHostingResearchCenter()
-										.getResearchCenter().getDisplayName();
-								// remove caNanoLab prefix
-								hostName = hostName.replace("caNanoLab-", "");
-							}
-						}
-					}
-
-					// remove local grid node from the list
-					if (!hostName.contains(appOwner)) {
-						GridNodeBean gridNode = new GridNodeBean(hostName,
-								address);
-						gridNodeMap.put(hostName, gridNode);
-					}
-				} catch (Exception e) {
-					String err = "Can't successfully obtain grid service metadata: "
-							+ address;
-					logger.error(err, e);
-					throw new GridAutoDiscoveryException(err);
+			for (EndpointReferenceType returnedService : returnedServices) {
+				// obtain the domain model version
+				DomainModel domainModel = MetadataUtils
+						.getDomainModel(returnedService);
+				if (domainModel.getProjectVersion().equals(domainModelVersion)) {
+					services.add(returnedService);
 				}
 			}
-			return gridNodeMap;
-		} else {
-			return null;
+		} catch (Exception e) {
+			logger.error(e.getMessage(), e);
 		}
+		return populateServiceMetadata(services, domainModelName);
 	}
 
 	/**
-	 * Query the grid index service by domain model name and return a map of
-	 * GridNodeBeans with keys being the hostnames. Remove the local node.
+	 * Discovered all new grid services by the given model name and version,
+	 * remove ones that already exists
 	 * 
 	 * @param indexServiceURL
 	 * @param domainModelName
+	 * @param domainModelVersion
+	 * @param extantURLs
 	 * @return
-	 * @throws GridAutoDiscoveryException
 	 */
-	public static Map<String, GridNodeBean> discoverServices(
+	public static List<GridNodeBean> discoverNewServices(
 			String indexServiceURL, String domainModelName,
-			String domainModelVersion, String appOwner)
-			throws GridAutoDiscoveryException {
-
-		Map<String, GridNodeBean> gridNodeMap = new TreeMap<String, GridNodeBean>();
-		EndpointReferenceType[] services = null;
-		List<EndpointReferenceType> matchedServices = null;
-		try {
-			DiscoveryClient discoveryClient = new DiscoveryClient(
-					indexServiceURL);
-			services = discoveryClient
-					.discoverDataServicesByDomainModel(domainModelName);
-		} catch (Exception e) {
-			String err = "Error in discovering caNanoLab nodes from the index server";
-			logger.error(err, e);
-			throw new GridAutoDiscoveryException(err, e);
-		}
-		if (services != null) {
-			matchedServices = new ArrayList<EndpointReferenceType>(Arrays
-					.asList(services));
-
-			for (EndpointReferenceType service : services) {
-				// catch RemoteResourcePropertyRetrievalException in case
-				// service is not successfully deregistered from the index
-				// server when the service is shut down.
-				String address = service.getAddress().toString();
-				try {
-					// obtain the domain model version
-					DomainModel domainModel = MetadataUtils
-							.getDomainModel(service);
-					if (domainModel.getProjectVersion().equals(
-							domainModelVersion)) {
-						matchedServices.add(service);
-					} else {
-						logger.warn("Different domain model version found ("
-								+ domainModel.getProjectVersion() + ") for "
-								+ address);
-					}
-				} catch (Exception e) {
-					String err = "Can't successfully obtain grid service domain model version from "
-							+ address;
-					logger.error(err, e);
-				}
-			}
-
-			for (EndpointReferenceType service : matchedServices) {
-				String address = service.getAddress().toString();
-				String hostName = "";
-				try {
-					ServiceMetadata serviceMetaData = MetadataUtils
-							.getServiceMetadata(service);
-					if (serviceMetaData != null) {
-						if (serviceMetaData.getHostingResearchCenter() != null) {
-							if (serviceMetaData.getHostingResearchCenter()
-									.getResearchCenter() != null) {
-								hostName = serviceMetaData
-										.getHostingResearchCenter()
-										.getResearchCenter().getDisplayName();
-								// remove caNanoLab prefix
-								hostName = hostName.replace("caNanoLab-", "");
-							}
-						}
-					}
-
-					// remove local grid node from the list
-					if (!hostName.contains(appOwner)) {
-						GridNodeBean gridNode = new GridNodeBean(hostName,
-								address);
-						gridNodeMap.put(hostName, gridNode);
-					}
-				} catch (Exception e) {
-					String err = "Can't successfully obtain grid service metadata: "
-							+ address;
-					logger.error(err, e);
-					// throw new GridAutoDiscoveryException(err);
-				}
-			}
-			return gridNodeMap;
-		} else {
-			return null;
-		}
-	}
-
-	public static Map<String, GridNodeBean> discoverServices(
-			String indexServiceURL, String domainModelName,
-			String domainModelVersion, Set<String> extantURLs)
-			throws GridAutoDiscoveryException {
+			String domainModelVersion, Set<String> extantURLs) {
 		EndpointReferenceType[] returnedServices = null;
 		List<EndpointReferenceType> services = new ArrayList<EndpointReferenceType>();
 		List<EndpointReferenceType> newRemoteServices = null;
@@ -214,22 +87,16 @@ public class GridService {
 					services.add(returnedService);
 				}
 			}
-		} catch (MalformedURIException e) {
-			logger.error(e.getMessage(), e);
-		} catch (RemoteResourcePropertyRetrievalException e) {
-			logger.error(e.getMessage(), e);
-		} catch (QueryInvalidException e) {
-			logger.error(e.getMessage(), e);
-		} catch (ResourcePropertyRetrievalException e) {
+		} catch (Exception e) {
 			logger.error(e.getMessage(), e);
 		}
 
-		if (services != null) {
+		if (!services.isEmpty()) {
 			newRemoteServices = new ArrayList<EndpointReferenceType>(services);
 			for (EndpointReferenceType service : services) {
 				for (String extantURL : extantURLs) {
-					// if the URL exists (because it's local or previously
-					// queried, than make it to be removed
+					// if the URL exists (because it's previously queried,
+					// remove itF
 					if (extantURL.equals(service.getAddress().toString())) {
 						servicesToRemove.add(service);
 					}
@@ -242,10 +109,18 @@ public class GridService {
 		return populateServiceMetadata(newRemoteServices, domainModelName);
 	}
 
-	public static Map<String, GridNodeBean> populateServiceMetadata(
+	/**
+	 * Populate grid service metadata such as hostname and grid service URL
+	 * 
+	 * @param services
+	 * @param domainModelName
+	 * @return
+	 */
+	public static List<GridNodeBean> populateServiceMetadata(
 			List<EndpointReferenceType> services, String domainModelName) {
-		Map<String, GridNodeBean> gridNodeMap = new HashMap<String, GridNodeBean>();
+		List<GridNodeBean> gridNodes = new ArrayList<GridNodeBean>();
 		if (services != null) {
+			// catch exception separately for each service
 			for (EndpointReferenceType service : services) {
 				String address = service.getAddress().toString();
 				String hostName = null;
@@ -258,53 +133,51 @@ public class GridService {
 							hostName = serviceMetaData
 									.getHostingResearchCenter()
 									.getResearchCenter().getDisplayName();
-							// remove caNanoLab prefix
-							hostName = hostName.replace("caNanoLab-", "");
+							// remove model prefix
+							hostName = hostName.replace(domainModelName + "-",
+									"");
 						}
 					}
 				} catch (Exception e) {
-					String err = "Can't successfully obtain grid service metadata: "
+					String err = "Can't successfully obtain grid service metadata for: "
 							+ address;
 					hostName = null;
 					logger.warn(err);
-					// throw new GridAutoDiscoveryException(err, e);
 				}
 				if (hostName != null) {
 					GridNodeBean gridNode = new GridNodeBean(hostName, address,
 							domainModelName, GridNodeBean.NodeType.REMOTE);
-					logger.info("Adding to GridNodeBean: " + hostName + " : "
-							+ address);
-					gridNodeMap.put(hostName, gridNode);
-				}
-			}
-		}
-		return gridNodeMap;
-	}
-
-	/**
-	 * Return an array of GridNodeBean based on grid host names
-	 * 
-	 * @param gridNodeMap
-	 * @param gridNodeHosts
-	 * @return
-	 */
-	public static GridNodeBean[] getGridNodesFromHostNames(
-			Map<String, GridNodeBean> gridNodeMap, String[] gridNodeHosts) {
-		GridNodeBean[] gridNodes = null;
-		if (gridNodeHosts.length == 0) {
-			Collection<GridNodeBean> selectedGrids = gridNodeMap.values();
-			gridNodes = new GridNodeBean[selectedGrids.size()];
-			selectedGrids.toArray(gridNodes);
-		} else {
-			gridNodes = new GridNodeBean[gridNodeHosts.length];
-			int i = 0;
-			for (String host : gridNodeHosts) {
-				if (host != null) {
-					gridNodes[i] = gridNodeMap.get(host);
-					i++;
+					logger.info("Adding to GridNodeBean list: " + hostName
+							+ " : " + address);
+					gridNodes.add(gridNode);
 				}
 			}
 		}
 		return gridNodes;
 	}
+
+	public static GridNodeBean getGridNodeByURL(List<GridNodeBean> gridNodes,
+			String gridServiceURL) {
+		GridNodeBean theNode = null;
+		for (GridNodeBean node : gridNodes) {
+			if (node.getAddress().equals(gridServiceURL)) {
+				theNode = node;
+				break;
+			}
+		}
+		return theNode;
+	}
+
+	public static GridNodeBean getGridNodeByHostName(
+			List<GridNodeBean> gridNodes, String gridServiceHostName) {
+		GridNodeBean theNode = null;
+		for (GridNodeBean node : gridNodes) {
+			if (node.getHostName().equals(gridServiceHostName)) {
+				theNode = node;
+				break;
+			}
+		}
+		return theNode;
+	}
+
 }
