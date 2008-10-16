28c81088de8c4cd3ecfb5f68fa412d124de2c795
Sue Pan <sue.pan@nih.gov>
2008-10-16 14:18:39 +0000

    separated the execute into dispatch methods to improve performance and ease of maintenance
    SVN-Revision: 13377

--- gov/nih/nci/cananolab/ui/core/CountAction.java
+++ gov/nih/nci/cananolab/ui/core/CountAction.java
@@ -1,5 +1,6 @@
 package gov.nih.nci.cananolab.ui.core;
 
+import gov.nih.nci.cananolab.dto.common.UserBean;
 import gov.nih.nci.cananolab.service.particle.NanoparticleSampleService;
 import gov.nih.nci.cananolab.service.particle.impl.NanoparticleSampleServiceLocalImpl;
 import gov.nih.nci.cananolab.service.particle.impl.NanoparticleSampleServiceRemoteImpl;
@@ -15,16 +16,60 @@ import java.io.PrintWriter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.struts.action.Action;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 import org.apache.struts.action.ActionMessage;
 import org.apache.struts.action.ActionMessages;
 
-public class CountAction extends Action {
+/**
+ * Count the number of public protocols, particles and publications
+ * 
+ * @author cais, pansu
+ * 
+ */
+public class CountAction extends AbstractDispatchAction {
 
-	public ActionForward execute(ActionMapping mapping, ActionForm form,
+	public ActionForward countProtocols(ActionMapping mapping, ActionForm form,
+			HttpServletRequest request, HttpServletResponse response)
+			throws Exception {
+
+		String gridNodeHostStr = (String) request
+				.getParameter("searchLocations");
+		String[] searchLocations = new String[0];
+		if (gridNodeHostStr != null) {
+			searchLocations = gridNodeHostStr.split("~");
+		}
+
+		int protocolCount = 0;
+		ProtocolService protocolService = null;
+		for (String location : searchLocations) {
+			if (location.equals("local")) {
+				protocolService = new ProtocolServiceLocalImpl();
+			} else {
+				String serviceUrl = InitSetup.getInstance().getGridServiceUrl(
+						request, location);
+				protocolService = new ProtocolServiceRemoteImpl(serviceUrl);
+			}
+			try {
+				protocolCount += protocolService
+						.getNumberOfPublicProtocolFiles();
+			} catch (Exception ex) {
+				ex.printStackTrace();
+				ActionMessages msgs = new ActionMessages();
+				ActionMessage msg = new ActionMessage("error.protocolCount",
+						location);
+				msgs.add(ActionMessages.GLOBAL_MESSAGE, msg);
+				this.saveErrors(request, msgs);
+			}
+		}
+
+		PrintWriter out = response.getWriter();
+		out.print(protocolCount);
+		return null;
+	}
+
+	public ActionForward countParticles(ActionMapping mapping, ActionForm form,
 			HttpServletRequest request, HttpServletResponse response)
 			throws Exception {
 
@@ -50,7 +95,8 @@ public class CountAction extends Action {
 			} catch (Exception ex) {
 				ex.printStackTrace();
 				ActionMessages msgs = new ActionMessages();
-				ActionMessage msg = new ActionMessage("error.nanoparticleCount", location);
+				ActionMessage msg = new ActionMessage(
+						"error.nanoparticleCount", location);
 				msgs.add(ActionMessages.GLOBAL_MESSAGE, msg);
 				this.saveErrors(request, msgs);
 			}
@@ -65,45 +111,71 @@ public class CountAction extends Action {
 			} else {
 				String serviceUrl = InitSetup.getInstance().getGridServiceUrl(
 						request, location);
-				publicationService = new PublicationServiceRemoteImpl(serviceUrl);
+				publicationService = new PublicationServiceRemoteImpl(
+						serviceUrl);
 			}
 			try {
-				publicationCount += publicationService.getNumberOfPublicPublications();
+				publicationCount += publicationService
+						.getNumberOfPublicPublications();
 			} catch (Exception ex) {
 				ex.printStackTrace();
 				ActionMessages msgs = new ActionMessages();
-				ActionMessage msg = new ActionMessage("error.publicationCount", location);
-				msgs.add(ActionMessages.GLOBAL_MESSAGE, msg);
-				this.saveErrors(request, msgs);
-			}
-		}
-
-		// protocol count
-		int protocolCount = 0;
-		ProtocolService protocolService = null;
-		for (String location : searchLocations) {
-			if (location.equals("local")) {
-				protocolService = new ProtocolServiceLocalImpl();
-			} else {
-				String serviceUrl = InitSetup.getInstance().getGridServiceUrl(
-						request, location);
-				protocolService = new ProtocolServiceRemoteImpl(serviceUrl);
-			}
-
-			try {
-				protocolCount += protocolService
-						.getNumberOfPublicProtocolFiles();
-			} catch (Exception ex) {
-				ex.printStackTrace();
-				ActionMessages msgs = new ActionMessages();
-				ActionMessage msg = new ActionMessage("error.protocolCount", location);
+				ActionMessage msg = new ActionMessage("error.publicationCount",
+						location);
 				msgs.add(ActionMessages.GLOBAL_MESSAGE, msg);
 				this.saveErrors(request, msgs);
 			}
 		}
 
 		PrintWriter out = response.getWriter();
-		out.print(particleCount + "\t" + publicationCount + "\t" + protocolCount);
+		out.print(particleCount);
 		return null;
 	}
+
+	public ActionForward countPublications(ActionMapping mapping,
+			ActionForm form, HttpServletRequest request,
+			HttpServletResponse response) throws Exception {
+
+		String gridNodeHostStr = (String) request
+				.getParameter("searchLocations");
+		String[] searchLocations = new String[0];
+		if (gridNodeHostStr != null) {
+			searchLocations = gridNodeHostStr.split("~");
+		}
+		int publicationCount = 0;
+		PublicationService publicationService = null;
+		for (String location : searchLocations) {
+			if (location.equals("local")) {
+				publicationService = new PublicationServiceLocalImpl();
+			} else {
+				String serviceUrl = InitSetup.getInstance().getGridServiceUrl(
+						request, location);
+				publicationService = new PublicationServiceRemoteImpl(
+						serviceUrl);
+			}
+			try {
+				publicationCount += publicationService
+						.getNumberOfPublicPublications();
+			} catch (Exception ex) {
+				ex.printStackTrace();
+				ActionMessages msgs = new ActionMessages();
+				ActionMessage msg = new ActionMessage("error.publicationCount",
+						location);
+				msgs.add(ActionMessages.GLOBAL_MESSAGE, msg);
+				this.saveErrors(request, msgs);
+			}
+		}
+		PrintWriter out = response.getWriter();
+		out.print(publicationCount);
+		return null;
+	}
+
+	public boolean loginRequired() {
+		return false;
+	}
+
+	public boolean canUserExecute(UserBean user) {
+		return true;
+	}
+
 }
