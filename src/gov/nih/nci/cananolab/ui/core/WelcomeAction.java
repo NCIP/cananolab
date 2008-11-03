4b1bb30713f50bd181bbbf158d363a62ac4b4a4c
Sue Pan <sue.pan@nih.gov>
2008-11-03 21:45:59 +0000

    updated to extend ForwardAction
    SVN-Revision: 13622

--- src/gov/nih/nci/cananolab/ui/core/WelcomeAction.java
+++ src/gov/nih/nci/cananolab/ui/core/WelcomeAction.java
@@ -18,12 +18,8 @@ package gov.nih.nci.cananolab.ui.core;
 
 /* CVS $Id: WelcomeAction.java,v 1.1 2008-09-30 18:44:48 pansu Exp $ */
 
-import gov.nih.nci.cananolab.dto.common.GridNodeBean;
 import gov.nih.nci.cananolab.dto.common.UserBean;
 import gov.nih.nci.cananolab.exception.CaNanoLabSecurityException;
-import gov.nih.nci.cananolab.util.CaNanoLabConstants;
-
-import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -31,19 +27,16 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
-import org.apache.struts.action.ActionMessage;
-import org.apache.struts.action.ActionMessages;
 import org.apache.struts.actions.ForwardAction;
 
-public class WelcomeAction extends AbstractBaseAction {
-	public ActionForward executeTask(ActionMapping mapping, ActionForm form,
+public class WelcomeAction extends ForwardAction {
+	public ActionForward execute(ActionMapping mapping, ActionForm form,
 			HttpServletRequest request, HttpServletResponse response)
 			throws Exception {
 		saveToken(request); // save token to avoid back and refresh on the login
 		// page.
-		InitSetup.getInstance().getGridNodesInContext(request);
-		ForwardAction forwardAction = new ForwardAction();
-		return forwardAction.execute(mapping, form, request, response);
+		InitSetup.getInstance().getGridNodesInContext(request);		
+		return super.execute(mapping, form, request, response);
 	}
 
 	public boolean loginRequired() {
