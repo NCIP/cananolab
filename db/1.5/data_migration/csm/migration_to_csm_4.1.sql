35dd64e2d4508f0511f4dd93b94c391443a79c9b
Sue Pan <sue.pan@nih.gov>
2008-11-18 21:35:18 +0000

    remove unnecessary unique constraint statements
    SVN-Revision: 13710

--- db/1.5/data_migration/csm/migration_to_csm_4.1.sql
+++ db/1.5/data_migration/csm/migration_to_csm_4.1.sql
@@ -10,7 +10,6 @@ ALTER TABLE csm_protection_element ADD COLUMN protection_element_type VARCHAR(10
 ALTER TABLE csm_pg_pe MODIFY COLUMN update_date DATE;
 ALTER TABLE csm_application MODIFY COLUMN application_name VARCHAR(255) NOT NULL;
 ALTER TABLE csm_group MODIFY COLUMN group_name VARCHAR(255) NOT NULL;
-ALTER TABLE csm_protection_element ADD unique uq_pe_object_id_attribute_app_id(protection_element_name, attribute, application_id);
 
 ALTER TABLE csm_application ADD COLUMN database_url VARCHAR(100);
 ALTER TABLE csm_application ADD COLUMN database_user_name VARCHAR(100);
@@ -38,8 +37,6 @@ CREATE TABLE csm_filter_clause (
 ;
  
 ALTER TABLE csm_protection_element ADD ( attribute_value VARCHAR(100) );
-
-ALTER TABLE csm_protection_element DROP CONSTRAINT uq_pe_pe_name_attribute_app_id;
  
 ALTER TABLE csm_protection_element ADD CONSTRAINT uq_pe_pe_name_attribute_value_app_id unique (object_id, attribute, attribute_value, application_id);
 
