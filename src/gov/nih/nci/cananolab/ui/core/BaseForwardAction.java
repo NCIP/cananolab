c24f66fcbc5ab783af861340898fb28e0127d5f2
Sue Pan <sue.pan@nih.gov>
2008-11-20 19:22:13 +0000

    SVN-Revision: 13743

---
+++ src/gov/nih/nci/cananolab/ui/core/BaseForwardAction.java
@@ -0,0 +1,60 @@
+/*
+ The caLAB Software License, Version 0.5
+
+ Copyright 2006 SAIC. This software was developed in conjunction with the National 
+ Cancer Institute, and so to the extent government employees are co-authors, any 
+ rights in such works shall be subject to Title 17 of the United States Code, 
+ section 105.
+
+ */
+package gov.nih.nci.cananolab.ui.core;
+
+/**
+ * This class calls the Struts ForwardAction to forward to a page, aslo extends
+ * AbstractBaseAction to inherit the user authentication features.
+ * 
+ * @author pansu
+ */
+
+/* CVS $Id: BaseForwardAction.java,v 1.5 2008-09-23 21:53:24 tanq Exp $ */
+
+import gov.nih.nci.cananolab.dto.common.UserBean;
+import gov.nih.nci.cananolab.service.security.AuthorizationService;
+import gov.nih.nci.cananolab.util.CaNanoLabConstants;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+import org.apache.struts.actions.ForwardAction;
+
+public class BaseForwardAction extends ForwardAction {
+	public ActionForward execute(ActionMapping mapping, ActionForm form,
+			HttpServletRequest request, HttpServletResponse response)
+			throws Exception {
+		HttpSession session = request.getSession();
+
+		AuthorizationService authorizationService = new AuthorizationService(
+				CaNanoLabConstants.CSM_APP_NAME);
+		UserBean user = (UserBean) request.getSession().getAttribute("user");
+		
+		Boolean createProtocol = false;
+		Boolean createPublication = false;
+		Boolean createParticle = false;
+		if (user != null) {
+			createProtocol = authorizationService.checkCreatePermission(user,
+					CaNanoLabConstants.CSM_PG_PROTOCOL);
+			createPublication = authorizationService.checkCreatePermission(
+					user, CaNanoLabConstants.CSM_PG_PUBLICATION);
+			createParticle = authorizationService.checkCreatePermission(user,
+					CaNanoLabConstants.CSM_PG_PARTICLE);
+		}
+		session.setAttribute("canCreateProtocol", createProtocol);
+		session.setAttribute("canCreatePublication", createPublication);
+		session.setAttribute("canCreateNanoparticle", createParticle);
+		return super.execute(mapping, form, request, response);
+	}
+}
---
+++ src/test/TestConnectionPool.java
@@ -0,0 +1,51 @@
+package test;
+
+import gov.nih.nci.cananolab.dto.common.PublicationBean;
+import gov.nih.nci.cananolab.dto.common.UserBean;
+import gov.nih.nci.cananolab.service.publication.PublicationService;
+import gov.nih.nci.cananolab.service.publication.impl.PublicationServiceLocalImpl;
+import gov.nih.nci.cananolab.service.security.AuthorizationService;
+import gov.nih.nci.cananolab.util.CaNanoLabConstants;
+
+import java.util.List;
+
+import org.apache.log4j.Logger;
+
+public class TestConnectionPool {
+	private static Logger logger = Logger.getLogger(TestConnectionPool.class);
+
+	public void testPublicationService(String publicationType) {
+		try {
+			PublicationService service = new PublicationServiceLocalImpl();
+			List<PublicationBean> publications = service.findPublicationsBy(
+					null, publicationType, null, null, null, null, null, null,
+					null, null, null, null, null, null);
+			for (PublicationBean publication : publications) {
+				System.out.println("Publication: "
+						+ publication.getDisplayName());
+				logger.info("Publication: " + publication.getDisplayName());
+			}
+		} catch (Exception e) {
+			logger.error(e);
+		}
+	}
+
+	public void testCSM() {
+		try {
+			AuthorizationService service = new AuthorizationService(
+					CaNanoLabConstants.CSM_APP_NAME);			
+			List<UserBean> users = service.getAllUsers();
+			for (UserBean user:users) {
+				System.out.println("USer: "+user.getFullName());
+			}
+		} catch (Exception e) {
+			logger.error(e);
+		}
+	}
+
+	public static void main(String[] args) {
+		TestConnectionPool test = new TestConnectionPool();
+		test.testPublicationService("peer review article");
+		test.testCSM();
+	}
+}
