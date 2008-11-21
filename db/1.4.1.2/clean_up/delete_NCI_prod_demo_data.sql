27bee9cb0298b08aa7d09aea5615590e46a73099
Sue Pan <sue.pan@nih.gov>
2008-11-21 19:59:59 +0000

    first deposit
    SVN-Revision: 13773

---
+++ db/1.4.1.2/clean_up/delete_NCI_prod_demo_data.sql
@@ -0,0 +1,70 @@
+use canano;
+
+delete from csm_protection_element
+where protection_element_name in
+(select particle_sample_name
+from nanoparticle_sample
+where created_by='janedoe');
+
+delete from csm_protection_element
+where protection_element_name in
+(select file_pk_id
+from lab_file
+where created_by='janedoe');
+
+delete from csm_protection_group
+where protection_group_name in
+(select particle_sample_name
+from nanoparticle_sample
+where created_by='janedoe');
+
+delete from csm_protection_group
+where protection_group_name in
+(select file_pk_id
+from lab_file
+where created_by='janedoe');
+
+delete from csm_user_group_role_pg
+where protection_group_id not in
+(select protection_group_id
+from csm_protection_group);
+
+delete from csm_pg_pe
+where protection_group_id not in
+(select protection_group_id
+from csm_protection_group);
+
+delete from csm_group 
+where group_name in ('CCNE', 'Demo');
+
+delete from csm_user_group
+where group_id not in
+(select group_id
+from csm_group);
+
+delete from source
+where organization_name in ('CCNE', 'Demo');
+
+delete from nanoparticle_sample
+where created_by='janedoe';
+
+delete from lab_file
+where created_by='janedoe';
+
+delete from publication
+where publication_pk_id not in
+(select file_pk_id
+from lab_file);
+
+delete from nanoparticle_sample_publication
+where particle_sample_pk_id not in
+(select particle_sample_pk_id
+from nanoparticle_sample);
+
+delete from nanoparticle_sample_publication 
+where file_pk_id not in
+(select publication_pk_id
+from publication);
+
+commit;
+
