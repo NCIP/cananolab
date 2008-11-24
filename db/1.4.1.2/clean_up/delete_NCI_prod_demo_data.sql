868350dc7f5e4862e538f9eb7d266c16d1ffbb53
Sue Pan <sue.pan@nih.gov>
2008-11-24 17:54:49 +0000

    correct delete order
    SVN-Revision: 13780

--- db/1.4.1.2/clean_up/delete_NCI_prod_demo_data.sql
+++ db/1.4.1.2/clean_up/delete_NCI_prod_demo_data.sql
@@ -1,79 +1,51 @@
 use canano;
 
-delete from csm_protection_element
-where protection_element_name in
-(select particle_sample_name
+delete from composition
+where particle_sample_pk_id in
+(select particle_sample_pk_id
 from nanoparticle_sample
 where created_by='janedoe');
 
-delete from csm_protection_element
-where protection_element_name in
+delete from keyword_lab_file
+where file_pk_id in
+(select file_pk_id 
+from lab_file
+where created_by='janedoe');
+
+delete from nanoparticle_sample_publication
+where particle_sample_pk_id in
+(select particle_sample_pk_id
+from nanoparticle_sample
+where created_by='janedoe');
+
+delete from nanoparticle_sample_publication 
+where file_pk_id in
 (select file_pk_id
 from lab_file
 where created_by='janedoe');
 
-delete from csm_protection_group
-where protection_group_name in
-(select particle_sample_name
-from nanoparticle_sample
-where created_by='janedoe');
-
-delete from csm_protection_group
-where protection_group_name in
+delete from author_publication
+where publication_pk_id in
 (select file_pk_id
 from lab_file
 where created_by='janedoe');
 
-delete from csm_user_group_role_pg
-where protection_group_id not in
-(select protection_group_id
-from csm_protection_group);
-
-delete from csm_pg_pe
-where protection_group_id not in
-(select protection_group_id
-from csm_protection_group);
-
-delete from csm_group 
-where group_name in ('CCNE', 'Demo');
-
-delete from csm_user_group
-where group_id not in
-(select group_id
-from csm_group);
-
-delete from source
-where organization_name in ('CCNE', 'Demo');
-
-delete from nanoparticle_sample
-where created_by='janedoe';
+delete from publication
+where publication_pk_id in
+(select file_pk_id
+from lab_file
+where created_by='janedoe');
 
 delete from lab_file
 where created_by='janedoe';
 
-delete from publication
-where publication_pk_id not in
-(select file_pk_id
-from lab_file);
-
-delete from nanoparticle_sample_publication
-where particle_sample_pk_id not in
-(select particle_sample_pk_id
-from nanoparticle_sample);
-
-delete from nanoparticle_sample_publication 
-where file_pk_id not in
-(select publication_pk_id
-from publication);
-
 delete from author
-where author_pk_id not in
-(select author_pk_id
-from author_publication);
+where created_by='janedoe';
 
-delete from author_publication
-where publication_pk_id not in
-(select file_pk_id
-from lab_file);
+delete from nanoparticle_sample
+where created_by='janedoe';
+
+delete from source
+where organization_name in ('CCNE', 'Demo');
 
 commit;
