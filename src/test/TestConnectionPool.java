483e9cf8538139a4a3f14855e5275ea9025dcae3
Qina Tan <tanq>
2008-12-03 21:41:16 +0000

    add testPublicationURL to verify all the URL
    SVN-Revision: 13841

--- src/test/TestConnectionPool.java
+++ src/test/TestConnectionPool.java
@@ -7,6 +7,11 @@ import gov.nih.nci.cananolab.service.publication.impl.PublicationServiceLocalImp
 import gov.nih.nci.cananolab.service.security.LoginService;
 import gov.nih.nci.cananolab.util.CaNanoLabConstants;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.List;
 
 import org.apache.log4j.Logger;
@@ -29,6 +34,46 @@ public class TestConnectionPool {
 			logger.error(e);
 		}
 	}
+	
+	public void testPublicationURLService(String publicationType) {
+		PrintStream p = null;
+		try {
+			p = new PrintStream("C:\\temp\\publication\\urlLog.txt");
+			PublicationService service = new PublicationServiceLocalImpl();
+			List<PublicationBean> publications = service.findPublicationsBy(
+					null, publicationType, null, null, null, null, null, null,
+					null, null, null, null, null, null);
+			for (PublicationBean publication : publications) {				
+				//System.out.println("Publication: "
+				//		+ publication.getDisplayName());
+				//logger.info("Publication: " + publication.getDisplayName());
+				if (publication.getDomainFile().getUri()!=null && 
+						publication.getDomainFile().getUri().trim().length()>0) {
+					p.println(publication.getDomainFile().getUri());
+					URL yahoo = new URL(publication.getDomainFile().getUri());
+			        URLConnection yc = yahoo.openConnection();
+			        BufferedReader in = new BufferedReader(
+			                                new InputStreamReader(
+			                                yc.getInputStream()));
+			        String inputLine;
+			        while ((inputLine = in.readLine()) != null) {
+			           if (inputLine.indexOf("Page Not Found")!=-1) {
+			        	   p.println("ERROR NOT FOUND:"+publication.getDomainFile().getUri());
+			           }
+			        }
+			        in.close();					
+				}				
+			}		
+			
+		} catch (Exception e) {
+			logger.error(e);
+			p.println("EXCEPTION ERROR NOT FOUND:");
+		}finally {
+			if (p!=null) {
+				p.close();
+			}
+		}
+	}
 
 	public void testCSM() {
 		try {
@@ -46,7 +91,10 @@ public class TestConnectionPool {
 	public static void main(String[] args) {
 		TestConnectionPool test = new TestConnectionPool();
 		test.testPublicationService("peer review article");
-		test.testCSM();
+		//test.testCSM();
+		//test.testPublicationURLService("peer review article");	
+		System.out.println("COMPLETED");
+	    
 		System.exit(0);
 	}
 }
