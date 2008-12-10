3f9b8c0984ad414a9a796027555a6d5a9ca0f509
Shuang Cai <cais>
2008-12-10 20:20:42 +0000

    change organizationManager.js to POCManager.js
    SVN-Revision: 13917

---
+++ web/javascript/POCManager.js
@@ -0,0 +1,32 @@
+
+function setupPOC(form, selectEleId) {
+	var selectEle = document.getElementById(selectEleId);
+	var otext = selectEle.options[selectEle.options.selectedIndex].text;
+	if(otext == "[Other]") {
+		form.action = "submitNanoparticleSample.do?dispatch=setupPointOfContact&page=0&location=local";
+		form.submit();
+	}
+	return false;
+}
+
+function setupOrgDetailView(form, selectEleId) {
+	var selectEle = document.getElementById(selectEleId);
+	var org = selectEle.options[selectEle.options.selectedIndex].text;
+	if(org != "[Other]") {
+		form.action = "submitNanoparticleSample.do?dispatch=pointOfContactDetailView&page=0&location=local" + 
+			"&organizationName=" + org;
+		form.submit();
+	}
+	return false;
+}
+
+function setPOCDetailLink(selectEleId, linkId) {
+	var selectEle = document.getElementById(selectEleId);
+	var linkEle = document.getElementById(linkId);
+	var org = selectEle.options[selectEle.options.selectedIndex].text;
+	if(org != "[Other]" && org != "") {
+		linkEle.style.display = "inline";
+	} else {
+		linkEle.style.display = "none";	
+	}
+}
