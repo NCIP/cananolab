eeb009d6d59bfbaf9c3c26bc1424d95f01e56197
Qina Tan <tanq>
2009-01-12 18:34:36 +0000

    add technique and instrument when a new experiemntConfigBean is created
    SVN-Revision: 14208

--- src/gov/nih/nci/cananolab/dto/common/ExperimentConfigBean.java
+++ src/gov/nih/nci/cananolab/dto/common/ExperimentConfigBean.java
@@ -1,6 +1,11 @@
 package gov.nih.nci.cananolab.dto.common;
 
 import gov.nih.nci.cananolab.domain.common.ExperimentConfig;
+import gov.nih.nci.cananolab.domain.common.Instrument;
+import gov.nih.nci.cananolab.domain.common.Technique;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * View bean for technique and associated instruments;
@@ -11,9 +16,16 @@ import gov.nih.nci.cananolab.domain.common.ExperimentConfig;
 public class ExperimentConfigBean {
 	private ExperimentConfig domain;
 	private String displayName;
+	private List<Instrument> instruments = new ArrayList<Instrument>(20);
+	
 
 	public ExperimentConfigBean() {
 		super();
+		//TODO::  add by Qina temporary (to test)
+		domain = new ExperimentConfig();
+		domain.setTechnique(new Technique());
+		instruments.add(new Instrument());
+		domain.setInstrumentCollection(instruments);
 	}
 
 	public ExperimentConfigBean(ExperimentConfig config) {
@@ -37,4 +49,27 @@ public class ExperimentConfigBean {
 		}
 		return displayName;
 	}
+	
+	public void addInstrument() {
+		instruments.add(new Instrument());
+	}
+
+	public void removeInstrument(int ind) {
+		instruments.remove(ind);
+	}
+
+	/**
+	 * @return the instruments
+	 */
+	public List<Instrument> getInstruments() {
+		return instruments;
+	}
+
+	/**
+	 * @param instruments the instruments to set
+	 */
+	public void setInstruments(List<Instrument> instruments) {
+		this.instruments = instruments;
+	}
+
 }
