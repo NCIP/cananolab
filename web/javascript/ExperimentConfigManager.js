1167763b3ec6b07c3f723c875f8c0a0a8eb612d6
Qina Tan <tanq>
2009-01-13 20:14:24 +0000

    add resetTheExperimentConfig()
    SVN-Revision: 14267

--- web/javascript/ExperimentConfigManager.js
+++ web/javascript/ExperimentConfigManager.js
@@ -30,17 +30,33 @@ function setTheExperimentConfig(configId) {
 	show('newExperimentConfig');
 	ExperimentConfigManager.findExperimentConfigById(configId, populateExperimentConfig);
 }
+
 function populateExperimentConfig(experimentConfig) {
 	if (experimentConfig != null) {
-		ExperimentConfigManager.findInstrumentTypesByTechniqueType(experimentConfig.technique.type,updateInstrumentDropDown);
 		dwr.util.setValue("techniqueType", experimentConfig.technique.type);
 		dwr.util.setValue("techniqueAbbr", experimentConfig.technique.abbreviation);
 		dwr.util.setValue("configDescription", experimentConfig.description);
-		for (var i = 0; i < experimentConfig.instrumentCollection.length; i++) {			
+		ExperimentConfigManager.findInstrumentTypesByTechniqueType(experimentConfig.technique.type,updateInstrumentDropDown);
+		for (var i = 0; i < experimentConfig.instrumentCollection.length; i++) {	
 			dwr.util.setValue("instrumentType" + i, experimentConfig.instrumentCollection[i].type);
 			dwr.util.setValue("instrumentManufacturer" + i, experimentConfig.instrumentCollection[i].manufacturer);
 			dwr.util.setValue("instrumentModelName" + i, experimentConfig.instrumentCollection[i].modelName);
-		}			
+		}	
+	}	
+}
+
+function resetTheExperimentConfig() {
+	show('newExperimentConfig');
+	dwr.util.setValue("techniqueType", "");
+	dwr.util.setValue("techniqueAbbr", "");
+	dwr.util.setValue("configDescription", "");
+	for ( var i = 0; i < 1; i++) {
+		var id = "instrumentType" + i;
+		dwr.util.removeAllOptions(id);
+		dwr.util.addOptions(id, [""]);
+		dwr.util.addOptions(id, ["[Other]"]);
+		dwr.util.setValue("instrumentManufacturer" + i, "");
+		dwr.util.setValue("instrumentModelName" + i, "");
 	}
 }
 
