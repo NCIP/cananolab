664e6d3c635bf6d47af61d1e1bc427cbf491e468
Qina Tan <tanq>
2009-01-14 14:48:37 +0000

    fix issue that 2 dwr request async, add setTimeout
    SVN-Revision: 14285

--- web/javascript/ExperimentConfigManager.js
+++ web/javascript/ExperimentConfigManager.js
@@ -15,9 +15,9 @@ function updateTechniqueAbbreviation(technique) {
 }
 
 function updateInstrumentDropDown(instrumentTypes) {
-	for (var i = 0; i < instrumentCount; i++) {
+	for (var i = 0; i < instrumentCount; i++) {	
 		var id = "instrumentType" + i;
-		var selectedValue = dwr.util.getValue(id);
+		var selectedValue = dwr.util.getValue(id);		
 		dwr.util.removeAllOptions(id);
 		dwr.util.addOptions(id, [""]);
 		dwr.util.addOptions(id, instrumentTypes);
@@ -25,26 +25,39 @@ function updateInstrumentDropDown(instrumentTypes) {
 		dwr.util.setValue(id, selectedValue);
 	}
 }
+var thisConfigId = 0;
 function setTheExperimentConfig(configId) {
 	show('newExperimentConfig');
-	ExperimentConfigManager.findExperimentConfigById(configId, populateExperimentConfig);
+	ExperimentConfigManager.findInstrumentTypesByConfigId(configId,updateInstrumentDropDown);
+	thisConfigId = configId;
+	window.setTimeout("doSetTheExperimentConfig()", 300);
 }
 
+function doSetTheExperimentConfig() {
+	ExperimentConfigManager.findExperimentConfigById(thisConfigId, populateExperimentConfig);
+}
+
+
 var instrumentCount = 1;
+function populateExperimentConfig(experimentConfig) {
+	if (experimentConfig != null) {
+		ExperimentConfigManager.findInstrumentTypesByTechniqueType(experimentConfig.technique.type,updateInstrumentDropDown);		
+	}	
+}
+
 function populateExperimentConfig(experimentConfig) {
 	if (experimentConfig != null) {
 		dwr.util.setValue("techniqueType", experimentConfig.technique.type);
 		dwr.util.setValue("techniqueAbbr", experimentConfig.technique.abbreviation);
 		dwr.util.setValue("configDescription", experimentConfig.description);
-		dwr.util.setValue("configId", experimentConfig.id);
-		ExperimentConfigManager.findInstrumentTypesByTechniqueType(experimentConfig.technique.type,updateInstrumentDropDown);
+		dwr.util.setValue("configId", experimentConfig.id);		
 		instrumentCount = experimentConfig.instrumentCollection.length;
 		for (var i = 0; i < experimentConfig.instrumentCollection.length; i++) {
 			dwr.util.setValue("instrumentType" + i, experimentConfig.instrumentCollection[i].type);
 			dwr.util.setValue("instrumentManufacturer" + i, experimentConfig.instrumentCollection[i].manufacturer);
 			dwr.util.setValue("instrumentModelName" + i, experimentConfig.instrumentCollection[i].modelName);
-		}
-	}
+		}	
+	}	
 }
 
 function resetTheExperimentConfig() {
