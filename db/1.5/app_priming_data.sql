5c390eccd9b9bb7adcb537eaf1b9a2de8eaec424
Sue Pan <sue.pan@nih.gov>
2009-01-30 18:39:02 +0000

    added instruments
    SVN-Revision: 14405

--- db/1.5/app_priming_data.sql
+++ db/1.5/app_priming_data.sql
@@ -6,29 +6,60 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 INSERT INTO hibernate_unique_key(next_hi)
 VALUES (1);
 
-INSERT INTO instrument(instrument_pk_id,type,abbreviation,manufacturer) 
-values (1,'Asymmetrical Flow Field-Flow Fractionation with Multi-Angle Light Scattering','AFFF-MALLS','Wyatt'),
- (2,'Atomic Force Microscope','AFM','Molecular Imaging'),
- (3,'Capillary Electrophoresis',null,'NA'),
- (4,'Clot Detection System',null,'Diagnostica'),
- (5,'Coulter Counter',null,'Beckman/Coulter'),
- (6,'Dynamic Light Scattering','DLS','Malvern'),
- (7,'Dynamic Light Scattering','DLS','Wyatt Technologies'),
- (8,'Energy Dispersive Spectroscopy','EDS','EDAX'),
- (9,'Flow Cytometer',null,'Becton Dickinson'),
- (10,'Hemocytometer',null,'Unknown'),
- (11,'High Performance Liquid Chromatography','HPLC','Agilent'),
- (12,'High Performance Liquid Chromatography','HPLC','Shimadzu'),
- (13,'Imaging System',null,'Kodak'),
- (14,'Liquid Chromatography','LC','Amersham'),
- (15,'Refractometer',null,'Waters'),
- (16,'Scintillation Counter',null,'Beckman/Coulter'),
- (17,'Size Exclusion Chromatography with Multi-Angle Light Scattering','SEC-MALLS','Wyatt'),
- (18,'Spectrophotometer',null,'Molecular Devices'),
- (19,'Spectrophotometer',null,'Tecan'),
- (20,'Spectrophotometer',null,'Thermo Electron'),
- (21,'Thermal Cycler',null,'Biorad'),
- (22,'Zetasizer',null,'Malvern');
+ALTER TABLE instrument
+ CHANGE instrument_pk_id instrument_pk_id BIGINT AUTO_INCREMENT NOT NULL;
+
+INSERT into instrument (type,manufacturer,created_date,created_by) values ('Asymmetrical Flow Field-Flow Fractionation with Multi-Angle Light Scattering Instrument',null,sysdate(),'SEED_DATA');
+INSERT into instrument (type,manufacturer,created_date,created_by) values ('Atomic Force Microscope','Molecular Imaging',sysdate(),'SEED_DATA');
+INSERT into instrument (type,manufacturer,created_date,created_by) values ('Capillary Electrophoresis Instrument',null,sysdate(),'SEED_DATA');
+INSERT into instrument (type,manufacturer,created_date,created_by) values ('Hemocytometer',null,sysdate(),'SEED_DATA');
+INSERT into instrument (type,manufacturer,created_date,created_by) values ('Coulter Counter','Beckman/Coulter',sysdate(),'SEED_DATA');
+INSERT into instrument (type,manufacturer,created_date,created_by) values ('Dynamic Light Scattering Instrument','Brookhaven Instruments',sysdate(),'SEED_DATA');
+INSERT into instrument (type,manufacturer,created_date,created_by) values ('Dynamic Light Scattering Instrument','Malvern',sysdate(),'SEED_DATA');
+INSERT into instrument (type,manufacturer,created_date,created_by) values ('Dynamic Light Scattering Instrument','Molecular Devices',sysdate(),'SEED_DATA');
+INSERT into instrument (type,manufacturer,created_date,created_by) values ('Dynamic Light Scattering Instrument','Wyatt Technologies',sysdate(),'SEED_DATA');
+INSERT into instrument (type,manufacturer,created_date,created_by) values ('Electron Microprobe',null,sysdate(),'SEED_DATA');
+INSERT into instrument (type,manufacturer,created_date,created_by) values ('Electrophoretic Light Scattering Instrument','Brookhaven Instruments',sysdate(),'SEED_DATA');
+INSERT into instrument (type,manufacturer,created_date,created_by) values ('Electrophoretic Light Scattering Instrument','Malvern',sysdate(),'SEED_DATA');
+INSERT into instrument (type,manufacturer,created_date,created_by) values ('Coagulation Monitor',null,sysdate(),'SEED_DATA');
+INSERT into instrument (type,manufacturer,created_date,created_by) values ('Energy Dispersive Spectrometer','EDAX',sysdate(),'SEED_DATA');
+INSERT into instrument (type,manufacturer,created_date,created_by) values ('Environmental Transmission Electron Microscope','JEOL',sysdate(),'SEED_DATA');
+INSERT into instrument (type,manufacturer,created_date,created_by) values ('Environmental Transmission Electron Microscope','Philips',sysdate(),'SEED_DATA');
+INSERT into instrument (type,manufacturer,created_date,created_by) values ('Coagulation Monitor','Diagnostica Stago',sysdate(),'SEED_DATA');
+INSERT into instrument (type,manufacturer,created_date,created_by) values ('Flow Cytometer','Becton Dickinson',sysdate(),'SEED_DATA');
+INSERT into instrument (type,manufacturer,created_date,created_by) values ('Surface Area and Pore Size Analyzer','Quantachrome Instruments',sysdate(),'SEED_DATA');
+INSERT into instrument (type,manufacturer,created_date,created_by) values ('Scanning Electron Microscope','Hitachi',sysdate(),'SEED_DATA');
+INSERT into instrument (type,manufacturer,created_date,created_by) values ('Scanning Electron Microscope','Brookhaven Instruments',sysdate(),'SEED_DATA');
+INSERT into instrument (type,manufacturer,created_date,created_by) values ('High Performance Liquid Chromatography System','Agilent',sysdate(),'SEED_DATA');
+INSERT into instrument (type,manufacturer,created_date,created_by) values ('High Performance Liquid Chromatography System','Shimadzu',sysdate(),'SEED_DATA');
+INSERT into instrument (type,manufacturer,created_date,created_by) values ('Transmission Electron Microscope',null,sysdate(),'SEED_DATA');
+INSERT into instrument (type,manufacturer,created_date,created_by) values ('Imaging System','Kodak',sysdate(),'SEED_DATA');
+INSERT into instrument (type,manufacturer,created_date,created_by) values ('Laser Doppler Velocimeter',null,sysdate(),'SEED_DATA');
+INSERT into instrument (type,manufacturer,created_date,created_by) values ('Liquid Chromatography System','Amersham',sysdate(),'SEED_DATA');
+INSERT into instrument (type,manufacturer,created_date,created_by) values ('Hemocytometer',null,sysdate(),'SEED_DATA');
+INSERT into instrument (type,manufacturer,created_date,created_by) values ('Coulter Counter',null,sysdate(),'SEED_DATA');
+INSERT into instrument (type,manufacturer,created_date,created_by) values ('Phase Light Scattering Instrument','Brookhaven Instruments',sysdate(),'SEED_DATA');
+INSERT into instrument (type,manufacturer,created_date,created_by) values ('Phase Light Scattering Instrument','Malvern',sysdate(),'SEED_DATA');
+INSERT into instrument (type,manufacturer,created_date,created_by) values ('Thermal Cycler','Biorad',sysdate(),'SEED_DATA');
+INSERT into instrument (type,manufacturer,created_date,created_by) values ('Powder Diffractometer',null,sysdate(),'SEED_DATA');
+INSERT into instrument (type,manufacturer,created_date,created_by) values ('Scintillation Counter','Beckman/Coulter',sysdate(),'SEED_DATA');
+INSERT into instrument (type,manufacturer,created_date,created_by) values ('Refractometer','Waters',sysdate(),'SEED_DATA');
+INSERT into instrument (type,manufacturer,created_date,created_by) values ('Scanning Auger Spectrometer',null,sysdate(),'SEED_DATA');
+INSERT into instrument (type,manufacturer,created_date,created_by) values ('Scanning Electron Microscope',null,sysdate(),'SEED_DATA');
+INSERT into instrument (type,manufacturer,created_date,created_by) values ('Scanning Probe Microscope',null,sysdate(),'SEED_DATA');
+INSERT into instrument (type,manufacturer,created_date,created_by) values ('Scanning Tunneling Microscope',null,sysdate(),'SEED_DATA');
+INSERT into instrument (type,manufacturer,created_date,created_by) values ('Size Exclusion Chromatography with Multi-Angle Light Scattering Instrument','Wyatt Technologies',sysdate(),'SEED_DATA');
+INSERT into instrument (type,manufacturer,created_date,created_by) values ('Spectrophotometer','Thermo Electron',sysdate(),'SEED_DATA');
+INSERT into instrument (type,manufacturer,created_date,created_by) values ('Spectrophotometer','Tecan',sysdate(),'SEED_DATA');
+INSERT into instrument (type,manufacturer,created_date,created_by) values ('Spectrophotometer','Molecular Devices',sysdate(),'SEED_DATA');
+INSERT into instrument (type,manufacturer,created_date,created_by) values ('X-Ray Photoelectron Spectroscope',null,sysdate(),'SEED_DATA');
+INSERT into instrument (type,manufacturer,created_date,created_by) values ('Microplate Reader','Biorad',sysdate(),'SEED_DATA');
+INSERT into instrument (type,manufacturer,created_date,created_by) values ('Matrix Assisted Laser Desorption Ionisation - Time Of Flight Mass Spectrometer','Waters',sysdate(),'SEED_DATA');
+INSERT into instrument (type,manufacturer,created_date,created_by) values ('Confocal Microscope','Carl Zeiss',sysdate(),'SEED_DATA');
+
+
+ALTER TABLE canano.instrument
+ CHANGE instrument_pk_id instrument_pk_id BIGINT NOT NULL;
 
 -- Re-enable foreign key checks
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
