7d265f1ffdbd373695b6847b988fc7a451bccda5
Qina Tan <tanq>
2009-02-19 18:26:01 +0000

    update summaryView
    SVN-Revision: 14567

--- src/gov/nih/nci/cananolab/ui/particle/CompositionAction.java
+++ src/gov/nih/nci/cananolab/ui/particle/CompositionAction.java
@@ -1,18 +1,44 @@
 package gov.nih.nci.cananolab.ui.particle;
 
+import gov.nih.nci.cananolab.dto.common.UserBean;
+import gov.nih.nci.cananolab.dto.particle.composition.CompositionBean;
+import gov.nih.nci.cananolab.dto.particle.composition.NanoparticleEntityBean;
+import gov.nih.nci.cananolab.service.particle.NanoparticleCompositionService;
+import gov.nih.nci.cananolab.service.particle.impl.NanoparticleCompositionServiceLocalImpl;
+import gov.nih.nci.cananolab.service.particle.impl.NanoparticleCompositionServiceRemoteImpl;
+import gov.nih.nci.cananolab.ui.core.BaseAnnotationAction;
+import gov.nih.nci.cananolab.ui.core.InitSetup;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
-
-import gov.nih.nci.cananolab.ui.core.BaseAnnotationAction;
+import org.apache.struts.validator.DynaValidatorForm;
 
 public class CompositionAction extends BaseAnnotationAction {
 	public ActionForward summaryView(ActionMapping mapping, ActionForm form,
 			HttpServletRequest request, HttpServletResponse response)
 			throws Exception {
+		DynaValidatorForm theForm = (DynaValidatorForm) form;
+		String location = request.getParameter("location");
+		//setupParticle(theForm, request, location);
+		HttpSession session = request.getSession();
+		UserBean user = (UserBean) session.getAttribute("user");
+		String particleId = request.getParameter("particleId");
+		NanoparticleCompositionService compService = null;
+		if (location.equals("local")) {
+			compService = new NanoparticleCompositionServiceLocalImpl();
+		} else {
+			String serviceUrl = InitSetup.getInstance().getGridServiceUrl(
+					request, location);
+			compService = new NanoparticleCompositionServiceRemoteImpl(
+					serviceUrl);
+		}
+		CompositionBean compositionBean = compService.findCompositionByParticleSampleId(particleId);
+		theForm.set("comp", compositionBean);		
 		return mapping.findForward("summaryView");
 	}
 
