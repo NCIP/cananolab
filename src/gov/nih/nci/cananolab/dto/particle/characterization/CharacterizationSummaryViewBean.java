ff4b0eb29a5f0defaaabc11a686304e06d13da2f
Sue Pan <sue.pan@nih.gov>
2009-02-19 22:35:46 +0000

    new for release 1.5
    SVN-Revision: 14580

---
+++ src/gov/nih/nci/cananolab/dto/particle/characterization/CharacterizationSummaryViewBean.java
@@ -0,0 +1,35 @@
+package gov.nih.nci.cananolab.dto.particle.characterization;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+public class CharacterizationSummaryViewBean {
+	private Set<String> characterizationTypes;
+	private SortedMap<String, List<CharacterizationBean>> type2Characterizations = new TreeMap<String, List<CharacterizationBean>>();
+
+	public CharacterizationSummaryViewBean(List<CharacterizationBean> chars) {
+		List<CharacterizationBean> typeChars = null;
+		for (CharacterizationBean achar : chars) {
+			String type = achar.getCharacterizationType();
+			if (type2Characterizations.get(type) != null) {
+				typeChars = type2Characterizations.get(type);
+			} else {
+				typeChars = new ArrayList<CharacterizationBean>();
+				type2Characterizations.put(type, typeChars);
+			}
+			typeChars.add(achar);
+		}
+		characterizationTypes = type2Characterizations.keySet();
+	}
+
+	public Set<String> getCharacterizationTypes() {
+		return characterizationTypes;
+	}
+
+	public SortedMap<String, List<CharacterizationBean>> getType2Characterizations() {
+		return type2Characterizations;
+	}
+}
