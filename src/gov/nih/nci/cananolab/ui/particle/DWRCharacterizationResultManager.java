69d724b01a401ca9829ef2fc9a9ac945b29fd2d9
Qina Tan <tanq>
2009-02-24 19:24:28 +0000

    add deleteRow()
    SVN-Revision: 14621

--- src/gov/nih/nci/cananolab/ui/particle/DWRCharacterizationResultManager.java
+++ src/gov/nih/nci/cananolab/ui/particle/DWRCharacterizationResultManager.java
@@ -15,11 +15,12 @@ import org.directwebremoting.WebContextFactory;
 
 /**
  * Methods for DWR Ajax
- *
+ * 
  * @author pansu, tanq
- *
+ * 
  */
 public class DWRCharacterizationResultManager {
+	long tempDataRowId = -1;
 	private NanoparticleCharacterizationResultService service = new NanoparticleCharacterizationResultServiceLocalImpl();
 
 	public DataSetBean resetDataSet() {
@@ -33,74 +34,100 @@ public class DWRCharacterizationResultManager {
 	}
 
 	// addColumnHeader includes the function add and edit
-	public DataSetBean addColumnHeader(Datum datum) throws CharacterizationResultException {
-		DynaValidatorForm charForm = (DynaValidatorForm) (WebContextFactory
-				.get().getSession().getAttribute("characterizationForm"));
-		CharacterizationBean charBean = (CharacterizationBean) (charForm
-				.get("achar"));
-		//System.out.println("xxxxxxxxx datum.id="+datum.getId());
-		DataSetBean theDataSet = charBean.getTheDataSet();
-		theDataSet.getTheDataRow().addDatumColumn(datum);
-		return theDataSet;
-	}
-
-	// addRow includes the function add and edit
-	public DataSetBean addRow(List<Datum> data) throws CharacterizationResultException {
-		DynaValidatorForm charForm = (DynaValidatorForm) (WebContextFactory
-				.get().getSession().getAttribute("characterizationForm"));
-		CharacterizationBean charBean = (CharacterizationBean) (charForm
-				.get("achar"));
-		DataSetBean theDataSet = charBean.getTheDataSet();
-		if (data != null) {
-			//TODO:: if rowid!=null, get the row then update
-			//DataRowBean dataRowBean = theDataSet.getTheDataRow();
-			DataRowBean dataRowBean = null;
-			boolean hasData = false;
-			int i=0;
-			for (Datum datum : data) {
-				if (theDataSet.getDomain().getId()!=null) {
-					datum.setDataSet(theDataSet.getDomain());
-				}
-				if (i==0) {
-					dataRowBean = theDataSet.getDataRowBean(datum);
-					if (dataRowBean == null){
-						dataRowBean = new DataRowBean();
-					}
-				}
-
-				dataRowBean.addDatum(datum);
-				hasData = true;
-				i++;
-			}
-			if (hasData) {
-				theDataSet.addDataRow(dataRowBean);
-			}
-		}
-		return theDataSet;
-	}
-
-	public DataSetBean deleteDataRow(DataRowBean dataRowBean)
+	public DataSetBean addColumnHeader(Datum datum)
 			throws CharacterizationResultException {
 		DynaValidatorForm charForm = (DynaValidatorForm) (WebContextFactory
 				.get().getSession().getAttribute("characterizationForm"));
 		CharacterizationBean charBean = (CharacterizationBean) (charForm
 				.get("achar"));
 		DataSetBean theDataSet = charBean.getTheDataSet();
-		if (dataRowBean != null) {
-			// TODO:: remove base on rowID?? or row index??
-			theDataSet.removeDataRow(dataRowBean);
+		theDataSet.getTheDataRow().addDatumColumn(datum);
+		return theDataSet;
+	}
+
+	// addRow includes the function add and edit
+	public DataSetBean addRow(List<Datum> data)
+			throws CharacterizationResultException {
+		DynaValidatorForm charForm = (DynaValidatorForm) (WebContextFactory
+				.get().getSession().getAttribute("characterizationForm"));
+		CharacterizationBean charBean = (CharacterizationBean) (charForm
+				.get("achar"));
+		DataSetBean theDataSet = charBean.getTheDataSet();
+		if (data != null) {
+			// TODO:: if rowid!=null, get the row then update
+			// DataRowBean dataRowBean = theDataSet.getTheDataRow();
+			DataRowBean dataRowBean = null;
+			boolean hasData = false;
+			int i = 0;
+			for (Datum datum : data) {
+				if (theDataSet.getDomain().getId() != null) {
+					datum.setDataSet(theDataSet.getDomain());
+				}
+				if (i == 0) {
+					dataRowBean = theDataSet.getDataRowBean(datum);
+					if (dataRowBean == null) {
+						dataRowBean = new DataRowBean();
+					}
+				}
+				dataRowBean.addDatum(datum);
+				hasData = true;
+				i++;
+			}
+			if (hasData) {
+				// if dataRowBean.id is null, set a negative id temporary
+				// need to handle set nagative id to null when save to db
+				if (dataRowBean.getDomain().getId() == null) {
+					dataRowBean.getDomain().setId(tempDataRowId--);
+				}
+				theDataSet.addDataRow(dataRowBean);
+			}
 		}
 		return theDataSet;
 	}
 
-	public DataSetBean getDataSetBeanBy(String dataSetId) throws Exception{
-		List<Datum> data=service.getDataForDataSet(dataSetId);
-		DataSetBean dataSetBean=new DataSetBean(data);
+	// addRow includes the function add and edit
+	public DataSetBean deleteRow(List<Datum> data)
+			throws CharacterizationResultException {
+		DynaValidatorForm charForm = (DynaValidatorForm) (WebContextFactory
+				.get().getSession().getAttribute("characterizationForm"));
+		CharacterizationBean charBean = (CharacterizationBean) (charForm
+				.get("achar"));
+		DataSetBean theDataSet = charBean.getTheDataSet();
+		if (data != null) {
+			DataRowBean dataRowBean = null;
+			int i = 0;
+			for (Datum datum : data) {
+				if (theDataSet.getDomain().getId() != null) {
+					datum.setDataSet(theDataSet.getDomain());
+				}
+				if (i == 0) {
+					dataRowBean = theDataSet.getDataRowBean(datum);
+					if (dataRowBean == null) {
+						return theDataSet;
+					}
+				}
+				dataRowBean.removeDatum(datum);
+				i++;
+			}
+			if (dataRowBean != null
+					&& (dataRowBean.getData() == null || dataRowBean.getData()
+							.size() == 0)
+					&& (dataRowBean.getConditions() == null || dataRowBean
+							.getConditions().size() == 0)) {
+				theDataSet.removeDataRow(dataRowBean);
+			}
+		}
+		return theDataSet;
+	}
+
+	public DataSetBean getDataSetBeanBy(String dataSetId) throws Exception {
+		List<Datum> data = service.getDataForDataSet(dataSetId);
+		DataSetBean dataSetBean = new DataSetBean(data);
 		return dataSetBean;
 	}
 
 	public DataSetBean findDataSetById(String dataSetId)
-		throws CharacterizationResultException {
+			throws CharacterizationResultException {
 		List<Datum> data = service.getDataForDataSet(dataSetId);
 		DataSetBean dataSetBean = new DataSetBean(data);
 		dataSetBean.setTheDataRow(dataSetBean.getDataRows().get(0));
