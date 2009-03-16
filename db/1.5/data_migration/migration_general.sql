b6d68d21df3096d33d69a5d9715551765535e597
Sue Pan <sue.pan@nih.gov>
2009-03-16 21:51:31 +0000

    renaming nanoparticle_entity to nanomaterial_entity and nanoparticle_sample to sample
    SVN-Revision: 14792

--- db/1.5/data_migration/migration_general.sql
+++ db/1.5/data_migration/migration_general.sql
@@ -45,19 +45,8 @@ ALTER TABLE chemical_association_file ADD CONSTRAINT FK_chemical_association_fil
 	FOREIGN KEY (chemical_association_pk_id) REFERENCES chemical_association (chemical_association_pk_id)
 ;
 
-ALTER TABLE nanoparticle_entity_lab_file RENAME nanoparticle_entity_file;
-ALTER TABLE nanoparticle_entity_file
- DROP FOREIGN KEY FK_nanoparticle_entity_lab_file_lab_file;
-ALTER TABLE nanoparticle_entity_file
- DROP FOREIGN KEY FK_nanoparticle_entity_lab_file_nanoparticle_entity;
-ALTER TABLE nanoparticle_entity_file ADD CONSTRAINT FK_nanoparticle_entity_file_file
-	FOREIGN KEY (file_pk_id) REFERENCES file (file_pk_id);
-ALTER TABLE nanoparticle_entity_file ADD CONSTRAINT FK_nanoparticle_entity_file_nanoparticle_entity
-	FOREIGN KEY (nanoparticle_entity_pk_id) REFERENCES nanoparticle_entity (nanoparticle_entity_pk_id)
-;
-
 ALTER TABLE keyword_lab_file RENAME keyword_file;
-ALTER TABLE canano.keyword_file
+ALTER TABLE keyword_file
  DROP FOREIGN KEY FK_keyword_lab_file_keyword,
  DROP FOREIGN KEY FK_keyword_lab_file_lab_file,
  CHANGE lab_file_pk_id file_pk_id BIGINT(20) NOT NULL;
@@ -77,34 +66,13 @@ ALTER TABLE functionalizing_entity_file ADD CONSTRAINT FK_functionalizing_entity
 	FOREIGN KEY (functionalizing_entity_pk_id) REFERENCES functionalizing_entity (functionalizing_entity_pk_id)
 ;
 
-ALTER TABLE nanoparticle_sample_publication
-	DROP FOREIGN KEY FK_nanoparticle_sample_publication_publication,
-	DROP FOREIGN KEY FK_nanoparticle_sample_publication_nanoparticle_sample
-;
-
-ALTER TABLE nanoparticle_sample_publication
-	DROP INDEX particle_sample_pk_id,
-	DROP INDEX file_pk_id
-;
-
-ALTER TABLE nanoparticle_sample_publication
-	CHANGE file_pk_id publication_pk_id BIGINT NOT NULL
-;
-
-delete
-from nanoparticle_sample_publication
+DELETE FROM nanoparticle_sample_publication
 where particle_sample_pk_id not in
 (select particle_sample_pk_id from nanoparticle_sample);
-
-ALTER TABLE nanoparticle_sample_publication
-   ADD PRIMARY KEY (particle_sample_pk_id, publication_pk_id),
-   ADD CONSTRAINT FK_nanoparticle_sample_publication_nanoparticle_sample FOREIGN KEY (particle_sample_pk_id) REFERENCES nanoparticle_sample (particle_sample_pk_id),
-   ADD CONSTRAINT FK_nanoparticle_sample_publication FOREIGN KEY (publication_pk_id) REFERENCES publication (publication_pk_id)
-;
 --end of lab_file to file
 
 -- fix, some records exist in csm_protection_group not in csm_protection_element
-INSERT INTO canano.csm_protection_element (
+INSERT INTO csm_protection_element (
 	protection_element_name,
 	object_id,
 	application_id,
@@ -114,12 +82,12 @@ INSERT INTO canano.csm_protection_element (
 	protection_group_name,
 	application_id,
 	sysdate()
-FROM canano.csm_protection_group
+FROM csm_protection_group
 where protection_group_name not in (
 select protection_element_name from csm_protection_element
 )
 ;
-INSERT INTO canano.csm_pg_pe (
+INSERT INTO csm_pg_pe (
 	protection_group_id,
 	protection_element_id,
 	update_date
@@ -127,11 +95,69 @@ INSERT INTO canano.csm_pg_pe (
 	g.protection_group_id,
 	e.protection_element_id,
 	sysdate()
-FROM canano.csm_protection_group g, canano.csm_protection_element e
+FROM csm_protection_group g, csm_protection_element e
 where  e.update_date = CURRENT_DATE()
 and g.protection_group_name = e.protection_element_name
 ;
 --end of csm fix
+
+--missing constraint between author and publication
+ALTER TABLE author RENAME author0;
+ALTER TABLE author_publication RENAME author_publication0;
+
+CREATE TABLE author_publication
+(
+	author_pk_id BIGINT NOT NULL,
+	publication_pk_id BIGINT NOT NULL,
+	PRIMARY KEY (author_pk_id, publication_pk_id),
+	KEY (author_pk_id),
+	KEY (publication_pk_id)
+) TYPE=InnoDB
+;
+
+CREATE TABLE author
+(
+	author_pk_id BIGINT NOT NULL,
+	first_name VARCHAR(200) NOT NULL,
+	last_name VARCHAR(200) NOT NULL,
+	initial VARCHAR(10),
+	created_by VARCHAR(200) NOT NULL,
+	created_date DATETIME NOT NULL,
+	PRIMARY KEY (author_pk_id),
+	UNIQUE (author_pk_id)
+) TYPE=InnoDB
+;
+
+ALTER TABLE author_publication ADD CONSTRAINT FK_author_publication_author
+	FOREIGN KEY (author_pk_id) REFERENCES author (author_pk_id)
+;
+
+ALTER TABLE author_publication ADD CONSTRAINT FK_author_publication_publication
+	FOREIGN KEY (publication_pk_id) REFERENCES publication (publication_pk_id)
+;
+
+INSERT INTO author (author_pk_id, first_name, last_name, initial, created_by, created_date)
+SELECT  author_pk_id, first_name, last_name, initial, created_by, created_date FROM author0;
+
+INSERT INTO author_publication (author_pk_id, publication_pk_id)
+SELECT author_pk_id, publication_pk_id FROM author_publication0;
+
+DROP TABLE author0;
+DROP TABLE author_publication0;
+
+--missing constraint between characterization and file
+ALTER TABLE characterization_file ADD CONSTRAINT FK_characterization_file_characterization
+	FOREIGN KEY (characterization_pk_id) REFERENCES characterization (characterization_pk_id)
+;
+
+ALTER TABLE characterization_file ADD CONSTRAINT FK_characterization_file_file
+	FOREIGN KEY (file_pk_id) REFERENCES file (file_pk_id)
+;
+--nanoparticle_sample to sample
+source sample.sql
+
+--nanoparticle_entity to nanomaterial_entity
+source nanomaterial_entity.sql
 
 --source enhancement
 source poc_migration.sql
@@ -144,6 +170,8 @@ source datum_migration.sql
 
 --characterization
 source characterization_migration.sql
+
+DROP TABLE composition_temp IF EXISTS;
 
 -- Re-enable foreign key checks
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
--- db/1.5/data_migration/poc_migration.sql
+++ db/1.5/data_migration/poc_migration.sql
@@ -62,28 +62,23 @@ group by nanoparticle_sample.source_pk_id;
 
 DROP TABLE IF EXISTS source;
 
-CREATE TABLE nanoparticle_sample_other_poc
+CREATE TABLE sample_other_poc
 (
-	particle_sample_pk_id BIGINT NOT NULL,
+	sample_pk_id BIGINT NOT NULL,
 	poc_pk_id BIGINT NOT NULL,
-	PRIMARY KEY (particle_sample_pk_id, poc_pk_id),
-	KEY (particle_sample_pk_id),
+	PRIMARY KEY (sample_pk_id, poc_pk_id),
+	KEY (sample_pk_id),
 	KEY (poc_pk_id)
 ) TYPE=InnoDB
 ;
-ALTER TABLE nanoparticle_sample_other_poc ADD CONSTRAINT FK_nanoparticle_sample_other_poc_nanoparticle_sample
-	FOREIGN KEY (particle_sample_pk_id) REFERENCES nanoparticle_sample (particle_sample_pk_id)
+ALTER TABLE sample_other_poc ADD CONSTRAINT FK_sample_other_poc_sample
+	FOREIGN KEY (sample_pk_id) REFERENCES sample (sample_pk_id)
 ;
-ALTER TABLE nanoparticle_sample_other_poc ADD CONSTRAINT FK_nanoparticle_sample_other_poc_point_of_contact
+ALTER TABLE sample_other_poc ADD CONSTRAINT FK_sample_other_poc_point_of_contact
 	FOREIGN KEY (poc_pk_id) REFERENCES point_of_contact (poc_pk_id)
 ;
-ALTER TABLE nanoparticle_sample
-	DROP FOREIGN KEY FK_nanoparticle_sample_source;
-ALTER TABLE nanoparticle_sample
-	DROP KEY source_pk_id;
-ALTER TABLE nanoparticle_sample
-	CHANGE source_pk_id primary_contact_pk_id BIGINT;
-ALTER TABLE nanoparticle_sample ADD CONSTRAINT FK_nanoparticle_sample_point_of_contact
+
+ALTER TABLE sample ADD CONSTRAINT FK_sample_point_of_contact
 	FOREIGN KEY (primary_contact_pk_id) REFERENCES point_of_contact (poc_pk_id)
 ;
 
@@ -97,11 +92,13 @@ AND o.name = c.source
 
 ALTER TABLE characterization DROP COLUMN source;
 
-ALTER TABLE canano.common_lookup
+ALTER TABLE common_lookup
  CHANGE common_lookup_pk_id common_lookup_pk_id BIGINT(20) AUTO_INCREMENT NOT NULL;
 
-insert into `common_lookup`(`name`,`attribute`,`value`) values ('PointOfContact','role','Manufacturer');
-insert into `common_lookup`(`name`,`attribute`,`value`) values ('PointOfContact','role','Investigator');
+insert into common_lookup(name,attribute,value) values ('PointOfContact','role','Manufacturer');
+insert into common_lookup(name,attribute,value) values ('PointOfContact','role','Investigator');
 
-ALTER TABLE canano.common_lookup
+ALTER TABLE common_lookup
  CHANGE common_lookup_pk_id common_lookup_pk_id BIGINT(20)  NOT NULL;
+
+ DROP TABLE  nanoparticle_sample;
