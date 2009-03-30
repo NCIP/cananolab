c3556d3ec0972299629330d490eedc20ab825750
Sue Pan <sue.pan@nih.gov>
2009-03-30 21:27:55 +0000

    added setCharacterizationDetailPage
    SVN-Revision: 14871

--- src/gov/nih/nci/cananolab/ui/sample/DWRCharacterizationManager.java
+++ src/gov/nih/nci/cananolab/ui/sample/DWRCharacterizationManager.java
@@ -1,6 +1,13 @@
 package gov.nih.nci.cananolab.ui.sample;
 
+import gov.nih.nci.cananolab.exception.BaseException;
+import gov.nih.nci.cananolab.ui.core.InitSetup;
+
+import java.io.IOException;
 import java.util.SortedSet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
 
 import org.directwebremoting.WebContext;
 import org.directwebremoting.WebContextFactory;
@@ -24,5 +31,32 @@ public class DWRCharacterizationManager {
 				.getAssayTypesByCharName(wctx.getHttpServletRequest(),
 						characterizationName);
 		return assayTypes.toArray(new String[assayTypes.size()]);
+	}
+
+	public String getCharacterizationDetailPage(String charName)
+			throws ServletException, IOException, BaseException {
+		try {
+			WebContext wctx = WebContextFactory.get();
+			ServletContext appContext = wctx.getServletContext();
+			String charClassName = InitSetup.getInstance().getClassName(
+					charName, appContext);
+			String includePage = null;
+			if (charClassName.equals("PhysicalState")
+					|| charClassName.equals("Shape")
+					|| charClassName.equals("Solubility")
+					|| charClassName.equals("Surface")) {
+				includePage = "/sample/characterization/physical/body" + charClassName + "Info.jsp";
+			} else if (charClassName.equals("Cytotoxicity")
+					|| charClassName.equals("EnzymeInduction")) {
+				includePage = "/sample/characterization/invitro/body" + charClassName + "Info.jsp";
+			}
+			if (includePage != null) {
+				return wctx.forwardToString(includePage);
+			} else {
+				return "";
+			}
+		} catch (Exception e) {
+			return "";
+		}
 	}
 }
