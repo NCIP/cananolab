9cd01f3763054b3dd39720b39c72e89c52fe6fb2
Sue Pan <sue.pan@nih.gov>
2009-04-03 16:13:02 +0000

    added finding and finding_file
    SVN-Revision: 14899

--- db/1.5/data_migration/datum_migration.sql
+++ db/1.5/data_migration/datum_migration.sql
@@ -1,4 +1,4 @@
---change derived_datum to datum, derived_bioassay to data_set, add condition
+--change derived_datum to datum, derived_bioassay to finding, add condition
 CREATE TABLE datum
 (
 	datum_pk_id BIGINT NOT NULL,
@@ -6,44 +6,45 @@ CREATE TABLE datum
 	value VARCHAR(200) NOT NULL,
 	value_type VARCHAR(200),
 	value_unit VARCHAR(200),
-	description TEXT,
 	created_by VARCHAR(200) NOT NULL,
 	created_date DATETIME NOT NULL,
-	data_set_pk_id BIGINT,
-	data_row_pk_id BIGINT,
-	characterization_pk_id BIGINT,
+	finding_pk_id BIGINT,
+	file_pk_id BIGINT,
 	PRIMARY KEY (datum_pk_id),
-	KEY (characterization_pk_id),
-	KEY (data_row_pk_id)
+	KEY (file_pk_id),
+	KEY (finding_pk_id)
 ) TYPE=InnoDB
 ;
+
 
 CREATE TABLE datum_condition
 (
 	datum_pk_id BIGINT NOT NULL,
 	condition_pk_id BIGINT NOT NULL,
 	PRIMARY KEY (datum_pk_id, condition_pk_id),
-	KEY (condition_pk_id),
-	KEY (datum_pk_id)
+	KEY (datum_pk_id),
+	KEY (condition_pk_id)
 ) TYPE=InnoDB
 ;
 
-
-CREATE TABLE data_set
+CREATE TABLE finding
 (
-	data_set_pk_id BIGINT NOT NULL,
-	file_pk_id BIGINT,
-	PRIMARY KEY (data_set_pk_id),
-	UNIQUE (data_set_pk_id),
-	KEY (file_pk_id)
+	finding_pk_id BIGINT NOT NULL,
+	characterization_pk_id BIGINT,
+	created_by VARCHAR(200) NOT NULL,
+	created_date DATETIME NOT NULL,
+	UNIQUE (finding_pk_id),
+	KEY (characterization_pk_id)
 ) TYPE=InnoDB
 ;
 
-
-CREATE TABLE data_row
+CREATE TABLE finding_file
 (
-	data_row_pk_id BIGINT NOT NULL,
-	PRIMARY KEY (data_row_pk_id)
+	finding_pk_id BIGINT NOT NULL,
+	file_pk_id BIGINT NOT NULL,
+	PRIMARY KEY (finding_pk_id, file_pk_id),
+	KEY (file_pk_id),
+	KEY (finding_pk_id)
 ) TYPE=InnoDB
 ;
 
@@ -61,46 +62,49 @@ CREATE TABLE experiment_condition
 ) TYPE=InnoDB
 ;
 
-ALTER TABLE datum ADD CONSTRAINT FK_datum_characterization
-	FOREIGN KEY (characterization_pk_id) REFERENCES characterization (characterization_pk_id)
-;
-
-ALTER TABLE datum ADD CONSTRAINT FK_datum_data_row
-	FOREIGN KEY (data_row_pk_id) REFERENCES data_row (data_row_pk_id)
-;
-
-ALTER TABLE datum_condition ADD CONSTRAINT FK_datum_condition_condition
-	FOREIGN KEY (condition_pk_id) REFERENCES experiment_condition (condition_pk_id)
-;
-
-ALTER TABLE datum_condition ADD CONSTRAINT FK_datum_condition_data_row
+ALTER TABLE datum_condition ADD CONSTRAINT FK_datum_condition_datum
 	FOREIGN KEY (datum_pk_id) REFERENCES datum (datum_pk_id)
 ;
 
-ALTER TABLE data_set ADD CONSTRAINT FK_data_set_file
+ALTER TABLE datum_condition ADD CONSTRAINT FK_datum_condition_experiment_condition
+	FOREIGN KEY (condition_pk_id) REFERENCES experiment_condition (condition_pk_id)
+;
+
+ALTER TABLE datum ADD CONSTRAINT FK_datum_file
 	FOREIGN KEY (file_pk_id) REFERENCES file (file_pk_id)
 ;
 
-INSERT INTO data_set(data_set_pk_id, file_pk_id)
-	SELECT derived_bioassay_data_pk_id, file_pk_id
+ALTER TABLE datum ADD CONSTRAINT FK_datum_finding
+	FOREIGN KEY (finding_pk_id) REFERENCES finding (finding_pk_id)
+;
+
+ALTER TABLE finding ADD CONSTRAINT FK_finding_characterization
+	FOREIGN KEY (characterization_pk_id) REFERENCES characterization (characterization_pk_id)
+;
+
+ALTER TABLE finding_file ADD CONSTRAINT FK_finding_file_file
+	FOREIGN KEY (file_pk_id) REFERENCES file (file_pk_id)
+;
+
+ALTER TABLE finding_file ADD CONSTRAINT FK_finding_file_finding
+	FOREIGN KEY (finding_pk_id) REFERENCES finding (finding_pk_id)
+;
+
+INSERT INTO datum(datum_pk_id, name, value, value_type, value_unit,created_by, created_date, finding_pk_id)
+	SELECT datum_pk_id, datum_name, value, value_type, value_unit, created_by, created_date, derived_bioassay_data_pk_id
+	FROM derived_datum dd
+;
+
+INSERT INTO finding(finding_pk_id, characterization_pk_id, created_date, created_by)
+	SELECT derived_bioassay_data_pk_id, characterization_pk_id, created_date, created_by
 	FROM derived_bioassay_data
 ;
 
-INSERT INTO data_row(data_row_pk_id)
-SELECT datum_pk_id
-FROM derived_datum
-;
-
-INSERT INTO datum(datum_pk_id, name, value, value_type, value_unit,
-	description, created_by, created_date, data_set_pk_id, data_row_pk_id,
-	characterization_pk_id)
-	SELECT dd.datum_pk_id, dd.datum_name, dd.value, dd.value_type,
-		dd.value_unit, dd.description, dd.created_by, dd.created_date,
-		dd.derived_bioassay_data_pk_id, dd.datum_pk_id,
-		b.characterization_pk_id
-	FROM derived_datum dd, derived_bioassay_data b
-	WHERE dd.derived_bioassay_data_pk_id = b.derived_bioassay_data_pk_id
-;
+INSERT INTO finding_file(finding_pk_id, file_pk_id)
+SELECT derived_bioassay_data_pk_id, dd.file_pk_id
+FROM derived_bioassay_data dd, file
+WHERE dd.file_pk_id=file.file_pk_id
+ and file.file_name is not null;
 
 ALTER TABLE canano.common_lookup
  CHANGE common_lookup_pk_id common_lookup_pk_id BIGINT AUTO_INCREMENT NOT NULL;
@@ -227,7 +231,6 @@ and attribute='datumName';
 ALTER TABLE canano.common_lookup
  CHANGE common_lookup_pk_id common_lookup_pk_id BIGINT  NOT NULL;
 
---TODO migrate data from derived_datum to datum
---DROP TABLE derived_datum;
---DROP TABLE derived_bioassay_data;
+DROP TABLE derived_datum;
+DROP TABLE derived_bioassay_data;
 --set datum drop down for assay endpoints
