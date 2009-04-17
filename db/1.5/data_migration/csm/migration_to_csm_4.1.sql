210753b843e98354c33a1edcc62562e6f3c3401c
Henry Hou <houy>
2009-04-17 19:41:58 +0000

    update comment, replace '#' with "-- " for MySql version 5.0.77.
    SVN-Revision: 14981

--- db/1.5/data_migration/csm/migration_to_csm_4.1.sql
+++ db/1.5/data_migration/csm/migration_to_csm_4.1.sql
@@ -1,8 +1,8 @@
-# Replace the <<database_name>> with proper database name that is to be created.
+-- Replace the <<database_name>> with proper database name that is to be created.
 
 USE canano;
 
-# update to csm 3.2
+-- update to csm 3.2
 UPDATE csm_application SET csm_application.DECLARATIVE_FLAG='0';
 
 ALTER TABLE csm_protection_element DROP COLUMN protection_element_type_id;
@@ -19,7 +19,7 @@ ALTER TABLE csm_application ADD COLUMN database_driver VARCHAR(100);
 
 UPDATE csm_pg_pe SET UPDATE_DATE=NULL;
 
-# upgrade to csm 4.0
+-- upgrade to csm 4.0
 CREATE TABLE csm_filter_clause ( 
 	filter_clause_id BIGINT AUTO_INCREMENT  NOT NULL,
 	class_name VARCHAR(100) NOT NULL,
@@ -40,7 +40,7 @@ ALTER TABLE csm_protection_element ADD ( attribute_value VARCHAR(100) );
  
 ALTER TABLE csm_protection_element ADD CONSTRAINT uq_pe_pe_name_attribute_value_app_id unique (object_id, attribute, attribute_value, application_id);
 
-# upgrade to csm 4.1
+-- upgrade to csm 4.1
 ALTER TABLE csm_user MODIFY COLUMN login_name VARCHAR(500);
 ALTER TABLE csm_user ADD COLUMN migrated_flag BOOL NOT NULL DEFAULT 0;
 ALTER TABLE csm_user ADD COLUMN premgrt_login_name VARCHAR(100) ;
@@ -54,6 +54,6 @@ ALTER TABLE csm_user_pe DROP COLUMN update_date;
 ALTER TABLE csm_filter_clause CHANGE generated_sql generated_sql_user VARCHAR (4000) NOT NULL;
 ALTER TABLE csm_filter_clause ADD COLUMN generated_sql_group VARCHAR (4000) NOT NULL;
 
-# update upt context name to be default value
+-- update upt context name to be default value
 update csm_application set application_name='csmupt' where application_name='caNanoLab-upt';
 update csm_protection_element set protection_element_name='csmupt', object_id='csmupt' where protection_element_name='caNanoLab-upt';
