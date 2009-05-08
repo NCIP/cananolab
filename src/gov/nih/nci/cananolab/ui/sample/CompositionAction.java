32ce3cfecd23367015d69e4a04d00b8ddfa57a39
Sue Pan <sue.pan@nih.gov>
2009-05-08 20:31:36 +0000

    updated prepareSummary
    SVN-Revision: 15159

--- src/gov/nih/nci/cananolab/ui/sample/CompositionAction.java
+++ src/gov/nih/nci/cananolab/ui/sample/CompositionAction.java
@@ -1,8 +1,7 @@
 package gov.nih.nci.cananolab.ui.sample;
 
-import java.util.Collections;
-
 import gov.nih.nci.cananolab.dto.common.UserBean;
+import gov.nih.nci.cananolab.dto.particle.composition.ChemicalAssociationBean;
 import gov.nih.nci.cananolab.dto.particle.composition.CompositionBean;
 import gov.nih.nci.cananolab.dto.particle.composition.FunctionalizingEntityBean;
 import gov.nih.nci.cananolab.dto.particle.composition.NanomaterialEntityBean;
@@ -11,6 +10,8 @@ import gov.nih.nci.cananolab.service.sample.impl.CompositionServiceLocalImpl;
 import gov.nih.nci.cananolab.ui.core.BaseAnnotationAction;
 import gov.nih.nci.cananolab.ui.core.InitSetup;
 import gov.nih.nci.cananolab.util.StringUtils;
+
+import java.util.Collections;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -22,48 +23,51 @@ import org.apache.struts.action.ActionMapping;
 import org.apache.struts.validator.DynaValidatorForm;
 
 public class CompositionAction extends BaseAnnotationAction {
-	
+
 	/**
 	 * Handle Composition Summary View request.
-	 * 
+	 *
 	 * @param mapping
 	 * @param form
 	 * @param request
 	 * @param response
-	 * @return ActionForward 
-	 * @throws Exception if error happened.
+	 * @return ActionForward
+	 * @throws Exception
+	 *             if error happened.
 	 */
 	public ActionForward summaryView(ActionMapping mapping, ActionForm form,
 			HttpServletRequest request, HttpServletResponse response)
 			throws Exception {
-		// Call sharing function to prepare CompositionBean for viewing and printing.
+		// Call sharing function to prepare CompositionBean for viewing and
+		// printing.
 		this.prepareSummary(mapping, form, request, response);
-		
+
 		/**
-		 * Added by houyh for implementing Print/Export report page. 
+		 * Added by houyh for implementing Print/Export report page.
 		 */
 		request.setAttribute("actionName", request.getRequestURL().toString());
-		
+
 		return mapping.findForward("summaryView");
 	}
 
 	/**
-	 * Shared function for summaryView() and summaryPrint().
-	 * Prepare CompositionBean for displaying based in SampleId and location.
-	 * 
+	 * Shared function for summaryView() and summaryPrint(). Prepare
+	 * CompositionBean for displaying based in SampleId and location.
+	 *
 	 * @param mapping
 	 * @param form
 	 * @param request
 	 * @param response
-	 * @return ActionForward 
-	 * @throws Exception if error happened.
+	 * @return ActionForward
+	 * @throws Exception
+	 *             if error happened.
 	 */
 	protected void prepareSummary(ActionMapping mapping, ActionForm form,
 			HttpServletRequest request, HttpServletResponse response)
 			throws Exception {
 		String location = request.getParameter("location");
 		String sampleId = request.getParameter("sampleId");
-		
+
 		// setupSample(theForm, request, location);
 		HttpSession session = request.getSession();
 		UserBean user = (UserBean) session.getAttribute("user");
@@ -77,10 +81,11 @@ public class CompositionAction extends BaseAnnotationAction {
 			// compService = new CompositionServiceRemoteImpl(
 			// serviceUrl);
 		}
-		CompositionBean compositionBean = compService.findCompositionBySampleId(sampleId);
+		CompositionBean compositionBean = compService
+				.findCompositionBySampleId(sampleId);
 		DynaValidatorForm theForm = (DynaValidatorForm) form;
 		theForm.set("comp", compositionBean);
-		// set entity type
+		// set entity type and association type
 		for (NanomaterialEntityBean entityBean : compositionBean
 				.getNanomaterialEntities()) {
 			entityBean.setType(InitSetup.getInstance().getDisplayName(
@@ -91,31 +96,40 @@ public class CompositionAction extends BaseAnnotationAction {
 			entityBean.setType(InitSetup.getInstance().getDisplayName(
 					entityBean.getClassName(), session.getServletContext()));
 		}
+		for (ChemicalAssociationBean assocBean : compositionBean
+				.getChemicalAssociations()) {
+			assocBean.setType(InitSetup.getInstance().getDisplayName(
+					assocBean.getClassName(), session.getServletContext()));
+		}
 	}
-	
+
 	/**
 	 * Handle Composition Summary Print request.
-	 * 
+	 *
 	 * @param mapping
 	 * @param form
 	 * @param request
 	 * @param response
-	 * @return ActionForward 
-	 * @throws Exception if error happened.
+	 * @return ActionForward
+	 * @throws Exception
+	 *             if error happened.
 	 */
 	public ActionForward summaryPrint(ActionMapping mapping, ActionForm form,
 			HttpServletRequest request, HttpServletResponse response)
 			throws Exception {
-		// Call sharing function to prepare CompositionBean for viewing and printing.
+		// Call sharing function to prepare CompositionBean for viewing and
+		// printing.
 		this.prepareSummary(mapping, form, request, response);
-		
-		// Marker that indicates page is for printing (do not show tabs, links, etc).
+
+		// Marker that indicates page is for printing (do not show tabs, links,
+		// etc).
 		request.setAttribute("printView", Boolean.TRUE);
-		
+
 		String type = request.getParameter("type");
 		if (!StringUtils.isEmpty(type)) {
 			DynaValidatorForm theForm = (DynaValidatorForm) form;
-			CompositionBean compositionBean = (CompositionBean) theForm.get("comp");
+			CompositionBean compositionBean = (CompositionBean) theForm
+					.get("comp");
 			if (!type.equals(CompositionBean.CHEMICAL_SELECTION)) {
 				compositionBean.setChemicalAssociations(Collections.EMPTY_LIST);
 			}
@@ -123,7 +137,8 @@ public class CompositionAction extends BaseAnnotationAction {
 				compositionBean.setFiles(Collections.EMPTY_LIST);
 			}
 			if (!type.equals(CompositionBean.FUNCTIONALIZING_SELECTION)) {
-				compositionBean.setFunctionalizingEntities(Collections.EMPTY_LIST);
+				compositionBean
+						.setFunctionalizingEntities(Collections.EMPTY_LIST);
 			}
 			if (!type.equals(CompositionBean.NANOMATERIAL_SELECTION)) {
 				compositionBean.setNanomaterialEntities(Collections.EMPTY_LIST);
@@ -131,7 +146,7 @@ public class CompositionAction extends BaseAnnotationAction {
 		}
 		return mapping.findForward("summaryPrint");
 	}
-	
+
 	public ActionForward summaryEdit(ActionMapping mapping, ActionForm form,
 			HttpServletRequest request, HttpServletResponse response)
 			throws Exception {
@@ -152,6 +167,14 @@ public class CompositionAction extends BaseAnnotationAction {
 				.getFunctionalizingEntities()) {
 			entityBean.setType(InitSetup.getInstance().getDisplayName(
 					entityBean.getClassName(), session.getServletContext()));
+		}
+		for (ChemicalAssociationBean assocBean : compositionBean
+				.getChemicalAssociations()) {
+			assocBean.setType(InitSetup.getInstance().getDisplayName(
+					assocBean.getClassName(), session.getServletContext()));
+			assocBean.updateType(InitSetup.getInstance()
+					.getClassNameToDisplayNameLookup(
+							session.getServletContext()));
 		}
 		theForm.set("comp", compositionBean);
 		return mapping.findForward("summaryEdit");
