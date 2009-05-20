c74c3be78fcd0be8cba51138869a06d44acefd5c
Sue Pan <sue.pan@nih.gov>
2009-05-20 21:47:10 +0000

    refactored nanomaterial specific functions to DWRNanomaterialEntityManager
    SVN-Revision: 15321

--- src/gov/nih/nci/cananolab/ui/sample/DWRCompositionManager.java
+++ src/gov/nih/nci/cananolab/ui/sample/DWRCompositionManager.java
@@ -2,12 +2,9 @@ package gov.nih.nci.cananolab.ui.sample;
 
 import gov.nih.nci.cananolab.dto.common.FileBean;
 import gov.nih.nci.cananolab.dto.particle.composition.ChemicalAssociationBean;
-import gov.nih.nci.cananolab.dto.particle.composition.ComposingElementBean;
-import gov.nih.nci.cananolab.dto.particle.composition.FunctionBean;
 import gov.nih.nci.cananolab.dto.particle.composition.FunctionalizingEntityBean;
 import gov.nih.nci.cananolab.dto.particle.composition.NanomaterialEntityBean;
 import gov.nih.nci.cananolab.exception.BaseException;
-import gov.nih.nci.cananolab.service.sample.helper.CompositionServiceHelper;
 
 import java.io.IOException;
 import java.util.List;
@@ -62,33 +59,5 @@ public class DWRCompositionManager {
 		}
 		FileBean theFile = files.get(index);
 		return theFile;
-	}
-
-	public ComposingElementBean addInherentFunction(
-			ComposingElementBean composingElementBean, FunctionBean function) {
-		composingElementBean.addFunction(function);
-		DynaValidatorForm compositionForm = (DynaValidatorForm) (WebContextFactory
-				.get().getSession().getAttribute("compositionForm"));
-		NanomaterialEntityBean entity = (NanomaterialEntityBean) compositionForm
-				.get("nanomaterialEntity");
-		entity.setTheComposingElement(composingElementBean);
-		return composingElementBean;
-	}
-
-	public ComposingElementBean deleteInherentFunction(
-			ComposingElementBean composingElementBean, FunctionBean function) {
-		composingElementBean.removeFunction(function);
-		return composingElementBean;
-	}
-
-	public ComposingElementBean getComposingElementFromList(int index)
-			throws Exception {
-		DynaValidatorForm compositionForm = (DynaValidatorForm) (WebContextFactory
-				.get().getSession().getAttribute("compositionForm"));
-		NanomaterialEntityBean entity = (NanomaterialEntityBean) compositionForm
-				.get("nanomaterialEntity");
-		List<ComposingElementBean> composingElements = entity
-				.getComposingElements();
-		return composingElements.get(index);
 	}
 }
