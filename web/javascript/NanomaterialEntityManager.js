4862cd21f759a4dec07bf55d9f11e48aae5f2a85
Sue Pan <sue.pan@nih.gov>
2009-05-22 20:53:10 +0000

    updated logic to create function id
    SVN-Revision: 15374

--- web/javascript/NanomaterialEntityManager.js
+++ web/javascript/NanomaterialEntityManager.js
@@ -1,8 +1,10 @@
-var inherentFunctionCache = {};
-var currentComposingElement = null;
 
+var inherentFunctionCache = null;
+var currentComposingElement = null;
+var numberOfFunctions;
 /* set the submit composing element form */
 function clearComposingElement() {
+	numberOfFunctions = 0;
 	//go to server and clean form bean
 	NanomaterialEntityManager.resetTheComposingElement(populateComposingElement);
 	hide("deleteComposingElement");
@@ -28,7 +30,11 @@ function displayImageModality() {
 }
 function addInherentFunction() {
 	var imagingFunction = {modality:dwr.util.getValue("imagingModality")};
-	var theFunction = {id:dwr.util.getValue("functionId"), type:dwr.util.getValue("functionType"), imagingFunction:imagingFunction, description:dwr.util.getValue("functionDescription")};
+	var funcId = dwr.util.getValue("functionId");
+	if (funcId == null || funcId == "") {
+		funcId = -1000 - numberOfFunctions;
+	}
+	var theFunction = {id:funcId, type:dwr.util.getValue("functionType"), imagingFunction:imagingFunction, description:dwr.util.getValue("functionDescription")};
 	if (theFunction.type != "" || theFunction.description != "") {
 		NanomaterialEntityManager.addInherentFunction(theFunction, function (composingElement) {
 			currentComposingElement = composingElement;
@@ -69,11 +75,15 @@ function populateInherentFunctions() {
 		dwr.util.setValue("functionDescriptionValue" + id, theFunction.description);
 		dwr.util.setValue("functionId", id);
 		$("pattern" + id).style.display = "";
+		if (inherentFunctionCache[id] == null) {
+			numberOfFunctions++;
+		}
 		inherentFunctionCache[id] = theFunction;
 	}
 }
-function setTheComposingElement(index) {
-	NanomaterialEntityManager.getComposingElementFromList(index, populateComposingElement);
+function setTheComposingElement(id) {
+	numberOfFunctions = 0;
+	NanomaterialEntityManager.getComposingElementById(id, populateComposingElement);
 	show("newComposingElement");
 	hide("newInherentFunction");
 	show("deleteComposingElement");
@@ -88,13 +98,17 @@ function populateComposingElement(element) {
 		dwr.util.setValue("elementDescription", element.domain.description);
 		dwr.util.setValue("molFormulaType", element.domain.molecularFormulaType);
 		dwr.util.setValue("molFormula", element.domain.molecularFormula);
-		show("deleteComposingElement");
+		if (element.domain.type != null) {
+			show("deleteComposingElement");
+		}
+		inherentFunctionCache = {};
 		populateInherentFunctions();
 	}
 }
 function editInherentFunction(eleid) {
 	// we were an id of the form "edit{id}", eg "edit42". We lookup the "42"
 	var func = inherentFunctionCache[eleid.substring(4)];
+	alert("function id: "+func.id);
 	dwr.util.setValue("functionType", func.type);
 	if (func.type == "imaging") {
 		show("modalityLabel");
@@ -106,6 +120,7 @@ function editInherentFunction(eleid) {
 		dwr.util.setValue("imagingModality", "");
 	}
 	dwr.util.setValue("functionDescription", func.description);
+	dwr.util.setValue("functionId", func.id);
 	show("deleteFunction");
 }
 function deleteTheInherentFunction() {
@@ -128,3 +143,4 @@ function removeComposingElement(actionName) {
 	submitAction(document.forms[0], actionName, "removeComposingElement");
 }
 /* end of submit composing element form */
+
