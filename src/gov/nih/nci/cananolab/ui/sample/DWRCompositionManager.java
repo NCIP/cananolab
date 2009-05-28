4f68ecbddce7ddce95efc130f174b4d0f192ab11
Sue Pan <sue.pan@nih.gov>
2009-05-28 21:26:26 +0000

    updated for submit chemical association
    SVN-Revision: 15461

--- src/gov/nih/nci/cananolab/ui/sample/DWRCompositionManager.java
+++ src/gov/nih/nci/cananolab/ui/sample/DWRCompositionManager.java
@@ -1,9 +1,12 @@
 package gov.nih.nci.cananolab.ui.sample;
 
 import gov.nih.nci.cananolab.domain.common.File;
+import gov.nih.nci.cananolab.domain.particle.NanomaterialEntity;
 import gov.nih.nci.cananolab.dto.common.FileBean;
 import gov.nih.nci.cananolab.dto.common.UserBean;
+import gov.nih.nci.cananolab.dto.particle.composition.BaseCompositionEntityBean;
 import gov.nih.nci.cananolab.dto.particle.composition.ChemicalAssociationBean;
+import gov.nih.nci.cananolab.dto.particle.composition.ComposingElementBean;
 import gov.nih.nci.cananolab.dto.particle.composition.CompositionBean;
 import gov.nih.nci.cananolab.dto.particle.composition.FunctionalizingEntityBean;
 import gov.nih.nci.cananolab.dto.particle.composition.NanomaterialEntityBean;
@@ -11,15 +14,21 @@ import gov.nih.nci.cananolab.exception.BaseException;
 import gov.nih.nci.cananolab.service.common.FileService;
 import gov.nih.nci.cananolab.service.common.helper.FileServiceHelper;
 import gov.nih.nci.cananolab.service.common.impl.FileServiceLocalImpl;
+import gov.nih.nci.cananolab.service.sample.helper.CompositionServiceHelper;
+import gov.nih.nci.cananolab.util.DataLinkBean;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.SortedSet;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
 
 import org.apache.struts.validator.DynaValidatorForm;
 import org.directwebremoting.WebContext;
 import org.directwebremoting.WebContextFactory;
+import org.directwebremoting.impl.DefaultWebContextBuilder;
 
 /**
  * Work with DWR to set up drop-downs required in the composition pages
@@ -28,7 +37,8 @@ import org.directwebremoting.WebContextFactory;
  *
  */
 public class DWRCompositionManager {
-	private FileServiceHelper helper = new FileServiceHelper();
+	private FileServiceHelper fileHelper = new FileServiceHelper();
+	private CompositionServiceHelper compHelper = new CompositionServiceHelper();
 
 	public DWRCompositionManager() {
 	}
@@ -48,7 +58,7 @@ public class DWRCompositionManager {
 	}
 
 	public FileBean getFileById(String type, String id) throws Exception {
-		File file = helper.findFileById(id);
+		File file = fileHelper.findFileById(id);
 		FileBean fileBean = new FileBean(file);
 		FileService service = new FileServiceLocalImpl();
 		WebContext wctx = WebContextFactory.get();
@@ -99,5 +109,36 @@ public class DWRCompositionManager {
 			assoc.setTheFile(fileBean);
 		}
 		return fileBean;
+	}
+
+	public List<BaseCompositionEntityBean> getAssociatedElementOptions(
+			String compositionType) {
+		DefaultWebContextBuilder dwcb = new DefaultWebContextBuilder();
+		org.directwebremoting.WebContext webContext = dwcb.get();
+		HttpServletRequest request = webContext.getHttpServletRequest();
+		List<BaseCompositionEntityBean> entities = null;
+
+		if (compositionType.equals("Nanomaterial Entity")) {
+			entities = (List<BaseCompositionEntityBean>) request.getSession()
+					.getAttribute("sampleMaterialEntities");
+
+		} else if (compositionType.equals("Functionalizing Entity")) {
+			entities = (List<BaseCompositionEntityBean>) request.getSession()
+					.getAttribute("sampleFunctionalizingEntities");
+		}
+
+		return entities;
+	}
+
+	public List<ComposingElementBean> getComposingElementsByNanomaterialEntityId(
+			String id) throws Exception {
+		if (id == null || id.length() == 0) {
+			return null;
+		}
+		NanomaterialEntity entity = compHelper.findNanomaterialEntityById(id);
+		NanomaterialEntityBean entityBean = new NanomaterialEntityBean(entity);
+		List<ComposingElementBean> composingElements = entityBean
+				.getComposingElements();
+		return composingElements;
 	}
 }
