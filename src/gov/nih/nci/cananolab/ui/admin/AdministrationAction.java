cec19a8b49012ad8e7d117faeab11e11f5d9eb4a
Henry Hou <houy>
2009-06-02 21:56:51 +0000

    Updated
    SVN-Revision: 15528

--- src/gov/nih/nci/cananolab/ui/admin/AdministrationAction.java
+++ src/gov/nih/nci/cananolab/ui/admin/AdministrationAction.java
@@ -66,7 +66,7 @@ public class AdministrationAction extends AbstractDispatchAction {
 			logger.info("siteName = " + siteName);
 		}
 		
-		return mapping.findForward("sitePreference");
+		return mapping.getInputForward();
 	}
 
 	/**
@@ -144,7 +144,7 @@ public class AdministrationAction extends AbstractDispatchAction {
 			saveErrors(request, messages);
 		}
 		
-		return mapping.findForward("sitePreference");
+		return mapping.getInputForward();
 	}
 
 	/**
@@ -218,7 +218,7 @@ public class AdministrationAction extends AbstractDispatchAction {
 			saveErrors(request, messages);
 		}
 		
-		return mapping.findForward("success");
+		return mapping.getInputForward();
 	}
 
 	/**
