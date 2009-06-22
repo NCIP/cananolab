4eb0f50146999a828aed9ea1764787350a627605
Henry Hou <houy>
2009-06-22 03:43:57 +0000

    Update getEditSampleURL() as the "<a href" tag isn't close, which causes sample name won't be displayed under curator view.
    SVN-Revision: 15707

--- src/gov/nih/nci/cananolab/dto/particle/SampleDecorator.java
+++ src/gov/nih/nci/cananolab/dto/particle/SampleDecorator.java
@@ -19,10 +19,11 @@ import org.displaytag.decorator.TableDecorator;
  *
  */
 public class SampleDecorator extends TableDecorator {
+	
 	public SortableName getEditSampleURL() {
 		SampleBean sample = (SampleBean) getCurrentRowObject();
-		if (!sample.getLocation().equals(Constants.LOCAL_SITE)) {
-			return getViewSampleURL();
+		if (!Constants.LOCAL_SITE.equals(sample.getLocation())) {
+			return this.getViewSampleURL();
 		}
 		String sampleId = sample.getDomain().getId().toString();
 		String sampleName = sample.getDomain().getName();
@@ -34,11 +35,9 @@ public class SampleDecorator extends TableDecorator {
 
 		StringBuilder sb = new StringBuilder("<a href=");
 		sb.append("sample.do?dispatch=summaryEdit&sampleId=");
-		sb.append(sampleId);
-		sb.append("&location=");
-		sb.append(sample.getLocation());
-		sb.append(sampleName);
-		sb.append("</a>");
+		sb.append(sampleId).append("&location=");
+		sb.append(sample.getLocation()).append('>');
+		sb.append(sampleName).append("</a>");
 		String link = sb.toString();
 
 		SortableName sortableLink = new SortableName(sampleName, link);
@@ -57,13 +56,11 @@ public class SampleDecorator extends TableDecorator {
 
 		StringBuilder sb = new StringBuilder("<a href=");
 		sb.append("sample.do?dispatch=summaryView&sampleId=");
-		sb.append(sampleId);
-		sb.append("&location=");
-		sb.append(sample.getLocation());
-		sb.append(">");
-		sb.append(sampleName);
-		sb.append("</a>");
+		sb.append(sampleId).append("&location=");
+		sb.append(sample.getLocation()).append('>');
+		sb.append(sampleName).append("</a>");
 		String link = sb.toString();
+		
 		SortableName sortableLink = new SortableName(sampleName, link);
 		return sortableLink;
 	}
