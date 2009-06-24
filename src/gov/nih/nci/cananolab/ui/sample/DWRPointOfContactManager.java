ef20cd729c948acb80ccf55ba650e911a5736f93
Sue Pan <sue.pan@nih.gov>
2009-06-24 21:03:08 +0000

    updated to reference helper
    SVN-Revision: 15782

--- src/gov/nih/nci/cananolab/ui/sample/DWRPointOfContactManager.java
+++ src/gov/nih/nci/cananolab/ui/sample/DWRPointOfContactManager.java
@@ -1,9 +1,12 @@
 package gov.nih.nci.cananolab.ui.sample;
 
+import gov.nih.nci.cananolab.domain.common.Organization;
+import gov.nih.nci.cananolab.domain.common.PointOfContact;
 import gov.nih.nci.cananolab.dto.common.PointOfContactBean;
 import gov.nih.nci.cananolab.dto.common.UserBean;
 import gov.nih.nci.cananolab.dto.particle.SampleBean;
 import gov.nih.nci.cananolab.service.sample.SampleService;
+import gov.nih.nci.cananolab.service.sample.helper.SampleServiceHelper;
 import gov.nih.nci.cananolab.service.sample.impl.SampleServiceLocalImpl;
 import gov.nih.nci.cananolab.ui.security.InitSecuritySetup;
 
@@ -20,16 +23,19 @@ public class DWRPointOfContactManager {
 
 	private Logger logger = Logger.getLogger(DWRPointOfContactManager.class);
 	private SampleService service = new SampleServiceLocalImpl();
+	private SampleServiceHelper helper = new SampleServiceHelper();
 
 	public DWRPointOfContactManager() {
 	}
 
-	public PointOfContactBean getPointOfContactById(String id) throws Exception {
+	public PointOfContactBean getPointOfContactById(String id,
+			Boolean primaryStatus) throws Exception {
 		DefaultWebContextBuilder dwcb = new DefaultWebContextBuilder();
 		org.directwebremoting.WebContext webContext = dwcb.get();
 		HttpServletRequest request = webContext.getHttpServletRequest();
 		UserBean user = (UserBean) request.getSession().getAttribute("user");
 		PointOfContactBean poc = service.findPointOfContactById(id, user);
+		poc.setPrimaryStatus(primaryStatus);
 		return poc;
 	}
 
@@ -39,7 +45,31 @@ public class DWRPointOfContactManager {
 		SampleBean sample = (SampleBean) sampleForm.get("sampleBean");
 		PointOfContactBean poc = new PointOfContactBean();
 		sample.setThePOC(poc);
+		//if primary POC already exists, the POC is secondar
+		if (sample.getPrimaryPOCBean().getDomain().getId()!=null) {
+			poc.setPrimaryStatus(false);
+		}
 		return poc;
+	}
+
+	public PointOfContactBean getPointOfContactByNameAndOrg(String firstName,
+			String lastName, String orgName) throws Exception {
+		DefaultWebContextBuilder dwcb = new DefaultWebContextBuilder();
+		org.directwebremoting.WebContext webContext = dwcb.get();
+		HttpServletRequest request = webContext.getHttpServletRequest();
+		UserBean user = (UserBean) request.getSession().getAttribute("user");
+		PointOfContact poc=helper.findPointOfContactByNameAndOrg(firstName, lastName, orgName, user);
+		PointOfContactBean pocBean = new PointOfContactBean(poc);
+		return pocBean;
+	}
+
+	public Organization getOrganizationByName(String name) throws Exception {
+		DefaultWebContextBuilder dwcb = new DefaultWebContextBuilder();
+		org.directwebremoting.WebContext webContext = dwcb.get();
+		HttpServletRequest request = webContext.getHttpServletRequest();
+		UserBean user = (UserBean) request.getSession().getAttribute("user");
+		Organization org = helper.findOrganizationByName(name, user);
+		return org;
 	}
 
 	/* remove organization associated with the POC from the visiblity group */
