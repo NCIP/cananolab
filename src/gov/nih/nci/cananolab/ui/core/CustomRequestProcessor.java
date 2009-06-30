08bf279ec4e9fd3dce7087d2bb03882d667416b1
Sue Pan <sue.pan@nih.gov>
2009-06-30 19:56:29 +0000

    disabled for now
    SVN-Revision: 15856

--- src/gov/nih/nci/cananolab/ui/core/CustomRequestProcessor.java
+++ src/gov/nih/nci/cananolab/ui/core/CustomRequestProcessor.java
@@ -9,12 +9,12 @@ import org.apache.struts.action.ActionMapping;
 import org.apache.struts.tiles.TilesRequestProcessor;
 
 /**
- * 
+ *
  * This class extends the default Tiles RequesetProcessor to check and make sure
  * session is not expired before every request. This is useful for directing
  * requests to the log in page when action forms involve indexed properties and
  * session is expired.
- * 
+ *
  * @author pansu
  */
 public class CustomRequestProcessor extends TilesRequestProcessor {
@@ -22,21 +22,22 @@ public class CustomRequestProcessor extends TilesRequestProcessor {
 			javax.servlet.http.HttpServletRequest request,
 			javax.servlet.http.HttpServletResponse response,
 			ActionMapping mapping) {
-		HttpSession session = request.getSession();
-		String dispatch = request.getParameter("dispatch");
-		// private dispatch
-		boolean privateDispatch = false;
-		for (String theDispatch : Constants.PRIVATE_DISPATCHES) {
-			if (dispatch != null && dispatch.startsWith(theDispatch)) {
-				privateDispatch = true;
-				break;
-			}
-		}
-		if (session.isNew()
-				&& (dispatch == null || privateDispatch)) {
-			return null;
-		} else {
-			return super.processActionForm(request, response, mapping);
-		}
+//		HttpSession session = request.getSession();
+//		String dispatch = request.getParameter("dispatch");
+//		// private dispatch
+//		boolean privateDispatch = false;
+//		for (String theDispatch : Constants.PRIVATE_DISPATCHES) {
+//			if (dispatch != null && dispatch.startsWith(theDispatch)) {
+//				privateDispatch = true;
+//				break;
+//			}
+//		}
+//		if (session.isNew()
+//				&& (dispatch == null || privateDispatch)) {
+//			return null;
+//		} else {
+//			return super.processActionForm(request, response, mapping);
+//		}
+		return super.processActionForm(request, response, mapping);
 	}
 }
