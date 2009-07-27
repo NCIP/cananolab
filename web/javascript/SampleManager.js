10bac2e95efdaaacdf7587bdac02c9966a8d43eb
Sue Pan <sue.pan@nih.gov>
2009-07-27 21:44:48 +0000

    added updateCompositionEntityDropdowns
    SVN-Revision: 16140

--- web/javascript/SampleManager.js
+++ web/javascript/SampleManager.js
@@ -1,21 +1,25 @@
-var emptyOption = [{label:"", value:""}];
+var emptyOption = [ {
+	label : "",
+	value : ""
+} ];
 
 function retrieveParticleNames() {
 	var sampleType = document.getElementById("sampleType").value;
-	ParticleManager.getNewParticleNamesByType(sampleType, populateParticleNames);
+	ParticleManager
+			.getNewParticleNamesByType(sampleType, populateParticleNames);
 }
 function resetParticleNames() {
 	dwr.util.removeAllOptions("sampleName");
 	dwr.util.addOptions("sampleName", emptyOption, "value", "label");
 }
 function populateParticleNames(sampleNames) {
-   //get previous selection
+	// get previous selection
 	var selectedParticleName = dwr.util.getValue("sampleName");
-	//remove option that's the same as previous selection
+	// remove option that's the same as previous selection
 	var updatedParticleNames = new Array();
 	if (sampleNames != null) {
 		for (i = 0; i < sampleNames.length; i++) {
-			if (sampleNames[i]!= selectedParticleName) {
+			if (sampleNames[i] != selectedParticleName) {
 				updatedParticleNames.push(sampleNames[i]);
 			}
 		}
@@ -24,20 +28,38 @@ function populateParticleNames(sampleNames) {
 }
 
 function setSampleDropdowns() {
-	var searchLocations = getSelectedOptions(document.getElementById("searchLocations"));
-	SampleManager.getNanomaterialEntityTypes(searchLocations, function (data) {
-			dwr.util.removeAllOptions("nanomaterialEntityTypes");
-			dwr.util.addOptions("nanomaterialEntityTypes", data);
-		});
-	SampleManager.getFunctionalizingEntityTypes(searchLocations, function (data) {
-			dwr.util.removeAllOptions("functionalizingEntityTypes");
-			dwr.util.addOptions("functionalizingEntityTypes", data);
-		});
+	var searchLocations = getSelectedOptions(document
+			.getElementById("searchLocations"));
+	SampleManager.getNanomaterialEntityTypes(searchLocations, function(data) {
+		dwr.util.removeAllOptions("nanomaterialEntityTypes");
+		dwr.util.addOptions("nanomaterialEntityTypes", data);
+	});
+	SampleManager.getFunctionalizingEntityTypes(searchLocations,
+			function(data) {
+				dwr.util.removeAllOptions("functionalizingEntityTypes");
+				dwr.util.addOptions("functionalizingEntityTypes", data);
+			});
 
-	SampleManager.getFunctionTypes(searchLocations, function (data) {
-			dwr.util.removeAllOptions("functionTypes");
-			dwr.util.addOptions("functionTypes", data);
-		});
+	SampleManager.getFunctionTypes(searchLocations, function(data) {
+		dwr.util.removeAllOptions("functionTypes");
+		dwr.util.addOptions("functionTypes", data);
+	});
 	return false;
 }
 
+function updateCompositionEntityDropdowns() {
+	var compositionType = dwr.util.getValue("compType");
+	if (compositionType == "nanomaterial entity") {
+		SampleManager.getNanomaterialEntityTypes(null, function(data) {
+			dwr.util.removeAllOptions("entityType");
+			dwr.util.addOptions("entityType", emptyOption, "value", "label");
+			dwr.util.addOptions("entityType", data);
+		});
+	} else if (compositionType = "functionalizing entity") {
+		SampleManager.getFunctionalizingEntityTypes(null, function(data) {
+			dwr.util.removeAllOptions("entityType");
+			dwr.util.addOptions("entityType", emptyOption, "value", "label");
+			dwr.util.addOptions("entityType", data);
+		});
+	}
+}
