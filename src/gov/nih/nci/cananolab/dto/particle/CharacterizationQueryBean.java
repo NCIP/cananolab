7b9b2dc3b7440e2c32cc00af9ad4960465c5e38f
Sue Pan <sue.pan@nih.gov>
2009-07-28 16:20:03 +0000

    updated to extend BaseQueryBean
    SVN-Revision: 16152

--- src/gov/nih/nci/cananolab/dto/particle/CharacterizationQueryBean.java
+++ src/gov/nih/nci/cananolab/dto/particle/CharacterizationQueryBean.java
@@ -1,67 +1,66 @@
 package gov.nih.nci.cananolab.dto.particle;
 
+import gov.nih.nci.cananolab.dto.BaseQueryBean;
+
 /**
  * Information for the characterization query form
  * 
  * @author pansu
- *
+ * 
  */
-public class CharacterizationQueryBean {
-	private String id;
+public class CharacterizationQueryBean extends BaseQueryBean {
 	private String characterizationType;
 	private String characterizationName;
 	private String characterizationAssay;
 	private String datumName;
 	private String datumValue;
-	public String getId() {
-		return id;
-	}
-	public void setId(String id) {
-		this.id = id;
-	}
 	private String datumValueUnit;
-	private String operand;	
-	
+
 	public String getCharacterizationType() {
 		return characterizationType;
 	}
+
 	public void setCharacterizationType(String characterizationType) {
 		this.characterizationType = characterizationType;
 	}
+
 	public String getCharacterizationName() {
 		return characterizationName;
 	}
+
 	public void setCharacterizationName(String characterizationName) {
 		this.characterizationName = characterizationName;
 	}
+
 	public String getCharacterizationAssay() {
 		return characterizationAssay;
 	}
+
 	public void setCharacterizationAssay(String characterizationAssay) {
 		this.characterizationAssay = characterizationAssay;
 	}
+
 	public String getDatumName() {
 		return datumName;
 	}
+
 	public void setDatumName(String datumName) {
 		this.datumName = datumName;
 	}
+
 	public String getDatumValue() {
 		return datumValue;
 	}
+
 	public void setDatumValue(String datumValue) {
 		this.datumValue = datumValue;
 	}
+
 	public String getDatumValueUnit() {
 		return datumValueUnit;
 	}
+
 	public void setDatumValueUnit(String datumValueUnit) {
 		this.datumValueUnit = datumValueUnit;
-	}
-	public String getOperand() {
-		return operand;
-	}
-	public void setOperand(String operand) {
-		this.operand = operand;
 	}
 }
--- src/gov/nih/nci/cananolab/dto/particle/CompositionQueryBean.java
+++ src/gov/nih/nci/cananolab/dto/particle/CompositionQueryBean.java
@@ -1,46 +1,39 @@
 package gov.nih.nci.cananolab.dto.particle;
 
+import gov.nih.nci.cananolab.dto.BaseQueryBean;
+
 /**
  * Information for the composition query form
  * 
  * @author pansu
- *
+ * 
  */
-public class CompositionQueryBean {
-	private String id;
+public class CompositionQueryBean extends BaseQueryBean {
 	private String compositionType;
 	private String entityType;
 	private String chemicalName;
-	private String operand;
-	
+
 	public String getCompositionType() {
 		return compositionType;
 	}
-	public String getId() {
-		return id;
-	}
-	public void setId(String id) {
-		this.id = id;
-	}
+
 	public void setCompositionType(String compositionType) {
 		this.compositionType = compositionType;
 	}
+
 	public String getEntityType() {
 		return entityType;
 	}
+
 	public void setEntityType(String entityType) {
 		this.entityType = entityType;
 	}
+
 	public String getChemicalName() {
 		return chemicalName;
 	}
+
 	public void setChemicalName(String chemicalName) {
 		this.chemicalName = chemicalName;
-	}
-	public String getOperand() {
-		return operand;
-	}
-	public void setOperand(String operand) {
-		this.operand = operand;
 	}
 }
