3e85781c1d050d4a2aec33925a82733a985c3303
Sue Pan <sue.pan@nih.gov>
2009-07-28 16:20:15 +0000

    added addQuery and removeQuery
    SVN-Revision: 16153

--- src/gov/nih/nci/cananolab/dto/particle/AdvancedSampleSearchBean.java
+++ src/gov/nih/nci/cananolab/dto/particle/AdvancedSampleSearchBean.java
@@ -1,5 +1,7 @@
 package gov.nih.nci.cananolab.dto.particle;
 
+import gov.nih.nci.cananolab.dto.BaseQueryBean;
+import gov.nih.nci.cananolab.dto.particle.composition.FunctionBean;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -8,7 +10,7 @@ import java.util.List;
  * information needed for the advanced sample search form
  * 
  * @author pansu
- *
+ * 
  */
 public class AdvancedSampleSearchBean {
 	private List<CompositionQueryBean> compositionQueries = new ArrayList<CompositionQueryBean>();
@@ -17,43 +19,87 @@ public class AdvancedSampleSearchBean {
 	private CharacterizationQueryBean theCharacterizationQuery = new CharacterizationQueryBean();
 	private String compositionLogicalOperator;
 	private String characterizationLogicalOperator;
+
 	public List<CompositionQueryBean> getCompositionQueries() {
 		return compositionQueries;
 	}
-	public void setCompositionQueries(List<CompositionQueryBean> compositionQueries) {
+
+	public void setCompositionQueries(
+			List<CompositionQueryBean> compositionQueries) {
 		this.compositionQueries = compositionQueries;
 	}
+
 	public List<CharacterizationQueryBean> getCharacterizationQueries() {
 		return characterizationQueries;
 	}
+
 	public void setCharacterizationQueries(
 			List<CharacterizationQueryBean> characterizationQueries) {
 		this.characterizationQueries = characterizationQueries;
 	}
+
 	public CompositionQueryBean getTheCompositionQuery() {
 		return theCompositionQuery;
 	}
+
 	public void setTheCompositionQuery(CompositionQueryBean theCompositionQuery) {
 		this.theCompositionQuery = theCompositionQuery;
 	}
+
 	public CharacterizationQueryBean getTheCharacterizationQuery() {
 		return theCharacterizationQuery;
 	}
+
 	public void setTheCharacterizationQuery(
 			CharacterizationQueryBean theCharacterizationQuery) {
 		this.theCharacterizationQuery = theCharacterizationQuery;
 	}
+
 	public String getCompositionLogicalOperator() {
 		return compositionLogicalOperator;
 	}
+
 	public void setCompositionLogicalOperator(String compositionLogicalOperator) {
 		this.compositionLogicalOperator = compositionLogicalOperator;
 	}
+
 	public String getCharacterizationLogicalOperator() {
 		return characterizationLogicalOperator;
 	}
+
 	public void setCharacterizationLogicalOperator(
 			String characterizationLogicalOperator) {
 		this.characterizationLogicalOperator = characterizationLogicalOperator;
-	}	
+	}
+
+	public void addQuery(BaseQueryBean query) {
+		if (query instanceof CompositionQueryBean) {
+			int index = compositionQueries.indexOf(query);
+			if (index != -1) {
+				compositionQueries.remove(query);
+				// retain the original order`
+				compositionQueries.add(index, (CompositionQueryBean) query);
+			} else {
+				compositionQueries.add((CompositionQueryBean) query);
+			}
+		} else if (query instanceof CharacterizationQueryBean) {
+			int index = characterizationQueries.indexOf(query);
+			if (index != -1) {
+				characterizationQueries.remove(query);
+				// retain the original order
+				characterizationQueries.add(index,
+						(CharacterizationQueryBean) query);
+			} else {
+				characterizationQueries.add((CharacterizationQueryBean) query);
+			}
+		}
+	}
+
+	public void removeQuery(BaseQueryBean query) {
+		if (query instanceof CompositionQueryBean) {
+			compositionQueries.remove((CompositionQueryBean) query);
+		} else if (query instanceof CharacterizationQueryBean) {
+			characterizationQueries.remove((CharacterizationQueryBean) query);
+		}
+	}
 }
