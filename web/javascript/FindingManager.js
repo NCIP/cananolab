243e73166686e819b94950380ea7ae9d33d63af4
Sue Pan <sue.pan@nih.gov>
2009-07-29 22:52:51 +0000

    updated setNameOptionsByCharName
    SVN-Revision: 16182

--- web/javascript/FindingManager.js
+++ web/javascript/FindingManager.js
@@ -1,48 +1,54 @@
-
-function setNameOptionsByCharName(charName, columnNumber) {
-	if (charName == "") {
-		charName = document.getElementById("charName").value;
-	}
+function setNameOptionsByCharName(columnNumber) {
+	var charName = document.getElementById("charName").value;
 	var columnType = document.getElementById("columnType" + columnNumber).value;
 	if (columnType == "Datum") {
 		hide("conditionPropertyPrompt" + columnNumber);
 		hide("conditionPropertyLabel" + columnNumber);
-		FindingManager.getDatumNameOptions(charName, function (data) {
+		FindingManager.getDatumNameOptions(charName, function(data) {
 			dwr.util.removeAllOptions("columnName" + columnNumber);
-			dwr.util.addOptions("columnName" + columnNumber, [""]);
+			dwr.util.addOptions("columnName" + columnNumber, [ "" ]);
 			dwr.util.addOptions("columnName" + columnNumber, data);
-			dwr.util.addOptions("columnName" + columnNumber, ["[Other]"]);
+			dwr.util.addOptions("columnName" + columnNumber, [ "[Other]" ]);
 		});
 	} else {
 		if (columnType == "Condition") {
 			show("conditionPropertyPrompt" + columnNumber);
 			show("conditionPropertyLabel" + columnNumber);
-			FindingManager.getConditionOptions(function (data) {
-				dwr.util.removeAllOptions("columnName" + columnNumber);
-				dwr.util.addOptions("columnName" + columnNumber, [""]);
-				dwr.util.addOptions("columnName" + columnNumber, data);
-				dwr.util.addOptions("columnName" + columnNumber, ["[Other]"]);
-			});
+			FindingManager
+					.getConditionOptions( function(data) {
+						dwr.util.removeAllOptions("columnName" + columnNumber);
+						dwr.util
+								.addOptions("columnName" + columnNumber, [ "" ]);
+						dwr.util.addOptions("columnName" + columnNumber, data);
+						dwr.util.addOptions("columnName" + columnNumber,
+								[ "[Other]" ]);
+					});
 		} else {
 			hide("conditionPropertyPrompt" + columnNumber);
 			hide("conditionPropertyLabel" + columnNumber);
 			dwr.util.removeAllOptions("columnName" + columnNumber);
-			dwr.util.addOptions("columnName" + columnNumber, [""]);
-			dwr.util.addOptions("columnName" + columnNumber, ["[Other]"]);
+			dwr.util.addOptions("columnName" + columnNumber, [ "" ]);
+			dwr.util.addOptions("columnName" + columnNumber, [ "[Other]" ]);
 		}
 	}
 }
 function setConditionPropertyOptionsByCharName(conditionName, columnNumber) {
 	if (document.getElementById("columnType" + columnNumber).value == "Condition") {
 		if (conditionName == null) {
-			conditionName = document.getElementById("columnName" + columnNumber).value;
+			conditionName = document
+					.getElementById("columnName" + columnNumber).value;
 		}
-		FindingManager.getConditionPropertyOptions(conditionName, function (data) {
-			dwr.util.removeAllOptions("conditionProperty" + columnNumber);
-			dwr.util.addOptions("conditionProperty" + columnNumber, [""]);
-			dwr.util.addOptions("conditionProperty" + columnNumber, data);
-			dwr.util.addOptions("conditionProperty" + columnNumber, ["[Other]"]);
-		});
+		FindingManager.getConditionPropertyOptions(conditionName,
+				function(data) {
+					dwr.util.removeAllOptions("conditionProperty"
+							+ columnNumber);
+					dwr.util.addOptions("conditionProperty" + columnNumber,
+							[ "" ]);
+					dwr.util.addOptions("conditionProperty" + columnNumber,
+							data);
+					dwr.util.addOptions("conditionProperty" + columnNumber,
+							[ "[Other]" ]);
+				});
 	}
 }
 function setColumnValueUnit(columnNumber) {
@@ -51,11 +57,11 @@ function setColumnValueUnit(columnNumber) {
 		property = document.getElementById("conditionProperty" + columnNumber).value;
 	}
 	name = document.getElementById("columnName" + columnNumber).value;
-	FindingManager.getColumnValueUnitOptions(name, property, function (data) {
+	FindingManager.getColumnValueUnitOptions(name, property, function(data) {
 		dwr.util.removeAllOptions("valueUnit" + columnNumber);
-		dwr.util.addOptions("valueUnit" + columnNumber, [""]);
+		dwr.util.addOptions("valueUnit" + columnNumber, [ "" ]);
 		dwr.util.addOptions("valueUnit" + columnNumber, data);
-		dwr.util.addOptions("valueUnit" + columnNumber, ["[Other]"]);
+		dwr.util.addOptions("valueUnit" + columnNumber, [ "[Other]" ]);
 	});
 }
 function resetTheFinding(form) {
@@ -63,14 +69,15 @@ function resetTheFinding(form) {
 	form.submit();
 }
 function setTheFinding(form, actionName, findingId) {
-	form.action = actionName + ".do?dispatch=getFinding&findingId=" + findingId + "&page=0";
+	form.action = actionName + ".do?dispatch=getFinding&findingId=" + findingId
+			+ "&page=0";
 	form.submit();
 }
 function saveFinding(actionName) {
 	/**
-	form.action = "characterization.do?dispatch=saveFinding&page=3";
-	form.submit();
-	*/
+	 * form.action = "characterization.do?dispatch=saveFinding&page=3";
+	 * form.submit();
+	 */
 	submitAction(document.forms[0], actionName, "saveFinding", 3);
 }
 function deleteTheFinding(form) {
@@ -89,23 +96,34 @@ function updateMatrix(form) {
 	}
 }
 function reduceMatrix(form, type, index) {
-	form.action = "characterization.do?dispatch=drawMatrix&page=0&remove" + type + "=" + index;
+	form.action = "characterization.do?dispatch=drawMatrix&page=0&remove"
+			+ type + "=" + index;
 	form.submit();
 }
 function openColumnForm(characterizationName, columnNumber) {
 	var promptbox = document.createElement("div");
 	promptbox.setAttribute("id", "newColumn" + columnNumber);
 	document.getElementById("column" + columnNumber).appendChild(promptbox);
-	FindingManager.getSubmitColumnPage(columnNumber, function (pageData) {
-		document.getElementById("newColumn" + columnNumber).innerHTML = pageData;
-		//populate column entry table from hidden values, use timeout to populate drop down first
-		dwr.util.setValue("columnType" + columnNumber, dwr.util.getValue("theColumnType" + columnNumber));
-		setNameOptionsByCharName(characterizationName, columnNumber);
-		window.setTimeout("delaySetColumnName(" + columnNumber + ")", 100);
-		dwr.util.setValue("valueType" + columnNumber, dwr.util.getValue("theValueType" + columnNumber));
-		dwr.util.setValue("constantValue" + columnNumber, dwr.util.getValue("theConstantValue" + columnNumber));
-	});
-	    //set style of the column entry table
+	FindingManager
+			.getSubmitColumnPage(
+					columnNumber,
+					function(pageData) {
+						document.getElementById("newColumn" + columnNumber).innerHTML = pageData;
+						// populate column entry table from hidden values, use
+						// timeout to populate drop down first
+						dwr.util.setValue("columnType" + columnNumber, dwr.util
+								.getValue("theColumnType" + columnNumber));
+						setNameOptionsByCharName(characterizationName,
+								columnNumber);
+						window.setTimeout("delaySetColumnName(" + columnNumber
+								+ ")", 100);
+						dwr.util.setValue("valueType" + columnNumber, dwr.util
+								.getValue("theValueType" + columnNumber));
+						dwr.util.setValue("constantValue" + columnNumber,
+								dwr.util.getValue("theConstantValue"
+										+ columnNumber));
+					});
+	// set style of the column entry table
 	document.getElementById("newColumn" + columnNumber).style.display = "block";
 	document.getElementById("newColumn" + columnNumber).style.position = "absolute";
 	document.getElementById("newColumn" + columnNumber).style.top = "20px";
@@ -114,46 +132,68 @@ function openColumnForm(characterizationName, columnNumber) {
 	document.getElementById("newColumn" + columnNumber).style.zIndex = "5";
 }
 function delaySetColumnName(columnNumber) {
-	dwr.util.setValue("columnName" + columnNumber, dwr.util.getValue("theColumnName" + columnNumber));
+	dwr.util.setValue("columnName" + columnNumber, dwr.util
+			.getValue("theColumnName" + columnNumber));
 	setConditionPropertyOptionsByCharName(null, columnNumber);
 	setColumnValueUnit(columnNumber);
 	window.setTimeout("delaySetConditionProperty(" + columnNumber + ")", 100);
 	window.setTimeout("delaySetColumnValueUnit(" + columnNumber + ")", 100);
 }
 function delaySetConditionProperty(columnNumber) {
-	dwr.util.setValue("conditionProperty" + columnNumber, dwr.util.getValue("theConditionProperty" + columnNumber));
+	dwr.util.setValue("conditionProperty" + columnNumber, dwr.util
+			.getValue("theConditionProperty" + columnNumber));
 	setColumnValueUnit(columnNumber);
 	window.setTimeout("delaySetColumnValueUnit(" + columnNumber + ")", 100);
 }
 function delaySetColumnValueUnit(columnNumber) {
-	dwr.util.setValue("valueUnit" + columnNumber, dwr.util.getValue("theValueUnit" + columnNumber));
+	dwr.util.setValue("valueUnit" + columnNumber, dwr.util
+			.getValue("theValueUnit" + columnNumber));
 }
 function cancelColumn(columnNumber) {
-	document.getElementById("column" + columnNumber).removeChild(document.getElementById("newColumn" + columnNumber));
+	document.getElementById("column" + columnNumber).removeChild(
+			document.getElementById("newColumn" + columnNumber));
 }
 function addColumnHeader(columnNumber) {
-	var columnHeader = {"columnName":dwr.util.getValue("columnName" + columnNumber), "columnType":dwr.util.getValue("columnType" + columnNumber), "conditionProperty":dwr.util.getValue("conditionProperty" + columnNumber), "valueType":dwr.util.getValue("valueType" + columnNumber), "valueUnit":dwr.util.getValue("valueUnit" + columnNumber), "constantValue":dwr.util.getValue("constantValue" + columnNumber)};
+	var columnHeader = {
+		"columnName" : dwr.util.getValue("columnName" + columnNumber),
+		"columnType" : dwr.util.getValue("columnType" + columnNumber),
+		"conditionProperty" : dwr.util.getValue("conditionProperty"
+				+ columnNumber),
+		"valueType" : dwr.util.getValue("valueType" + columnNumber),
+		"valueUnit" : dwr.util.getValue("valueUnit" + columnNumber),
+		"constantValue" : dwr.util.getValue("constantValue" + columnNumber)
+	};
 	var numberOfRows = dwr.util.getValue("rowNum");
 	if (columnHeader.constantValue != null) {
 		for (i = 0; i < numberOfRows; i++) {
-			dwr.util.setValue("cellValue" + i + ":" + columnNumber, columnHeader.constantValue);
+			dwr.util.setValue("cellValue" + i + ":" + columnNumber,
+					columnHeader.constantValue);
 		}
 	}
-	FindingManager.addColumnHeader(columnHeader, function (displayName) {
-		dwr.util.setValue("columnHeaderDisplayName" + columnNumber, displayName, {escapeHtml:false});
-		//set the hidden properties
-		dwr.util.setValue("theColumnType" + columnNumber, columnHeader.columnType);
-		dwr.util.setValue("theColumnName" + columnNumber, columnHeader.columnName);
-		dwr.util.setValue("theConditionProperty" + columnNumber, columnHeader.conditionProperty);
-		dwr.util.setValue("theValueType" + columnNumber, columnHeader.valueType);
-		dwr.util.setValue("theValueUnit" + columnNumber, columnHeader.valueUnit);
-		dwr.util.setValue("theConstantValue" + columnNumber, columnHeader.constantValue);
-	});
+	FindingManager.addColumnHeader(columnHeader, function(displayName) {
+		dwr.util.setValue("columnHeaderDisplayName" + columnNumber,
+				displayName, {
+					escapeHtml : false
+				});
+		// set the hidden properties
+			dwr.util.setValue("theColumnType" + columnNumber,
+					columnHeader.columnType);
+			dwr.util.setValue("theColumnName" + columnNumber,
+					columnHeader.columnName);
+			dwr.util.setValue("theConditionProperty" + columnNumber,
+					columnHeader.conditionProperty);
+			dwr.util.setValue("theValueType" + columnNumber,
+					columnHeader.valueType);
+			dwr.util.setValue("theValueUnit" + columnNumber,
+					columnHeader.valueUnit);
+			dwr.util.setValue("theConstantValue" + columnNumber,
+					columnHeader.constantValue);
+		});
 	hide("newColumn" + columnNumber);
 }
 /* set submit file form */
 function clearFile() {
-	//go to server and clean form bean
+	// go to server and clean form bean
 	FindingManager.resetTheFile(populateFile);
 	hide("deleteFile");
 	show("load");
