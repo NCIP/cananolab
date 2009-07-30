9161a50ce472de7ddc4a9443deb84f4dba850f24
Sue Pan <sue.pan@nih.gov>
2009-07-30 17:50:17 +0000

    added getDecoratedCharacterizationOptions
    SVN-Revision: 16194

--- src/gov/nih/nci/cananolab/ui/sample/DWRCharacterizationManager.java
+++ src/gov/nih/nci/cananolab/ui/sample/DWRCharacterizationManager.java
@@ -3,11 +3,14 @@ package gov.nih.nci.cananolab.ui.sample;
 import gov.nih.nci.cananolab.exception.BaseException;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.SortedSet;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 
+import org.apache.struts.util.LabelValueBean;
 import org.directwebremoting.WebContext;
 import org.directwebremoting.WebContextFactory;
 
@@ -21,6 +24,33 @@ public class DWRCharacterizationManager {
 		String[] charNameArray = new String[charNames.size()];
 		charNames.toArray(charNameArray);
 		return charNameArray;
+	}
+
+	public List<LabelValueBean> getDecoratedCharacterizationOptions(
+			String characterizationType) throws Exception {
+		WebContext wctx = WebContextFactory.get();
+		SortedSet<String> charNames = InitCharacterizationSetup.getInstance()
+				.getCharNamesByCharType(wctx.getHttpServletRequest(),
+						characterizationType);
+		List<LabelValueBean> charNamesWithAssayTypes = new ArrayList<LabelValueBean>();
+		for (String charName : charNames) {
+			SortedSet<String> assayTypes = InitCharacterizationSetup
+					.getInstance().getAssayTypesByCharName(
+							wctx.getHttpServletRequest(), charName);
+			LabelValueBean labelValue = new LabelValueBean();
+			labelValue.setLabel(charName);
+			labelValue.setValue(charName);
+			charNamesWithAssayTypes.add(labelValue);
+			if (!assayTypes.isEmpty()) {
+				for (String assayType : assayTypes) {
+					LabelValueBean labelValueWithAssay = new LabelValueBean();
+					labelValueWithAssay.setLabel("--" + assayType);
+					labelValueWithAssay.setValue(assayType);
+					charNamesWithAssayTypes.add(labelValueWithAssay);
+				}
+			}
+		}
+		return charNamesWithAssayTypes;
 	}
 
 	public String[] getAssayTypeOptions(String characterizationName)
@@ -40,7 +70,7 @@ public class DWRCharacterizationManager {
 			String includePage = InitCharacterizationSetup.getInstance()
 					.getDetailPage(appContext, charName);
 			if (includePage != null) {
-				String content=wctx.forwardToString(includePage);
+				String content = wctx.forwardToString(includePage);
 				return content;
 			} else {
 				return "";
