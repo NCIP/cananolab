f614efdae38d1a4889faa9e347987395e0986e9e
Sue Pan <sue.pan@nih.gov>
2009-08-11 16:54:02 +0000

    updated search
    SVN-Revision: 16362

--- src/gov/nih/nci/cananolab/ui/sample/AdvancedSampleSearchAction.java
+++ src/gov/nih/nci/cananolab/ui/sample/AdvancedSampleSearchAction.java
@@ -9,12 +9,15 @@ package gov.nih.nci.cananolab.ui.sample;
 /* CVS $Id: SearchSampleAction.java,v 1.28 2008-10-01 18:41:26 tanq Exp $ */
 
 import gov.nih.nci.cananolab.dto.common.UserBean;
+import gov.nih.nci.cananolab.dto.particle.AdvancedSampleBean;
 import gov.nih.nci.cananolab.dto.particle.AdvancedSampleSearchBean;
 import gov.nih.nci.cananolab.exception.SecurityException;
 import gov.nih.nci.cananolab.service.sample.SampleService;
 import gov.nih.nci.cananolab.service.sample.impl.SampleServiceLocalImpl;
 import gov.nih.nci.cananolab.ui.core.AbstractDispatchAction;
+import gov.nih.nci.cananolab.util.Constants;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
@@ -24,6 +27,8 @@ import javax.servlet.http.HttpSession;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
+import org.apache.struts.action.ActionMessage;
+import org.apache.struts.action.ActionMessages;
 import org.apache.struts.validator.DynaValidatorForm;
 
 public class AdvancedSampleSearchAction extends AbstractDispatchAction {
@@ -32,7 +37,6 @@ public class AdvancedSampleSearchAction extends AbstractDispatchAction {
 			HttpServletRequest request, HttpServletResponse response)
 			throws Exception {
 
-		ActionForward forward = null;
 		HttpSession session = request.getSession();
 		UserBean user = (UserBean) session.getAttribute("user");
 		DynaValidatorForm theForm = (DynaValidatorForm) form;
@@ -41,8 +45,23 @@ public class AdvancedSampleSearchAction extends AbstractDispatchAction {
 		SampleService service = new SampleServiceLocalImpl();
 		List<String> sampleNames = service.findSampleNamesByAdvancedSearch(
 				searchBean, user);
+		List<AdvancedSampleBean> sampleBeans = new ArrayList<AdvancedSampleBean>();
+		for (String name : sampleNames) {
+			AdvancedSampleBean sampleBean = new AdvancedSampleBean(name,
+					Constants.APP_OWNER);
+			sampleBeans.add(sampleBean);
+		}
+		if (sampleBeans.isEmpty()) {
+			ActionMessages msgs = new ActionMessages();
+			ActionMessage msg = new ActionMessage(
+					"message.advancedSampleSearch.noresult");
+			msgs.add(ActionMessages.GLOBAL_MESSAGE, msg);
+			saveMessages(request, msgs);
+			return mapping.getInputForward();
+		}
+		request.setAttribute("advancedSamples", sampleBeans);
 		// TODO to be implemented
-		return forward;
+		return mapping.findForward("success");
 	}
 
 	public ActionForward setup(ActionMapping mapping, ActionForm form,
