7f1ab271a6633d47fcebfff9ac785cf63d6de1ed
Sue Pan <sue.pan@nih.gov>
2009-09-08 15:29:13 +0000

    updated to include node discovery
    SVN-Revision: 16594

--- src/test/GridClientTest.java
+++ src/test/GridClientTest.java
@@ -1,5 +1,7 @@
 package test;
 
+import java.util.List;
+
 import gov.nih.nci.cagrid.cananolab.client.CaNanoLabServiceClient;
 import gov.nih.nci.cagrid.cqlquery.Attribute;
 import gov.nih.nci.cagrid.cqlquery.CQLQuery;
@@ -8,6 +10,8 @@ import gov.nih.nci.cagrid.cqlresultset.CQLQueryResults;
 import gov.nih.nci.cagrid.data.utilities.CQLQueryResultsIterator;
 import gov.nih.nci.cananolab.domain.particle.Characterization;
 import gov.nih.nci.cananolab.domain.particle.NanomaterialEntity;
+import gov.nih.nci.cananolab.dto.common.GridNodeBean;
+import gov.nih.nci.cananolab.service.common.GridService;
 
 public class GridClientTest {
 	CaNanoLabServiceClient gridClient;
@@ -35,7 +39,7 @@ public class GridClientTest {
 		while (iter.hasNext()) {
 			java.lang.Object obj = iter.next();
 			nanoEntity = (NanomaterialEntity) obj;
-			System.out.println("nano entity: "+nanoEntity.getDescription());
+			System.out.println("nano entity: " + nanoEntity.getDescription());
 		}
 	}
 
@@ -58,7 +62,8 @@ public class GridClientTest {
 		while (iter.hasNext()) {
 			java.lang.Object obj = iter.next();
 			chara = (Characterization) obj;
-			System.out.println("characterization: "+chara.getDesignMethodsDescription());
+			System.out.println("characterization: "
+					+ chara.getDesignMethodsDescription());
 		}
 	}
 
@@ -69,15 +74,30 @@ public class GridClientTest {
 				if (args[0].equals("-url")) {
 					CaNanoLabServiceClient client = new CaNanoLabServiceClient(
 							args[1]);
-					GridClientTest test=new GridClientTest(client);
+					GridClientTest test = new GridClientTest(client);
 					test.testCharacterization();
 					test.testNanomaterialEntity();
-				} else {
-					System.exit(1);
+				} else if (args[0].equals("-indexUrl")) {
+					// String indexServiceURL =
+					// "http://cbiovdev5012.nci.nih.gov:8080/wsrf/services/DefaultIndexService";
+					// test autodiscovery
+					List<GridNodeBean> gridNodes = GridService
+							.discoverAllServices(args[1], "caNanoLab", "1.5");
+					for (GridNodeBean node : gridNodes) {
+						System.out.println(node.getAddress());
+						CaNanoLabServiceClient client = new CaNanoLabServiceClient(
+								node.getAddress());
+						GridClientTest test = new GridClientTest(client);
+						test.testCharacterization();
+						test.testNanomaterialEntity();
+					}
 				}
-			} else {
+			}
+			else {
+				System.out.println("Please enter either an argument either as -url or -indexUrl");
 				System.exit(1);
 			}
+
 		} catch (Exception e) {
 			e.printStackTrace();
 			System.exit(1);
