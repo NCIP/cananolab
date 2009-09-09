aa90438c28f008c6658a89f1a61a5af5e5904c19
Sue Pan <sue.pan@nih.gov>
2009-09-09 18:00:35 +0000

    added loading summary info in findSampleByName
    SVN-Revision: 16604

--- src/gov/nih/nci/cananolab/service/sample/impl/SampleServiceLocalImpl.java
+++ src/gov/nih/nci/cananolab/service/sample/impl/SampleServiceLocalImpl.java
@@ -393,6 +393,19 @@ public class SampleServiceLocalImpl implements SampleService {
 			SampleBean sampleBean = null;
 			if (sample != null) {
 				sampleBean = new SampleBean(sample);
+				// load summary information
+				sampleBean.setCharacterizationClassNames(helper
+						.getStoredCharacterizationClassNames(sample).toArray(
+								new String[0]));
+				sampleBean.setFunctionalizingEntityClassNames(helper
+						.getStoredFunctionalizingEntityClassNames(sample)
+						.toArray(new String[0]));
+				sampleBean.setNanomaterialEntityClassNames(helper
+						.getStoredNanomaterialEntityClassNames(sample).toArray(
+								new String[0]));
+				sampleBean.setFunctionClassNames(helper
+						.getStoredFunctionClassNames(sample).toArray(
+				 new String[0]));
 				if (user != null)
 					retrieveVisibility(sampleBean, user);
 			}
