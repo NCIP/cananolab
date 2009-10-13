09526bf027e908f24cd3d4a4aac91de8e3ae8ec9
Sue Pan <sue.pan@nih.gov>
2009-10-13 04:20:33 +0000

    removed updating datum names
    SVN-Revision: 17074

--- db/1.5/data_migration/migration_general.sql
+++ db/1.5/data_migration/migration_general.sql
@@ -193,7 +193,7 @@ source characterization_migration.sql
 
 -- csm migration
 source csm_migration.sql
- 
+
 --update affected values because changes in common_lookup
 update chemical_association
 set attachment_bond_type=lcase(attachment_bond_type);
@@ -206,11 +206,6 @@ update datum
 set name='size', value_type='Z-average'
 where name='Z-average';
 
-update datum
-set name=lcase(name)
-where name in ('Aspect Ratio', 'Diameter', 'Height', 'Length', 'Surface Area')
-or name like ('Width%');
-
 DROP TABLE derived_datum;
 DROP TABLE derived_bioassay_data;
 -- set datum drop down for assay endpoints
@@ -219,6 +214,6 @@ DROP TABLE IF EXISTS composition_temp;
 
 -- Re-enable foreign key checks
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
- 
+
 -- End of script
 
