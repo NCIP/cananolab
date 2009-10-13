0eca2c3f17c6380302d66bef98f2f7945e8ec0a4
Sue Pan <sue.pan@nih.gov>
2009-10-13 04:18:47 +0000

    updated to capture all camel case name
    SVN-Revision: 17073

--- db/1.5/data_migration/common_lookup_migration.sql
+++ db/1.5/data_migration/common_lookup_migration.sql
@@ -1,10 +1,6 @@
 -- common lookup
 RENAME TABLE common_lookup to common_lookup_old;
 
-update common_lookup_old
-set attribute='otherDatumName'
-where attribute='otherDerivedDatumName';
-
 CREATE TABLE common_lookup
 (
 	common_lookup_pk_id BIGINT NOT NULL,
@@ -19,76 +15,68 @@ source ../insert_common_lookup.sql;
 
 ALTER TABLE canano.common_lookup
  CHANGE common_lookup_pk_id common_lookup_pk_id BIGINT(20) AUTO_INCREMENT NOT NULL;
- 
-INSERT INTO common_lookup (name,attribute,value) 
-SELECT lcase(name), attribute, value 
+
+--copy the other values and exclude cell lines
+INSERT INTO common_lookup (name,attribute,value)
+SELECT name, attribute, value
 FROM common_lookup_old
 where attribute like 'other%' and attribute!='otherCellLine';
 
+--update camel case to lower cases separated by space
+update common_lookup c2, common_lookup_old c1
+set c2.name=lower(c1.value)
+where c1.attribute='displayName' and
+c1.name=c2.name and
+c2.attribute like 'other%';
+
+--update special cases
+update common_lookup
+set attribute='otherDatumName'
+where attribute='otherDerivedDatumName';
+
+UPDATE common_lookup
+set name='caspase 3 apoptosis'
+where name='caspase 3 activation'
+and attribute like 'other%';
+
 UPDATE common_lookup
 set name='imaging function'
-where name='imagingfunction';
+where name='imaging'
+and attribute like 'other%';
+
+UPDATE common_lookup
+set name='cytotoxic activity of nk cells'
+where name='nk cell cytotoxic activity'
+and attribute like 'other%';
+
+UPDATE common_lookup
+set name='CFU-GM'
+where name='cfu gm'
+and attribute like 'other%';
 
 UPDATE common_lookup
 set name='composing element'
-where name='composingelement';
+where name='ComposingElement'
+and attribute like 'other%';
 
 UPDATE common_lookup
-set name='functionalizing entity'
-where name='functionalizingentity';
+set name='publication'
+where name='Publication'
+and attribute like 'other%';
 
-UPDATE common_lookup
-set name='molecular weight'
-where name='molecularweight';
-
-UPDATE common_lookup
-set name='cell viability'
-where name='cellviability';
-
-UPDATE common_lookup
-set name=ucase(name)
-where name in ('lc50', 'ic50');
-
-UPDATE common_lookup
-set attribute='unit'
-where attribute='otherUnit'
-and name ='LC50';
-
-UPDATE common_lookup
-set name='Feret''s Diameter'
-where name='feret''s Diameter';
-
-UPDATE common_lookup
-set value=lcase(value)
-where name in ('size', 'molecular weight')
-and attribute='otherDatumName';
-
-UPDATE common_lookup
-set value=lcase(value)
-where name = ('attachment')
-and attribute='otherBondType';
-
-UPDATE common_lookup
-set value='Feret''s Diameter'
-where value='feret''s Diameter';
-
-UPDATE common_lookup
-set value='bioluminescence'
-where value='Bioluminescence';
-
-delete from common_lookup
-where value='Bioluminiscence';
+delete
+from common_lookup
+where name='LC50'
+and attribute='otherUnit'
+and value in ('mg/L', 'nmol/L');
 
 UPDATE common_lookup
 set value='proceeding'
 where value='in proceedings';
 
-UPDATE common_lookup
-set value='MALDI-TOF mass spectrometer'
-where value='maldi-tof mass spectrometer';
-
 DELETE FROM common_lookup
-where value='[OTHER]';
+where value in ('other', '[OTHER]')
+and attribute like 'other%';
 
 ALTER TABLE canano.common_lookup
  CHANGE common_lookup_pk_id common_lookup_pk_id BIGINT  NOT NULL;
