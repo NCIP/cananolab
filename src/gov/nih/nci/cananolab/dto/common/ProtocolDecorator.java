fb53b730048a36efd04702b3790ec715d358e557
Sue Pan <sue.pan@nih.gov>
2009-10-16 20:08:38 +0000

    updated to display file title if no file uploaded
    SVN-Revision: 17189

--- src/gov/nih/nci/cananolab/dto/common/ProtocolDecorator.java
+++ src/gov/nih/nci/cananolab/dto/common/ProtocolDecorator.java
@@ -11,22 +11,22 @@ import org.displaytag.decorator.TableDecorator;
 /**
  * This decorator is used to for decorate different properties of a protocol
  * file to be shown properly in the view page using display tag lib.
- * 
+ *
  * @author pansu
- * 
+ *
  */
 public class ProtocolDecorator extends TableDecorator {
 	public SortableName getEditURL() {
 		ProtocolBean protocol = (ProtocolBean) getCurrentRowObject();
 		FileBean file = protocol.getFileBean();
-		if (!file.getLocation().equals(Constants.LOCAL_SITE)) {
+		if (file != null && !file.getLocation().equals(Constants.LOCAL_SITE)) {
 			return null;
 		}
 		StringBuilder sb = new StringBuilder("<a href=");
 		sb.append("submitProtocol.do?dispatch=setupUpdate&protocolId=");
 		sb.append(protocol.getDomain().getId());
 		sb.append("&location=");
-		sb.append(file.getLocation()).append('>');
+		sb.append(protocol.getLocation()).append('>');
 		sb.append("Edit");
 		sb.append("</a>");
 		String link = sb.toString();
@@ -46,21 +46,31 @@ public class ProtocolDecorator extends TableDecorator {
 		SortableName sortableLink = null;
 		ProtocolBean protocol = (ProtocolBean) getCurrentRowObject();
 		FileBean file = protocol.getFileBean();
-		String fileName = file.getDomainFile().getName();
-		if (!StringUtils.isEmpty(fileName)) {
-			StringBuilder sb = new StringBuilder("<a href=");
-			sb.append("searchProtocol.do?dispatch=download&fileId=");
-			sb.append(file.getDomainFile().getId());
-			sb.append("&location=");
-			sb.append(file.getLocation());
-			sb.append(">");
-			String fileTitle = file.getDomainFile().getTitle();
-			if (StringUtils.isEmpty(fileTitle)) {
-				fileTitle = file.getDomainFile().getUri();
+
+		if (file != null) {
+			String fileName = file.getDomainFile().getName();
+			if (!StringUtils.isEmpty(file.getDomainFile().getName())) {
+				StringBuilder sb = new StringBuilder("<a href=");
+				sb.append("searchProtocol.do?dispatch=download&fileId=");
+				sb.append(file.getDomainFile().getId());
+				sb.append("&location=");
+				sb.append(file.getLocation());
+				sb.append(">");
+				String fileTitle = file.getDomainFile().getTitle();
+				if (StringUtils.isEmpty(fileTitle)) {
+					fileTitle = file.getDomainFile().getUri();
+				}
+				sb.append(fileTitle);
+				sb.append("</a>");
+				sortableLink = new SortableName(fileName, sb.toString());
+			} else {
+				String fileTitle = file.getDomainFile().getTitle();
+				if (!StringUtils.isEmpty(fileTitle)) {
+					sortableLink = new SortableName(fileTitle);
+				} else {
+					sortableLink = new SortableName("");
+				}
 			}
-			sb.append(fileTitle);
-			sb.append("</a>");
-			sortableLink = new SortableName(fileName, sb.toString());
 		} else {
 			sortableLink = new SortableName("");
 		}
