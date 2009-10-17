3c68503cb0ff1cec1be1d2afdfef53d1c7af6a4b
Sue Pan <sue.pan@nih.gov>
2009-10-17 03:31:22 +0000

    SVN-Revision: 17218

--- db/1.3/mysql_seed.sql
+++
@@ -1,6 +0,0 @@
-source app_schema.sql;
-source app_priming_data.sql;
-source csm_schema.sql;
-source csm_priming_data.sql;
-source app_csm_priming_data.sql;
-source app_triggers.sql
--- db/1.4/data_migration/fix/delete_image_contrast_agent.sql
+++
@@ -1,5 +0,0 @@
-use canano;
-
-delete from composing_element
-where element_type = 'image contrast agent'
-;
