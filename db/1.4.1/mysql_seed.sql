94c914dddef4c4cee502c9a1cb449e661348f480
Sue Pan <sue.pan@nih.gov>
2009-10-29 16:20:22 +0000

    SVN-Revision: 17296

--- db/1.4.1/mysql_seed.sql
+++
@@ -1,7 +0,0 @@
-source create_database_and_user.sql;
-source app_schema.sql;
-source insert_common_lookup.sql;
-source csm_schema.sql;
-source csm_priming_data.sql;
-source app_priming_data.sql;
-source app_csm_priming_data.sql;
