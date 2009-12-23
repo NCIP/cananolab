a8e6a925b6ebf1076a0ed2b5a49aba8e6e3c7555
Sue Pan <sue.pan@nih.gov>
2009-12-23 15:39:00 +0000

    moved to follow BDA requirements
    SVN-Revision: 17513

--- cananolab-war/db/1.5/data_migration/targeting_function_fix.sql
+++
@@ -1,9 +0,0 @@
-use canano;
-
-update nano_function nf
-set discriminator='TargetingFunction'
-where exists
-(SELECT t.targeting_function_pk_id
-  FROM target t
- WHERE nf.function_pk_id = t.targeting_function_pk_id
-and nf.discriminator!='TargetingFunction');
