078763d57fdf8073dc8cdc962d2a6579014fca98
Sue Pan <sue.pan@nih.gov>
2010-02-16 20:17:32 +0000

    added charName2Counts and charName2Characterizations
    SVN-Revision: 17623

--- src/gov/nih/nci/cananolab/dto/particle/characterization/CharacterizationSummaryViewBean.java
+++ src/gov/nih/nci/cananolab/dto/particle/characterization/CharacterizationSummaryViewBean.java
@@ -11,12 +11,17 @@ import java.util.TreeSet;
 
 public class CharacterizationSummaryViewBean {
 	private Set<String> characterizationTypes;
-	private Map<String, SortedSet<CharacterizationBean>> type2Characterizations = new HashMap<String, SortedSet<CharacterizationBean>>();
 	private Map<String, SortedSet<String>> type2CharacterizationNames = new HashMap<String, SortedSet<String>>();
+	private Map<String, SortedSet<CharacterizationBean>> type2Characterizations = new HashMap<String, SortedSet<CharacterizationBean>>();
+	private Map<String, SortedSet<CharacterizationBean>> charName2Characterizations = new HashMap<String, SortedSet<CharacterizationBean>>();
 
+	private Map<String, Integer> charName2Counts=new HashMap<String, Integer>();
+	
 	public CharacterizationSummaryViewBean(List<CharacterizationBean> chars) {
 		SortedSet<CharacterizationBean> typeChars = null;
+		SortedSet<CharacterizationBean> nameChars = null;
 		SortedSet<String> typeCharNames = null;
+		Integer charNameCount=0;
 		for (CharacterizationBean achar : chars) {
 			String type = achar.getCharacterizationType();
 			if (type2Characterizations.get(type) != null) {
@@ -34,7 +39,26 @@ public class CharacterizationSummaryViewBean {
 				typeCharNames = new TreeSet<String>();
 				type2CharacterizationNames.put(type, typeCharNames);
 			}
-			typeCharNames.add(achar.getCharacterizationName());
+			String charName=achar.getCharacterizationName();
+			typeCharNames.add(charName);
+			
+			if (charName2Characterizations.get(charName) != null) {
+				nameChars = charName2Characterizations.get(charName);
+			} else {
+				nameChars = new TreeSet<CharacterizationBean>(
+						new Comparators.CharacterizationBeanNameDateComparator());
+				charName2Characterizations.put(charName, nameChars);
+			}
+			nameChars.add(achar);
+
+			if (charName2Counts.get(charName)!=null) {
+				charNameCount=charName2Counts.get(charName);
+			}
+			else {				
+				charNameCount=0;
+			}
+			charNameCount++;
+			charName2Counts.put(charName, charNameCount);
 		}
 		characterizationTypes = type2Characterizations.keySet();
 	}
@@ -47,7 +71,15 @@ public class CharacterizationSummaryViewBean {
 		return type2Characterizations;
 	}
 
+	public Map<String, SortedSet<CharacterizationBean>> getCharName2Characterizations() {
+		return charName2Characterizations;
+	}
+	
 	public Map<String, SortedSet<String>> getType2CharacterizationNames() {
 		return type2CharacterizationNames;
+	}
+
+	public Map<String, Integer> getCharName2Counts() {
+		return charName2Counts;
 	}
 }
