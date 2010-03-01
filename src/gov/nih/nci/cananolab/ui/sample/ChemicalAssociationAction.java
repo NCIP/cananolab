53fdc6a5cd87fc01bbab50fea63a52b494f5ea75
Henry Hou <houy>
2010-03-01 22:44:20 +0000

    Implement feature request [26487] Deeper Edit Links for Finding. Put back logic for auto open File input box.
    SVN-Revision: 17703

--- src/gov/nih/nci/cananolab/ui/sample/ChemicalAssociationAction.java
+++ src/gov/nih/nci/cananolab/ui/sample/ChemicalAssociationAction.java
@@ -434,7 +434,6 @@ public class ChemicalAssociationAction extends BaseAnnotationAction {
 			openFile = true;
 		}
 		session.setAttribute("openFile", openFile);
-
 		/**
 		 * If user entered customized value selecting [other] on previous page,
 		 * we should show and highlight the entered value on the edit page.
@@ -442,13 +441,24 @@ public class ChemicalAssociationAction extends BaseAnnotationAction {
 		// Association Type
 		String assocType = assoc.getType();
 		setOtherValueOption(request, assocType, "chemicalAssociationTypes");
-
 		// Bond Type
 		String bondType = assoc.getAttachment().getBondType();
 		setOtherValueOption(request, bondType, "bondTypes");
-
 		// File Type
 		String fileType = assoc.getTheFile().getDomainFile().getType();
 		setOtherValueOption(request, fileType, "fileTypes");
+		
+		// Feature request [26487] Deeper Edit Links.
+		if ("setupUpdate".equals(dispatch)) {
+			List<FileBean> files = assoc.getFiles();
+			if (files.size() == 1) {
+				FileBean fileBean = files.get(0);
+				StringBuilder sb = new StringBuilder();
+				sb.append("setTheFile('assoc', ");
+				sb.append(fileBean.getDomainFile().getId());
+				sb.append(')');
+				request.setAttribute("onloadJavascript", sb.toString());
+			}
+		}
 	}
 }
