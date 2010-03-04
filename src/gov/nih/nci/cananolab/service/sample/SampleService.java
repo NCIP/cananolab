c83d2bd40248fbd889e218ab7ede777db0f6d21d
Sue Pan <sue.pan@nih.gov>
2010-03-04 22:19:06 +0000

    added NotExistException
    SVN-Revision: 17755

--- src/gov/nih/nci/cananolab/service/sample/SampleService.java
+++ src/gov/nih/nci/cananolab/service/sample/SampleService.java
@@ -7,6 +7,7 @@ import gov.nih.nci.cananolab.dto.particle.AdvancedSampleSearchBean;
 import gov.nih.nci.cananolab.dto.particle.SampleBean;
 import gov.nih.nci.cananolab.exception.DuplicateEntriesException;
 import gov.nih.nci.cananolab.exception.NoAccessException;
+import gov.nih.nci.cananolab.exception.NotExistException;
 import gov.nih.nci.cananolab.exception.PointOfContactException;
 import gov.nih.nci.cananolab.exception.SampleException;
 import gov.nih.nci.cananolab.util.SortableName;
@@ -79,5 +80,5 @@ public interface SampleService {
 
 	public void cloneSample(String originalSampleName, String newSampleName,
 			UserBean user) throws SampleException, NoAccessException,
-			DuplicateEntriesException;
+			DuplicateEntriesException, NotExistException;
 }
