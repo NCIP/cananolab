79c6965cee54a596e3efd8ba69bf6dbfcd2fc4e8
Sue Pan <sue.pan@nih.gov>
2010-03-11 00:04:26 +0000

    updated showMatchedSampleNameDropdown
    SVN-Revision: 17829

--- web/javascript/PublicationManager.js
+++ web/javascript/PublicationManager.js
@@ -1,21 +1,19 @@
+
 var authorCache = {};
 var currentPublication = null;
 var numberOfAuthors = 0; // number of unique authors in the cache, used to
 // generate author id
-
 function clearPublication() {
 	// clear submission form first
-	PublicationManager.clearPublication( function(publication) {
+	PublicationManager.clearPublication(function (publication) {
 		dwr.util.setValues(publication);
 		currentPublication = publication;
 		// not sure if we need to clear status, description, samples, file, and
 			// visibility
-			populateAuthors(false);
-		});
+		populateAuthors(false);
+	});
 }
-
 function showAuthors(publicationId) {
-
 }
 function updateSubmitFormBasedOnCategory() {
 	var category = dwr.util.getValue("category");
@@ -35,7 +33,6 @@ function updateSubmitFormBasedOnCategory() {
 		dwr.util.setValue("status", "published");
 	}
 }
-
 function enableAutoFields() {
 	document.getElementById("domainFile.digitalObjectId").readOnly = false;
 	document.getElementById("domainFile.title").readOnly = false;
@@ -47,7 +44,6 @@ function enableAutoFields() {
 	show("addAuthor");
 	show("fileSection");
 }
-
 function updateSearchFormBasedOnCategory() {
 	var category = dwr.util.getValue("publicationCategory");
 	if (category != "report" && category != "book chapter" && category != "") {
@@ -56,7 +52,6 @@ function updateSearchFormBasedOnCategory() {
 		hide("pubMedRow");
 	}
 }
-
 function showMatchedSampleNameDropdown() {
 	// display progress.gif while waiting for the response.
 	show("loaderImg");
@@ -64,7 +59,10 @@ function showMatchedSampleNameDropdown() {
 	hide("selectMatchedSampleButton");
 	var selected = dwr.util.getValue("matchedSampleSelect");
 	var otherSamples = dwr.util.getValue("otherSamples");
-	SampleManager.getMatchedSampleNames(otherSamples, function(data) {
+	PublicationManager.getMatchedSampleNames(otherSamples, function (data) {
+		if (data.length == 0) {
+			dwr.util.setValue("otherSamples", "");
+		}
 		dwr.util.removeAllOptions("matchedSampleSelect");
 		dwr.util.addOptions("matchedSampleSelect", data);
 		dwr.util.setValue("matchedSampleSelect", selected);
@@ -73,54 +71,59 @@ function showMatchedSampleNameDropdown() {
 		show("selectMatchedSampleButton");
 	});
 }
-
 function updateOtherSamples() {
-	
+	var selected = dwr.util.getValue("matchedSampleSelect");
+	if (selected.length == 0) {
+		return;
+	}
+	if (selected.length == 1) {
+		dwr.util.setValue("otherSamples", selected);
+		return;
+	}
+	var sampleNames = "";
+	for (i = 0; i < selected.length - 1; i++) {
+		sampleNames = sampleNames + selected[i] + "\n";
+	}
+	sampleNames = sampleNames + selected[i];
+	dwr.util.setValue("otherSamples", sampleNames, {escapeHtml:false});
+	hide("matchedSampleSelect");
+	hide("selectMatchedSampleButton");
 }
-
 function setPublicationDropdowns() {
-	var searchLocations = getSelectedOptions(document
-			.getElementById("searchLocations"));
-	PublicationManager.getPublicationCategories(searchLocations,
-			function(data) {
-				dwr.util.removeAllOptions("publicationCategories");
-				dwr.util.addOptions("publicationCategories", data);
-			});
-	SampleManager.getNanomaterialEntityTypes(searchLocations, function(data) {
+	var searchLocations = getSelectedOptions(document.getElementById("searchLocations"));
+	PublicationManager.getPublicationCategories(searchLocations, function (data) {
+		dwr.util.removeAllOptions("publicationCategories");
+		dwr.util.addOptions("publicationCategories", data);
+	});
+	SampleManager.getNanomaterialEntityTypes(searchLocations, function (data) {
 		dwr.util.removeAllOptions("nanomaterialEntityTypes");
 		dwr.util.addOptions("nanomaterialEntityTypes", data);
 	});
-	SampleManager.getFunctionalizingEntityTypes(searchLocations,
-			function(data) {
-				dwr.util.removeAllOptions("functionalizingEntityTypes");
-				dwr.util.addOptions("functionalizingEntityTypes", data);
-			});
-
-	SampleManager.getFunctionTypes(searchLocations, function(data) {
+	SampleManager.getFunctionalizingEntityTypes(searchLocations, function (data) {
+		dwr.util.removeAllOptions("functionalizingEntityTypes");
+		dwr.util.addOptions("functionalizingEntityTypes", data);
+	});
+	SampleManager.getFunctionTypes(searchLocations, function (data) {
 		dwr.util.removeAllOptions("functionTypes");
 		dwr.util.addOptions("functionTypes", data);
 	});
 	return false;
 }
-
 function searchPublication() {
 	var pubMedId = dwr.util.getValue("pubMedId");
 	if (pubMedId != null && pubMedId != 0) {
-	   PublicationManager.searchPubMedById(pubMedId, validatePubMedInfo);		
-	}
-	else {
-	   submitAction(document.forms[0], "searchPublication", "search", 1);
-	}
-}
-
-function validatePubMedInfo(publication) {	
-	if (publication.domainFile.pubMedId == null) {
-		alert("Invalid PubMed ID entered.");		
+		PublicationManager.searchPubMedById(pubMedId, validatePubMedInfo);
 	} else {
 		submitAction(document.forms[0], "searchPublication", "search", 1);
 	}
 }
-
+function validatePubMedInfo(publication) {
+	if (publication.domainFile.pubMedId == null) {
+		alert("Invalid PubMed ID entered.");
+	} else {
+		submitAction(document.forms[0], "searchPublication", "search", 1);
+	}
+}
 function fillPubMedInfo() {
 	var pubMedId = dwr.util.getValue("domainFile.pubMedId");
 	if (pubMedId != null && pubMedId != 0) {
@@ -129,7 +132,6 @@ function fillPubMedInfo() {
 		PublicationManager.retrieveCurrentPub(populateAuthorInfo);
 	}
 }
-
 function populatePubMedInfo(publication) {
 	if (publication != null) {
 		currentPublication = publication;
@@ -142,10 +144,8 @@ function populatePubMedInfo(publication) {
 			show("fileSection");
 		} else {
 			// Set keywordsStr & description again for special characters.
-			dwr.util.setValue("keywordsStr", publication.keywordsStr, 
-				{escapeHtml:false});
-			dwr.util.setValue("domainFile.description",
-				publication.domainFile.description, {escapeHtml:false});
+			dwr.util.setValue("keywordsStr", publication.keywordsStr, {escapeHtml:false});
+			dwr.util.setValue("domainFile.description", publication.domainFile.description, {escapeHtml:false});
 			document.getElementById("domainFile.digitalObjectId").readOnly = true;
 			document.getElementById("domainFile.title").readOnly = true;
 			document.getElementById("domainFile.journalName").readOnly = true;
@@ -161,7 +161,6 @@ function populatePubMedInfo(publication) {
 		sessionTimeout();
 	}
 }
-
 function populateAuthorInfo(publication) {
 	if (publication != null) {
 		currentPublication = publication;
@@ -170,29 +169,24 @@ function populateAuthorInfo(publication) {
 		sessionTimeout();
 	}
 }
-
 function populateAuthors(hideEdit) {
 	var authors = currentPublication.authors;
-	dwr.util.removeAllRows("authorRows", {
-		filter : function(tr) {
-			return (tr.id != "pattern" && tr.id != "patternHeader");
-		}
-	});
+	dwr.util.removeAllRows("authorRows", {filter:function (tr) {
+		return (tr.id != "pattern" && tr.id != "patternHeader");
+	}});
 	var author, id;
 	if (authors.length > 0) {
 		show("authorTable");
 	} else {
 		hide("authorTable");
 	}
-	for ( var i = 0; i < authors.length; i++) {
+	for (var i = 0; i < authors.length; i++) {
 		author = authors[i];
 		if (author.id == null || author.id == "") {
 			author.id = -1000 - numberOfAuthors;
 		}
 		id = author.id;
-		dwr.util.cloneNode("pattern", {
-			idSuffix : id
-		});
+		dwr.util.cloneNode("pattern", {idSuffix:id});
 		dwr.util.setValue("id" + id, author.id);
 		dwr.util.setValue("firstNameValue" + id, author.firstName);
 		dwr.util.setValue("lastNameValue" + id, author.lastName);
@@ -207,21 +201,14 @@ function populateAuthors(hideEdit) {
 		}
 	}
 }
-
 function addAuthor() {
-	var author = {
-		id : null,
-		firstName : null,
-		lastName : null,
-		initial : null
-	};
+	var author = {id:null, firstName:null, lastName:null, initial:null};
 	dwr.util.getValues(author);
 	if (author.id == null || author.id.length == 0) {
 		author.id = -1000 - numberOfAuthors;
 	}
-
 	if (author.firstName != "" || author.lastName != "" || author.initial != "") {
-		PublicationManager.addAuthor(author, function(publication) {
+		PublicationManager.addAuthor(author, function (publication) {
 			if (publication == null) {
 				sessionTimeout();
 			}
@@ -233,12 +220,7 @@ function addAuthor() {
 	}
 }
 function clearAuthor() {
-	var author = {
-		id : null,
-		firstName : "",
-		lastName : "",
-		initial : ""
-	};
+	var author = {id:null, firstName:"", lastName:"", initial:""};
 	dwr.util.setValues(author);
 	hide("deleteAuthor");
 }
@@ -255,9 +237,8 @@ function deleteTheAuthor() {
 	// var author = authorCache[eleid.substring(6)];
 	if (eleid != null && eleid != NaN) {
 		var author = authorCache[eleid];
-		if (confirm("Are you sure you want to delete '" + author.firstName
-				+ " " + author.lastName + "'?")) {
-			PublicationManager.deleteAuthor(author, function(publication) {
+		if (confirm("Are you sure you want to delete '" + author.firstName + " " + author.lastName + "'?")) {
+			PublicationManager.deleteAuthor(author, function (publication) {
 				if (publication == null) {
 					sessionTimeout();
 				}
@@ -268,3 +249,4 @@ function deleteTheAuthor() {
 		}
 	}
 }
+
