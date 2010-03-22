32ee9963919188158d8c3ecf1ae98ffec3e8516b
Henry Hou <houy>
2010-03-22 17:25:38 +0000

    Fix for [calab-Bugs][26587] Order of contact persons. Sort other POC by created date.
    SVN-Revision: 17938

--- src/gov/nih/nci/cananolab/util/Comparators.java
+++ src/gov/nih/nci/cananolab/util/Comparators.java
@@ -47,6 +47,12 @@ public class Comparators {
 	public static class PointOfContactBeanNameOrgComparator implements
 			Comparator<PointOfContactBean> {
 		public int compare(PointOfContactBean poc1, PointOfContactBean poc2) {
+			//[calab-Bugs][26587] Order of contact persons.
+			Date date1 = poc1.getDomain().getCreatedDate();
+			Date date2 = poc2.getDomain().getCreatedDate();
+			if (date1 != null && date2 != null) {
+				return date1.compareTo(date2);
+			}
 			if (poc1.getPersonDisplayName().equals(poc2.getPersonDisplayName())) {
 				return poc1.getOrganizationDisplayName().compareTo(
 						poc2.getOrganizationDisplayName());
