c05fae43d7d8f5b8c071ad2641a66796dbd19df5
Sue Pan <sue.pan@nih.gov>
2010-03-24 00:05:02 +0000

    added additional fields
    SVN-Revision: 17966

--- src/gov/nih/nci/cananolab/dto/common/PublicDataCountBean.java
+++ src/gov/nih/nci/cananolab/dto/common/PublicDataCountBean.java
@@ -6,52 +6,66 @@ public class PublicDataCountBean {
 	private Integer numOfPublicSources;
 	private Integer numOfPublicCharacterizations;
 	private Integer numOfPublicPhysicoChemicalCharacterizations;
-	private Integer numOfPublicInVitroCharacterizations;
+	private Integer numOfPublicInvitroCharacterizations;
 	private Integer numOfPublicPublications;
-	
+
 	public Integer getNumOfPublicProtocols() {
 		return numOfPublicProtocols;
 	}
+
 	public void setNumOfPublicProtocols(Integer numOfPublicProtocols) {
 		this.numOfPublicProtocols = numOfPublicProtocols;
 	}
+
 	public Integer getNumOfPublicSamples() {
 		return numOfPublicSamples;
 	}
+
 	public void setNumOfPublicSamples(Integer numOfPublicSamples) {
 		this.numOfPublicSamples = numOfPublicSamples;
 	}
+
 	public Integer getNumOfPublicSources() {
 		return numOfPublicSources;
 	}
+
 	public void setNumOfPublicSources(Integer numOfPublicSources) {
 		this.numOfPublicSources = numOfPublicSources;
 	}
+
 	public Integer getNumOfPublicCharacterizations() {
 		return numOfPublicCharacterizations;
 	}
-	public void setNumOfPublicCharacterizations(Integer numOfPublicCharacterizations) {
+
+	public void setNumOfPublicCharacterizations(
+			Integer numOfPublicCharacterizations) {
 		this.numOfPublicCharacterizations = numOfPublicCharacterizations;
 	}
+
 	public Integer getNumOfPublicPhysicoChemicalCharacterizations() {
 		return numOfPublicPhysicoChemicalCharacterizations;
 	}
+
 	public void setNumOfPublicPhysicoChemicalCharacterizations(
 			Integer numOfPublicPhysicoChemicalCharacterizations) {
 		this.numOfPublicPhysicoChemicalCharacterizations = numOfPublicPhysicoChemicalCharacterizations;
 	}
-	public Integer getNumOfPublicInVitroCharacterizations() {
-		return numOfPublicInVitroCharacterizations;
+
+	public Integer getNumOfPublicInvitroCharacterizations() {
+		return numOfPublicInvitroCharacterizations;
 	}
-	public void setNumOfPublicInVitroCharacterizations(
-			Integer numOfPublicInVitroCharacterizations) {
-		this.numOfPublicInVitroCharacterizations = numOfPublicInVitroCharacterizations;
+
+	public void setNumOfPublicInvitroCharacterizations(
+			Integer numOfPublicInvitroCharacterizations) {
+		this.numOfPublicInvitroCharacterizations = numOfPublicInvitroCharacterizations;
 	}
+
 	public Integer getNumOfPublicPublications() {
 		return numOfPublicPublications;
 	}
+
 	public void setNumOfPublicPublications(Integer numOfPublicPublications) {
 		this.numOfPublicPublications = numOfPublicPublications;
 	}
-	
+
 }
