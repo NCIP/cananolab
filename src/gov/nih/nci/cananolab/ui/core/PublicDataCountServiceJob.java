0028efa3c5bd54bca7c6fb3b9bc6f0898c92c36f
Sue Pan <sue.pan@nih.gov>
2010-03-24 00:06:30 +0000

    updated execute
    SVN-Revision: 17970

--- src/gov/nih/nci/cananolab/ui/core/PublicDataCountServiceJob.java
+++ src/gov/nih/nci/cananolab/ui/core/PublicDataCountServiceJob.java
@@ -8,7 +8,10 @@ import gov.nih.nci.cananolab.service.protocol.impl.ProtocolServiceRemoteImpl;
 import gov.nih.nci.cananolab.service.publication.PublicationService;
 import gov.nih.nci.cananolab.service.publication.impl.PublicationServiceLocalImpl;
 import gov.nih.nci.cananolab.service.publication.impl.PublicationServiceRemoteImpl;
+import gov.nih.nci.cananolab.service.sample.CharacterizationService;
 import gov.nih.nci.cananolab.service.sample.SampleService;
+import gov.nih.nci.cananolab.service.sample.impl.CharacterizationServiceLocalImpl;
+import gov.nih.nci.cananolab.service.sample.impl.CharacterizationServiceRemoteImpl;
 import gov.nih.nci.cananolab.service.sample.impl.SampleServiceLocalImpl;
 import gov.nih.nci.cananolab.service.sample.impl.SampleServiceRemoteImpl;
 import gov.nih.nci.cananolab.util.Constants;
@@ -33,7 +36,7 @@ public class PublicDataCountServiceJob implements Job {
 
 	public PublicDataCountServiceJob() {
 	}
-	
+
 	/*
 	 * (non-Javadoc)
 	 * 
@@ -41,7 +44,7 @@ public class PublicDataCountServiceJob implements Job {
 	 */
 	public void execute(JobExecutionContext context)
 			throws JobExecutionException {
-//		 query for public data counts
+		// query for public data counts
 		List<GridNodeBean> gridNodes = (List<GridNodeBean>) context
 				.getJobDetail().getJobDataMap().get("gridNodes");
 		if (gridNodes == null) {
@@ -61,17 +64,30 @@ public class PublicDataCountServiceJob implements Job {
 			locations.add(gridNode.getAddress());
 		}
 		Integer sampleCount = 0;
+		Integer sourceCount = 0;
 		Integer publicationCount = 0;
 		Integer protocolCount = 0;
+		Integer charCount = 0;
+		Integer physicoCharCount = 0;
+		Integer invitroCharCount = 0;
 		for (String location : locations) {
 			sampleCount += getPublicSampleCount(location);
+			sourceCount += getPublicSampleSourceCount(location);
 			protocolCount += getPublicProtocolCount(location);
 			publicationCount += getPublicPublicationCount(location);
+			charCount += getPublicCharacterizationCount(location);
+			physicoCharCount += getPublicPhysicoChemicalCharacterizationCount(location);
+			invitroCharCount += getPublicInvitroCharacterizationCount(location);
 		}
 		dataCounts = new PublicDataCountBean();
 		dataCounts.setNumOfPublicSamples(sampleCount);
+		dataCounts.setNumOfPublicSources(sourceCount);
 		dataCounts.setNumOfPublicPublications(publicationCount);
 		dataCounts.setNumOfPublicProtocols(protocolCount);
+		dataCounts.setNumOfPublicCharacterizations(charCount);
+		dataCounts
+				.setNumOfPublicPhysicoChemicalCharacterizations(physicoCharCount);
+		dataCounts.setNumOfPublicInvitroCharacterizations(invitroCharCount);
 	}
 
 	private Integer getPublicSampleCount(String serviceUrl) {
@@ -92,6 +108,30 @@ public class PublicDataCountServiceJob implements Job {
 			} catch (Exception e) {
 				logger
 						.error("Error obtaining counts of public samples from grid service at"
+								+ serviceUrl);
+			}
+		}
+		return count;
+	}
+
+	private Integer getPublicSampleSourceCount(String serviceUrl) {
+		Integer count = 0;
+		SampleService service = null;
+		if (serviceUrl.equals(Constants.LOCAL_SITE)) {
+			try {
+				service = new SampleServiceLocalImpl();
+				count += service.getNumberOfPublicSampleSources();
+			} catch (Exception e) {
+				logger
+						.error("Error obtaining counts of public sample sources from local site.");
+			}
+		} else {
+			try {
+				service = new SampleServiceRemoteImpl(serviceUrl);
+				count = service.getNumberOfPublicSamples();
+			} catch (Exception e) {
+				logger
+						.error("Error obtaining counts of public sample sources from grid service at"
 								+ serviceUrl);
 			}
 		}
@@ -140,6 +180,81 @@ public class PublicDataCountServiceJob implements Job {
 			} catch (Exception e) {
 				logger
 						.error("Error obtaining counts of public publications from grid service at"
+								+ serviceUrl);
+			}
+		}
+		return count;
+	}
+
+	private Integer getPublicCharacterizationCount(String serviceUrl) {
+		Integer count = 0;
+		CharacterizationService service = null;
+		if (serviceUrl.equals(Constants.LOCAL_SITE)) {
+			try {
+				service = new CharacterizationServiceLocalImpl();
+				count += service.getNumberOfPublicCharacterizations();
+			} catch (Exception e) {
+				logger
+						.error("Error obtaining counts of public characterizations from local site.");
+			}
+		} else {
+			try {
+				service = new CharacterizationServiceRemoteImpl(serviceUrl);
+				count = service.getNumberOfPublicCharacterizations();
+			} catch (Exception e) {
+				logger
+						.error("Error obtaining counts of public characterizations from grid service at"
+								+ serviceUrl);
+			}
+		}
+		return count;
+	}
+
+	private Integer getPublicPhysicoChemicalCharacterizationCount(
+			String serviceUrl) {
+		Integer count = 0;
+		CharacterizationService service = null;
+		if (serviceUrl.equals(Constants.LOCAL_SITE)) {
+			try {
+				service = new CharacterizationServiceLocalImpl();
+				count += service
+						.getNumberOfPublicPhysicoChemicalCharacterizations();
+			} catch (Exception e) {
+				logger
+						.error("Error obtaining counts of public physico-chemical characterizations from local site.");
+			}
+		} else {
+			try {
+				service = new CharacterizationServiceRemoteImpl(serviceUrl);
+				count = service
+						.getNumberOfPublicPhysicoChemicalCharacterizations();
+			} catch (Exception e) {
+				logger
+						.error("Error obtaining counts of public  physico-chemical characterizations from grid service at"
+								+ serviceUrl);
+			}
+		}
+		return count;
+	}
+
+	private Integer getPublicInvitroCharacterizationCount(String serviceUrl) {
+		Integer count = 0;
+		CharacterizationService service = null;
+		if (serviceUrl.equals(Constants.LOCAL_SITE)) {
+			try {
+				service = new CharacterizationServiceLocalImpl();
+				count += service.getNumberOfPublicInvitroCharacterizations();
+			} catch (Exception e) {
+				logger
+						.error("Error obtaining counts of public in vitro characterizations from local site.");
+			}
+		} else {
+			try {
+				service = new CharacterizationServiceRemoteImpl(serviceUrl);
+				count = service.getNumberOfPublicInvitroCharacterizations();
+			} catch (Exception e) {
+				logger
+						.error("Error obtaining counts of public in vitro characterizations from grid service at"
 								+ serviceUrl);
 			}
 		}
