5d61455a861022d50e2d352010733c6cd871f1cd
Sue Pan <sue.pan@nih.gov>
2010-04-09 20:04:32 +0000

    updated to use own StringUtils
    SVN-Revision: 18102

--- src/gov/nih/nci/cananolab/ui/admin/AdministrationAction.java
+++ src/gov/nih/nci/cananolab/ui/admin/AdministrationAction.java
@@ -25,6 +25,7 @@ import gov.nih.nci.cananolab.util.Constants;
 import gov.nih.nci.cananolab.util.DateUtils;
 import gov.nih.nci.cananolab.util.ExportUtils;
 import gov.nih.nci.cananolab.util.PropertyUtils;
+import gov.nih.nci.cananolab.util.StringUtils;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
@@ -42,7 +43,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import org.apache.axis.utils.StringUtils;
 import org.apache.log4j.Logger;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
@@ -54,7 +54,7 @@ import org.apache.struts.upload.FormFile;
 
 public class AdministrationAction extends AbstractDispatchAction {
 	private static Logger logger = Logger.getLogger(AdministrationAction.class);
-	
+
 	private AdminService adminService;
 
 	/**
@@ -74,7 +74,7 @@ public class AdministrationAction extends AbstractDispatchAction {
 
 	/**
 	 * Action to clear site preferences: remove site and site logo.
-	 *
+	 * 
 	 * @param
 	 * @return
 	 */
@@ -86,14 +86,13 @@ public class AdministrationAction extends AbstractDispatchAction {
 		SitePreferenceBean siteBean = null;
 		siteBean = this.getNewSiteBean(user, null, null);
 		if (this.saveSiteBean(user, siteBean) > 0) {
-			//set success message in request if everything is fine now.
+			// set success message in request if everything is fine now.
 			ActionMessage msg = new ActionMessage(
-				"admin.sitePreference.success");
+					"admin.sitePreference.success");
 			messages.add(ActionMessages.GLOBAL_MESSAGE, msg);
 			saveMessages(request, messages);
 		} else {
-			ActionMessage msg = new ActionMessage(
-				"admin.sitePreference.error");
+			ActionMessage msg = new ActionMessage("admin.sitePreference.error");
 			messages.add(ActionMessages.GLOBAL_MESSAGE, msg);
 			saveErrors(request, messages);
 		}
@@ -102,7 +101,7 @@ public class AdministrationAction extends AbstractDispatchAction {
 
 	/**
 	 * Action to update site preference settings.
-	 *
+	 * 
 	 * @param
 	 * @return
 	 */
@@ -115,32 +114,35 @@ public class AdministrationAction extends AbstractDispatchAction {
 		UserBean user = (UserBean) request.getSession().getAttribute("user");
 		String siteName = (String) theForm.getString(Constants.SITE_NAME);
 		FormFile file = (FormFile) theForm.get(Constants.SITE_LOGO);
-		
-		//1.check if uploaded file is empty first.
+
+		// 1.check if uploaded file is empty first.
 		if (file == null || file.getFileSize() == 0) {
 			siteBean = this.getNewSiteBean(user, siteName, null);
 		} else {
-			//2.do nothing if uploaded file is too large, set error msg.
+			// 2.do nothing if uploaded file is too large, set error msg.
 			if (file.getFileSize() > Constants.MAX_LOGO_SIZE) {
 				ActionMessage msg = new ActionMessage(
 						"admin.sitePreference.error.logoTooLarge",
 						Constants.MAX_LOGO_SIZE);
 				messages.add(ActionMessages.GLOBAL_MESSAGE, msg);
 			} else {
-				//3.save uploaded file first, then update database.
+				// 3.save uploaded file first, then update database.
 				StringBuilder sb = new StringBuilder();
-				String logoFilename = this.getNewLogoFileName(file.getFileName());
+				String logoFilename = this.getNewLogoFileName(file
+						.getFileName());
 				String fileRoot = PropertyUtils.getProperty(
-					Constants.CANANOLAB_PROPERTY, Constants.FILE_REPOSITORY_DIR);
+						Constants.CANANOLAB_PROPERTY,
+						Constants.FILE_REPOSITORY_DIR);
 				sb.append(fileRoot).append(File.separator);
 				sb.append(logoFilename);
-				
+
 				OutputStream out = null;
 				try {
-					out = new BufferedOutputStream(new FileOutputStream(
-							sb.toString()));
+					out = new BufferedOutputStream(new FileOutputStream(sb
+							.toString()));
 					out.write(file.getFileData());
-					siteBean = this.getNewSiteBean(user, siteName, logoFilename);
+					siteBean = this
+							.getNewSiteBean(user, siteName, logoFilename);
 				} catch (Exception e) {
 					ActionMessage msg = new ActionMessage(
 							"admin.sitePreference.error.savingLogofile");
@@ -157,15 +159,15 @@ public class AdministrationAction extends AbstractDispatchAction {
 				}
 			}
 		}
-		//4.update database record.
+		// 4.update database record.
 		if (siteBean != null) {
 			if (this.saveSiteBean(user, siteBean) <= 0) {
 				ActionMessage msg = new ActionMessage(
-					"admin.sitePreference.error");
+						"admin.sitePreference.error");
 				messages.add(ActionMessages.GLOBAL_MESSAGE, msg);
 			}
 		}
-		//5.set success message in request if everything is fine now.
+		// 5.set success message in request if everything is fine now.
 		if (messages.isEmpty()) {
 			ActionMessage msg = new ActionMessage(
 					"admin.sitePreference.success");
@@ -179,17 +181,18 @@ public class AdministrationAction extends AbstractDispatchAction {
 
 	/**
 	 * Action for loading site preferences for cananoHeader.jsp.
-	 *
+	 * 
 	 * @param
 	 * @return
 	 */
-	public ActionForward getCaNanoHeader(ActionMapping mapping, ActionForm form,
-			HttpServletRequest request, HttpServletResponse response)
-			throws Exception {
+	public ActionForward getCaNanoHeader(ActionMapping mapping,
+			ActionForm form, HttpServletRequest request,
+			HttpServletResponse response) throws Exception {
 		SitePreferenceBean siteBean = this.getSiteBean();
 		if (siteBean != null) {
 			if (!StringUtils.isEmpty(siteBean.getSiteName())) {
-				request.setAttribute(Constants.SITE_NAME, siteBean.getSiteName());
+				request.setAttribute(Constants.SITE_NAME, siteBean
+						.getSiteName());
 			}
 			if (!StringUtils.isEmpty(siteBean.getSiteLogoFilename())) {
 				request.setAttribute("hasSiteLogo", Boolean.TRUE);
@@ -197,17 +200,17 @@ public class AdministrationAction extends AbstractDispatchAction {
 		}
 		return mapping.findForward("cananoHeader");
 	}
-	
+
 	/**
 	 * Action for loading Visitor Counter for cananoSidemenu.jsp.
-	 *
+	 * 
 	 * @param
 	 * @return
 	 */
-	public ActionForward getCaNanoSidemenu(ActionMapping mapping, ActionForm form,
-			HttpServletRequest request, HttpServletResponse response)
-			throws Exception {
-		//1.load counter from application scope, set it if not exists.
+	public ActionForward getCaNanoSidemenu(ActionMapping mapping,
+			ActionForm form, HttpServletRequest request,
+			HttpServletResponse response) throws Exception {
+		// 1.load counter from application scope, set it if not exists.
 		HttpSession session = request.getSession();
 		ServletContext context = session.getServletContext();
 		Integer counter = (Integer) context.getAttribute("visitorCounter");
@@ -216,27 +219,27 @@ public class AdministrationAction extends AbstractDispatchAction {
 			counter = counterBean.getVisitorCount();
 			context.setAttribute("visitorCount", counter);
 			context.setAttribute("countString", counter.toString());
-			context.setAttribute("counterStartDate", 
-				DateUtils.convertDateToString(counterBean.getCounterStartDate(),
+			context.setAttribute("counterStartDate", DateUtils
+					.convertDateToString(counterBean.getCounterStartDate(),
 							Constants.DATE_FORMAT));
 		}
-		//2.increase counter if requester's IP is not in session.
+		// 2.increase counter if requester's IP is not in session.
 		String visitorIP = (String) session.getAttribute("visitorIP");
 		if ((visitorIP == null || !visitorIP.equals(request.getRemoteAddr()))
-			&& request.getAttribute("justLogout") == null) {
+				&& request.getAttribute("justLogout") == null) {
 			session.setAttribute("visitorIP", request.getRemoteAddr());
 			context.setAttribute("visitorCount", ++counter);
 			context.setAttribute("countString", counter.toString());
 			this.adminService.increaseVisitorCount();
 		}
 		request.setAttribute("showVisitorCount", Boolean.TRUE);
-		
+
 		return mapping.findForward("cananoSidemenu");
 	}
-	
+
 	/**
 	 * Action to handle site logo file download request.
-	 *
+	 * 
 	 * @param
 	 * @return
 	 */
@@ -244,15 +247,16 @@ public class AdministrationAction extends AbstractDispatchAction {
 			HttpServletRequest request, HttpServletResponse response)
 			throws Exception {
 		SitePreferenceBean siteBean = this.getSiteBean();
-		if (siteBean != null && 
-			!StringUtils.isEmpty(siteBean.getSiteLogoFilename())) {
-			//1.compose logo's full file name.
+		if (siteBean != null
+				&& !StringUtils.isEmpty(siteBean.getSiteLogoFilename())) {
+			// 1.compose logo's full file name.
 			StringBuilder sb = new StringBuilder();
-			String fileRoot = PropertyUtils.getProperty(
-				Constants.CANANOLAB_PROPERTY, Constants.FILE_REPOSITORY_DIR);
+			String fileRoot = PropertyUtils
+					.getProperty(Constants.CANANOLAB_PROPERTY,
+							Constants.FILE_REPOSITORY_DIR);
 			sb.append(fileRoot).append(File.separator).append(
-				siteBean.getSiteLogoFilename());
-			//2.load logo file from file system.
+					siteBean.getSiteLogoFilename());
+			// 2.load logo file from file system.
 			File siteLogo = new File(sb.toString());
 			if (siteLogo.exists() && siteLogo.length() > 0) {
 				ExportUtils.prepareReponseForImage(response,
@@ -268,13 +272,13 @@ public class AdministrationAction extends AbstractDispatchAction {
 						out.write(bytes, 0, numRead);
 					}
 				} finally {
-					if (in != null) { //Close the InputStream
+					if (in != null) { // Close the InputStream
 						try {
 							in.close();
 						} catch (IOException e) {
 						}
 					}
-					if (out != null) {//Close the OutputStream
+					if (out != null) {// Close the OutputStream
 						try {
 							out.flush();
 							out.close();
@@ -293,7 +297,7 @@ public class AdministrationAction extends AbstractDispatchAction {
 
 	/**
 	 * Retrieve site preference bean from database.
-	 *
+	 * 
 	 * @return SitePreferenceBean.
 	 */
 	private SitePreferenceBean getSiteBean() {
@@ -313,7 +317,7 @@ public class AdministrationAction extends AbstractDispatchAction {
 
 	/**
 	 * Save site preference bean to database.
-	 *
+	 * 
 	 * @param user
 	 * @return SitePreferenceBean.
 	 */
@@ -334,24 +338,25 @@ public class AdministrationAction extends AbstractDispatchAction {
 
 	/**
 	 * Create a new site preference bean for save/update.
-	 *
+	 * 
 	 * @param user
 	 * @return SitePreferenceBean.
 	 */
-	private SitePreferenceBean getNewSiteBean(UserBean user, String siteName, String siteLogo) {
+	private SitePreferenceBean getNewSiteBean(UserBean user, String siteName,
+			String siteLogo) {
 		SitePreferenceBean siteBean = new SitePreferenceBean();
-		
+
 		siteBean.setSiteLogoFilename(siteLogo);
 		siteBean.setSiteName(siteName);
 		siteBean.setUpdatedBy(user.getLoginName());
 		siteBean.setUpdatedDate(Calendar.getInstance().getTime());
-		
+
 		return siteBean;
 	}
-	
+
 	/**
 	 * Get an unique file name for logo in format of "fileName_{timestamp}.ext".
-	 *
+	 * 
 	 * @param fileName
 	 * @return an unique file name of site logo.
 	 */
@@ -368,7 +373,7 @@ public class AdministrationAction extends AbstractDispatchAction {
 		}
 		return sb.toString();
 	}
-	
+
 	public Boolean canUserExecutePrivateDispatch(UserBean user)
 			throws SecurityException {
 		return InitSecuritySetup.getInstance().userHasAdminPrivilege(user);
@@ -376,6 +381,7 @@ public class AdministrationAction extends AbstractDispatchAction {
 
 	/**
 	 * Getter for Srping dependency injection.
+	 * 
 	 * @return
 	 */
 	public AdminService getAdminService() {
@@ -384,6 +390,7 @@ public class AdministrationAction extends AbstractDispatchAction {
 
 	/**
 	 * Setter for Srping dependency injection.
+	 * 
 	 * @param adminService
 	 */
 	public void setAdminService(AdminService adminService) {
