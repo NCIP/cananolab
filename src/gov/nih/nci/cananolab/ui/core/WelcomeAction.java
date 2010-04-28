f4c05fa01b367758ce1a107d22335545cbbbd15c
Sue Pan <sue.pan@nih.gov>
2010-04-28 18:35:51 +0000

    revert accidental check in
    SVN-Revision: 18202

--- src/gov/nih/nci/cananolab/ui/core/WelcomeAction.java
+++ src/gov/nih/nci/cananolab/ui/core/WelcomeAction.java
@@ -20,11 +20,6 @@ package gov.nih.nci.cananolab.ui.core;
 
 import gov.nih.nci.cananolab.dto.common.UserBean;
 import gov.nih.nci.cananolab.exception.SecurityException;
-import gov.nih.nci.cananolab.ui.sample.InitCharacterizationSetup;
-import gov.nih.nci.cananolab.util.Constants;
-
-import java.util.List;
-import java.util.SortedSet;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -33,7 +28,6 @@ import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 import org.apache.struts.actions.ForwardAction;
-import org.apache.struts.util.LabelValueBean;
 
 public class WelcomeAction extends ForwardAction {
 	public ActionForward execute(ActionMapping mapping, ActionForm form,
@@ -43,23 +37,6 @@ public class WelcomeAction extends ForwardAction {
 		// page.
 		InitSetup.getInstance().getGridNodesInContext(request);
 		InitSetup.getInstance().getPublicDataCountsInContext(request);
-
-		// TODO remove tmp code for wireframes
-		List<LabelValueBean> physChars = InitCharacterizationSetup
-				.getInstance().getDecoratedCharNamesByCharType(request,
-						Constants.PHYSICOCHEMICAL_CHARACTERIZATION);
-		request.getSession().setAttribute("physicoChars", physChars);
-		List<LabelValueBean> invitroChars = InitCharacterizationSetup
-				.getInstance().getDecoratedCharNamesByCharType(request,
-						Constants.INVITRO_CHARACTERIZATION);
-		List<LabelValueBean> invivoChars = InitCharacterizationSetup
-				.getInstance().getDecoratedCharNamesByCharType(request,
-						Constants.INVIVO_CHARACTERIZATION);
-		request.getSession().setAttribute("physicoChars", physChars);
-
-		request.getSession().setAttribute("physicoChars", physChars);
-		request.getSession().setAttribute("invitroChars", invitroChars);
-		request.getSession().setAttribute("invivoChars", invivoChars);
 		return super.execute(mapping, form, request, response);
 	}
 
@@ -67,7 +44,8 @@ public class WelcomeAction extends ForwardAction {
 		return false;
 	}
 
-	public boolean canUserExecute(UserBean user) throws SecurityException {
+	public boolean canUserExecute(UserBean user)
+			throws SecurityException {
 		return true;
 	}
 }
