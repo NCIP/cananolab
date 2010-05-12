b7d17b5138048db0786d389c4f58f0a0c94756ca
Sue Pan <sue.pan@nih.gov>
2010-05-12 04:41:49 +0000

    improved performance of updating dynamic dropdowns from Ajax
    SVN-Revision: 18321

--- web/javascript/ProtocolManager.js
+++ web/javascript/ProtocolManager.js
@@ -1,67 +1,60 @@
-
-var emptyOption = [{label:"", value:""}];
-function retrieveProtocols() {
+var emptyOption = [ {
+	label : "",
+	value : ""
+} ];
+function retrieveProtocolNames() {
 	var protocolType = document.getElementById("protocolType").value;
-	ProtocolManager.getProtocols(protocolType, populateProtocols);
+	ProtocolManager.getProtocolNames(protocolType, populateProtocolNames);
 }
 function resetProtocols() {
 	dwr.util.removeAllOptions("protocolName");
 	dwr.util.removeAllOptions("protocolVersion");
 	dwr.util.addOptions("protocolName", emptyOption, "value", "label");
 	dwr.util.addOptions("protocolVersion", emptyOption, "value", "label");
-	populateProtocol(null);
 }
-function populateProtocols(protocols) {
+
+function populateProtocolNames(protocolNames) {
 	resetProtocols();
-	if (protocols != null) {
-		var protocolNames = new Array();
-		var protocolVersions = new Array();
-		for (i = 0; i < protocols.length; i++) {
-			protocolNames[i] = protocols[i].domain.name;
-			protocolVersions[i] = protocols[i].domain.version;
-		}
+	if (protocolNames != null) {
 		dwr.util.addOptions("protocolName", protocolNames);
-		dwr.util.addOptions("protocolName", ["[other]"]);
+		dwr.util.addOptions("protocolName", [ "[other]" ]);
 	} else {
-		dwr.util.addOptions("protocolName", ["[other]"]);
+		dwr.util.addOptions("protocolName", [ "[other]" ]);
 	}
 }
 
 function retrieveProtocolVersions() {
 	var protocolType = document.getElementById("protocolType").value;
 	var protocolName = document.getElementById("protocolName").value;
-	ProtocolManager.getProtocolsByTypeAndName(protocolType, protocolName, populateProtocolVersions);
+	ProtocolManager.getProtocolVersions(protocolType, protocolName,
+			populateProtocolVersions);
 }
 
-function populateProtocolVersions(protocols) {
+function populateProtocolVersions(protocolVersions) {
 	dwr.util.removeAllOptions("protocolVersion");
 	dwr.util.addOptions("protocolVersion", emptyOption, "value", "label");
-
-	if (protocols != null) {
-		var protocolVersions = new Array();
-		for (i = 0; i < protocols.length; i++) {
-			protocolVersions[i] = protocols[i].domain.version;
-		}
+	if (protocolVersions != null) {
 		dwr.util.addOptions("protocolVersion", protocolVersions);
-		dwr.util.addOptions("protocolVersion", ["[other]"]);
+		dwr.util.addOptions("protocolVersion", [ "[other]" ]);
 	} else {
-		dwr.util.addOptions("protocolVersion", ["[other]"]);
+		dwr.util.addOptions("protocolVersion", [ "[other]" ]);
 	}
 }
 var appOwner;
 function retrieveProtocol(applicationOwner) {
-	appOwner=applicationOwner;
+	appOwner = applicationOwner;
 	var protocolType = document.getElementById("protocolType").value;
 	var protocolName = document.getElementById("protocolName").value;
 	var protocolVersion = document.getElementById("protocolVersion").value;
-	ProtocolManager.getProtocol(protocolType, protocolName, protocolVersion, populateProtocol);
+	ProtocolManager.getProtocol(protocolType, protocolName, protocolVersion,
+			populateProtocol);
 }
 function populateProtocol(protocol) {
 	if (protocol == null) {
 		writeLink(null);
 		dwr.util.setValue("fileTitle", "");
 		dwr.util.setValue("fileDescription", "");
-		dwr.util.setValue("visibility", [""]);
+		dwr.util.setValue("visibility", [ "" ]);
 		dwr.util.setValue("protocolAbbreviation", "");
 		return;
 	}
@@ -70,7 +63,8 @@ function populateProtocol(protocol) {
 	dwr.util.setValue("visibility", protocol.visibilityGroups);
 	if (protocol.fileBean != null) {
 		dwr.util.setValue("fileTitle", protocol.fileBean.domainFile.title);
-		dwr.util.setValue("fileDescription", protocol.fileBean.domainFile.description);
+		dwr.util.setValue("fileDescription",
+				protocol.fileBean.domainFile.description);
 		dwr.util.setValue("fileId", protocol.fileBean.domainFile.id);
 		dwr.util.setValue("fileUri", protocol.fileBean.domainFile.uri);
 		dwr.util.setValue("fileName", protocol.fileBean.domainFile.name);
@@ -89,17 +83,18 @@ function writeLink(protocol) {
 		fileId = protocol.fileBean.domainFile.id;
 	}
 	if (uri != null && fileId != null) {
-		document.getElementById("protocolFileLink").innerHTML = "<a href='searchProtocol.do?dispatch=download&amp;location="+appOwner+"&amp;fileId=" + fileId + "'>" + uri + "</a>";
+		document.getElementById("protocolFileLink").innerHTML = "<a href='searchProtocol.do?dispatch=download&amp;location="
+				+ appOwner + "&amp;fileId=" + fileId + "'>" + uri + "</a>";
 	} else {
 		document.getElementById("protocolFileLink").innerHTML = "";
 	}
 }
 function setProtocolNameDropdown() {
-	var searchLocations = getSelectedOptions(document.getElementById("searchLocations"));
-	ProtocolManager.getProtocolTypes(searchLocations, function (data) {
+	var searchLocations = getSelectedOptions(document
+			.getElementById("searchLocations"));
+	ProtocolManager.getProtocolTypes(searchLocations, function(data) {
 		dwr.util.removeAllOptions("protocolType");
 		dwr.util.addOptions("protocolType", data);
 	});
 	return false;
 }
-
