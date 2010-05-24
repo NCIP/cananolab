4fd12c4cd09c842696e51520ba9cdc9002f6e792
Sue Pan <sue.pan@nih.gov>
2010-05-24 17:38:55 +0000

    SVN-Revision: 18449

--- cananolab-war/db/1.5/data_migration/fix/expand_value_column.sql
+++
@@ -1,9 +0,0 @@
-use canano;
-
--- Expand datum.value from decimal(22,3) to decimal(30,10).
-alter table canano.datum
-change value value decimal(30,10) not null;
-
--- Expand associated_element.value from decimal(22,3) to decimal(30,10).
-alter table canano.associated_element
-change value value decimal(30,10);
