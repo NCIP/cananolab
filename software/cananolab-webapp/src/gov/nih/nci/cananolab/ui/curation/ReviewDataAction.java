d03459eb26bb32222f94094094eaa5e90964413c
Sue Pan <sue.pan@nih.gov>
2010-06-17 16:49:38 +0000

    updated for 1.5.2 wireframes
    SVN-Revision: 18503

---
+++ software/cananolab-webapp/src/gov/nih/nci/cananolab/ui/curation/ReviewDataAction.java
@@ -0,0 +1,49 @@
+package gov.nih.nci.cananolab.ui.curation;
+
+/**
+ * This class sets up the submit a new sample page and submits a new sample
+ *
+ * @author pansu
+ */
+
+/* CVS $Id: SubmitNanoparticleAction.java,v 1.37 2008-09-18 21:35:25 cais Exp $ */
+
+import gov.nih.nci.cananolab.ui.core.BaseAnnotationAction;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+import org.apache.struts.validator.DynaValidatorForm;
+
+public class ReviewDataAction extends BaseAnnotationAction {
+	// logger
+	// private static Logger logger =
+	// Logger.getLogger(ReviewDataAction.class);
+
+	/**
+	 * Handle edit sample request on sample search result page (curator view).
+	 *
+	 * @param mapping
+	 * @param form
+	 * @param request
+	 * @param response
+	 * @return
+	 * @throws Exception
+	 */
+	public ActionForward setup(ActionMapping mapping, ActionForm form,
+			HttpServletRequest request, HttpServletResponse response)
+			throws Exception {
+		DynaValidatorForm theForm = (DynaValidatorForm) form;
+		return mapping.findForward("setup");
+	}
+
+	public ActionForward input(ActionMapping mapping, ActionForm form,
+			HttpServletRequest request, HttpServletResponse response)
+			throws Exception {
+		DynaValidatorForm theForm = (DynaValidatorForm) form;
+		return mapping.findForward("setup");
+	}
+}
