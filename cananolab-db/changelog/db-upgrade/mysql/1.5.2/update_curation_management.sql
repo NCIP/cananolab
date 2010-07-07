7bc19c003165857a41df491019bf768d9e51377b
Sue Pan <sue.pan@nih.gov>
2010-07-07 17:11:32 +0000

    updated comments from -- to /* */
    SVN-Revision: 18576

--- cananolab-db/changelog/db-upgrade/mysql/1.5.2/update_curation_management.sql
+++ cananolab-db/changelog/db-upgrade/mysql/1.5.2/update_curation_management.sql
@@ -1,4 +1,4 @@
---remove protection on sample, publication, protocol
+/* remove protection on sample, publication, protocol */
 DELETE FROM csm_user_group_role_pg
  WHERE protection_group_id IN
           (SELECT protection_group_id
@@ -19,22 +19,22 @@ DELETE FROM csm_protection_group
 DELETE FROM csm_protection_element
  WHERE protection_element_name IN ('sample', 'publication', 'protocol');
 
--- remove application owner from Curator group
+/* remove application owner from Curator group */
 UPDATE csm_group
    SET group_name = 'Curator'
  WHERE group_name = 'NCICBIIT_DataCurator';
 
--- update pe to use sample ID
+/* update pe to use sample ID */
 update csm_protection_element pe, sample s
 set pe.protection_element_name=s.sample_pk_id, pe.object_id=s.sample_pk_id
 where pe.protection_element_name=s.sample_name;
 
--- update pg to use sample ID
+/* update pg to use sample ID */
 update csm_protection_group pg, sample s
 set pg.protection_group_name=s.sample_pk_id
 where pg.protection_group_name=s.sample_name;
 
---update data to be CURD by curator
+/* update data to be CURD by curator */
 update csm_user_group_role_pg
 set role_id=5
 where role_id=1
