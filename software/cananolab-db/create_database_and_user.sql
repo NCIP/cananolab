28569c2caedce3ec219da9fa4837e3f3bc35e1f8
Sue Pan <sue.pan@nih.gov>
2010-07-12 21:48:19 +0000

    updated directory structure
    SVN-Revision: 18622

--- software/cananolab-db/create_database_and_user.sql
+++
@@ -1,8 +0,0 @@
-DROP DATABASE IF EXISTS canano;
-CREATE DATABASE canano
-  CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-
-DELETE FROM mysql.user WHERE User='@database.user@';
-
-GRANT SELECT,INSERT,UPDATE,DELETE ON canano.* TO '@database.user@'@'localhost' IDENTIFIED BY '@database.password@' WITH GRANT OPTION;
-GRANT SELECT,INSERT,UPDATE,DELETE ON canano.* TO '@database.user@'@'%' IDENTIFIED BY '@database.password@' WITH GRANT OPTION;
