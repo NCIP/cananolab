9920061900bf274614cc3ad55a38f838871cd176
Sue Pan <sue.pan@nih.gov>
2010-07-14 15:11:35 +0000

    removed alter table on csm_user_group_role_pg
    SVN-Revision: 18655

--- cananolab-db/sql/mysql/1.5.2/update_curator_access.sql
+++ cananolab-db/sql/mysql/1.5.2/update_curator_access.sql
@@ -5,14 +5,8 @@ where role_id=1
 and group_id=2;
 
 /* insert public data to be CURD by curator */
-ALTER TABLE csm_user_group_role_pg
- CHANGE user_group_role_pg_id user_group_role_pg_id BIGINT(20) AUTO_INCREMENT NOT NULL;
-
 insert into csm_user_group_role_pg
 (group_id, role_id, update_date, protection_group_id)
 select distinct '2', '5', sysdate(), protection_group_id
 from csm_user_group_role_pg
 where role_id=1;
-
-ALTER TABLE csm_user_group_role_pg
- CHANGE user_group_role_pg_id user_group_role_pg_id BIGINT(20) NOT NULL;
