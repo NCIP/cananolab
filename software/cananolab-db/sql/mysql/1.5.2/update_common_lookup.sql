4d1fb594f56acb441205a01d4f655f9e7f49e982
Thai Le <lethai>
2010-07-14 15:11:11 +0000

    rearrange name and attribute
    SVN-Revision: 18654

--- software/cananolab-db/sql/mysql/1.5.2/update_common_lookup.sql
+++ software/cananolab-db/sql/mysql/1.5.2/update_common_lookup.sql
@@ -13,23 +13,12 @@ INSERT INTO common_lookup (name,attribute,value) VALUES ('MINChar','entity','sur
 INSERT INTO common_lookup (name,attribute,value) VALUES ('MINChar','entity','particle size/size distribution');
 INSERT INTO common_lookup (name,attribute,value) VALUES ('MINChar','entity','chemical composition');
 
-INSERT INTO common_lookup (name,attribute,value) VALUES ('purity','caNano2MINChar','purity');
-INSERT INTO common_lookup (name,attribute,value) VALUES ('shape', 'caNano2MINChar','shape');
-INSERT INTO common_lookup (name,attribute,value) VALUES ('surface area ', 'caNano2MINChar','surface area');
-INSERT INTO common_lookup (name,attribute,value) VALUES ('surface charge', 'caNano2MINChar','surface charge');
-INSERT INTO common_lookup (name,attribute,value) VALUES ('zeta potential', 'caNano2MINChar','surface charge');
-INSERT INTO common_lookup (name,attribute,value) VALUES ('attachment', 'caNano2MINChar','surface chemistry');
-INSERT INTO common_lookup (name,attribute,value) VALUES ('entrapment', 'caNano2MINChar','surface chemistry');
-INSERT INTO common_lookup (name,attribute,value) VALUES ('size', 'caNano2MINChar','particle size/size distribution');
-INSERT INTO common_lookup (name,attribute,value) VALUES ('sample composition', 'caNano2MINChar','chemical composition');
-
 INSERT INTO common_lookup (name,attribute,value) VALUES ('caNano2MINChar','purity','purity');
 INSERT INTO common_lookup (name,attribute,value) VALUES ('caNano2MINChar','shape', 'shape');
 INSERT INTO common_lookup (name,attribute,value) VALUES ('caNano2MINChar','surface area ', 'surface area');
 INSERT INTO common_lookup (name,attribute,value) VALUES ('caNano2MINChar','surface charge', 'surface charge');
 INSERT INTO common_lookup (name,attribute,value) VALUES ('caNano2MINChar','zeta potential', 'surface charge');
 INSERT INTO common_lookup (name,attribute,value) VALUES ('caNano2MINChar','attachment','surface chemistry');
-INSERT INTO common_lookup (name,attribute,value) VALUES ('caNano2MINChar','entrapment', 'surface chemistry');
 INSERT INTO common_lookup (name,attribute,value) VALUES ('caNano2MINChar','size', 'particle size/size distribution');
 INSERT INTO common_lookup (name,attribute,value) VALUES ('caNano2MINChar','sample composition','chemical composition');
 
