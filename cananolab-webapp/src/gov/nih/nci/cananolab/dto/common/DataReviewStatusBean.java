8a233ceda5b430f317538ec3fa0d800d859d6e43
Sue Pan <sue.pan@nih.gov>
2010-07-15 06:35:31 +0000

    first deposit for release 1.5.2
    SVN-Revision: 18671

---
+++ cananolab-webapp/src/gov/nih/nci/cananolab/dto/common/DataReviewStatusBean.java
@@ -0,0 +1,83 @@
+package gov.nih.nci.cananolab.dto.common;
+
+import java.util.Date;
+
+public class DataReviewStatusBean {
+	private String dataId;
+	private String dataName;
+	private String dataType;
+	private Date submittedDate;
+	private String submittedBy;
+	private String reviewStatus;
+	private String reviewLink;
+
+	private final static String SAMPLE_LINK_PREFIX = "sample.do?dispatch=summaryEdit&page=0&sampleId=";
+	private final static String PUBLICATION_LINK_PREFIX = "publication.do?page=0&dispatch=setupUpdate&publicationId=";
+	private final static String PROTOCOL_LINK_PREFIX = "protocol.do?dispatch=setupUpdate&protocolId=";
+
+	public DataReviewStatusBean() {
+	}
+
+	public String getDataId() {
+		return dataId;
+	}
+
+	public void setDataId(String dataId) {
+		this.dataId = dataId;
+	}
+
+	public String getDataName() {
+		return dataName;
+	}
+
+	public void setDataName(String dataName) {
+		this.dataName = dataName;
+	}
+
+	public String getDataType() {
+		return dataType;
+	}
+
+	public void setDataType(String dataType) {
+		this.dataType = dataType;
+	}
+
+	public Date getSubmittedDate() {
+		return submittedDate;
+	}
+
+	public void setSubmittedDate(Date submittedDate) {
+		this.submittedDate = submittedDate;
+	}
+
+	public String getSubmittedBy() {
+		return submittedBy;
+	}
+
+	public void setSubmittedBy(String submittedBy) {
+		this.submittedBy = submittedBy;
+	}
+
+	public String getReviewStatus() {
+		return reviewStatus;
+	}
+
+	public void setReviewStatus(String reviewStatus) {
+		this.reviewStatus = reviewStatus;
+	}
+
+	public String getReviewLink() {
+		if (dataType.equalsIgnoreCase("sample")) {
+			reviewLink = SAMPLE_LINK_PREFIX;
+		} else if (dataType.equalsIgnoreCase("publication")) {
+			reviewLink = PUBLICATION_LINK_PREFIX;
+		} else if (dataType.equalsIgnoreCase("protocol")) {
+			reviewLink = PROTOCOL_LINK_PREFIX;
+		}
+		else {
+			reviewLink="";
+		}
+		reviewLink += dataId;
+		return reviewLink;
+	}
+}
