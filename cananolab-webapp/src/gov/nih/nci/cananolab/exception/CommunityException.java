38435b826850a42e10ce8a3306e331508b041999
Sue Pan <sue.pan@nih.gov>
2010-07-15 06:46:25 +0000

    first deposit for release 1.5.2
    SVN-Revision: 18678

--- cananolab-webapp/src/gov/nih/nci/cananolab/exception/CommunityException.java
+++ cananolab-webapp/src/gov/nih/nci/cananolab/exception/CommunityException.java
@@ -2,14 +2,14 @@ package gov.nih.nci.cananolab.exception;
 
 /**
  * @author pansu
- * 
+ *
  */
 public class CommunityException extends BaseException {
 
 	private static final long serialVersionUID = 1234567890L;
 
 	public CommunityException() {
-		super("Exception working with particles");
+		super("Exception working with community related services");
 	}
 
 	public CommunityException(String message) {
