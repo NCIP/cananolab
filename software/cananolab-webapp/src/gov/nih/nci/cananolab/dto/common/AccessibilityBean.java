dcaf550b5329b6949afdd12d45354dec22684f0b
Sue Pan <sue.pan@nih.gov>
2010-07-16 18:21:45 +0000

    added constants and roleDisplayName
    SVN-Revision: 18693

--- software/cananolab-webapp/src/gov/nih/nci/cananolab/dto/common/AccessibilityBean.java
+++ software/cananolab-webapp/src/gov/nih/nci/cananolab/dto/common/AccessibilityBean.java
@@ -1,9 +1,15 @@
 package gov.nih.nci.cananolab.dto.common;
 
+import gov.nih.nci.cananolab.util.Constants;
+
 public class AccessibilityBean {
 	private UserBean user = new UserBean();
 	private String groupName;
 	private String roleName;
+	private String roleDisplayName;
+
+	public static final String CURD_ROLE_DISPLAY_NAME = "read update delete";
+	public static final String R_ROLE_DISPLAY_NAME = "read";
 
 	public AccessibilityBean() {
 	}
@@ -32,4 +38,15 @@ public class AccessibilityBean {
 		this.roleName = roleName;
 	}
 
+	public String getRoleDisplayName() {
+		if (roleName == null) {
+			return null;
+		}
+		else if (roleName.equals(Constants.CSM_CURD_ROLE)) {
+			roleDisplayName = this.CURD_ROLE_DISPLAY_NAME;
+		} else if (roleName.equals(Constants.CSM_READ_ROLE)) {
+			roleDisplayName = this.R_ROLE_DISPLAY_NAME;
+		}
+		return roleDisplayName;
+	}
 }
