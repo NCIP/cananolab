52d510dd48ac6cf2574f26c4ca3fd0191af2c8c0
Sue Pan <sue.pan@nih.gov>
2010-07-19 17:29:40 +0000

    new for release 1.5.2
    SVN-Revision: 18701

---
+++ cananolab-webapp/src/gov/nih/nci/cananolab/ui/core/AbstractForwardAction.java
@@ -0,0 +1,46 @@
+package gov.nih.nci.cananolab.ui.core;
+
+import gov.nih.nci.cananolab.dto.common.UserBean;
+import gov.nih.nci.cananolab.exception.InvalidSessionException;
+import gov.nih.nci.cananolab.exception.NoAccessException;
+import gov.nih.nci.cananolab.exception.SecurityException;
+import gov.nih.nci.cananolab.util.Constants;
+import gov.nih.nci.cananolab.util.StringUtils;
+
+import java.util.Enumeration;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+import org.apache.struts.actions.ForwardAction;
+
+public abstract class AbstractForwardAction extends ForwardAction {
+
+	public ActionForward execute(ActionMapping mapping, ActionForm form,
+			HttpServletRequest request, HttpServletResponse response)
+			throws Exception {
+		saveToken(request);
+		if (loginRequired()) {
+			HttpSession session = request.getSession();
+			UserBean user = (UserBean) session.getAttribute("user");
+			if (user == null) {
+				throw new NoAccessException("Log in is required");
+			}
+		}
+		return super.execute(mapping, form, request, response);
+	}
+
+	/**
+	 * Check whether the current user can execute the action with the specified
+	 * dispatch
+	 *
+	 * @param user
+	 * @return
+	 * @throws SecurityException
+	 */
+	public abstract boolean loginRequired();
+}
