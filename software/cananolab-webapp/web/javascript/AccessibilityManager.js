09e9bc6151889af056265809ca132cd953ff4458
Sue Pan <sue.pan@nih.gov>
2010-07-22 18:59:05 +0000

    updated setTheAccess
    SVN-Revision: 18781

--- software/cananolab-webapp/web/javascript/AccessibilityManager.js
+++ software/cananolab-webapp/web/javascript/AccessibilityManager.js
@@ -1,19 +1,26 @@
 function displayAccessNameLabel() {
-	var byGroup = document.getElementById("accessByGroup");
-	var byUser = document.getElementById("accessByUser");
-	if (byGroup.checked && !byUser.checked) {
-		dwr.util.setValue("accessNameLabel", "Collaboration Group Name");
+	var byGroup = dwr.util.getValue("byGroup");
+	var byUser = dwr.util.getValue("byUser");
+	if (byGroup == true && byUser == false) {
+		dwr.util.setValue("accessNameLabel", "Collaboration Group Name *");
 		show("groupName");
 		hide("userName");
-	} else {
-		dwr.util.setValue("accessNameLabel", "User Login Name");
+	} else if (byGroup == false && byUser == true) {
+		dwr.util.setValue("accessNameLabel", "User Login Name *");
 		hide("groupName");
 		show("userName");
 	}
 }
 
-function setTheAccess(accessId) {
-	AccessManager.getAccessById(accessId, populateAccess);
+function setTheAccess(parentFormName, accessType, accessName, parentType,
+		protectedData) {
+	if (accessType == "group") {
+		AccessibilityManager.getGroupAccess(parentFormName, accessName,
+				parentType, protectedData, populateAccess);
+	} else {
+		AccessibilityManager.getUserAccess(parentFormName, accessName,
+				parentType, protectedData, populateAccess);
+	}
 	show("deleteAccess");
 	hide("newAccessLabel");
 	openSubmissionForm("Access");
@@ -22,22 +29,29 @@ function setTheAccess(accessId) {
 // Populate the Access form.
 function populateAccess(access) {
 	if (access != null) {
-		if (access.name != null) {
-			dwr.util.setValue("accessName", access.name);
-			dwr.util.setValue("accessDescription", access.description);
-			dwr.util.setValue("accessId", access.id);
+		if (access.groupAccess == true) {
+			dwr.util.setValue("byGroup", true);
+			dwr.util.setValue("groupName", access.groupName);
+		} else {
+			dwr.util.setValue("byUser", true);
+			dwr.util.setValue("userName", access.userBean.loginName);
 		}
+		dwr.util.setValue("roleName", access.roleName);
 	} else {
 		sessionTimeout();
 	}
 }
 
+function addAccess(actionName) {
+	enableButtons( [ "submitButton", "resetButton" ]);
+	submitAction(document.forms[0], actionName, "saveAccess", 4);
+}
+
 function clearAccess() {
 	// go to server and clean form bean
-	AccessManager.resetTheAccess(populateAccess);
+	AccessibilityManager.resetTheAccess('sampleForm', 'sample', populateAccess);
 	hide("deleteAccess");
 	hide("newUser");
-	numberOfUserAccesses = 0;
 }
 
 function deleteTheAccess() {
