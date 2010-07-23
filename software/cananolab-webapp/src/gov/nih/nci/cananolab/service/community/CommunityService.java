a6e29ee3f6418f1ac3384b20380c764a5ec87717
Sue Pan <sue.pan@nih.gov>
2010-07-23 03:01:26 +0000

    updated to extend BaseService
    SVN-Revision: 18793

--- software/cananolab-webapp/src/gov/nih/nci/cananolab/service/community/CommunityService.java
+++ software/cananolab-webapp/src/gov/nih/nci/cananolab/service/community/CommunityService.java
@@ -2,13 +2,16 @@ package gov.nih.nci.cananolab.service.community;
 
 import gov.nih.nci.cananolab.dto.common.CollaborationGroupBean;
 import gov.nih.nci.cananolab.exception.CommunityException;
+import gov.nih.nci.cananolab.exception.DuplicateEntriesException;
 import gov.nih.nci.cananolab.exception.NoAccessException;
+import gov.nih.nci.cananolab.service.BaseService;
 
 import java.util.List;
 
-public interface CommunityService {
+public interface CommunityService extends BaseService {
 	public void saveCollaborationGroup(CollaborationGroupBean collaborationGroup)
-			throws CommunityException, NoAccessException;
+			throws CommunityException, NoAccessException,
+			DuplicateEntriesException;
 
 	public List<CollaborationGroupBean> findCollaborationGroups()
 			throws CommunityException;
