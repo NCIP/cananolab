048f7c99d622430a6ae6f046d7b7f33703a5bc42
Sue Pan <sue.pan@nih.gov>
2010-08-02 18:41:50 +0000

    added SecurityService as an argument
    SVN-Revision: 18872

--- software/cananolab-webapp/src/gov/nih/nci/cananolab/service/curation/CurationService.java
+++ software/cananolab-webapp/src/gov/nih/nci/cananolab/service/curation/CurationService.java
@@ -2,13 +2,21 @@ package gov.nih.nci.cananolab.service.curation;
 
 import gov.nih.nci.cananolab.dto.common.DataReviewStatusBean;
 import gov.nih.nci.cananolab.exception.CurationException;
+import gov.nih.nci.cananolab.exception.NoAccessException;
+import gov.nih.nci.cananolab.service.security.SecurityService;
 
 import java.util.List;
 
 public interface CurationService {
-	public void submitDataForReview(DataReviewStatusBean dataReviewStatusBean)
-			throws CurationException;
+	public void submitDataForReview(DataReviewStatusBean dataReviewStatusBean,
+			SecurityService securityService) throws CurationException,
+			NoAccessException;
 
-	public List<DataReviewStatusBean> findDataPendingReview()
-			throws CurationException;
+	public List<DataReviewStatusBean> findDataPendingReview(
+			SecurityService securityService) throws CurationException,
+			NoAccessException;
+
+	public DataReviewStatusBean findDataReviewStatusBeanByDataId(String dataId,
+			SecurityService securityService) throws CurationException,
+			NoAccessException;
 }
