d29c57f22ecec0c3b578cd1706fdc8f9207750dc
Thai Le <lethai>
2010-08-03 21:23:44 +0000

    modified to use SecurityService
    SVN-Revision: 18900

--- cananolab-webapp/src/gov/nih/nci/cananolab/service/sample/DataAvailabilityService.java
+++ cananolab-webapp/src/gov/nih/nci/cananolab/service/sample/DataAvailabilityService.java
@@ -1,9 +1,11 @@
 package gov.nih.nci.cananolab.service.sample;
 
-import gov.nih.nci.cananolab.dto.common.UserBean;
 import gov.nih.nci.cananolab.dto.particle.DataAvailabilityBean;
 import gov.nih.nci.cananolab.dto.particle.SampleBean;
 import gov.nih.nci.cananolab.exception.DataAvailabilityException;
+import gov.nih.nci.cananolab.exception.NoAccessException;
+import gov.nih.nci.cananolab.exception.SecurityException;
+import gov.nih.nci.cananolab.service.security.SecurityService;
 
 import java.util.List;
 
@@ -12,11 +14,14 @@ public interface DataAvailabilityService {
 	/**
 	 * find data availability for a sample
 	 * @param sampleId
+	 * @param securityService
 	 * @return
 	 * @throws DataAvailabilityException
+	 * @throws NoAccessException
+	 * @throws SecurityException
 	 */
-	public List<DataAvailabilityBean> findDataAvailabilityBySampleId(String sampleId) 
-		throws DataAvailabilityException;
+	public List<DataAvailabilityBean> findDataAvailabilityBySampleId(String sampleId, SecurityService securityService) 
+		throws DataAvailabilityException, NoAccessException, SecurityException;
 	
 	/**
 	 * generate data availability for the sample and persist to
@@ -25,24 +30,41 @@ public interface DataAvailabilityService {
 	 * @param user
 	 * @return
 	 * @throws DataAvailabilityException
-	 */
+	 *//*
 	public List<DataAvailabilityBean> generateDataAvailability(SampleBean sampleBean, UserBean user) 
-		throws DataAvailabilityException;
+		throws DataAvailabilityException;*/
+	/**
+	 * generate data availability for the sample and persist to
+	 * database table
+	 * @param sampleBean
+	 * @param securityService
+	 * @return
+	 * @throws DataAvailabilityException
+	 * @throws NoAccessException 
+	 * @throws SecurityException 
+	 */
+	public List<DataAvailabilityBean> generateDataAvailability(SampleBean sampleBean, SecurityService securityService) 
+		throws DataAvailabilityException, NoAccessException, SecurityException;
 	
 	/**
 	 * find update to data availability for the sample and persist to database table
 	 * @param sampleBean
-	 * @param user
+	 * @param securityService
 	 * @return
 	 * @throws DataAvailabilityException
+	 * @throws NoAccessException
+	 * @throws SecurityException
 	 */
-	public List<DataAvailabilityBean> saveDataAvailability(SampleBean sampleBean, UserBean user) 
-		throws DataAvailabilityException;
+	public List<DataAvailabilityBean> saveDataAvailability(SampleBean sampleBean, SecurityService securityService) 
+		throws DataAvailabilityException, NoAccessException, SecurityException;
 	
 	/**
 	 * delete data availability for the sample from database table
 	 * @param sampleId
+	 * @param securityService
 	 * @throws DataAvailabilityException
+	 * @throws NoAccessException
+	 * @throws SecurityException
 	 */
-	public void deleteDataAvailability(String sampleId) throws DataAvailabilityException;
+	public void deleteDataAvailability(String sampleId, SecurityService securityService) throws DataAvailabilityException, NoAccessException, SecurityException;
 }
