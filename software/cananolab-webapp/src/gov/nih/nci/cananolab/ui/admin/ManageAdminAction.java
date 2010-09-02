d86a3855bfed1165ed7fdb504a090853574f11a6
Sue Pan <sue.pan@nih.gov>
2010-09-02 17:47:32 +0000

    added to control access to the tab
    SVN-Revision: 19154

---
+++ software/cananolab-webapp/src/gov/nih/nci/cananolab/ui/admin/ManageAdminAction.java
@@ -0,0 +1,46 @@
+/*
+ The caNanoLab Software License, Version 1.5.2
+
+ Copyright 2006 SAIC. This software was developed in conjunction with the National
+ Cancer Institute, and so to the extent government employees are co-authors, any
+ rights in such works shall be subject to Title 17 of the United States Code,
+ section 105.
+
+ */
+package gov.nih.nci.cananolab.ui.admin;
+
+/**
+ * This class calls the Struts ForwardAction to forward to a page
+ *
+ * @author pansu
+ */
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+
+import gov.nih.nci.cananolab.dto.common.UserBean;
+import gov.nih.nci.cananolab.exception.NoAccessException;
+import gov.nih.nci.cananolab.ui.core.AbstractForwardAction;
+
+public class ManageAdminAction extends AbstractForwardAction {
+	public ActionForward execute(ActionMapping mapping, ActionForm form,
+			HttpServletRequest request, HttpServletResponse response)
+			throws Exception {
+		HttpSession session = request.getSession();
+		UserBean user = (UserBean) session.getAttribute("user");
+		if (user != null && !user.isAdmin()) {
+			throw new NoAccessException(
+					"You need to be an administrator to access the page");
+		}
+		return super.execute(mapping, form, request, response);
+	}
+
+	public boolean loginRequired() {
+		return true;
+	}
+}
