db00f99a65517dcae02b6cfb1e6b1f14604d3798
Thai Le <lethai>
2010-09-09 17:17:14 +0000

    SVN-Revision: 19219

---
+++ software/cananolab-webapp/web/javascript/TransferOwnerManager.js
@@ -0,0 +1,31 @@
+
+function showMatchedUserDropdown(userLoginName, styleId,cancelStyleId) {
+	// display progress.gif while waiting for the response.
+	show("loaderImg");
+	hide(styleId);
+	hide(cancelStyleId);
+	var selected = dwr.util.getValue(styleId);
+	var loginName = dwr.util.getValue(userLoginName);
+	
+	AccessibilityManager.getMatchedUsers("", loginName, function(data) {
+		dwr.util.removeAllOptions(styleId);
+		dwr.util.addOptions(styleId, data, "loginName",
+				"fullName");
+		dwr.util.setValue(styleId, selected);
+		hide("loaderImg");
+		show(styleId);
+		show(cancelStyleId);
+	});
+}
+
+function updateOwnerLoginName(ownerLoginName, styleId, cancelStyleId) {
+	var selected = dwr.util.getValue(styleId);
+	dwr.util.setValue(ownerLoginName, selected);
+	hide(styleId);
+	hide(cancelStyleId);
+}
+
+function cancelBrowseSelect(styleId, cancelStyleId) {
+	hide(styleId);
+	hide(cancelStyleId);
+}
