0616443632a5b4be726b93c7af03eea6cd914846
Sue Pan <sue.pan@nih.gov>
2010-10-06 20:37:15 +0000

    updated to handle a list of long running processes
    SVN-Revision: 19396

--- cananolab-webapp/src/gov/nih/nci/cananolab/ui/core/ManageResultAction.java
+++ cananolab-webapp/src/gov/nih/nci/cananolab/ui/core/ManageResultAction.java
@@ -17,7 +17,10 @@ package gov.nih.nci.cananolab.ui.core;
 
 import gov.nih.nci.cananolab.dto.common.UserBean;
 import gov.nih.nci.cananolab.exception.NoAccessException;
-import gov.nih.nci.cananolab.service.sample.impl.BatchDataAvailabilityProcess;
+import gov.nih.nci.cananolab.service.common.LongRunningProcess;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -26,8 +29,6 @@ import javax.servlet.http.HttpSession;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
-import org.apache.struts.action.ActionMessage;
-import org.apache.struts.action.ActionMessages;
 
 public class ManageResultAction extends AbstractForwardAction {
 	public ActionForward execute(ActionMapping mapping, ActionForm form,
@@ -39,65 +40,27 @@ public class ManageResultAction extends AbstractForwardAction {
 			throw new NoAccessException(
 					"You need to be a curator to access the page");
 		}
-		ActionMessages messages = new ActionMessages();
-		String processType = request.getParameter("processType");
-		if (processType == null) {
-			processType = (String) session.getAttribute("processType");
-			if (processType == null) {
-				ActionMessage msg = new ActionMessage(
-						"message.batchDataAvailability",
-						"Please specify a process type in the request");
-				messages.add(ActionMessages.GLOBAL_MESSAGE, msg);
-				saveMessages(request, messages);
-				request.setAttribute("processComplete", true);
-				//hide the results tab
-				session.setAttribute("hasResultsWaiting", false);
-				return super.execute(mapping, form, request, response);
-			}
-		}
-		if (processType.equals("dataAvailability")) {
-			// get the batch process from the session
-			return manageDataAvailability(mapping, form, request, response,
-					messages);
-		}
-		return super.execute(mapping, form, request, response);
-	}
-
-	private ActionForward manageDataAvailability(ActionMapping mapping,
-			ActionForm form, HttpServletRequest request,
-			HttpServletResponse response, ActionMessages messages)
-			throws Exception {
-		HttpSession session = request.getSession();
-		BatchDataAvailabilityProcess batchDataAvailabilityProcess = (BatchDataAvailabilityProcess) session
-				.getAttribute("batchDataAvailabilityProcess");
-
-		if (batchDataAvailabilityProcess != null) {
-			ActionMessage msg = new ActionMessage(
-					"message.batchDataAvailability",
-					batchDataAvailabilityProcess.getStatusMessage());
-			messages.add(ActionMessages.GLOBAL_MESSAGE, msg);
-			if (batchDataAvailabilityProcess.isWithError()) {
-				saveErrors(request, messages);
-			} else {
-				saveMessages(request, messages);
-			}
-			if (batchDataAvailabilityProcess.isComplete()) {
-				request.setAttribute("processComplete", true);
-				//
-				// Once the process is complete, remove the binding from
-				// the session.
-				//
-				session.removeAttribute("batchDataAvailabilityProcess");
-				session.removeAttribute("processType");
-			}
+		// obtain the list of long running processes
+		List<LongRunningProcess> processes = (List<LongRunningProcess>) session
+				.getAttribute("longRunningProcesses");
+		if (processes == null || processes.isEmpty()) {
+			// hide the results tab
+			session.setAttribute("hasResultsWaiting", false);
 			return super.execute(mapping, form, request, response);
 		} else {
-			ActionMessage msg = new ActionMessage(
-					"message.batchDataAvailability",
-					"No batch data availability process started");
-			messages.add(ActionMessages.GLOBAL_MESSAGE, msg);
-			saveMessages(request, messages);
-			request.setAttribute("processComplete", true);
+			// store the previous state for JSP display
+			session.setAttribute("previousLongRunningProcesses", processes);
+			List<LongRunningProcess> updatedProcesses = new ArrayList<LongRunningProcess>(
+					processes);
+			int i = 0;
+			// remove completed process
+			for (LongRunningProcess process : processes) {
+				if (process.isComplete()) {
+					updatedProcesses.remove(i);
+				}
+				i++;
+			}
+			session.setAttribute("longRunningProcesses", updatedProcesses);
 			return super.execute(mapping, form, request, response);
 		}
 	}
