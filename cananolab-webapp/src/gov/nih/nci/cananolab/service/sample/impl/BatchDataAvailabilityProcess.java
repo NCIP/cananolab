b93e7891672831ca431190958c87cb2237146c63
Sue Pan <sue.pan@nih.gov>
2010-10-06 20:35:40 +0000

    updated constructor to take a list of sampleIds and updated to extend LongRunningProcess
    SVN-Revision: 19393

--- cananolab-webapp/src/gov/nih/nci/cananolab/service/sample/impl/BatchDataAvailabilityProcess.java
+++ cananolab-webapp/src/gov/nih/nci/cananolab/service/sample/impl/BatchDataAvailabilityProcess.java
@@ -2,9 +2,11 @@ package gov.nih.nci.cananolab.service.sample.impl;
 
 import gov.nih.nci.cananolab.dto.common.AccessibilityBean;
 import gov.nih.nci.cananolab.dto.common.UserBean;
+import gov.nih.nci.cananolab.service.common.LongRunningProcess;
 import gov.nih.nci.cananolab.service.sample.DataAvailabilityService;
 import gov.nih.nci.cananolab.service.sample.SampleService;
 import gov.nih.nci.cananolab.service.security.SecurityService;
+import gov.nih.nci.cananolab.util.ClassUtils;
 
 import java.util.List;
 
@@ -16,104 +18,117 @@ import org.apache.log4j.Logger;
  * @author pansu
  *
  */
-public class BatchDataAvailabilityProcess implements Runnable {
+public class BatchDataAvailabilityProcess extends LongRunningProcess {
 	public static final String BATCH_OPTION1 = "generate all";
 	public static final String BATCH_OPTION2 = "regenerate old";
 	public static final String BATCH_OPTION3 = "delete all";
 
 	private DataAvailabilityService dataAvailabilityService;
-	private String batchOption;
 	private SecurityService securityService;
-	private SampleService sampleService;
-
+	private String batchOption;
 	private Logger logger = Logger
 			.getLogger(BatchDataAvailabilityProcess.class);
-	private boolean complete = false;
-	private boolean running = false;
-	private String statusMessage = "Data availability batch process has not been started";
-	private boolean withError = false;
+	private List<String> sampleIds = null; // sample Ids that need to run batch
 
+	/**
+	 * Find the sampleIds to process
+	 *
+	 * @param dataAvailabilityService
+	 * @param securityService
+	 * @param batchOption
+	 * @param user
+	 * @throws Exception
+	 */
 	public BatchDataAvailabilityProcess(
 			DataAvailabilityService dataAvailabilityService,
-			String batchOption, UserBean user) throws Exception {
+			SecurityService securityService, String batchOption, UserBean user)
+			throws Exception {
 		this.dataAvailabilityService = dataAvailabilityService;
+		this.securityService = securityService;
 		this.batchOption = batchOption;
-		securityService = new SecurityService(AccessibilityBean.CSM_APP_NAME,
-				user);
-		sampleService = new SampleServiceLocalImpl(securityService);
+		SampleService sampleService = new SampleServiceLocalImpl(
+				securityService);
 		logger.info("New data availability batch process is created");
+		// find all sampleIds
+		List<String> allSampleIds = sampleService.findSampleIdsBy("", "", null,
+				null, null, null, null, null, null, null, null);
+		// find sampleIds with existing data availability
+		List<String> dataAvailabilitySampleIds = dataAvailabilityService
+				.findSampleIdsWithDataAvailability(securityService);
+		if (batchOption.equals(BATCH_OPTION1)
+				|| batchOption.equals(BATCH_OPTION3)) {
+			sampleIds = allSampleIds;
+		} else {
+			sampleIds = dataAvailabilitySampleIds;
+		}
+		this.processType = ClassUtils
+				.getDisplayName("BatchDataAvailabilityProcess");
 	}
 
 	/**
-	 * Hides threading details from the web app.
+	 * Pass in the sample Ids that needs to run batch data availability process
+	 *
+	 * @param dataAvailabilityService
+	 * @param sampleIds
+	 * @param batchOption
+	 * @param user
+	 * @throws Exception
 	 */
-	public void process() {
-		if (!isRunning()) {
-			this.statusMessage = "Starting data availability batch process with option "
-					+ batchOption;
-			try {
-				Thread t = new Thread(this);
-				// Daemon threads are used for background supporting tasks and
-				// are only needed while normal threads are executing. If normal
-				// threads are not running and remaining threads are daemon
-				// threads then the JVM exits.
-				t.setDaemon(true);
-				t.start();
-
-				//
-				// don't return until the new thread is running.
-				//
-				while (false == this.isRunning()) {
-					logger.info("Trying to run the new thread");
-				}
-			} catch (Exception e) {
-				this.statusMessage = "Data availability batch process with option "
-						+ batchOption + " is terminated with error";
-				this.withError = true;
-				e.printStackTrace();
-				logger.error(this.statusMessage, e);
-			}
-		}
+	public BatchDataAvailabilityProcess(
+			DataAvailabilityService dataAvailabilityService,
+			SecurityService securityService, List<String> sampleIds,
+			String batchOption, UserBean user) throws Exception {
+		this.dataAvailabilityService = dataAvailabilityService;
+		this.securityService = securityService;
+		this.batchOption = batchOption;
+		this.sampleIds = sampleIds;
+		securityService = new SecurityService(AccessibilityBean.CSM_APP_NAME,
+				user);
+		this.processType = ClassUtils
+				.getDisplayName("BatchDataAvailabilityProcess");
+		logger.info("New data availability batch process is created");
 	}
 
 	/**
 	 * Triggers the long running process.
 	 */
 	public void run() {
-		this.statusMessage = "Running batch data availability process with option "
-				+ batchOption;
-		logger.info(this.statusMessage);
+		String optionMessage = "";
+		if (batchOption.equals(BatchDataAvailabilityProcess.BATCH_OPTION1)) {
+			optionMessage = "Generate data availability for all samples";
+		} else if (batchOption
+				.equals(BatchDataAvailabilityProcess.BATCH_OPTION2)) {
+			optionMessage = "Regenerate data availability for samples with existing data availability";
+		} else if (batchOption
+				.equals(BatchDataAvailabilityProcess.BATCH_OPTION3)) {
+			optionMessage = "Delete existing data availability";
+		}
+		this.statusMessage = "Running the option \"" + optionMessage + ".\"";
+		logger.info("Running batch data availability process with option "
+				+ batchOption);
 		try {
 			this.running = true;
 			this.complete = false;
-			// find all sampleIds
-			List<String> sampleIds = sampleService.findSampleIdsBy("", "",
-					null, null, null, null, null, null, null, null, null);
-
 			long start = System.currentTimeMillis();
-			// generate data availability for all sampleIds
-			if (batchOption.equalsIgnoreCase(BATCH_OPTION1)) {
-				dataAvailabilityService.saveBatchDataAvailability(sampleIds,
-						securityService);
-			}
-			// regenerate data availability for sampleIds with existing ones
-			else if (batchOption.equalsIgnoreCase(BATCH_OPTION2)) {
-				// find sampleIds with existing data availability
-				List<String> dataAvailabilitySampleIds = dataAvailabilityService
-						.findSampleIdsWithDataAvailability(securityService);
-				dataAvailabilityService.saveBatchDataAvailability(
-						dataAvailabilitySampleIds, securityService);
+			// generate data availability for sampleIds
+			int numFailures = 0;
+			if (batchOption.equalsIgnoreCase(BATCH_OPTION1)
+					|| batchOption.equalsIgnoreCase(BATCH_OPTION2)) {
+				numFailures = dataAvailabilityService
+						.saveBatchDataAvailability(sampleIds, securityService);
 			} else if (batchOption.equalsIgnoreCase(BATCH_OPTION3)) {
-				dataAvailabilityService.deleteBatchDataAvailability(sampleIds,
-						securityService);
+				numFailures = dataAvailabilityService
+						.deleteBatchDataAvailability(sampleIds, securityService);
 			}
 			long end = System.currentTimeMillis();
 			long duration = (end - start) / 1000;
-			logger.info("total time to run option " + batchOption + ": "
-					+ duration + " seconds.");
-			this.statusMessage = "Data availability batch process with option "
+			int numSuccesses = sampleIds.size() - numFailures;
+
+			this.statusMessage = "Completed the option \"" + optionMessage + "\" for "+ numSuccesses
+					+ " out of " + sampleIds.size() + " samples after "+duration+" seconds.";
+			logger.info("Data availability batch process with option "
 					+ batchOption + " is completed after " + duration
-					+ " second(s)";
+					+ " second(s)");
 			this.complete = true;
 			this.running = false;
 		} catch (Exception e) {
@@ -122,25 +137,8 @@ public class BatchDataAvailabilityProcess implements Runnable {
 			this.complete = true;
 			this.running = false;
 			this.withError = true;
-			e.printStackTrace();
 			logger.error(e);
 			return;
 		}
-	}
-
-	public boolean isComplete() {
-		return complete;
-	}
-
-	public boolean isRunning() {
-		return running;
-	}
-
-	public String getStatusMessage() {
-		return statusMessage;
-	}
-
-	public boolean isWithError() {
-		return withError;
 	}
 }
