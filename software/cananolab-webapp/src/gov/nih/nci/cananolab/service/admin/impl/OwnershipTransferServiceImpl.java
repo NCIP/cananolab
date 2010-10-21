c4cd0568630c4f5c10368dca9a922530617b7c85
Thai Le <lethai>
2010-10-21 20:16:12 +0000

    SVN-Revision: 19594

--- software/cananolab-webapp/src/gov/nih/nci/cananolab/service/admin/impl/OwnershipTransferServiceImpl.java
+++ software/cananolab-webapp/src/gov/nih/nci/cananolab/service/admin/impl/OwnershipTransferServiceImpl.java
@@ -672,11 +672,13 @@ public class OwnershipTransferServiceImpl implements OwnershipTransferService {
 		if (copyIndex >= 0) {
 			newCreatedBy = newOwner + ":" + existingOwner.substring(copyIndex);
 		} else {
-			String test = existingOwner.substring(0, currentOwner.length());
-			if (test.equals(currentOwner)) {
-				newCreatedBy = newOwner;
-			}else{
-				newCreatedBy = existingOwner;
+			if(existingOwner.length() >= currentOwner.length()){
+				String test = existingOwner.substring(0, currentOwner.length());
+				if (test.equals(currentOwner)) {
+					newCreatedBy = newOwner;
+				}else{
+					newCreatedBy = existingOwner;
+				}
 			}
 		}
 		return newCreatedBy;
