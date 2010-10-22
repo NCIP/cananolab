bce6e872d1d943408f8da3c3c488dd47f52cec65
Sue Pan <sue.pan@nih.gov>
2010-10-22 13:32:37 +0000

    updated showMatchedUserDropdown to pass in search str
    SVN-Revision: 19608

--- software/cananolab-webapp/web/javascript/TransferOwnerManager.js
+++ software/cananolab-webapp/web/javascript/TransferOwnerManager.js
@@ -1,16 +1,14 @@
-
-function showMatchedUserDropdown(userLoginName, styleId,cancelStyleId) {
+function showMatchedUserDropdown(userLoginName, styleId, cancelStyleId) {
 	// display progress.gif while waiting for the response.
 	show("loaderImg");
 	hide(styleId);
 	hide(cancelStyleId);
 	var selected = dwr.util.getValue(styleId);
 	var loginName = dwr.util.getValue(userLoginName);
-	
-	AccessibilityManager.getUsers( function(data) {
+
+	AccessibilityManager.getUsers(loginName, function(data) {
 		dwr.util.removeAllOptions(styleId);
-		dwr.util.addOptions(styleId, data, "loginName",
-				"fullName");
+		dwr.util.addOptions(styleId, data, "loginName", "displayName");
 		dwr.util.setValue(styleId, selected);
 		hide("loaderImg");
 		show(styleId);
