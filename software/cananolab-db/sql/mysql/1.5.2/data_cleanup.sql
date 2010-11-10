85917dbdfc696a6c8ff6579326d0950ec3789fbf
Sue Pan <sue.pan@nih.gov>
2010-11-10 20:32:04 +0000

    updated zero data to null
    SVN-Revision: 19764

--- software/cananolab-db/sql/mysql/1.5.2/data_cleanup.sql
+++ software/cananolab-db/sql/mysql/1.5.2/data_cleanup.sql
@@ -35,3 +35,40 @@ DELETE FROM csm_protection_element
        AND protection_element_name NOT IN
               ('csmupt', 'caNanoLab', 'curation')
        AND protection_element_name NOT LIKE 'CollaborationGroup%';
+
+/* update 0 data to null */
+UPDATE associated_element
+   SET value = NULL
+ WHERE value = 0;
+
+UPDATE fullerene
+   SET average_diameter = NULL
+ WHERE average_diameter = 0;
+
+UPDATE dendrimer
+   SET generation = NULL
+ WHERE generation = 0;
+
+UPDATE carbon_nanotube
+   SET average_length = NULL
+ WHERE average_length = 0;
+
+UPDATE carbon_nanotube
+   SET diameter = NULL
+ WHERE diameter = 0;
+
+UPDATE fullerene
+   SET number_of_carbon = NULL
+ WHERE number_of_carbon = 0;
+
+UPDATE polymer
+   SET cross_link_degree = NULL
+ WHERE cross_link_degree = 0;
+
+UPDATE shape
+   SET aspect_ratio = NULL
+ WHERE aspect_ratio = 0;
+
+UPDATE solubility
+   SET critical_concentration = NULL
+ WHERE critical_concentration = 0;
