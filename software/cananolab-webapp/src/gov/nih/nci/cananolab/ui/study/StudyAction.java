dd7f39dcbcc3745eaf1ffa9f8b448898bf18b1a4
Thai Le <lethai>
2010-12-03 19:30:07 +0000

    study related actions
    SVN-Revision: 19848

---
+++ software/cananolab-webapp/src/gov/nih/nci/cananolab/ui/study/StudyAction.java
@@ -0,0 +1,42 @@
+package gov.nih.nci.cananolab.ui.study;
+
+/**
+ * This class sets up the submit a new study page and submits a new study.
+ *
+ * @author houyh
+ */
+import gov.nih.nci.cananolab.ui.core.BaseAnnotationAction;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+
+public class StudyAction extends BaseAnnotationAction {
+	// logger
+	// private static Logger logger = Logger.getLogger(StudyAction.class);
+	public static final String PAGE_TITLE = "pageTitle";
+
+	public ActionForward input(ActionMapping mapping, ActionForm form,
+			HttpServletRequest request, HttpServletResponse response)
+			throws Exception {
+		return mapping.findForward("inputForm");
+	}
+	
+	public ActionForward setupNew(ActionMapping mapping, ActionForm form,
+			HttpServletRequest request, HttpServletResponse response)
+			throws Exception {
+		request.setAttribute(PAGE_TITLE, "Submit New Study");
+		return mapping.getInputForward();
+	}
+
+	public ActionForward setupUpdate(ActionMapping mapping, ActionForm form,
+			HttpServletRequest request, HttpServletResponse response)
+			throws Exception {
+		request.setAttribute(PAGE_TITLE, "Update Study");
+		return mapping.findForward("studyEdit");
+	}
+
+}
