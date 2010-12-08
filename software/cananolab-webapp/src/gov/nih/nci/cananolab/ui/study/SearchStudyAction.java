63d90c9da2e5a1c3dfc2e786164eaf8a2eb5af8b
Thai Le <lethai>
2010-12-08 18:44:58 +0000

    updated for search study
    SVN-Revision: 19856

--- software/cananolab-webapp/src/gov/nih/nci/cananolab/ui/study/SearchStudyAction.java
+++ software/cananolab-webapp/src/gov/nih/nci/cananolab/ui/study/SearchStudyAction.java
@@ -5,7 +5,12 @@ package gov.nih.nci.cananolab.ui.study;
  *
  * @author houyh
  */
+import gov.nih.nci.cananolab.dto.common.StudyBean;
+import gov.nih.nci.cananolab.dto.particle.SampleBean;
 import gov.nih.nci.cananolab.ui.core.BaseAnnotationAction;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -33,6 +38,30 @@ public class SearchStudyAction extends BaseAnnotationAction {
 	public ActionForward search(ActionMapping mapping, ActionForm form,
 			HttpServletRequest request, HttpServletResponse response)
 			throws Exception {
+		
+		StudyBean study1 = new StudyBean();
+		study1.setStudyName("study 1");
+		//SampleBean studySample = new SampleBean();
+		//studySample.se
+		study1.setStudySample(new SampleBean());
+		study1.setOwnerName("Guest");
+		study1.setPointOfContact("Guest");
+		
+		StudyBean study2 = new StudyBean();
+		study1.setStudyName("study 1");
+		//SampleBean studySample = new SampleBean();
+		//studySample.se
+		study2.setStudySample(new SampleBean());
+		study2.setOwnerName("Guest2");
+		study2.setPointOfContact("Guest2");
+		List<StudyBean> studyBeansPerPage = new ArrayList<StudyBean>();
+		studyBeansPerPage.add(study1);
+		studyBeansPerPage.add(study2);
+		request.setAttribute("studies", studyBeansPerPage);
+		// get the total size of collection , required for display tag to
+		// get the pagination to work
+		request.setAttribute("resultSize", new Integer(20));
 		return mapping.findForward("searchResult");
 	}
+	
 }
