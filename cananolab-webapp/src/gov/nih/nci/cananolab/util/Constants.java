9a16a475384ba11ef9a457a8aba8df4b9e04908e
Sue Pan <sue.pan@nih.gov>
2010-12-09 19:51:46 +0000

    updated UNIT_PATTERN to include numbers
    SVN-Revision: 19870

--- cananolab-webapp/src/gov/nih/nci/cananolab/util/Constants.java
+++ cananolab-webapp/src/gov/nih/nci/cananolab/util/Constants.java
@@ -69,7 +69,7 @@ public class Constants {
 
 	public static final String TEXTFIELD_WHITELIST_PATTERN = "^[a-zA-Z0-9\\-\\_\\s\\(\\)\\:\\.\\/\\?\\*]*$";
 
-	public static final String UNIT_PATTERN = "^[a-zA-Z\\%\\/\\-\\_\\s\\(\\)\\^]*$";
+	public static final String UNIT_PATTERN = "^[a-zA-Z0-9\\%\\/\\-\\_\\s\\(\\)\\^]*$";
 
 	public static final String ALPHANUMERIC_PATTERN = "^[a-zA-Z0-9]*$";
 
