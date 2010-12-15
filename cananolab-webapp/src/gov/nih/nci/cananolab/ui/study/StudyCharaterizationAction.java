d4814f013575ee2afe4b46dad9edc873c6dede7f
Sue Pan <sue.pan@nih.gov>
2010-12-15 21:57:50 +0000

    first deposit for release 1.6
    SVN-Revision: 19899

---
+++ cananolab-webapp/src/gov/nih/nci/cananolab/ui/study/StudyCharaterizationAction.java
@@ -0,0 +1,50 @@
+package gov.nih.nci.cananolab.ui.study;
+
+/**
+ * This class sets up the study sample submission page and submits a new sample.
+ *
+ * @author houyh
+ */
+import gov.nih.nci.cananolab.ui.core.BaseAnnotationAction;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+
+public class StudyCharaterizationAction extends BaseAnnotationAction {
+	// logger
+	// private static Logger logger = Logger.getLogger(StudyAction.class);
+
+	public ActionForward input(ActionMapping mapping, ActionForm form,
+			HttpServletRequest request, HttpServletResponse response)
+			throws Exception {
+		return mapping.findForward("inputForm");
+	}
+
+	public ActionForward summaryEdit(ActionMapping mapping, ActionForm form,
+			HttpServletRequest request, HttpServletResponse response)
+			throws Exception {
+		return mapping.findForward("summaryEdit");
+	}
+
+	public ActionForward summaryView(ActionMapping mapping, ActionForm form,
+			HttpServletRequest request, HttpServletResponse response)
+			throws Exception {
+		return mapping.findForward("summaryView");
+	}
+
+	public ActionForward setupNew(ActionMapping mapping, ActionForm form,
+			HttpServletRequest request, HttpServletResponse response)
+			throws Exception {
+		return mapping.getInputForward();
+	}
+
+	public ActionForward setupUpdate(ActionMapping mapping, ActionForm form,
+			HttpServletRequest request, HttpServletResponse response)
+			throws Exception {
+		return mapping.getInputForward();
+	}
+}
