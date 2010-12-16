a96e165d5bde230bb30587d1fd6c6e339587acea
Thai Le <lethai>
2010-12-16 21:37:41 +0000

    SVN-Revision: 19922

--- cananolab-webapp/src/gov/nih/nci/cananolab/dto/common/StudyDecorator.java
+++ cananolab-webapp/src/gov/nih/nci/cananolab/dto/common/StudyDecorator.java
@@ -1,6 +1,7 @@
 package gov.nih.nci.cananolab.dto.common;
 
 import gov.nih.nci.cananolab.exception.BaseException;
+import gov.nih.nci.cananolab.service.security.UserBean;
 import gov.nih.nci.cananolab.util.SortableName;
 
 import org.displaytag.decorator.TableDecorator;
@@ -9,21 +10,23 @@ import org.displaytag.decorator.TableDecorator;
  * This decorator is used to for decorate different properties of a study to be
  * shown properly in the view page using display tag lib.
  *
- * @author pansu
+ * @author lethai
  *
  */
 public class StudyDecorator extends TableDecorator {
-
+ 
 	public String getDetailURL() {
+		UserBean user = (UserBean)this.getPageContext().getSession().getAttribute("user");
+		
 		
 		//SampleBean sample = (SampleBean) getCurrentRowObject();
 		//String sampleId = sample.getDomain().getId().toString();
 		String dispatch = "summaryView";
 		String linkLabel = "View";
-		/*if (sample.getUserUpdatable()) {
+		if (user!= null) {
 			dispatch = "summaryEdit";
 			linkLabel = "Edit";
-		}*/
+		}
 		StringBuilder sb = new StringBuilder("<a href=");
 		sb.append("study.do?dispatch=").append(dispatch).append(
 				"&page=0&studyId=");
@@ -35,7 +38,7 @@ public class StudyDecorator extends TableDecorator {
 	}
 
 	public SortableName getStudyName() {
-		String studyName = "study test";
+		String studyName = "Efficacy of nanoparticle";
 		return new SortableName(studyName);
 	}
 	public SortableName getSampleName() {
@@ -56,6 +59,6 @@ public class StudyDecorator extends TableDecorator {
 
 	public String getPointOfContactName() throws BaseException {
 		//SampleBean sample = (SampleBean) getCurrentRowObject();
-		return "Guest";
+		return "leth";
 	}
 }
