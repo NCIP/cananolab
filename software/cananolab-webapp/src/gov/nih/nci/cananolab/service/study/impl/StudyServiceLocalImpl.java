7247e2632ea875c2774591824d8577d0a2b3703a
Thai Le <lethai>
2010-12-21 21:24:52 +0000

    updated to return non null results
    SVN-Revision: 19973

--- software/cananolab-webapp/src/gov/nih/nci/cananolab/service/study/impl/StudyServiceLocalImpl.java
+++ software/cananolab-webapp/src/gov/nih/nci/cananolab/service/study/impl/StudyServiceLocalImpl.java
@@ -3,23 +3,18 @@
  */
 package gov.nih.nci.cananolab.service.study.impl;
 
-import java.util.List;
-
-import org.apache.log4j.Logger;
-
 import gov.nih.nci.cananolab.dto.common.StudyBean;
 import gov.nih.nci.cananolab.exception.NoAccessException;
 import gov.nih.nci.cananolab.exception.StudyException;
 import gov.nih.nci.cananolab.service.BaseServiceLocalImpl;
-import gov.nih.nci.cananolab.service.publication.impl.PublicationServiceLocalImpl;
-import gov.nih.nci.cananolab.service.sample.helper.AdvancedSampleServiceHelper;
-import gov.nih.nci.cananolab.service.sample.helper.SampleServiceHelper;
-import gov.nih.nci.cananolab.service.sample.impl.CharacterizationServiceLocalImpl;
-import gov.nih.nci.cananolab.service.sample.impl.CompositionServiceLocalImpl;
-import gov.nih.nci.cananolab.service.sample.impl.SampleServiceLocalImpl;
 import gov.nih.nci.cananolab.service.security.SecurityService;
 import gov.nih.nci.cananolab.service.security.UserBean;
 import gov.nih.nci.cananolab.service.study.StudyService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.log4j.Logger;
 
 /**
  * @author lethai
@@ -64,13 +59,13 @@ public class StudyServiceLocalImpl extends BaseServiceLocalImpl implements Study
 	public StudyBean findStudyById(String id, Boolean loadAccessInfo)
 			throws StudyException, NoAccessException {
 		// TODO Auto-generated method stub
-		return null;
+		return new StudyBean();
 	}
 
 
 	public List<String> findStudyIdsBy() throws StudyException{
 		// TODO Auto-generated method stub
-		return null;
+		return new ArrayList<String>();
 	}
 
 
