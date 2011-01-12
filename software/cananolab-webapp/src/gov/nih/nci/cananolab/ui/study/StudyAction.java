daee9a5dd119dd3b55b49d1ddebcb8cc4a1bab56
Thai Le <lethai>
2011-01-12 21:31:23 +0000

    updated for study search feature
    SVN-Revision: 20063

--- software/cananolab-webapp/src/gov/nih/nci/cananolab/ui/study/StudyAction.java
+++ software/cananolab-webapp/src/gov/nih/nci/cananolab/ui/study/StudyAction.java
@@ -5,7 +5,13 @@ package gov.nih.nci.cananolab.ui.study;
  *
  * @author houyh, lethai
  */
+import gov.nih.nci.cananolab.dto.common.StudyBean;
+import gov.nih.nci.cananolab.exception.NotExistException;
+import gov.nih.nci.cananolab.service.security.SecurityService;
+import gov.nih.nci.cananolab.service.study.StudyService;
+import gov.nih.nci.cananolab.service.study.impl.StudyServiceLocalImpl;
 import gov.nih.nci.cananolab.ui.core.BaseAnnotationAction;
+import gov.nih.nci.cananolab.util.StringUtils;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -13,6 +19,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
+import org.apache.struts.validator.DynaValidatorForm;
 
 public class StudyAction extends BaseAnnotationAction {
 	// logger
@@ -51,6 +58,30 @@ public class StudyAction extends BaseAnnotationAction {
 	public ActionForward summaryView(ActionMapping mapping, ActionForm form,
 			HttpServletRequest request, HttpServletResponse response)
 			throws Exception {
+		
+		DynaValidatorForm theForm = (DynaValidatorForm) form;
+		this.setServiceInSession(request);
+		
+		String studyId = request.getParameter("studyId");
+		if (!StringUtils.isEmpty(studyId)) {
+			theForm.set("studyId", studyId);
+		} else {
+			studyId = (String) request.getAttribute("studyId");
+			if (studyId == null) {
+				studyId = theForm.getString("studyId");
+			}
+		}
+		// study service has been created earlier
+		StudyService service = (StudyService) request.getSession()
+				.getAttribute("studyService");
+
+		StudyBean studyBean = service.findStudyById(studyId, true);
+		if (studyBean == null) {
+			throw new NotExistException("No such study in the system");
+		}
+		request.setAttribute("theStudy", studyBean);
+		
+		theForm.set("studyBean", studyBean);
 		return mapping.findForward("summaryView");
 	}
 
@@ -65,5 +96,15 @@ public class StudyAction extends BaseAnnotationAction {
 			HttpServletRequest request, HttpServletResponse response)
 			throws Exception {
 		return mapping.findForward("summaryViewPerSample");
+	}
+	
+	private StudyService setServiceInSession(HttpServletRequest request)
+		throws Exception {
+		SecurityService securityService = super
+				.getSecurityServiceFromSession(request);
+		StudyService studyService = new StudyServiceLocalImpl(
+				securityService);
+		request.getSession().setAttribute("studyService", studyService);
+		return studyService;
 	}
 }
