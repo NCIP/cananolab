2dcb8d57377a6dbdd08d9b5dbe3c4d4d94ed6c5f
Sue Pan <sue.pan@nih.gov>
2011-01-14 15:09:31 +0000

    added populateTextAreaWithSelection
    SVN-Revision: 20085

--- cananolab-webapp/web/javascript/StudyManager.js
+++ cananolab-webapp/web/javascript/StudyManager.js
@@ -6,16 +6,15 @@ function showMatchedDesignTypes() {
 	hide("searchMessage");
 	var searchStr = dwr.util.getValue("searchStr");
 	StudyManager.getMatchedDesignTypes(searchStr, function(data) {
-		if (data.length>0) {
-		  dwr.util.removeAllOptions("matchedTermSelect");
-		  dwr.util.addOptions("matchedTermSelect", data);
-		  hide("loaderImg");
-		  hide("searchMessage");
-		  show("matchedTermSelect");
-		  show("addButton");
-		}
-		else {
-		  show("searchMessage");
+		if (data.length > 0) {
+			dwr.util.removeAllOptions("matchedTermSelect");
+			dwr.util.addOptions("matchedTermSelect", data);
+			hide("loaderImg");
+			hide("searchMessage");
+			show("matchedTermSelect");
+			show("addButton");
+		} else {
+			show("searchMessage");
 		}
 	});
 }
@@ -25,23 +24,5 @@ function populateDesignTypes() {
 	hide("termBrowser");
 	hide("matchedTermSelect");
 	hide("addButton");
-	var selected = dwr.util.getValue("matchedTermSelect");
-	if (selected.length == 0) {
-		return;
-	}
-	var existingTypes=dwr.util.getValue("designTypes");
-	var types="";
-	if (existingTypes=="") {
-		types=existingTypes;
-	}
-	else {
-		types=existingTypes+"\n";
-	}
-	for (i=0; i<selected.length-1; i++) {
-		types=types+selected[i]+"\n";
-	}
-	types=types+selected[i];
-	dwr.util.setValue("designTypes", types, {
-		escapeHtml : false
-	});
+	populateTextAreaWithSelection("designTypes", "matchedTermSelect");
 }
