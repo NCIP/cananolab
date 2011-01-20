de251b3b2c185de687f1be80fd1d60eece779cd7
Sue Pan <sue.pan@nih.gov>
2011-01-20 20:11:15 +0000

    updated getMatchedDiseases
    SVN-Revision: 20117

--- cananolab-webapp/web/javascript/StudyManager.js
+++ cananolab-webapp/web/javascript/StudyManager.js
@@ -1,57 +1,58 @@
 function showMatchedDesignTypes(ind) {
 	// display progress.gif while waiting for the response.
-	show("loaderImg"+ind);
-	hide("matchedTermSelect"+ind);
-	hide("selectButton"+ind);
-	hide("searchMessage"+ind);
-	var searchStr = dwr.util.getValue("searchStr"+ind);
+	show("loaderImg" + ind);
+	hide("matchedTermSelect" + ind);
+	hide("selectButton" + ind);
+	hide("searchMessage" + ind);
+	var searchStr = dwr.util.getValue("searchStr" + ind);
 	StudyManager.getMatchedDesignTypes(searchStr, function(data) {
 		if (data.length > 0) {
-			dwr.util.removeAllOptions("matchedTermSelect"+ind);
-			dwr.util.addOptions("matchedTermSelect"+ind, data);
-			hide("loaderImg"+ind);
-			hide("searchMessage"+ind);
-			show("matchedTermSelect"+ind);
-			show("addButton"+ind);
+			dwr.util.removeAllOptions("matchedTermSelect" + ind);
+			dwr.util.addOptions("matchedTermSelect" + ind, data);
+			hide("loaderImg" + ind);
+			hide("searchMessage" + ind);
+			show("matchedTermSelect" + ind);
+			show("addButton" + ind);
 		} else {
-			show("searchMessage"+ind);
+			show("searchMessage" + ind);
 		}
 	});
 }
 
 function populateDesignTypes(ind) {
 	// display progress.gif while waiting for the response.
-	hide("termBrowser"+ind);
-	hide("matchedTermSelect"+ind);
-	hide("addButton"+ind);
-	populateTextAreaWithSelection("designTypes", "matchedTermSelect"+ind);
+	hide("termBrowser" + ind);
+	hide("matchedTermSelect" + ind);
+	hide("addButton" + ind);
+	populateTextAreaWithSelection("designTypes", "matchedTermSelect" + ind);
 }
 
 function showMatchedDiseases(ind) {
 	// display progress.gif while waiting for the response.
-	show("loaderImg"+ind);
-	hide("matchedTermSelect"+ind);
-	hide("selectButton"+ind);
-	hide("searchMessage"+ind);
-	var searchStr = dwr.util.getValue("searchStr"+ind);
-	StudyManager.getMatchedDesignTypes(searchStr, function(data) {
+	show("loaderImg" + ind);
+	hide("matchedTermSelect" + ind);
+	hide("selectButton" + ind);
+	hide("searchMessage" + ind);
+	var searchStr = dwr.util.getValue("searchStr" + ind);
+	var algorithm = dwr.util.getValue("algorithm" + ind);
+	StudyManager.getMatchedDiseases(searchStr, algorithm, function(data) {
 		if (data.length > 0) {
-			dwr.util.removeAllOptions("matchedTermSelect"+ind);
-			dwr.util.addOptions("matchedTermSelect"+ind, data);
-			hide("loaderImg"+ind);
-			hide("searchMessage"+ind);
-			show("matchedTermSelect"+ind);
-			show("addButton"+ind);
+			dwr.util.removeAllOptions("matchedTermSelect" + ind);
+			dwr.util.addOptions("matchedTermSelect" + ind, data);
+			hide("loaderImg" + ind);
+			hide("searchMessage" + ind);
+			show("matchedTermSelect" + ind);
+			show("addButton" + ind);
 		} else {
-			show("searchMessage"+ind);
+			show("searchMessage" + ind);
 		}
 	});
 }
 
 function populateDiseases(ind) {
 	// display progress.gif while waiting for the response.
-	hide("termBrowser"+ind);
-	hide("matchedTermSelect"+ind);
-	hide("addButton"+ind);
-	populateTextAreaWithSelection("diseases", "matchedTermSelect"+ind);
+	hide("termBrowser" + ind);
+	hide("matchedTermSelect" + ind);
+	hide("addButton" + ind);
+	populateTextAreaWithSelection("diseases", "matchedTermSelect" + ind);
 }
