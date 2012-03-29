ac33c3076dfd6f4c86ac19044deec6d6a1813558
Sue Pan <sue.pan@nih.gov>
2012-03-29 20:05:00 +0000

    SVN-Revision: 20454

---
+++ cananolab-webapp/web/ONLINE_HELP_caNanoLab_1_5_2/wwhdata/js/search/pairs/pair11.js
@@ -0,0 +1,23 @@
+function FileData_Pairs(x)
+{
+x.t("searching","existing");
+x.t("protocols","managing");
+x.t("protocols","overview");
+x.t("protocols","cananolab");
+x.t("protocols","function");
+x.t("protocols","section");
+x.t("managing","protocols");
+x.t("describes","manage");
+x.t("includes","following");
+x.t("overview","submitting");
+x.t("cananolab","protocols");
+x.t("function","section");
+x.t("section","describes");
+x.t("section","includes");
+x.t("submitting","protocol");
+x.t("existing","protocols");
+x.t("topics","protocols");
+x.t("protocol","searching");
+x.t("manage","protocols");
+x.t("following","topics");
+}
