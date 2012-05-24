288f5e79bb40f6b2f1ea1e9f83f69aedbf875132
Sue Pan <sue.pan@nih.gov>
2012-05-24 14:29:51 +0000

    remediated for jboss 5.1 upgrade
    SVN-Revision: 20581

--- software/cananolab-webapp/src/gov/nih/nci/cananolab/ui/core/SchedulerPlugin.java
+++ software/cananolab-webapp/src/gov/nih/nci/cananolab/ui/core/SchedulerPlugin.java
@@ -3,10 +3,7 @@ package gov.nih.nci.cananolab.ui.core;
 import gov.nih.nci.cananolab.service.CSMCleanupJob;
 import gov.nih.nci.cananolab.service.PublicDataCountJob;
 
-import java.util.Date;
-
 import javax.servlet.ServletConfig;
-import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 
 import org.apache.log4j.Logger;
@@ -19,14 +16,12 @@ import org.quartz.SchedulerException;
 import org.quartz.SimpleTrigger;
 import org.quartz.Trigger;
 import org.quartz.TriggerUtils;
-import org.quartz.ee.servlet.QuartzInitializerServlet;
 import org.quartz.impl.StdSchedulerFactory;
 
 /**
- * Create a scheduler using Quartz when container starts. Borrowed concepts from
- * LSD browser application.
+ * Create a scheduler using Quartz when container starts. 
  * 
- * @author pansu, sahnih
+ * @author pansu
  * 
  */
 public class SchedulerPlugin implements PlugIn {
@@ -38,42 +33,10 @@ public class SchedulerPlugin implements PlugIn {
 	public void init(ActionServlet actionServlet, ModuleConfig config)
 			throws ServletException {
 		logger.info("Initializing Scheduler Plugin for Jobs...");
-		ServletContext context = actionServlet.getServletContext();
-		// Retrieve the factory from the ServletContext.
-		// It will be put there by the Quartz Servlet
-		StdSchedulerFactory factory = (StdSchedulerFactory) context
-				.getAttribute(QuartzInitializerServlet.QUARTZ_FACTORY_KEY);
 		try {
-			// // Start the scheduler in case, it isn't started yet
-			// if (m_startOnLoad != null
-			// && m_startOnLoad.equals(Boolean.TRUE.toString())) {
-			// System.out.println("Scheduler Will start in "
-			// + m_startupDelayString + " milliseconds!");
-			// // wait the specified amount of time before
-			// // starting the process.
-			// Thread delayedScheduler = new Thread(
-			// new DelayedSchedulerStarted(scheduler, m_startupDelay));
-			// // give the scheduler a name. All good code needs a name
-			// delayedScheduler.setName("Delayed_Scheduler");
-			// // Start out scheduler
-			// delayedScheduler.start();
-			// }
-
-			// Retrieve the scheduler from the factory
-			scheduler = factory.getScheduler();
+			scheduler = new StdSchedulerFactory().getScheduler();
 			if (scheduler != null) {
 				scheduler.start();
-				// int gridDiscoveryIntervalInMinutes = getIntervalInMinutes(
-				// actionServlet.getServletConfig(),
-				// "gridDiscoveryIntervalInMinutes");
-				// initialiseGridDiscoveryJob(gridDiscoveryIntervalInMinutes);
-				// ServletContext appContext =
-				// actionServlet.getServletContext();
-				// List<GridNodeBean> gridNodes = (List<GridNodeBean>)
-				// appContext
-				// .getAttribute("allGridNodes");
-				// initialisePublicDataCountJob(gridNodes,
-				// gridDiscoveryIntervalInMinutes);
 				int csmCleanupIntervalInMinutes = getInterval(
 						actionServlet.getServletConfig(),
 						"csmCleanupIntervalInMinutes");
@@ -82,7 +45,7 @@ public class SchedulerPlugin implements PlugIn {
 				int publicCountPullIntervalInHours = getInterval(
 						actionServlet.getServletConfig(),
 						"publicCountPullIntervalInHours");
-				initialisePublicCountPullJob(publicCountPullIntervalInHours);				
+				initialisePublicCountPullJob(publicCountPullIntervalInHours);
 			}
 		} catch (SchedulerException e) {
 			logger.error("Error setting up scheduler", e);
