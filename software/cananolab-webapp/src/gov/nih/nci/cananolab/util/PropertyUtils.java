a52a718eca1cd4b968cb573f7da9c5df721b6592
Sue Pan <sue.pan@nih.gov>
2012-05-24 14:31:44 +0000

    remediated for jboss 5.1 upgrade
    SVN-Revision: 20583

--- software/cananolab-webapp/src/gov/nih/nci/cananolab/util/PropertyUtils.java
+++ software/cananolab-webapp/src/gov/nih/nci/cananolab/util/PropertyUtils.java
@@ -1,22 +1,20 @@
 package gov.nih.nci.cananolab.util;
 
-import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
-import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.Properties;
 
 /**
- * This class is caNanoLab Util class. It will read user property file, and fetch
- * property value by property name.
+ * This class is caNanoLab Util class. It will read user property file, and
+ * fetch property value by property name.
  * 
- * @author houyh
+ * @author houyh, pansu
  * 
  */
 public class PropertyUtils {
-	
+
 	private static org.apache.log4j.Logger logger = org.apache.log4j.Logger
 			.getLogger(PropertyUtils.class);
 
@@ -32,34 +30,32 @@ public class PropertyUtils {
 	 */
 	public static String getProperty(String propertyFileName,
 			String propertyName) {
-		if (properties == null) {
-			loadProperty(propertyFileName, false);
-		} else if (properties.get(propertyName) == null) {
-			loadProperty(propertyFileName, true);
-		}
+		loadProperty(propertyFileName);
 		return (String) properties.get(propertyName);
 	}
-	
+
 	/**
-	 * This method is for users to get property value. It requires users to
-	 * provide propert file name, and property name.
+	 * This method is for users to set property value. It requires users to
+	 * provide property file name, and property name.
+	 * This doesn't work for the properties in a war file
 	 * 
 	 * @param propertyFileName
 	 * @param propertyName
 	 * @return property value
 	 */
-	public static boolean setProperty(String propertyFileName, String name, String value) {
+	public static boolean setProperty(String propertyFileName, String name,
+			String value) {
 		boolean result = true;
-		
-		// Need to load property first, otherwise will lose other settings. 
-		loadProperty(propertyFileName, true);
-		
+
+		// Need to load property first, otherwise will lose other settings.
+		loadProperty(propertyFileName);
+
 		OutputStream out = null;
 		try {
-			out = new BufferedOutputStream(
-				new FileOutputStream(getPropertyFileName(propertyFileName)));
-			
-			if (value == null){
+			out = new BufferedOutputStream(new FileOutputStream(
+					getPropertyFileName(propertyFileName)));
+
+			if (value == null) {
 				properties.remove(name);
 			} else {
 				properties.setProperty(name, value);
@@ -79,27 +75,21 @@ public class PropertyUtils {
 		}
 		return result;
 	}
-	
+
 	/**
 	 * This method reads property file, and load all properties into properties
 	 * Properties class by using name/value pair. All property files must reside
-	 * in gov/nih/nci/calab/resource directory.
+	 * in classpath.
 	 * 
 	 * @param propertyFileName
 	 * @param reload
 	 */
-	protected static void loadProperty(String propertyFileName, boolean reload) {
+	protected static void loadProperty(String propertyFileName) {
 		InputStream istream = null;
 		properties = new Properties();
 		try {
-			if (reload) {
-				// Directly load from property file to avoid caching issue.
-				istream = new BufferedInputStream(
-						new FileInputStream(getPropertyFileName(propertyFileName)));
-			} else {
-				istream = Thread.currentThread().getContextClassLoader().
-						getResourceAsStream(propertyFileName);
-			}
+			istream = Thread.currentThread().getContextClassLoader()
+					.getResourceAsStream(propertyFileName);
 			properties.load(istream);
 		} catch (Exception exp) {
 			logger.error("Property file cannot be read: " + exp.toString(), exp);
@@ -115,14 +105,14 @@ public class PropertyUtils {
 
 	/**
 	 * Get the file name of caNanolab property file.
-	 *
+	 * 
 	 * @param propertyFileName
 	 * @return file name of caNanolab property file.
 	 */
 	private static String getPropertyFileName(String propertyFileName) {
-		String fileName = Thread.currentThread().getContextClassLoader().
-					getResource(propertyFileName).getPath();
-		
+		String fileName = Thread.currentThread().getContextClassLoader()
+				.getResource(propertyFileName).getPath();
+
 		return fileName;
 	}
 }
