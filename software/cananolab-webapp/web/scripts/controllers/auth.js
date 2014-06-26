dab7db78f9c631c53282dd29cc455c4b560ff8eb
Shaun Einolf <seinolf@gmail.com>
2014-06-25 23:44:01 -0400

    Added pull down list with options. Not functional yet. Homepage changes including links, text and bullet items should be in place.

---
+++ software/cananolab-webapp/web/scripts/controllers/auth.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the angularApp
+ */
+angular.module('angularApp')
+  .controller('AuthCtrl', function ($scope) {
+    $scope.auth_cookie="";
+  });
--- software/cananolab-webapp/web/scripts/controllers/main.js
+++ software/cananolab-webapp/web/scripts/controllers/main.js
@@ -1,13 +1,22 @@
 'use strict';
 
-/**
- * @ngdoc function
- * @name angularApp.controller:MainCtrl
- * @description
- * # MainCtrl
- * Controller of the angularApp
- */
 angular.module('angularApp')
   .controller('MainCtrl', function ($scope) {
-  	$scope.test = "This is a test";
+
+  	$scope.userActions = 1;
+  	$scope.loginShow = 0;
+  	$scope.doUserAction = function() {
+  		if ($scope.userActions==2) {
+  			$scope.loginShow = 1;
+  		}
+  		else {
+  			$scope.loginShow = 0;
+  		}
+  	}
+
+  	$scope.loginDo = function() {
+  		window.location="/caNanoLab/index.html#/auth"
+  	}  	
+
+
   });
