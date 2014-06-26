b9863a93915d63206e5326ef7deb22ae3ecf187b
Shaun Einolf <seinolf@gmail.com>
2014-06-26 14:11:50 -0400

    add register view and controller.

---
+++ software/cananolab-webapp/web/scripts/controllers/register.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularApp
+ */
+angular.module('angularApp')
+  .controller('RegisterCtrl', function ($scope, $http) {
+	$scope.submitData = function(isValid) {
+		$scope.submitted = true;
+		if (isValid) { 
+			//$http({method: 'POST', url: 'http://localhost:8080/caNanoLab/rest/security/register', data: {"title":$scope.title,"firstName":$scope.firstName,"lastName":$scope.lastName,"email":$scope.email,"phone":$scope.phone,"organization":$scope.organization,"fax":$scope.fax,"comment":$scope.comment,"registerToUserList":$scope.registerToUserList}, headers: {'Content-Type':'application/json'}}).
+	    	  $http({method: 'GET', url: 'http://localhost:8080/caNanoLab/rest/security/register', params: {"title":$scope.title,"firstName":$scope.firstName,"lastName":$scope.lastName,"email":$scope.email,"phone":$scope.phone,"organization":$scope.organization,"fax":$scope.fax,"comment":$scope.comment,"registerToUserList":$scope.registerToUserList}}).
+			    success(function(data, status, headers, config) {
+			      // this callback will be called asynchronously
+			      // when the response is available
+			    	$scope.message="Your registration request has been sent to the administrator for assignment of your User ID and Password. You should receive this information via e-mail within one business day from time of submission.";
+			    }).
+			    error(function(data, status, headers, config) {
+			      // called asynchronously if an error occurs
+			      // or server returns response with an error status.
+			    	$scope.message="failure";
+	    	});
+		};
+	};
+  });
+
+
+
+
