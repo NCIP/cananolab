177b9636e92615511dbcd82333d6191f5552cdab
Shaun Einolf <seinolf@gmail.com>
2014-07-09 13:23:20 -0400

    Added sampleResults.js

--- software/cananolab-webapp/web/scripts/controllers/sampleSearch.js
+++ software/cananolab-webapp/web/scripts/controllers/sampleSearch.js
@@ -1,69 +1,56 @@
 'use strict';
 var app = angular.module('angularApp')
 
-  .controller('SampleCtrl', function (navigationFactory,groupsFactory,$rootScope,$scope,$http) {
-
-    $scope.myData = [{name: "Moroni", age: 50},
-                     {name: "Tiancum", age: 43},
-                     {name: "Jacob", age: 27},
-                     {name: "Nephi", age: 29},
-                     {name: "Enos", age: 34}];  
-
-    $scope.gridOptions = { data: 'myData' }; 
-
+  .controller('SampleCtrl', function (navigationFactory,groupsFactory,$rootScope,$scope,$http,$location) {
     $scope.searchSampleForm = {};
     $rootScope.tabs = navigationFactory.query();
     $rootScope.groups = groupsFactory.get();
 
-
     $scope.$on('$viewContentLoaded', function(){
-     $http({method: 'GET', url: '/caNanoLab/rest/sample/setup'}).
-     success(function(data, status, headers, config) {
+      $http({method: 'GET', url: '/caNanoLab/rest/sample/setup'}).
+      success(function(data, status, headers, config) {
         $scope.functionalizingEntityTypesList = data.functionalizingEntityTypes;
         $scope.characterizationTypesList = data.characterizationTypes;
         $scope.nanomaterialEntityTypesList = data.nanomaterialEntityTypes;
         $scope.functionTypesList = data.functionTypes;
-        }).
-        error(function(data, status, headers, config) {
+      }).
+      error(function(data, status, headers, config) {
           // called asynchronously if an error occurs
           // or server returns response with an error status.
-        $scope.mesage = data;
-
-         });
+      $scope.mesage = data;
+      });
     });     
 
     $scope.setCharacterizationOptionsByCharType = function() {
-    
-     $http({method: 'GET', url: '/caNanoLab/rest/sample/getCharacterizationByType',params: {"type":$scope.searchSampleForm.characterizationType}}).
-     success(function(data, status, headers, config) {
+      $http({method: 'GET', url: '/caNanoLab/rest/sample/getCharacterizationByType',params: {"type":$scope.searchSampleForm.characterizationType}}).
+      success(function(data, status, headers, config) {
         $scope.characterizationsList = data;
-        }).
-        error(function(data, status, headers, config) {
-          // called asynchronously if an error occurs
-          // or server returns response with an error status.
-          $scope.message = data;
-         });        
-      // make rest call with $scope.characterizationType parameter here.
+      }).
+      error(function(data, status, headers, config) {
+        // called asynchronously if an error occurs
+        // or server returns response with an error status.
+        $scope.message = data;
+      });        
     };
        
 
     $scope.doSearch = function() {
-     $http({method: 'POST', url: '/caNanoLab/rest/sample/searchSample',data: $scope.searchSampleForm}).
-     success(function(data, status, headers, config) {
-        $scope.results = data;
-        }).
-        error(function(data, status, headers, config) {
-          // called asynchronously if an error occurs
-          // or server returns response with an error status.
-          $scope.results = data;
-         }); 
+      $http({method: 'POST', url: '/caNanoLab/rest/sample/searchSample',data: $scope.searchSampleForm}).
+      success(function(data, status, headers, config) {
+        $rootScope.sampleresults = data;
+        $location.path("/sampleResults").replace();
+
+      }).
+      error(function(data, status, headers, config) {
+        // called asynchronously if an error occurs
+        // or server returns response with an error status.
+        $rootScope.sampleresults = data;
+      }); 
     };
 
     $scope.resetForm = function() {
       $scope.searchSampleForm = {};
       $scope.characterizationsList = [];  
-
-    }
-                      
+    };
 
   });
