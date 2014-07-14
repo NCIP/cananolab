249845c3dbe20d8fc0849ffc627ec6e9212ed6cf
Thiagarajan Prakash <prakasht@mail.nih.gov>
2014-07-14 17:53:35 -0400

    Added PrintPublication UI

---
+++ software/cananolab-webapp/web/scripts/controllers/printPublication.js
@@ -0,0 +1,18 @@
+'use strict';
+angular.module('angularApp')
+    .controller('PrintSamplePublicationCtrl', function ($rootScope,$scope,$http,$filter,$routeParams) {
+        var sampleId = $routeParams.sampleId;
+
+        $http({method: 'GET', url: 'http://localhost:8080/caNanoLab/rest/publication/summaryView?sampleId=' + sampleId}).
+            success(function(data, status, headers, config) {
+                $scope.publicationCategories = data.publicationCategories;
+                $scope.publicationBean = data.publicationBean;
+                $scope.category2Publications = data.category2Publications;
+            }).
+            error(function(data, status, headers, config) {
+                // called asynchronously if an error occurs
+                // or server returns response with an error status.
+                $scope.message = data;
+
+            });
+        });
