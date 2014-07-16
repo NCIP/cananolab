38f0dcc116f9bd38dd7122cf72b962d0d4a9c0a6
Shaun Einolf <seinolf@gmail.com>
2014-07-16 16:23:47 -0400

    added cross domain for publication view

--- software/cananolab-webapp/src/gov/nih/nci/cananolab/restful/PublicationServices.java
+++ software/cananolab-webapp/src/gov/nih/nci/cananolab/restful/PublicationServices.java
@@ -46,7 +46,8 @@ private Logger logger = Logger.getLogger(SampleServices.class);
 			SimplePublicationSummaryViewBean view = new SimplePublicationSummaryViewBean();
 			view.transferPublicationBeanForSummaryView(pubBean);
 			
-			return Response.ok(view).build();
+			// return Response.ok(view).build();
+			return Response.ok(view).header("Access-Control-Allow-Credentials", "true").header("Access-Control-Allow-Origin", "*").header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS").header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization").build();
 			
 		} catch (Exception e) {
 			return Response.ok("Error while viewing the publication results").build();
