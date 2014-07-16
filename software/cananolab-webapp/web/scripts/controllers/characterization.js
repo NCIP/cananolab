070d64804821c17bd43247900b9874dbe88836f3
Shaun Einolf <seinolf@gmail.com>
2014-07-15 22:21:32 -0400

    added back button to info, characterization, composition and publication pages, also added 4 links at the top of each page as requested. Colors and bar may not be exactly what is wanted but that can be modified

--- software/cananolab-webapp/web/scripts/controllers/characterization.js
+++ software/cananolab-webapp/web/scripts/controllers/characterization.js
@@ -1,12 +1,19 @@
 'use strict';
 var app = angular.module('angularApp')
-  .controller('CharacterizationCtrl', function (sampleService,navigationService, groupService, $rootScope,$scope,$http,$filter,$routeParams) {
+  .controller('CharacterizationCtrl', function (sampleService,navigationService, groupService, $rootScope,$scope,$http,$location,$filter,$routeParams) {
     $rootScope.tabs = navigationService.query();
     $rootScope.groups = groupService.get();   
+    $scope.sampleData = sampleService.sampleData;
     $scope.sampleId = sampleService.sampleId;
 
     // Displays left hand nav for samples section. navTree shows nav and navDetail is page index //
     $rootScope.navTree = true;
     $rootScope.navDetail = 2;  
     $scope.sampleData = sampleService.sampleData;
+
+    $scope.goBack = function() {
+      $location.path("/sampleResults").replace();      
+    };
+
   });
+
--- software/cananolab-webapp/web/scripts/controllers/composition.js
+++ software/cananolab-webapp/web/scripts/controllers/composition.js
@@ -1,8 +1,9 @@
 'use strict';
 var app = angular.module('angularApp')
-  .controller('CompositionCtrl', function (sampleService,navigationService, groupService, $rootScope,$scope,$http,$filter,$routeParams) {
+  .controller('CompositionCtrl', function (sampleService,navigationService, groupService, $rootScope,$scope,$http,$location,$filter,$routeParams) {
     $rootScope.tabs = navigationService.query();
     $rootScope.groups = groupService.get();   
+    $scope.sampleData = sampleService.sampleData;
     $scope.sampleId = sampleService.sampleId;
 
     // Displays left hand nav for samples section. navTree shows nav and navDetail is page index //
@@ -10,4 +11,9 @@ var app = angular.module('angularApp')
     $rootScope.navDetail = 1; 
     $scope.sampleData = sampleService.sampleData;
 
+     $scope.goBack = function() {
+      $location.path("/sampleResults").replace();      
+    };
+
   });
+
