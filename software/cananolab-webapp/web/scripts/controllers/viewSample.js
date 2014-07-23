1b6ad50b31f550db61a324afa3645ccff8fb601e
Shaun Einolf <seinolf@gmail.com>
2014-07-23 08:57:18 -0400

    Changed viewSample to include loader

--- software/cananolab-webapp/web/scripts/controllers/viewSample.js
+++ software/cananolab-webapp/web/scripts/controllers/viewSample.js
@@ -29,14 +29,18 @@ var app = angular.module('angularApp')
 
 
     $scope.$on('$viewContentLoaded', function(){
+      $scope.loader = true;      
      $http({method: 'GET', url: '/caNanoLab/rest/sample/view?sampleId=' + $scope.sampleId.data}).
      success(function(data, status, headers, config) {
       $scope.sampleData = data;
+      $scope.loader=false;
+      
         }).
         error(function(data, status, headers, config) {
           // called asynchronously if an error occurs
           // or server returns response with an error status.
         $scope.message = data;
+      $scope.loader=false;
 
          });
     });     
