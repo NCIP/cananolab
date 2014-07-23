103a3af1c0da65f83a37c583a4bc992b0c01e103
Thiagarajan Prakash <prakasht@mail.nih.gov>
2014-07-23 17:29:49 -0400

    checked in UI changes

--- software/cananolab-webapp/web/scripts/controllers/printPublication.js
+++ software/cananolab-webapp/web/scripts/controllers/printPublication.js
@@ -2,17 +2,20 @@
 angular.module('publicationApp',[
     'ngRoute'])
     .controller('PrintSamplePublicationCtrl', function ($rootScope,$scope,$http,$filter,$routeParams) {
-        var sampleId = $routeParams.sampleId;
-        $http({method: 'GET', url: '/caNanoLab/rest/publication/summaryView?sampleId=27131907'}).
+    	$scope.sampleId = sampleService.sampleId;
+    	$scope.loader = true;
+        $http({method: 'GET', url: '/caNanoLab/rest/publication/summaryPrint?sampleId=' + $scope.sampleId.data}).
             success(function(data, status, headers, config) {
                 $scope.publicationCategories = data.publicationCategories;
                 $scope.publicationBean = data.publicationBean;
                 $scope.category2Publications = data.category2Publications;
+                $scope.loader = false;
             }).
             error(function(data, status, headers, config) {
                 // called asynchronously if an error occurs
                 // or server returns response with an error status.
                 $scope.message = data;
+                $scope.loader = false;
 
             });
         });
