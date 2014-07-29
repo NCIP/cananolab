fe88df74b03f905e9418da414216cd0f43e9eb95
shanyang-saicf <shan.yang@nih.gov>
2014-07-29 14:28:24 -0400

    Added editable field

--- software/cananolab-webapp/src/gov/nih/nci/cananolab/restful/view/SimpleSearchSampleBean.java
+++ software/cananolab-webapp/src/gov/nih/nci/cananolab/restful/view/SimpleSearchSampleBean.java
@@ -2,29 +2,32 @@ package gov.nih.nci.cananolab.restful.view;
 
 import gov.nih.nci.cananolab.domain.particle.NanomaterialEntity;
 import gov.nih.nci.cananolab.domain.particle.SampleComposition;
+import gov.nih.nci.cananolab.dto.common.AccessibilityBean;
 import gov.nih.nci.cananolab.dto.particle.SampleBean;
 import gov.nih.nci.cananolab.restful.sample.SearchSampleBO;
+import gov.nih.nci.cananolab.service.security.UserBean;
 import gov.nih.nci.cananolab.util.ClassUtils;
 
 import java.util.Collection;
 import java.util.Date;
 import java.util.Iterator;
+import java.util.List;
 
 import org.apache.log4j.Logger;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.codehaus.jackson.map.ObjectWriter;
 
 /**
- * Class to support sample search view. It has exactly what needs to be displayed as result for
- * each sample row
+ * Class to support sample search view. It has exactly what needs to be
+ * displayed as result for each sample row
  * 
  * @author yangs8
- *
+ * 
  */
 public class SimpleSearchSampleBean {
-	
+
 	private Logger logger = Logger.getLogger(SimpleSearchSampleBean.class);
-	
+
 	long sampleId;
 	String sampleName;
 	String pointOfContact;
@@ -33,83 +36,145 @@ public class SimpleSearchSampleBean {
 	String[] characterizations;
 	String dataAvailability;
 	Date createdDate;
+
+	boolean editable;
+
+	public boolean isEditable() {
+		return editable;
+	}
+
+	public void setEditable(boolean editable) {
+		this.editable = editable;
+	}
+
 	public long getSampleId() {
 		return sampleId;
 	}
+
 	public void setSampleId(long sampleId) {
 		this.sampleId = sampleId;
 	}
+
 	public String getSampleName() {
 		return sampleName;
 	}
+
 	public void setSampleName(String sampleName) {
 		this.sampleName = sampleName;
 	}
+
 	public String getPointOfContact() {
 		return pointOfContact;
 	}
+
 	public void setPointOfContact(String pointOfContact) {
 		this.pointOfContact = pointOfContact;
 	}
-	
+
 	public String[] getComposition() {
 		return composition;
 	}
+
 	public void setComposition(String[] composition) {
 		this.composition = composition;
 	}
+
 	public String[] getFunctions() {
 		return functions;
 	}
+
 	public void setFunctions(String[] functions) {
 		this.functions = functions;
 	}
+
 	public String[] getCharacterizations() {
 		return characterizations;
 	}
+
 	public void setCharacterizations(String[] characterizations) {
 		this.characterizations = characterizations;
 	}
+
 	public String getDataAvailability() {
 		return dataAvailability;
 	}
+
 	public void setDataAvailability(String dataAvailability) {
 		this.dataAvailability = dataAvailability;
 	}
+
 	public Date getCreatedDate() {
 		return createdDate;
 	}
+
 	public void setCreatedDate(Date createdDate) {
 		this.createdDate = createdDate;
 	}
-	
-public void transferSampleBeanForBasicResultView(SampleBean sampleBean) {
-		
-		if (sampleBean == null) return;
+
+	public void transferSampleBeanForBasicResultView(SampleBean sampleBean,
+			UserBean user) {
+
+		if (sampleBean == null)
+			return;
 		setSampleId(sampleBean.getDomain().getId());
 		setSampleName(sampleBean.getDomain().getName());
 		setPointOfContact(sampleBean.getThePOC().getOrganizationDisplayName());
-		
+
 		SampleComposition comp = sampleBean.getDomain().getSampleComposition();
-		
+
 		if (comp != null) {
-			Collection<NanomaterialEntity> nanocoll = comp.getNanomaterialEntityCollection();
+			Collection<NanomaterialEntity> nanocoll = comp
+					.getNanomaterialEntityCollection();
 			String[] v = new String[nanocoll.size()];
 			Iterator ite = nanocoll.iterator();
 
 			int i = 0;
 			while (ite.hasNext()) {
-				NanomaterialEntity n = (NanomaterialEntity)ite.next();
-				String cn = ClassUtils.getShortClassName(n.getClass().getName());
-				v[i++] = cn; 
+				NanomaterialEntity n = (NanomaterialEntity) ite.next();
+				String cn = ClassUtils
+						.getShortClassName(n.getClass().getName());
+				v[i++] = cn;
 			}
 			setComposition(v);
-		}	
-		
+		}
+
 		setFunctions(sampleBean.getFunctionClassNames());
 		setCharacterizations(sampleBean.getCharacterizationClassNames());
 		setDataAvailability(sampleBean.getDataAvailabilityMetricsScore());
-		setCreatedDate(sampleBean.getPrimaryPOCBean().getDomain().getCreatedDate());
+		setCreatedDate(sampleBean.getPrimaryPOCBean().getDomain()
+				.getCreatedDate());
+
+		editable = isSampleEditableForUser(sampleBean.getUserAccesses(), user);
+	}
+	
+	/**
+	 * To determine if the user has edit right to the sample
+	 * 
+	 * @param userAccesses
+	 * @param user
+	 * @return
+	 */
+	protected boolean isSampleEditableForUser(List<AccessibilityBean> userAccesses, UserBean user) {
+		if (user == null || userAccesses == null) 
+			return false;
+		
+		if (user.isCurator())
+			return true;
+		
+		String loginName = user.getLoginName();
+		if (loginName == null || loginName.length() == 0)
+			return false;
+		
+		for (AccessibilityBean access : userAccesses) {
+			UserBean aUser = access.getUserBean();
+			if (aUser == null) continue;
+			
+			if (aUser.getLoginName().equals(loginName)) 
+				return true;
+		}
+		
+		return false;
 		
 	}
+
 }
