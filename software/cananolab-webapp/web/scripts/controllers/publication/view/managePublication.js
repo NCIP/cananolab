8e53b2c4b180b8fc7654b4fb0f478e5e51f6d416
Shaun Einolf <seinolf@gmail.com>
2014-08-01 16:07:29 -0400

    removed managePublication.js

--- software/cananolab-webapp/web/scripts/controllers/publication/view/managePublication.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-var app = angular.module('angularApp')
-  .controller('ManagePublicationCtrl', function (publicationService,navigationService,groupService,$scope,$rootScope,$filter,ngTableParams,$http,$location,$modal) {
-    $rootScope.tabs = navigationService.query();
-    $rootScope.groups = groupService.get();
-    $scope.publicationData = publicationService.publicationData;
-    $rootScope.navTree=false;
-
-  });
