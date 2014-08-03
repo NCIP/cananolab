b7c0978ed43d2c76283a120e06b9b71e03d1b8ae
shanyang-saicf <shan.yang@nih.gov>
2014-08-02 21:18:55 -0400

    Init checkin

---
+++ software/cananolab-webapp/test-rest/java/gov/nih/nci/cananolab/restful/sample/SampleBOTest.java
@@ -0,0 +1,14 @@
+package gov.nih.nci.cananolab.restful.sample;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SampleBOTest {
+
+	@Test
+	public void testFindMatchSampleInSession() {
+		fail("Not yet implemented");
+	}
+
+}
