b1590db70ba02daebd6610432fd39929760bb908
shanyang-saicf <shan.yang@nih.gov>
2014-08-02 21:12:45 -0400

    ObjectMapperProvider needed for running test on post services

---
+++ software/cananolab-webapp/test-rest/java/gov/nih/nci/cananolab/restful/view/SimplePublicationWithSamplesBeanTest.java
@@ -0,0 +1,19 @@
+package gov.nih.nci.cananolab.restful.view;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SimplePublicationWithSamplesBeanTest {
+
+	@Test
+	public void testTransferDataFromPublication() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testTransferSampleDataFromSampleList() {
+		fail("Not yet implemented");
+	}
+
+}
