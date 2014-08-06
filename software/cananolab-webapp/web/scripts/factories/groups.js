694884d3fd2854a78244a3143fe1a585f07a7d1b
Shaun Einolf <seinolf@gmail.com>
2014-08-06 18:36:54 -0400

    Added ability to check if curator

--- software/cananolab-webapp/web/scripts/factories/groups.js
+++ software/cananolab-webapp/web/scripts/factories/groups.js
@@ -1,9 +1,29 @@
 'use strict';
 
 var app = angular.module('angularApp');
-app.factory('groupService', function($resource){
+app.factory('groupService', function($resource,$http){
+	var that = this;
 	return {
 		getGroups: {data:$resource('/caNanoLab/rest/security/getUserGroups') },
-		isCurator: {data: 'test' }
-		}	
+		isCurator: function(groups) {
+			that.curator = 0;
+			$http({method: 'GET', url: '/caNanoLab/rest/security/getUserGroups' }).
+			success(function(data, status, headers, config) {
+
+			var x = data;
+			for (var key in x) {
+			  for (var y=0;y<x[key].length;y++) {
+			    if(x[key][y]=='Curator') {
+			    		that.curator = 1;
+			    };
+			  };
+			}
+			}).
+			error(function(data, status, headers, config) {
+				that.curator = "0";
+			});
+			return that;
+
+		},
+	}	
 });
