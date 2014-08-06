0cea3687fbd6407bda542907d3cc62ccf6b3ee8c
Thiagarajan Prakash <prakasht@mail.nih.gov>
2014-08-06 11:07:28 -0400

    Added id for Publication Results page

--- software/cananolab-webapp/web/scripts/controllers/message.js
+++ software/cananolab-webapp/web/scripts/controllers/message.js
@@ -1,5 +1,8 @@
 'use strict';
 angular.module('angularApp')
-    .controller('MessageCtrl', function ($rootScope,$scope,$http,$filter,$routeParams) {
+    .controller('MessageCtrl', function (navigationService, groupService, $rootScope,$scope,$http,$filter,$routeParams) {
+        $rootScope.tabs = navigationService.get();
+        $rootScope.groups = groupService.get();   
+
         $scope.message = $routeParams.message;
     });
