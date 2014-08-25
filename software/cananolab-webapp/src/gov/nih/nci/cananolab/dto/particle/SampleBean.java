f5e2c6668d0d0458b17c691388fbba7937f57fbe
yangs8 <yangs8@NCI-S077257-L.nci.nih.gov>
2014-08-25 12:26:28 -0400

    Added a null check on keywordStr

--- software/cananolab-webapp/src/gov/nih/nci/cananolab/dto/particle/SampleBean.java
+++ software/cananolab-webapp/src/gov/nih/nci/cananolab/dto/particle/SampleBean.java
@@ -153,7 +153,7 @@ public class SampleBean extends SecuredDataBean {
 		} else {
 			domain.setKeywordCollection(new HashSet<Keyword>());
 		}
-		if (keywordsStr.length() > 0) {
+		if (keywordsStr != null && keywordsStr.length() > 0) {
 			String[] strs = keywordsStr.split("\r\n");
 			for (String str : strs) {
 				// change to upper case
