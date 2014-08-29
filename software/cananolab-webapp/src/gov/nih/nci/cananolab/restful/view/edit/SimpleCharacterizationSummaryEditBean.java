085ea2115dcbdfc9eda5dc389e0694ff7c75cf76
yangs8 <yangs8@NCI-S077257-L.nci.nih.gov>
2014-08-29 08:12:51 -0400

    Init checkin

---
+++ software/cananolab-webapp/src/gov/nih/nci/cananolab/restful/view/edit/SimpleCharacterizationSummaryEditBean.java
@@ -0,0 +1,50 @@
+package gov.nih.nci.cananolab.restful.view.edit;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import gov.nih.nci.cananolab.dto.particle.characterization.CharacterizationSummaryViewBean;
+import gov.nih.nci.cananolab.restful.sample.InitCharacterizationSetup;
+import gov.nih.nci.cananolab.restful.view.SimpleCharacterizationSummaryViewBean;
+import gov.nih.nci.cananolab.restful.view.SimpleCharacterizationsByTypeBean;
+
+public class SimpleCharacterizationSummaryEditBean extends SimpleCharacterizationSummaryViewBean {
+	
+	public List<SimpleCharacterizationsByTypeBean> transferData(HttpServletRequest request, 
+			CharacterizationSummaryViewBean viewBean, String sampleId) 
+			throws Exception {
+		
+		List<String> allCharacterizationTypes = InitCharacterizationSetup
+				.getInstance().getCharacterizationTypes(request);
+		
+		List<SimpleCharacterizationsByTypeBean> charByTypeBeans = super.transferData(viewBean);
+		
+		List<SimpleCharacterizationsByTypeBean> allCharByTypeBeans = new ArrayList<SimpleCharacterizationsByTypeBean>();
+		//if (charByTypeBean.size() == 0) 
+		
+		for (String aType : allCharacterizationTypes) {
+			boolean exists = false;
+			for (SimpleCharacterizationsByTypeBean charBean : charByTypeBeans) {
+				if (charBean.getType().equals(aType)) {
+					charBean.setParentSampleId(Long.parseLong(sampleId));
+					allCharByTypeBeans.add(charBean);
+					exists = true;
+					break;
+				}
+			}
+			
+			if (!exists) {
+				SimpleCharacterizationsByTypeBean emptyBean = new SimpleCharacterizationsByTypeBean();
+				emptyBean.setType(aType);
+				emptyBean.setParentSampleId(Long.parseLong(sampleId));
+				allCharByTypeBeans.add(emptyBean);
+			}
+		}
+			
+		
+		return allCharByTypeBeans;
+	}
+
+}
