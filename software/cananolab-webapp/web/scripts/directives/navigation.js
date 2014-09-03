c03b75e7b95373544be939fce1a2ab94c735cd93
Shaun Einolf <seinolf@gmail.com>
2014-09-03 16:14:39 -0400

    added new route editPublication so the navigation will work

---
+++ software/cananolab-webapp/web/scripts/directives/navigation.js
@@ -0,0 +1,27 @@
+'use strict';
+var app = angular.module('angularApp')
+
+  .directive('navigation',function($location) {
+    var url = $location.path();
+    var isEdit = 0;
+    var edit = ['/editSample','/editComposition','/editCharacterization','/editPublication'];
+    var view = ['/sample','/composition','/characterization','/publication'];
+
+    for (var x=0;x<edit.length;x++) {
+      if (url==edit[x]) {
+        isEdit=1;
+      }
+    };
+    for (var x=0;x<view.length;x++) {
+      if (url==view[x]) {
+        isEdit=0;
+      }
+    }; 
+var link = function() { 
+sc   
+scope.$watch
+    return {
+      restrict: 'E',      
+      template: isEdit
+    };
+  });
