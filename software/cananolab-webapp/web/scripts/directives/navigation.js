cbfd7de073911c0911717dc5400b72100165d67e
Shaun Einolf <seinolf@gmail.com>
2014-09-03 18:01:38 -0400

    added navigation directive

--- software/cananolab-webapp/web/scripts/directives/navigation.js
+++ software/cananolab-webapp/web/scripts/directives/navigation.js
@@ -2,26 +2,50 @@
 var app = angular.module('angularApp')
 
   .directive('navigation',function($location) {
-    var url = $location.path();
-    var isEdit = 0;
-    var edit = ['/editSample','/editComposition','/editCharacterization','/updatePublication'];
-    var view = ['/sample','/composition','/characterization','/publication'];
 
-    for (var x=0;x<edit.length;x++) {
-      if (url==edit[x]) {
-        isEdit=1;
-      }
+
+
+    var link = function($scope, $element, $attrs) {
+        $scope.url = $location.path();
+        $scope.isEdit = 0;
+        $scope.buttons = [];
+        // $scope.edit = ['/editSample','/editComposition','/editCharacterization','/updatePublication'];
+        $scope.edit = [{'url':'/editSample','name':'GENERAL INFO'},{'url':'/editComposition','name':'COMPOSITION'},{'url':'/editCharacterization','name':'CHARACTERIZATION'},{'url':'/updatePublication','name':'PUBLICATION'}];
+        $scope.view = [{'url':'/sample','name':'GENERAL INFO'},{'url':'/composition','name':'COMPOSITION'},{'url':'/characterization','name':'CHARACTERIZATION'},{'url':'/publication','name':'PUBLICATION'}];
+        $scope.testClick = function(uri) {
+            $location.path(uri).replace();
+        }
+        $scope.$watch(function() {
+            return $location.path();
+
+        },
+        function(newValue, oldValue) {
+            $scope.buttons = [];
+            $scope.navTree = 0;
+            for (var x=0;x<$scope.edit.length;x++) {
+              if (newValue==$scope.edit[x].url) {
+                $scope.isEdit=1;
+                $scope.buttons = $scope.edit;
+                $scope.url = newValue;
+                $scope.navTree = 1;
+
+              }
+            };
+            for (var x=0;x<$scope.view.length;x++) {
+              if (newValue==$scope.view[x].url) {
+                $scope.isEdit=0;
+                $scope.buttons = $scope.view;  
+                $scope.url = newValue;
+                $scope.navTree = 1;
+
+              }
+            }; 
+        });
     };
-    for (var x=0;x<view.length;x++) {
-      if (url==view[x]) {
-        isEdit=0;
-      }
-    }; 
-var link = function() { 
-sc   
-scope.$watch
+ 
     return {
-      restrict: 'E',      
-      template: isEdit
+        restrict: 'E',
+        templateUrl:'scripts/directives/navigation.html',      
+        link: link
     };
   });
