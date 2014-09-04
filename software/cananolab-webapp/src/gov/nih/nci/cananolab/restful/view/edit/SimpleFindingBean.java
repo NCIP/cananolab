e777a61378a5cb133ace521be0f5440e064ff1b8
yangs8 <yangs8@NCI-S077257-L.nci.nih.gov>
2014-09-04 16:02:28 -0400

    Init checkin

---
+++ software/cananolab-webapp/src/gov/nih/nci/cananolab/restful/view/edit/SimpleFindingBean.java
@@ -0,0 +1,122 @@
+package gov.nih.nci.cananolab.restful.view.edit;
+
+import gov.nih.nci.cananolab.domain.common.Finding;
+import gov.nih.nci.cananolab.dto.common.ColumnHeader;
+import gov.nih.nci.cananolab.dto.common.FileBean;
+import gov.nih.nci.cananolab.dto.common.FindingBean;
+import gov.nih.nci.cananolab.dto.common.Row;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SimpleFindingBean {
+	
+	private int numberOfColumns;
+	private int numberOfRows;
+	
+	private List<Row> rows = new ArrayList<Row>();
+	private List<ColumnHeader> columnHeaders = new ArrayList<ColumnHeader>();
+	
+	private List<SimpleFileBean> files = new ArrayList<SimpleFileBean>();
+
+	private int theFileIndex;
+	
+	//private Finding domain = new Finding();
+	//private FileBean theFile = new FileBean();
+	
+	
+	
+	List<String> errors = new ArrayList<String>();
+	
+	public void transferToFindingBean(FindingBean findingBean) {
+		
+		findingBean.setNumberOfColumns(this.numberOfColumns);
+		findingBean.setNumberOfRows(this.numberOfRows);
+		
+	}
+	
+	
+
+	public int getNumberOfColumns() {
+		return numberOfColumns;
+	}
+
+
+
+	public void setNumberOfColumns(int numberOfColumns) {
+		this.numberOfColumns = numberOfColumns;
+	}
+
+
+
+	public int getNumberOfRows() {
+		return numberOfRows;
+	}
+
+
+
+	public void setNumberOfRows(int numberOfRows) {
+		this.numberOfRows = numberOfRows;
+	}
+
+
+
+	public List<Row> getRows() {
+		return rows;
+	}
+
+
+
+	public void setRows(List<Row> rows) {
+		this.rows = rows;
+	}
+
+
+
+	public List<ColumnHeader> getColumnHeaders() {
+		return columnHeaders;
+	}
+
+
+
+	public void setColumnHeaders(List<ColumnHeader> columnHeaders) {
+		this.columnHeaders = columnHeaders;
+	}
+
+
+
+	public List<SimpleFileBean> getFiles() {
+		return files;
+	}
+
+
+
+	public void setFiles(List<SimpleFileBean> files) {
+		this.files = files;
+	}
+
+
+
+	public int getTheFileIndex() {
+		return theFileIndex;
+	}
+
+
+
+	public void setTheFileIndex(int theFileIndex) {
+		this.theFileIndex = theFileIndex;
+	}
+
+
+
+	public List<String> getErrors() {
+		return errors;
+	}
+
+	public void setErrors(List<String> errors) {
+		this.errors = errors;
+	}
+	
+	
+	
+}
