11e986a6b0527f01fbd846035cc784e2b6bd14c8
Shaun Einolf <seinolf@gmail.com>
2014-09-05 12:42:32 -0400

    hooked up edit button. passing all parameters to setupUpdate rest call

--- software/cananolab-webapp/web/scripts/factories/sampleService.js
+++ software/cananolab-webapp/web/scripts/factories/sampleService.js
@@ -6,6 +6,8 @@ app.factory("sampleService", function(){
 		sampleData: {data: null },
 		sampleId: {data: null },
 		type: {data: null },
+		charId: {data: null },
+		charClassName: {data: null },
 		isEdit: {data: null },
 		scratchPad: {data: null },
 		pocData: {data: null},
