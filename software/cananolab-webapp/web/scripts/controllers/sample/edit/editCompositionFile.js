10ff139db3101bfbaf270b2f6b10c733ba74731d
Thiagarajan Prakash <prakasht@mail.nih.gov>
2014-09-07 17:59:14 -0400

    updated REST calls for CompositionFile

--- software/cananolab-webapp/web/scripts/controllers/sample/edit/editCompositionFile.js
+++ software/cananolab-webapp/web/scripts/controllers/sample/edit/editCompositionFile.js
@@ -2,7 +2,7 @@
 var app = angular.module('angularApp')
 
     .controller('EditCompositionFileCtrl', function (navigationService,groupService,$rootScope,$scope,$http,$location,$timeout,$routeParams,$upload) {
-        $scope.CompositionFileForm = {};
+        $scope.fileForm = {};
         $rootScope.navTree=false;
         //$rootScope.tabs = navigationService.query();
         //$rootScope.groups = groupService.getGroups.data.get();
@@ -36,28 +36,24 @@ var app = angular.module('angularApp')
                 error(function(data, status, headers, config) {
                     // called asynchronously if an error occurs
                     // or server returns response with an error status.
-                    $scope.message = data;	
+                    $scope.message = data;
                 });
         });
 
         $scope.loadCompositionFileData = function() {
-            if( $scope.funcEntityId != null ) {
+            if( $scope.compositionFileId != null ) {
                 $scope.loader = true;
-                $http({method: 'GET', url: '/caNanoLab/rest/compositionFile/edit?sampleId=' + $scope.sampleId + '&dataId=' + $scope.funcEntityId}).
+                $http({method: 'GET', url: '/caNanoLab/rest/compositionFile/edit?sampleId=' + $scope.sampleId + '&dataId=' + $scope.compositionFileId}).
                     success(function(data, status, headers, config) {
-                        $scope.CompositionFileForm = data;
-                        //$scope.CompositionFileForm = {"simpleCompBean":{"type":"coat","name":"coat","pubChemDataSourceName":"Compound","pubChemId":12,"value":null,"valueUnit":"%vol","molecularFormulaType":"Hill","molecularFormula":"","description":"Test","id":59637770,"functionId":"","functionType":"imaging function","imagingModality":"fluorescence","functionDescription":"Test img function","sampleId":"","modality":""},"fileBean":{"uriExternal":false,"uri":"","type":"movie","title":"movie","description":"","keywordsStr":"","id":null},"type":"dendrimer","id":0,"description":"Test Nano Entity","sampleId":"","userDeletable":false,"userUpdatable":false,"createdBy":"jonnalah","createdDate":1408630200000,"domainEntity":{"id":60260353,"createdBy":"jonnalah","fileCollection":[{"uriExternal":false,"uri":"","type":"movie","title":"movie","description":"","keywordsStr":"","id":null}],"composingElementCollection":[{"type":"coat","name":"coat","pubChemDataSourceName":"Compound","pubChemId":12,"value":null,"valueUnit":"%vol","molecularFormulaType":"Hill","molecularFormula":"","description":"Test","id":59637770,"functionId":"","functionType":"imaging function","imagingModality":"fluorescence","functionDescription":"Test img function","sampleId":"","modality":""}],"branch":null,"createdDate":"jonnalah","generation":null},"withProperties":true,"detailsPage":"/caNanoLab/views/sample/composition/nanomaterialEntity/PolymerInfoEdit.html","isPolymerized":null,"isCrossLinked":null};
-                        $scope.files = [];
-                        $scope.files.push($scope.CompositionFileForm.domainEntity.file);
-
-                        $scope.editFile($scope.file.id);
-
+                        $scope.fileForm = data;
+                        //$scope.fileForm = {"uriExternal":true,"uri":"http://www.cancer.gov/","type":"graph","title":"graph","description":"","keywordsStr":"","id":73433125,"createdBy":"jonnalah","createdDate":1410117656000,"sampleId":"57835522","errors":null,"externalUrl":"http://www.cancer.gov/"};
                         $scope.loader = false;
                     }).
                     error(function(data, status, headers, config) {
                         $scope.message = data;
                         $scope.loader = false;
                     });
+
             }
         }
 
@@ -97,11 +93,9 @@ var app = angular.module('angularApp')
             if (confirm("Are you sure you want to delete the Composition File?")) {
                 $scope.loader = true;
 
-                $http({method: 'POST', url: '/caNanoLab/rest/compositionFile/removeFile',data: $scope.fileForm}).
+                $http({method: 'POST', url: '/caNanoLab/rest/compositionFile/delete',data: $scope.fileForm}).
                     success(function(data, status, headers, config) {
-                        $scope.CompositionFileForm = data;
-                        $scope.files = $scope.CompositionFileForm.domainEntity.fileCollection;
-                        $scope.loader = false;
+                        $location.search('message', 'Composition File successfully deleted."').path('/editComposition').replace();
                     }).
                     error(function(data, status, headers, config) {
                         // called asynchronously if an error occurs
@@ -154,25 +148,14 @@ var app = angular.module('angularApp')
 
         $scope.saveFileData = function() {
             $scope.loader = true;
+            
+            if( $scope.sampleId != null ) {
+            	$scope.fileForm.sampleId = $scope.sampleId;
+            }            
 
-            if ( $scope.fileForm.id != null && $scope.fileForm.id != '' ) {
-                for (var k = 0; k < $scope.files.length; ++k) {
-                    var element = $scope.files[k];
-                    if (element.id == $scope.fileForm.id) {
-                        $scope.files[k] = $scope.fileForm;
-                    }
-                }
-            }
-            else {
-                $scope.files.push($scope.fileForm);
-            }
-
-            $scope.CompositionFileForm.domainEntity.fileCollection = $scope.files;
-
-            $http({method: 'POST', url: '/caNanoLab/rest/compositionFile/saveFile',data: $scope.CompositionFileForm}).
+            $http({method: 'POST', url: '/caNanoLab/rest/compositionFile/submit',data: $scope.fileForm}).
                 success(function(data, status, headers, config) {
-                    $scope.CompositionFileForm = data;
-                    $scope.files = $scope.CompositionFileForm.domainEntity.fileCollection;
+                    $location.search('message', 'Composition File successfully saved."').path('/editComposition').replace();
                     $scope.loader = false;
                 }).
                 error(function(data, status, headers, config) {
@@ -183,19 +166,6 @@ var app = angular.module('angularApp')
                     $scope.messages = data;
                 });
         };
-
-        $scope.getAddNewFile = function() {
-            return $scope.addNewFile;
-        }
-
-        $scope.closeAddNewFile = function() {
-            $scope.addNewFile = false;
-        }
-
-
-        $scope.openAddNewFile = function() {
-            $scope.addNewFile = true;
-        }
 
         /* End File Section */
 
