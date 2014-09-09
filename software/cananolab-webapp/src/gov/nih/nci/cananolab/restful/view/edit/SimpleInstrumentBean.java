911437d633cca4bbd83b87a22e190f25686afe4e
yangs8 <yangs8@NCI-S077257-L.nci.nih.gov>
2014-09-09 10:06:25 -0400

    Make sure type doesn't have null value. Otherwise, got hibernate error

--- software/cananolab-webapp/src/gov/nih/nci/cananolab/restful/view/edit/SimpleInstrumentBean.java
+++ software/cananolab-webapp/src/gov/nih/nci/cananolab/restful/view/edit/SimpleInstrumentBean.java
@@ -2,26 +2,30 @@ package gov.nih.nci.cananolab.restful.view.edit;
 
 public class SimpleInstrumentBean {
 	
-	String manufacturer;
-	String modelName;
-	String type;
+	String manufacturer = "";
+	String modelName = "";
+	String type = "";
+	
 	public String getManufacturer() {
 		return manufacturer;
 	}
 	public void setManufacturer(String manufacturer) {
-		this.manufacturer = manufacturer;
+		if (manufacturer != null)
+			this.manufacturer = manufacturer;
 	}
 	public String getModelName() {
 		return modelName;
 	}
 	public void setModelName(String modelName) {
-		this.modelName = modelName;
+		if (modelName != null)
+			this.modelName = modelName;
 	}
 	public String getType() {
 		return type;
 	}
 	public void setType(String type) {
-		this.type = type;
+		if (type != null)
+			this.type = type;
 	}
 	
 	
