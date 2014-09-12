020011227bc9515ba921d3c06c0fd575eb5a8b9c
yangs8 <yangs8@NCI-S077257-L.nci.nih.gov>
2014-09-12 09:36:43 -0400

    Now reads user name and password from local.properties

--- software/cananolab-webapp/test-rest/java/gov/nih/nci/cananolab/restful/util/RestTestLoginUtil.java
+++ software/cananolab-webapp/test-rest/java/gov/nih/nci/cananolab/restful/util/RestTestLoginUtil.java
@@ -2,12 +2,17 @@ package gov.nih.nci.cananolab.restful.util;
 
 import static com.jayway.restassured.RestAssured.with;
 
+import java.util.Properties;
+
 import com.jayway.restassured.response.Response;
 
 import static com.jayway.restassured.RestAssured.given;
 import static com.jayway.restassured.RestAssured.with;
 import static org.hamcrest.Matchers.hasItems;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 import gov.nih.nci.cananolab.restful.util.RestTestLoginUtil;
+import gov.nih.nci.cananolab.util.PropertyUtils;
 import static com.jayway.restassured.RestAssured.expect;
 
 import org.junit.Test;
@@ -22,7 +27,7 @@ import com.jayway.restassured.response.Response;
 public class RestTestLoginUtil {
 	
 	public static String jsessionId = null;
-	
+
 	/**
 	 * Need to set correct credentials before running test. Do not check in this file
 	 * with credential!!
@@ -32,7 +37,14 @@ public class RestTestLoginUtil {
 	public static String loginTest() {
 
 		if (jsessionId == null) {
-			Response response = with().parameters("username", "yangs8", "password", "")
+			String username = RestTestLoginUtil.readUserNameProperty();
+			String pwd = RestTestLoginUtil.readPasswordProperty();
+			
+			if (username == null || username.length() == 0 ||
+					pwd == null || pwd.length() == 0)
+				return null;
+			
+			Response response = with().parameters("username", username, "password", pwd)
 					.expect().statusCode(200).when().get("http://localhost:8080/caNanoLab/rest/security/login");
 
 			jsessionId = response.getCookie("JSESSIONID");
@@ -44,5 +56,12 @@ public class RestTestLoginUtil {
 		expect().statusCode(200).when().get("http://localhost:8080/caNanoLab/rest/security/logout");
 		jsessionId = null;
 	}
+	
+	public static String readUserNameProperty() {
+		return PropertyUtils.getPropertyCached("local.properties", "user.name");
+	}
 
+	public static String readPasswordProperty() {
+		return PropertyUtils.getPropertyCached("local.properties", "password");
+	}
 }
