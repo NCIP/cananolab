a1d522f4cec815c75c60caa5b9781b76ddad8968
Shaun Einolf <seinolf@gmail.com>
2014-09-23 21:31:36 -0400

    finished advancedSampleSearch page

--- software/cananolab-webapp/web/scripts/controllers/sample/view/advancedSampleSearch.js
+++ software/cananolab-webapp/web/scripts/controllers/sample/view/advancedSampleSearch.js
@@ -32,8 +32,8 @@ var app = angular.module('angularApp')
     $scope.theCharacterizationQuery = {};
 
 
-    $scope.data = {"functionalizingEntityTypes":["Magnetic Particle","Monomer","Polymer","Quantum Dot","antibody","biopolymer","radioisotope","small molecule"],"characterizationTypes":["physico-chemical characterization","in vitro characterization","in vivo characterization","[Canano_char_type]","[SY-New-Char-Type]","[TEST111]","[ex vivo]"],"nanomaterialEntityTypes":["biopolymer","carbon","carbon black","carbon nanotube","dendrimer","emulsion","fullerene","liposome","metal oxide","metal particle","metalloid","nanohorn","nanolipogel","nanorod","nanoshell","polymer","quantum dot","silica"],"functionTypes":["TEST TYPE","endosomolysis","imaging function","other","targeting function","test function","therapeutic function","transfection"]};
-
+    $scope.data = {"functionalizingEntityTypes":["Magnetic Particle","Monomer","Polymer","Quantum Dot","antibody","biopolymer","radioisotope","small molecule"],"numberOperands":["=",">",">=","<="],"characterizationTypes":[{"label":"physico-chemical characterization","value":"physico-chemical characterization"},{"label":"in vitro characterization","value":"in vitro characterization"},{"label":"in vivo characterization","value":"in vivo characterization"},{"label":"[Canano_char_type]","value":"Canano_char_type"},{"label":"[SY-New-Char-Type]","value":"SY-New-Char-Type"},{"label":"[TEST111]","value":"TEST111"},{"label":"[ex vivo]","value":"ex vivo"}],"nanomaterialEntityTypes":["biopolymer","carbon","carbon black","carbon nanotube","dendrimer","emulsion","fullerene","liposome","metal oxide","metal particle","metalloid","nanohorn","nanolipogel","nanorod","nanoshell","polymer","quantum dot","silica"],"functionTypes":["TEST TYPE","endosomolysis","imaging function","other","targeting function","test function","therapeutic function","transfection"]};
+    // $scope.characterizationNameList = [{"label":"pharmacokinetics","value":"pharmacokinetics"},{"label":" --[pharmacokinetics]","value":"pharmacokinetics:pharmacokinetics"},{"label":"toxicology","value":"toxicology"},{"label":" --[toxicity]","value":"toxicology:toxicity"},{"label":"[imaging]","value":"imaging"},{"label":" --[imaging]","value":"imaging:imaging"},{"label":" --[intra-operative photoacoustic imaging]","value":"imaging:intra-operative photoacoustic imaging"},{"label":" --[multimodality imaging]","value":"imaging:multimodality imaging"},{"label":" --[multimodality imaging sensitivity]","value":"imaging:multimodality imaging sensitivity"},{"label":" --[multimodality kinetics]","value":"imaging:multimodality kinetics"},{"label":" --[tumor resection]","value":"imaging:tumor resection"},{"label":"[other_vv]","value":"other_vv"},{"label":" --[activation of innate and adaptive immunity]","value":"other_vv:activation of innate and adaptive immunity"},{"label":" --[adaptive immunity]","value":"other_vv:adaptive immunity"},{"label":" --[biodistribution]","value":"other_vv:biodistribution"},{"label":" --[drug accumulation]","value":"other_vv:drug accumulation"},{"label":" --[dye accumulation]","value":"other_vv:dye accumulation"},{"label":" --[health status]","value":"other_vv:health status"},{"label":" --[NK depletion]","value":"other_vv:NK depletion"},{"label":" --[photoacoustic imaging detection sensitivity]","value":"other_vv:photoacoustic imaging detection sensitivity"},{"label":" --[photothermal response]","value":"other_vv:photothermal response"},{"label":" --[reproducibility of photoacoustic imaging system]","value":"other_vv:reproducibility of photoacoustic imaging system"},{"label":" --[SERS detection sensitivity]","value":"other_vv:SERS detection sensitivity"},{"label":" --[stability]","value":"other_vv:stability"},{"label":" --[therapeutic efficacy]","value":"other_vv:therapeutic efficacy"},{"label":" --[toxicity]","value":"other_vv:toxicity"}];
     // initial rest call to setup form dropdowns //
     $scope.$on('$viewContentLoaded', function(){
       $http({method: 'GET', url: '/caNanoLab/rest/sample/setupAdvancedSearch'}).
@@ -132,48 +132,103 @@ var app = angular.module('angularApp')
 
 //// Characterization Criteria ////
 
-    // called when selecting composition type. populates composition entity dropdown //
-    $scope.setCompositionEntityOptions = function(editType) {
+    // called when selecting characterization type. populates characterization name dropdown //
+    $scope.setCharacterizationOptions = function(editType) {
+      var charType = $scope.theCharacterizationQuery.characterizationType;
+      $scope.loader = true;
+      $http({method: 'GET', url: '/caNanoLab/rest/sample/getDecoratedCharacterizationOptions?charType='+$scope.theCharacterizationQuery.characterizationType}).
+      success(function(data, status, headers, config) {
+        $scope.characterizationNameList = data;
+        $scope.loader = false;        
+      }).
+      error(function(data, status, headers, config) {
+        $scope.message = data;
+        $scope.loader = false;      
+      });
       if (!editType) {
-        $scope.theCompositionQuery.entityType='';
+        $scope.theCharacterizationQuery = {}; 
+        $scope.theCharacterizationQuery.characterizationType = charType;        
       }
-      if ($scope.theCompositionQuery.compositionType=='nanomaterial entity') { $scope.entityTypes=$scope.data.nanomaterialEntityTypes };
-      if ($scope.theCompositionQuery.compositionType=='functionalizing entity') { $scope.entityTypes=$scope.data.functionalizingEntityTypes };
-      if ($scope.theCompositionQuery.compositionType=='function') { $scope.entityTypes=$scope.data.functionTypes };      
-    }
-
-    // called when adding a new composition or updating an existing one //
-    $scope.updateCompositionCriteria = function() {
-      if ($scope.isNewComposition) {
-        $scope.searchSampleForm.compositionQueries.push($scope.theCompositionQuery);
-      }
-      else {
-        angular.copy($scope.theCompositionQuery,$scope.currentComposition);
-      }
-      $scope.theCompositionQuery = {};
-      $scope.entityTypes = [];
-      $scope.isNewComposition=true;
     };
 
-    // called when clicking edit on existing composition. Copies composition //
-    $scope.editCompositionCriteria = function(composition) {
-      $scope.currentComposition = composition;
-      $scope.theCompositionQuery = angular.copy(composition); 
-      $scope.setCompositionEntityOptions('update');
-      $scope.isNewComposition=false;
+    // called when selecting characterization name. populates datum name dropdown //
+    $scope.setDatumNameOptionsByCharName = function(editType) {
+      $scope.loader = true;
+      $http({method: 'GET', url: '/caNanoLab/rest/sample/getDecoratedDatumOptions?charType='+$scope.theCharacterizationQuery.characterizationType+'&charName='+$scope.theCharacterizationQuery.characterizationName}).
+      success(function(data, status, headers, config) {
+        $scope.datumNameList = data;
+        $scope.loader = false;
+      }).
+      error(function(data, status, headers, config) {
+        $scope.message = data;
+        $scope.loader = false;
+      });  
+      if (!editType) {
+        $scope.theCharacterizationQuery.datumName = '';
+        $scope.theCharacterizationQuery.operand = '';
+        $scope.theCharacterizationQuery.datumValueUnit = '';        
+      }    
+    };
+
+
+    // called when selecting datum name. populates datum value unit options dropdown //
+    $scope.setDatumValueUnitOptions = function(editType) {
+      if (!$scope.theCharacterizationQuery.datumName) {
+        $scope.theCharacterizationQuery.operand = '';
+      };
+      $http({method: 'GET', url: '/caNanoLab/rest/sample/getDatumUnitOptions?datumName='+$scope.theCharacterizationQuery.datumName}).
+      success(function(data, status, headers, config) {
+        $scope.datumUnitOptionsList = data;
+        $scope.loader = false;
+      }).
+      error(function(data, status, headers, config) {
+        $scope.message = data;
+        $scope.loader = false;
+      });   
+      if (!editType) {
+        $scope.theCharacterizationQuery.datumValueUnit = '';         
+        $scope.theCharacterizationQuery.operand = '';         
+      }
+    };
+
+    // called when adding a new characterization or updating an existing one //
+    $scope.updateCompositionCriteria = function() {
+      if ($scope.isNewCharacterization) {
+        $scope.searchSampleForm.characterizationQueries.push($scope.theCharacterizationQuery);
+      }
+      else {
+        angular.copy($scope.theCharacterizationQuery,$scope.currentCharacterization);
+      }
+      $scope.theCharacterizationQuery = {};
+      $scope.datumUnitOptionsList = [];
+      $scope.datumNameList = [];
+      $scope.characterizationNameList = [];
+      $scope.isNewCharacterization=true;
+    };
+
+    // called when clicking edit on existing characterization. Copies characterization //
+    $scope.editCharacterizationCriteria = function(characterization) {
+      $scope.currentCharacterization = characterization;
+      $scope.theCharacterizationQuery = angular.copy(characterization); 
+
+      $scope.setCharacterizationOptions('update');
+      $scope.setDatumNameOptionsByCharName('update');
+      $scope.setDatumValueUnitOptions('update');
+      $scope.loader = false;
+      $scope.isNewCharacterization=false;
     }; 
 
-    // removes composition from searchSampleForm //
-    $scope.removeComposition = function() {
-      $scope.searchSampleForm.compositionQueries.splice($scope.searchSampleForm.compositionQueries.indexOf($scope.currentComposition),1);
-      $scope.theCompositionQuery = {};
-      $scope.isNewComposition = true;
+    // removes characterization from searchSampleForm //
+    $scope.removeCharacterization = function() {
+      $scope.searchSampleForm.characterizationQueries.splice($scope.searchSampleForm.characterizationQueries.indexOf($scope.currentCharacterization),1);
+      $scope.theCharacterizationQuery = {};
+      $scope.isNewCharacterization = true;
     };        
 
-    // resets composition criteria //
-    $scope.clearCompositionQuery = function() {
-      $scope.theCompositionQuery = {};
-      $scope.isNewComposition = true;
+    // resets characterization criteria //
+    $scope.clearCharacterizationQuery = function() {
+      $scope.theCharacterizationQuery = {};
+      $scope.isNewCharacterization = true;
     };    
 
     // resets overall form //
