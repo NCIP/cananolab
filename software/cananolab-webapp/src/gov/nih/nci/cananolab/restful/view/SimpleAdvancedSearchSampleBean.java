d09e59d3067f9f0c49d0bf192a8643b82758d23f
shanyang-saicf <shan.yang@nih.gov>
2014-10-07 12:01:50 -0400

    Restore a cleanup error

--- software/cananolab-webapp/src/gov/nih/nci/cananolab/restful/view/SimpleAdvancedSearchSampleBean.java
+++ software/cananolab-webapp/src/gov/nih/nci/cananolab/restful/view/SimpleAdvancedSearchSampleBean.java
@@ -47,40 +47,16 @@ public class SimpleAdvancedSearchSampleBean extends SimpleSearchSampleBean {
 		if (sampleBean == null)
 			return;
 		
-//		for (LabelValueBean col : colNames) {
-//			Map<String, String> data = new HashMap<String, String>();
-//			String colName = col.getLabel();
-//			
-//			if (colName.contains("Sample"))
-//				data.put(colName, sampleBean.getDomainSample().getName());
-//		}
-		
-//		setSampleId(sampleBean.getDomainSample().getId());
-//		setSampleName(sampleBean.getDomainSample().getName());
-//		
-//		List<String> sampleIdName = new ArrayList<String>();
-//		sampleIdName.add(this.sampleName);
-//		sampleIdName.add(String.valueOf(this.sampleId));
-		
-		//this.columns.add(new SimpleAdvancedResultCellBean("Sample", sampleIdName));
-		
-		//List<String> columnNames = searchBean.getQueryAsColumnNames();
+		setSampleId(sampleBean.getDomainSample().getId());
+		setSampleName(sampleBean.getDomainSample().getName());
+
 		Map<String, List<LinkableItem>> dataMap = sampleBean.getAttributeMap();
-		
-//		for (String columnName : columnNames) {
-//			List<LinkableItem> items = dataMap.get(columnName);
-//			populateColumnContent(columnName, items);
-//		}
-//		
 		
 		for (LabelValueBean col : colNames) {
 			Map<String, Object> data = new HashMap<String, Object>();
 			String colName = col.getLabel();
 			
-			if (colName.contains("Sample")) {
-				//data.put(colName, sampleBean.getDomainSample().getName());
-				//this.rowCells.add(data);
-				
+			if (colName.contains("Sample")) {			
 				this.dataRow.setSampleId(this.sampleId);
 				this.dataRow.setSampleName(this.sampleName);
 			} else {
@@ -118,17 +94,10 @@ public class SimpleAdvancedSearchSampleBean extends SimpleSearchSampleBean {
 		List<SimpleAdvancedResultCellUnitBean> units = transferFromLinkableItems(items, colName);
 		
 		String type = getHtmlFieldType(colName);
-		//this.columns.add(new SimpleAdvancedResultCellBean(type, units));
-		///this.rowCellRefs.put(colName, units);
 		
 		List<String> formattedDisplayables = new ArrayList<String>();
 		for (SimpleAdvancedResultCellUnitBean unit :units) {
 			String formatted = unit.getDisplayName() + "|" + unit.getDataId();
-			//formattedDisplayables.add(unit.getDisplayName() + "|" + unit.getDataId());
-			
-			if (unit.getDataId() == 10846280) {
-				String n = "stop and watch";
-			}
 			
 			if (type.equals("function")) {
 				String relatedType = unit.getRelatedEntityType();
@@ -147,71 +116,6 @@ public class SimpleAdvancedSearchSampleBean extends SimpleSearchSampleBean {
 			logger.error("Got NoSuchMethodException while setting adv. search result data field: " + nse.getMessage());
 		} catch (Exception e) {
 			logger.error("Got exception while setting adv. search result data field: " + e.getMessage());
-		}
-		
-		
-		
-		String methodName = "set" + type.substring(0, 1).toUpperCase() + type.substring(1);
-		
-//		//this.setProperty(this.dataRow, methodName, formattedDisplayables);
-//		
-////		//map to the type value the html is using.
-//		if (colName.contains("point of contact")) {
-//			//this.dataRow.setPointOfContact(formattedDisplayables);
-//			try {
-//			PropertyUtils.setProperty(this.dataRow, "pointOfContact", formattedDisplayables);
-//			} catch (Exception nse) {}
-//		} else if (colName.contains("nanomaterial entity"))
-//			this.dataRow.setNanomaterialEntity(formattedDisplayables);
-//		else if (colName.contains("functionalizing entity"))
-//			this.dataRow.setFunctionalizaingEntity(formattedDisplayables);
-//		else if (colName.equals("function"))
-//			this.dataRow.setFunction(formattedDisplayables);
-//		else if (colName.contains("physico")) 
-//			this.dataRow.setPhysicalChemicalChar(formattedDisplayables);
-//		else if (colName.contains("vivo"))
-//			this.dataRow.setInvivoChar(formattedDisplayables);
-//		else if (colName.contains("vitro"))
-//			this.dataRow.setInvitroChar(formattedDisplayables);
-//		else if (colName.contains("ex"))
-//			this.dataRow.setExvivoChar(formattedDisplayables);
-			
-//			
-//			
-//			List<String> r = new ArrayList<String>();
-//			for (SimpleAdvancedResultCellUnitBean unit :units) {
-//				r.add(unit.getDisplayName() + "|" + unit.getDataId());
-//			}
-//			
-//			this.dataRow.setPhysicalChemicalChar(r);;
-//		}
-//		
-	}
-	
-	protected void setProperty(Object targetObj, String methodName, List<String> value) {
-
-		try {
-			Class[] paramString = new Class[1];
-			paramString[0] = String.class;
-			Class currClass = targetObj.getClass();
-			Method method = currClass.getMethod(methodName, paramString);
-
-			method.invoke(targetObj, value);
-
-		} catch (SecurityException se) {
-			logger.debug(se);
-		} catch (IllegalArgumentException ex) {
-			ex.printStackTrace();
-		} catch (InvocationTargetException ex) {
-			ex.printStackTrace();
-			return;
-		} catch (IllegalAccessException ex) {
-			ex.printStackTrace();
-			return;
-		} catch (Exception ex) {
-			logger.debug(ex);
-		} catch (Throwable t) {
-			logger.debug(t);
 		}
 
 	}
