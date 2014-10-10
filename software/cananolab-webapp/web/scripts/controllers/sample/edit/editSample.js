78fd16b9a5d74db39a4e0e1c7f0e60a94d7110f8
Shaun Einolf <seinolf@gmail.com>
2014-10-10 14:05:18 -0400

    changed back button on view sample and edit sample page to go to advanced search results when coming from results page

--- software/cananolab-webapp/web/scripts/controllers/sample/edit/editSample.js
+++ software/cananolab-webapp/web/scripts/controllers/sample/edit/editSample.js
@@ -46,7 +46,9 @@ var app = angular.module('angularApp')
     $scope.editSampleForm = false;
 
 
-
+    if ($routeParams.isAdvancedSearch) {
+      $scope.isAdvancedSearch = 1;
+    }; 
 
     //goBack
     //Change location if user hits the Back button
@@ -61,7 +63,12 @@ var app = angular.module('angularApp')
             $location.search('fromMyWorkspace', null);
         }        
         else {
-            $location.path("/sampleResults").replace();
+        if ($scope.isAdvancedSearch) {
+          $location.path("/advancedSampleResults").replace();           
+        }
+        else {
+          $location.path("/sampleResults").replace();           
+        }
             $location.search('sampleId', null);
         }
 
@@ -74,6 +81,7 @@ var app = angular.module('angularApp')
     } else {
         $scope.sampleId.data = null;
     }
+     
 
     $scope.returnUserReadableBoolean = function(val) {
         if (val== true) {
@@ -199,8 +207,15 @@ var app = angular.module('angularApp')
                             $scope.sampleResultsData = b.splice(g,1);
                           };
                         };
-                     }   
-                    $location.path("/sampleResults").replace();
+                     }  
+                    if($routeParams.fromMyWorkspace=='true') {
+                        $location.path("/myWorkspace").replace();
+                        $location.search('sampleId', null);
+                        $location.search('fromMyWorkspace', null); 
+                        }
+                	else {
+                		$location.path("/sampleResults").replace();
+                	}
                 }).
                 error(function(data, status, headers, config) {
                     $scope.loader = false;
