f9a6fdd84f75b2476a5a4ea82a8380ac5f053750
Shaun Einolf <seinolf@gmail.com>
2015-04-30 17:57:56 -0400

    changed publicationSampleImage to call the rest call directly as the img src

--- software/cananolab-webapp/web/scripts/controllers/publication/view/publicationSampleImage.js
+++ software/cananolab-webapp/web/scripts/controllers/publication/view/publicationSampleImage.js
@@ -3,19 +3,5 @@ angular.module('angularApp',['ngRoute','ngSanitize','ngRoute'])
   .controller('PublicationSampleImageCtrl', function (utilsService, $rootScope,$scope,$http,$filter,$location,$routeParams) {
     $scope.id = utilsService.getParameterFromURL('id');  
     $scope.type = utilsService.getParameterFromURL('type'); 
-
-        $scope.loader = true;      
-        $http({method: 'GET', url: '/caNanoLab/rest/publication/searchByIdImage?type='+$scope.type+'&id='+$scope.id}).
-        success(function(data, status, headers, config) {
-        	$scope.fileInfo = data;
-        	$scope.loader = false;
-        }).
-        error(function(data, status, headers, config) {
-          // called asynchronously if an error occurs
-          // or server returns response with an error status.
-        	$scope.fileInfo = data;
-    		$scope.loader = false;
-
-         });
-
+    $scope.url = '/caNanoLab/rest/publication/searchByIdImage?type='+$scope.type+'&id='+$scope.id;
   });
