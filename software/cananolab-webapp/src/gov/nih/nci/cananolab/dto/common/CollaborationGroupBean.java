207caadb3634978b6fc7ed62bb177a54c4c95b86
jonnalah <harika.jonnala@nih.gov>
2015-05-28 09:45:56 -0400

    Collaboration group bug fix for editUserAccess

--- software/cananolab-webapp/src/gov/nih/nci/cananolab/dto/common/CollaborationGroupBean.java
+++ software/cananolab-webapp/src/gov/nih/nci/cananolab/dto/common/CollaborationGroupBean.java
@@ -58,14 +58,19 @@ public class CollaborationGroupBean extends SecuredDataBean {
 	}
 
 	public void addUserAccess(AccessibilityBean userAccess) {
-		int index = this.getUserAccesses().indexOf(userAccess);
-		if (index != -1) {
-			this.getUserAccesses().remove(userAccess);
-			// retain the original order
-			this.getUserAccesses().add(index, userAccess);
-		} else {
-			this.getUserAccesses().add(userAccess);
+		
+		for(int i = 0 ; i < this.getUserAccesses().size() ; i++){
+			if(this.getUserAccesses().get(i).getUserBean().getUserId().equals(userAccess.getUserBean().getUserId())){
+				this.getUserAccesses().remove(i);
+			}
 		}
+//		if (index != -1) {
+//			this.getUserAccesses().remove(userAccess);
+//			// retain the original order
+//			this.getUserAccesses().add(index, userAccess);
+//		} else {
+			this.getUserAccesses().add(userAccess);
+//		}
 	}
 
 	public void removeUserAccess(AccessibilityBean userAccess) {
