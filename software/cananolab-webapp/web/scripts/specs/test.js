1110915ebe73381c28e7f2c40ba8b35a354abd80
Subhash Vallala <sgvallala@gmail.com>
2017-02-02 16:24:42 -0500

    Jasmine test case comment

--- software/cananolab-webapp/web/scripts/specs/test.js
+++ software/cananolab-webapp/web/scripts/specs/test.js
@@ -1,4 +1,6 @@
 
+// CANANOLAB-479
+
 
 describe("editUser spec ", function() {
 	
@@ -13,10 +15,10 @@ describe("editUser spec ", function() {
 
         apiService = {
             query:  function() {
-      queryDeferred = $q.defer();
-      queryDeferred.resolve(read);
-      return {$promise: queryDeferred.promise};
-    }
+                queryDeferred = $q.defer();
+                queryDeferred.resolve(read);
+                return {$promise: queryDeferred.promise};
+            }
         };
 
         rootscope = $rootScope.$new();
@@ -30,17 +32,12 @@ describe("editUser spec ", function() {
         httpBackend = $httpBackend;
         controller = $controller;
 
-        
-
 		spyOn(apiService, 'query').and.callThrough();
-
         spyOn(location, 'path').and.returnValue('');
         spyOn(location, 'replace');
 
         read = {"username":"ttran","firstName":"tin","lastName":"tran","organization":"NCI","department":"CBIIT","title":null,"phoneNumber":null,"password":"$2a$10$EmfHzgyCiGdkNM6oM2jtEeMstTcNhM9pvsskD2fhy9cupEouWl3uq","emailId":"ttran@nih.gov","enabled":true,"roles":["ROLE_ANONYMOUS","ROLE_RESEARCHER"],"groups":["test_group","test group","a new test","Group A","Group B","TEST 1","TEST 2"],"authorities":[{"authority":"test_group"},{"authority":"test group"},{"authority":"a new test"},{"authority":"Group A"},{"authority":"Group B"},{"authority":"TEST 1"},{"authority":"TEST 2"},{"authority":"ROLE_ANONYMOUS"},{"authority":"ROLE_RESEARCHER"}],"public":true,"displayName":"tran, tin","researcher":true,"curator":false,"admin":false,"accountNonLocked":true,"credentialsNonExpired":true,"accountNonExpired":true};
         httpBackend.when('GET', '/caNanoLab/rest/useraccount/read?username=test').respond(read);
-        //queryDeferred.resolve(read);
-        //$rootScope.$apply();
 
         editUserCtrl = $controller('EditUserCtrl', {
         	navigationService:apiService,
@@ -67,35 +64,9 @@ describe("editUser spec ", function() {
 });
 
 
-
-
-	// beforeEach(inject(function($controller) {
-	//   $scope = $rootScope.$new();
-
-	//   mockBagelApiService = {
-	//     query: function() {
-	//       queryDeferred = $q.defer();
-	//       return {$promise: queryDeferred.promise};
-	//     }
-	//   }
-
-	//   spyOn(mockBagelApiService, 'query').andCallThrough();
-
-	//   $controller('BreakfastCtrl', {
-	//     '$scope': $scope,
-	//     'bagelApiService': mockBagelApiService
-	//   });
-	// }));
-
-
-
-
 // describe("manageUser spec", function() {
 	
 // 	var $scope = {}, controller, rootscope, http, httpBackend, location, timeout, route_params, upload, navigation_service, group_service, editUserCtrl;	
-
-// 	beforeEach(module('ngResource'));
-// 	beforeEach(module("angularApp"));
 
 // 	beforeEach(inject(function(userService,navigationService,groupService,$rootScope,$filter,ngTableParams,$http,$location,$modal,$httpBackend,$controller) {
 //         rootscope = $rootScope.$new();
@@ -131,27 +102,15 @@ describe("editUser spec ", function() {
 
 
 // describe("userSearch spec", function() {
-	
-// 	beforeEach(module('ngResource'));
-// 	beforeEach(module("angularApp"));
 
-// 	beforeEach(inject(function(userService,navigationService,groupService,utilsService,$rootScope,$filter,ngTableParams,$http,$location,$httpBackend,$controller) {
+// 	beforeEach(inject(function($rootScope,$httpBackend,$controller) {
 //         rootscope = $rootScope.$new();
 //         $scope = $rootScope.$new();
-//         http = $http,
-//         location = $location,
-//         timeout = $timeout,
-//         route_params = $routeParams,
-//         upload = $upload,
 //         httpBackend = $httpBackend;
 //         controller = $controller;
 
 //         userSearchCtrl = controller('UserResultsCtrl', {
-//         	$scope:$scope,   	
-//         	$http:http,
-//         	$location:location,
-//         	navigationService:navigation_service,
-//         	groupService:group_service
+//         	$scope:$scope
 //         });
 
 //     }));
